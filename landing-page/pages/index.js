8eff3ff80494fefa24e712d89cc68a0b8dad73a0
Jay Khatri <jaykhatrimail@gmail.com>
2020-09-05 15:21:44 -0400

    lets see what this does

--- landing-page/pages/index.js
+++ landing-page/pages/index.js
@@ -7,70 +7,41 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Highlight</title>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="preload" href="/font" as="font" crossOrigin="" />
-      </Head>
-      <div className={styles.wrapper}>
-        <div className={styles.navbar}>
-          <div className={styles.navbarContent}>
-            <img
-              src="static/logo.svg"
-              alt="triangle with all three sides equal"
-              height="55"
-              width="130"
-            />
-            <div className={styles.navbarRight}>
-              <a href="./pricing" className={styles.navbarLink}>
-                Pricing
-              </a>
-              <a className={styles.navbarLink}>Community</a>
-              <div className={styles.signInWrapper}>
-                <button className={styles.signInButton}>
-                  <FontAwesomeIcon style={{ width: 15 }} icon={faPlay} />
-                  <p className={styles.accessText}>Sign In</p>
+      <div className={styles.contentWrap}>
+        <div className={styles.copySection}>
+          <div className={styles.copyHeader}>
+            Step into the shoes of your users.
+          </div>
+          <div className={styles.copyBody}>
+            Highlight gives your team insight into how users are &nbsp;
+            <span className={styles.actually}>actually</span> interacting with
+            your site.
+          </div>
+          <div className={styles.formSection}>
+            <form>
+              <div className={styles.formWrapper}>
+                <input
+                  type="text"
+                  id="fname"
+                  name="fname"
+                  placeholder="Email Address"
+                  className={styles.requestInput}
+                />
+                <br />
+                <button className={styles.requestButton}>
+                  <FontAwesomeIcon style={{ width: 15 }} icon={faHandPointUp} />
+                  <p className={styles.accessText}>Request Access</p>
                 </button>
               </div>
-            </div>
+            </form>
           </div>
         </div>
-        <div className={styles.contentWrap}>
-          <div className={styles.copySection}>
-            <div className={styles.copyHeader}>
-              Step into the shoes of your users.
-            </div>
-            <div className={styles.copyBody}>
-              Highlight gives your team insight into how users are &nbsp;
-              <span className={styles.actually}>actually</span> interacting with your
-              site.
-            </div>
-            <div className={styles.formSection}>
-              <form>
-                <div className={styles.formWrapper}>
-                  <input
-                    type="text"
-                    id="fname"
-                    name="fname"
-                    placeholder="Email Address"
-                    className={styles.requestInput}
-                  />
-                  <br />
-                  <button className={styles.requestButton}>
-                    <FontAwesomeIcon style={{ width: 15 }} icon={faHandPointUp} />
-                    <p className={styles.accessText}>Request Access</p>
-                  </button>
-                </div>
-              </form>
-            </div>
-          </div>
-          <div className={styles.gifSection}>
-            <img
-              className={styles.gif}
-              src="static/animate.gif"
-              alt="description of gif"
-            />
-          </div>
+        <div className={styles.gifSection}>
+          <img
+            className={styles.gif}
+            src="static/animate.gif"
+            alt="description of gif"
+          />
         </div>
       </div>
     </>
