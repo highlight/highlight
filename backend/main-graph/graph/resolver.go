db8f5d845c6c54b1723ab7a33be04aa6e9331630
Jay Khatri <jaykhatrimail@gmail.com>
2020-09-14 11:10:21 -0400

    auth done, i think

--- backend/main-graph/graph/resolver.go
+++ backend/main-graph/graph/resolver.go
@@ -1,6 +1,13 @@
 package graph
 
-import "github.com/jinzhu/gorm"
+import (
+	"context"
+
+	"github.com/jay-khatri/fullstory/backend/model"
+	"github.com/jinzhu/gorm"
+
+	e "github.com/pkg/errors"
+)
 
 // This file will not be regenerated automatically.
 //
@@ -8,4 +15,32 @@ import "github.com/jinzhu/gorm"
 
 type Resolver struct {
 	DB *gorm.DB
+}
+
+// These are authentication methods used to make sure that data is secured.
+// This'll probably get expensive at some point; they can probably be cached.
+func (r *Resolver) isAdminInOrganization(ctx context.Context, org_id int) (*model.Organization, error) {
+	orgs, err := r.Query().Organizations(ctx)
+	if err != nil {
+		return nil, e.Wrap(err, "error querying orgs")
+	}
+	for _, o := range orgs {
+		if o.ID == org_id {
+			return o, nil
+		}
+	}
+	return nil, e.New("admin doesn't exist in organization")
+}
+
+func (r *Resolver) isAdminSessionOwner(ctx context.Context, session_id int) (*model.Session, error) {
+	session := &model.Session{}
+	res := r.DB.Where(&model.Session{Model: model.Model{ID: session_id}}).First(&session)
+	if err := res.Error; err != nil || res.RecordNotFound() {
+		return nil, e.Wrap(err, "error querying session")
+	}
+	_, err := r.isAdminInOrganization(ctx, session.OrganizationID)
+	if err != nil {
+		return nil, e.Wrap(err, "error validating admin in organization")
+	}
+	return session, nil
 }
