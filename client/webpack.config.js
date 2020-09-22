f8abb62304d107d0b215a30c441a5f4011317e71
Jay Khatri <jaykhatrimail@gmail.com>
2020-09-22 11:18:13 -0400

    change

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,32 +1,48 @@
 const path = require('path');
 const webpack = require('webpack'); //to access built-in plugins
+var WebpackObfuscator = require('webpack-obfuscator');
 
 module.exports = {
-	entry: path.resolve(__dirname, 'src/index.tsx'),
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'index.js',
-		library: '$',
-		libraryTarget: 'umd',
-	},
-	devServer: {
-		watchContentBase: true,
-		writeToDisk: true,
-	},
-	module: {
-		rules: [
-			{
-				test: /\.tsx?$/,
-				use: 'ts-loader',
-				exclude: /node_modules/,
-			},
-		],
-	},
-	resolve: {
-		extensions: ['.js', '.ts', '.tsx'],
-		modules: [path.resolve(__dirname, 'src'), 'node_modules'],
-	},
-	mode: 'development',
-	devtool: 'sourceMap',
-	plugins: [new webpack.EnvironmentPlugin(['BACKEND_URI'])],
+  entry: {
+    index: path.resolve(__dirname, 'src/index.tsx'),
+    firstload: path.resolve(__dirname, 'firstload_src/firstload.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+    library: '$',
+    libraryTarget: 'umd',
+  },
+  devServer: {
+    watchContentBase: true,
+    writeToDisk: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx'],
+    modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+  },
+  mode: 'development',
+  devtool: 'sourceMap',
+  plugins: [
+    new webpack.EnvironmentPlugin(['BACKEND_URI']),
+    new WebpackObfuscator(
+      {
+        compact: true,
+        identifierNamesGenerator: 'mangled',
+        splitStrings: true,
+        splitStringsChunkLength: 3,
+        shuffleStringArray: true,
+      },
+      []
+    ),
+  ],
 };
--- frontend/src/pages/Setup/SetupPage.tsx
+++ frontend/src/pages/Setup/SetupPage.tsx
@@ -1,38 +1,63 @@
 import React from "react";
 
-import styles from "./SetupPage.module.css";
 import { CodeBlock } from "./CodeBlock";
+import { Spinner } from "../../components/Spinner/Spinner";
 import { useParams } from "react-router-dom";
 
-export const SetupPage = () => {
-  let { organization_id } = useParams();
+import styles from "./SetupPage.module.css";
+import appStyles from "../../App.module.css";
+import useFetch from "use-http";
 
-  return (
-    <div className={styles.setupWrapper}>
-      <div className={styles.snippetCard}>
-        <div className={styles.snippetHeading}>Your Recording Snippet</div>
-        <div className={styles.snippetSubHeading}>
-          Copy and paste the{" "}
-          <span className={styles.codeBlockBasic}>{"<script/>"}</span> below
-          into the
-          <span className={styles.codeBlockBasic}>{"<head/>"}</span> of every
-          page you wish to record.
-        </div>
-        <CodeBlock
-          text={`<script>
+export const SetupPage = () => {
+	const { loading, error, data = "" } = useFetch(
+		"https://static.highlight.run/firstload_obfuscated.js",
+		{},
+		[]
+	);
+	let { organization_id } = useParams();
+
+	if (loading || error) {
+		return (
+			<div className={appStyles.loadingWrapper}>
+				<Spinner />
+			</div>
+		);
+	}
+
+	return (
+		<div className={styles.setupWrapper}>
+			<div className={styles.snippetCard}>
+				<div className={styles.snippetHeading}>
+					Your Recording Snippet
+				</div>
+				<div className={styles.snippetSubHeading}>
+					Copy and paste the{" "}
+					<span className={styles.codeBlockBasic}>{"<script/>"}</span>{" "}
+					below into the
+					<span className={styles.codeBlockBasic}>
+						{"<head/>"}
+					</span>{" "}
+					of every page you wish to record.
+				</div>
+				<CodeBlock
+					text={`<script>
 window['_h_debug'] = false;
 window['_h_script'] = 'https://static.highlight.run';
 window['_h_org'] = ${organization_id};
 var a=['script'];(function(b,e){var f=function(g){while(--g){b['push'](b['shift']());}};f(++e);}(a,0x72));var b=function(c,d){c=c-0x0;var e=a[c];return e;};var script=document['createElement'](b('0x0'));script['setAttribute']('src',window['_h_script']+'/index.js?'+new Date()['getMilliseconds']()),script['setAttribute']('type','text/javascript'),document['getElementsByTagName']('head')[0x0]['appendChild'](script),script['addEventListener']('load',()=>{window['highlight_obj']=new Highlight(window['_h_debug']),highlight_obj['initialize'](window['_h_org']);}),window['H']={},window['H']['Identify']=c=>{var d=setInterval(function(){window['highlight_obj']&&window['highlight_obj']['ready']&&(clearInterval(d),window['highlight_obj']['identify'](c));},0xc8);};
 </script>`}
-        />
-        <div className={styles.snippetSubHeading}>
-          To tag every session with a user identifier (name, email, etc.), run
-          the following code in your javascript app or in a subsequent
-          <span className={styles.codeBlockBasic}>{"<script/>"}</span> tag.
-        </div>
-        <CodeBlock text={`window.H.identify("<YOUR_IDENTIFIER>")`} />
-      </div>
-    </div>
-  );
+				/>
+				<div className={styles.snippetSubHeading}>
+					To tag every session with a user identifier (name, email,
+					etc.), run the following code in your javascript app or in a
+					subsequent
+					<span className={styles.codeBlockBasic}>
+						{"<script/>"}
+					</span>{" "}
+					tag.
+				</div>
+				<CodeBlock text={`window.H.identify("<YOUR_IDENTIFIER>")`} />
+			</div>
+		</div>
+	);
 };
