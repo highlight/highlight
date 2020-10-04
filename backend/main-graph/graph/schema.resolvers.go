5d73b26c45a7230178b8d90fb5c1866627c668e3
Jay Khatri <jaykhatrimail@gmail.com>
2020-10-04 14:54:23 -0400

    change

--- backend/main-graph/graph/schema.resolvers.go
+++ backend/main-graph/graph/schema.resolvers.go
@@ -138,6 +138,7 @@ func (r *queryResolver) Sessions(ctx context.Context, organizationID int, count 
 				filteredSessions = append(filteredSessions, sessions[i])
 			}
 		}
+		fmt.Println(len(filteredSessions))
 		return filteredSessions, nil
 	}
 	return sessions, nil
