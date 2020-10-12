8901e2592a37fd3a38b5e1ef0c3aa8e42a5efa29
Jay Khatri <jaykhatrimail@gmail.com>
2020-10-12 15:04:31 -0400

    get rid of type

--- firstload/src/index.d.ts
+++
@@ -1,7 +0,0 @@
-declare type HighlightPublicInterface = {
-    init: (orgID: number, debug?: boolean) => void;
-    identify: (identify: string, scriptPath: string) => void;
-    onHighlightReady: (func: () => void) => void;
-};
-export declare const Highlight: HighlightPublicInterface;
-export {};
