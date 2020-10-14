7ad22ce505124f882e914f47dad0a7b6328ed171
Jay Khatri <jaykhatrimail@gmail.com>
2020-10-13 22:32:43 -0400

    change

--- frontend/src/pages/Player/ConsolePage/ConsolePage.module.css.d.ts
+++ frontend/src/pages/Player/ConsolePage/ConsolePage.module.css.d.ts
@@ -2,6 +2,9 @@ declare const styles: {
   readonly "consoleButton": string;
   readonly "logStreamWrapper": string;
   readonly "consolePageWrapper": string;
+  readonly "consoleMessage": string;
+  readonly "directionIcon": string;
+  readonly "consoleTopBar": string;
 };
 export = styles;
 
--- frontend/src/pages/Player/ConsolePage/ConsolePage.tsx
+++ frontend/src/pages/Player/ConsolePage/ConsolePage.tsx
@@ -1,24 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import { useQuery, gql } from '@apollo/client';
 import { useParams } from 'react-router-dom';
+import { ReactComponent as DownIcon } from '../../../static/down.svg';
+import { ReactComponent as UpIcon } from '../../../static/up.svg';
 
 import styles from './ConsolePage.module.css';
 
 export const ConsolePage = ({ onClick }: { onClick: () => void }) => {
 	const [showStream, setShowStream] = useState(false);
-    const { session_id } = useParams();
-    const {
-        loading,
-        error,
-        data,
-    } = useQuery<{ events: any[] }, { session_id: string }>(
-        gql`
-            query GetEvents($session_id: ID!) {
-                messages(session_id: $session_id)
-            }
-        `,
-        { variables: { session_id } }
-    );
+	const { session_id } = useParams<{ session_id: string }>();
+	const { data } = useQuery<
+		{ messages: ConsoleMessage[] },
+		{ session_id: string }
+	>(
+		gql`
+			query GetEvents($session_id: ID!) {
+				messages(session_id: $session_id)
+			}
+		`,
+		{ variables: { session_id } }
+	);
 
 	useEffect(() => {
 		onClick();
@@ -26,19 +27,57 @@ export const ConsolePage = ({ onClick }: { onClick: () => void }) => {
 
 	return (
 		<>
-			{showStream && (
-				<div className={styles.logStreamWrapper}>
-			{JSON.stringify(data)}
+			{data?.messages?.length && (
+				<div className={styles.consolePageWrapper}>
+					<div
+						className={styles.consoleTopBar}
+						style={{
+							backgroundColor: showStream
+								? 'white'
+								: 'transparent',
+						}}
+					>
+						<div
+							onClick={() => {
+								setShowStream(!showStream);
+							}}
+							className={styles.consoleButton}
+						>
+							Console{' '}
+							{showStream ? (
+								<DownIcon className={styles.directionIcon} />
+							) : (
+								<UpIcon className={styles.directionIcon} />
+							)}
+						</div>
+					</div>
+					{showStream && (
+						<div className={styles.logStreamWrapper}>
+							{data?.messages?.map((m) => {
+								return (
+									<div className={styles.consoleMessage}>
+										{JSON.stringify(m.value)}
+									</div>
+								);
+							})}
+						</div>
+					)}
 				</div>
 			)}
-			<div
-				onClick={() => {
-					setShowStream(!showStream);
-				}}
-				className={styles.consoleButton}
-			>
-				Console
-			</div>
 		</>
 	);
 };
+
+// copied from 'client/src/index.tsx'
+type ConsoleMessage = {
+	value: IArguments;
+	time: number;
+	type: ConsoleType;
+};
+
+enum ConsoleType {
+	Log,
+	Debug,
+	Error,
+	Warn,
+}
