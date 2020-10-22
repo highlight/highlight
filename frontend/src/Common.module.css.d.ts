081e762da0b2e57374149e1f68950cb4c078271e
Jay Khatri <jaykhatrimail@gmail.com>
2020-10-22 11:51:00 -0400

    small change"

--- frontend/src/Common.module.css.d.ts
+++ frontend/src/Common.module.css.d.ts
@@ -1,7 +1,8 @@
 declare const styles: {
-    readonly input: string;
-    readonly submitButton: string;
-    readonly errorMessage: string;
-    readonly secondaryButton: string;
+  readonly "input": string;
+  readonly "submitButton": string;
+  readonly "errorMessage": string;
+  readonly "secondaryButton": string;
 };
 export = styles;
+
