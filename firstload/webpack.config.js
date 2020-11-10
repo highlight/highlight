8ae89b300545e1be0b026a5409d5583369016847
Jay Khatri <jaykhatrimail@gmail.com>
2020-11-09 23:54:27 -0500

    change

--- firstload/webpack.config.js
+++ firstload/webpack.config.js
@@ -32,8 +32,8 @@ module.exports = {
     extensions: ['.js', '.ts', '.tsx'],
     modules: [path.resolve(__dirname, 'src'), 'node_modules'],
   },
-  optimization: {	
-    minimize: true	
+  optimization: {
+    minimize: true
   },
   devtool: 'sourceMap',
 };
