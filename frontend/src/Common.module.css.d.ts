7b020a6611a885e6d455134ecdc6f10e088b45a0
Jay Khatri <j@jaykhatri.com>
2020-11-28 17:43:43 -0500

    woot (#159)

--- frontend/src/Common.module.css.d.ts
+++ frontend/src/Common.module.css.d.ts
@@ -1,5 +1,6 @@
 declare const styles: {
   readonly "input": string;
+  readonly "bodyWrapper": string;
   readonly "submitButton": string;
   readonly "errorMessage": string;
   readonly "secondaryButton": string;
--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { ReactComponent as HighlightLogoSmall } from '../../static/highlight-logo-small.svg';
+import { ReactComponent as Hamburger } from '../../static/hamburger.svg';
 import { Link } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 import { WorkspaceDropdown } from './WorkspaceDropdown/WorkspaceDropdown';
@@ -8,72 +9,34 @@ import { UserDropdown } from './UserDropdown/UserDropdown';
 import commonStyles from '../../Common.module.css';
 import styles from './Header.module.css';
 import { DemoContext } from '../../DemoContext';
+import { SidebarContext } from '../Sidebar/SidebarContext';
 
 export const Header = () => {
     const { organization_id } = useParams();
     const { demo } = useContext(DemoContext);
+    const { setOpenSidebar, openSidebar } = useContext(SidebarContext);
 
     return (
-        <div className={styles.header}>
-            <Link
-                className={styles.logoWrapper}
-                to={demo ? '/' : `/${organization_id}/sessions`}
-            >
-                <HighlightLogoSmall className={styles.logo} />
-                <span style={{ fontSize: 22, fontWeight: 400 }}>Highlight</span>
-            </Link>
-            <div className={styles.rightHeader}>
-                <Link
-                    onClick={() => {
-                        window.analytics.track('Settings Click', {
-                            foo: 'bar',
-                            bar: 'foo',
-                        });
-                    }}
-                    to={demo ? '#' : `/${organization_id}/settings`}
-                    className={commonStyles.headerLink}
-                >
-                    Settings
-                </Link>
-                <Link
-                    onClick={() => {
-                        window.analytics.track('Team Click', {
-                            foo: 'bar',
-                            bar: 'foo',
-                        });
-                    }}
-                    to={demo ? '#' : `/${organization_id}/team`}
-                    className={commonStyles.headerLink}
-                >
-                    Team
-                </Link>
-                <Link
-                    onClick={() => {
-                        window.analytics.track('Sessions Click', {
-                            foo: 'bar',
-                            bar: 'foo',
-                        });
-                    }}
-                    to={demo ? '#' : `/${organization_id}/sessions`}
-                    className={commonStyles.headerLink}
-                >
-                    Sessions
-                </Link>
-                <Link
-                    onClick={() => {
-                        window.analytics.track('Setup Click', {
-                            foo: 'bar',
-                            bar: 'foo',
-                        });
-                    }}
-                    to={demo ? '#' : `/${organization_id}`}
-                    className={commonStyles.headerLink}
-                >
-                    Setup
-                </Link>
-                <WorkspaceDropdown />
-                <UserDropdown />
+        <>
+            <div className={styles.header}>
+                <div className={styles.logoWrapper}>
+                    <Hamburger
+                        className={styles.hamburger}
+                        onClick={() => setOpenSidebar(!openSidebar)}
+                    />
+                    <Link
+                        className={styles.homeLink}
+                        to={demo ? '/' : `/${organization_id}/sessions`}
+                    >
+                        <HighlightLogoSmall className={styles.logo} />
+                        <span className={styles.logoText}>Highlight</span>
+                    </Link>
+                </div>
+                <div className={styles.rightHeader}>
+                    <UserDropdown />
+                </div>
             </div>
-        </div>
+            <div className={styles.headerPlaceholder} />
+        </>
     );
 };
