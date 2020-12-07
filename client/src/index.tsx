625332cacc8e32fbdf1353aecec40be6336749d2
Jay Khatri <j@jaykhatri.com>
2020-12-07 12:43:08 -0500

    Update webpack lib target (#180)
    * change
    
    * v1.0.44

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -48,7 +48,7 @@ export class Highlight {
     // If debug is set to false, disable all console logs.
     this.ready = false;
     this.logger = new Logger(options.debug ?? false);
-    const backend = "https://backend-2.onrender.com";
+    const backend = process.env.BACKEND_URI;
     this.client = new ApolloClient({
       uri: `${backend}/client`,
       cache: new InMemoryCache(),
