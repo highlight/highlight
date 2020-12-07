dabbbbb89832cc6b9b23c2727606e597b624f897
Jay Khatri <jaykhatrimail@gmail.com>
2020-12-07 12:37:16 -0500

    change

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].js',
-    library: '$',
+    library: 'highlightLib',
     libraryTarget: 'umd',
   },
   devServer: {
--- firstload/webpack.config.js
+++ firstload/webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist/firstload/src'),
     filename: '[name].js',
-    library: '$',
+    library: 'highlightLib',
     libraryTarget: 'umd',
     globalObject: 'this',
   },
