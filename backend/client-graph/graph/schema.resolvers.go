de1b46f915937a9e0c8eaa3e63b604b7c9af096c
Cameron Brill <c00brill@gmail.com>
2020-12-09 17:56:35 -0600

    Moved geolocation and user-agnet parsing to be serverside.

--- backend/client-graph/graph/schema.resolvers.go
+++ backend/client-graph/graph/schema.resolvers.go
@@ -7,14 +7,19 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"os"
 	"time"
 
 	"github.com/jay-khatri/fullstory/backend/client-graph/graph/generated"
 	"github.com/jay-khatri/fullstory/backend/model"
+	"github.com/mssola/user_agent"
 	e "github.com/pkg/errors"
 )
 
-func (r *mutationResolver) InitializeSession(ctx context.Context, organizationID int, details string) (*model.Session, error) {
+func (r *mutationResolver) InitializeSession(ctx context.Context, organizationID int) (*model.Session, error) {
 	organization := &model.Organization{}
 	res := r.DB.Where(&model.Organization{Model: model.Model{ID: organizationID}}).First(&organization)
 	if err := res.Error; err != nil || res.RecordNotFound() {
@@ -39,7 +44,79 @@ func (r *mutationResolver) InitializeSession(ctx context.Context, organizationID
 			return nil, e.Wrap(err, "error updating user")
 		}
 	}
-	session := &model.Session{UserID: user.ID, OrganizationID: organizationID, Details: details}
+
+	// Get the user's ip, get geolocation data
+	var locationString string
+	if ip, ok := ctx.Value("ip").(string); ok {
+		ip, _, _ = net.SplitHostPort(ip)
+		ipStr := net.ParseIP(ip).String()
+		if os.Getenv("DOPPLER_ENCLAVE_ENVIRONMENT") == "dev" {
+			ipStr = "99.98.244.156"
+		}
+		url := fmt.Sprintf("https://geolocation-db.com/json/%s", ipStr)
+		method := "GET"
+
+		client := &http.Client{}
+		req, err := http.NewRequest(method, url, nil)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		res, err := client.Do(req)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		defer res.Body.Close()
+
+		body, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		var location struct {
+			CountryCode *string      `json:"country_code,omitempty"`
+			CountryName *string      `json:"country_name,omitempty"`
+			City        *string      `json:"city,omitempty"`
+			Postal      *string      `json:"postal,omitempty"`
+			Latitude    *interface{} `json:"latitude,omitempty"`
+			Longitude   *interface{} `json:"longitude,omitempty"`
+			IPv4        *string      `json:"IPv4,omitempty"`
+			State       *string      `json:"state,omitempty"`
+		}
+		err = json.Unmarshal(body, &location)
+		if err != nil {
+			fmt.Println(err)
+		}
+		if *location.Longitude == "Not found" {
+			location.CountryCode = nil
+			location.CountryName = nil
+			location.City = nil
+			location.Postal = nil
+			location.Latitude = nil
+			location.Longitude = nil
+			location.IPv4 = nil
+			location.State = nil
+		}
+
+		locationByte, err := json.Marshal(location)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		locationString = string(locationByte)
+	}
+
+	var os string
+	var browser string
+	// Parse the user-agent string
+	if userAgentString, ok := ctx.Value("userAgent").(string); ok {
+		ua := user_agent.New(userAgentString)
+		os = ua.OSInfo().Name
+		browser, _ = ua.Browser()
+	}
+
+	session := &model.Session{UserID: user.ID, OrganizationID: organizationID, Location: locationString, OS: os, Browser: browser}
 	if err := r.DB.Create(session).Error; err != nil {
 		return nil, e.Wrap(err, "error creating session")
 	}
@@ -51,8 +128,20 @@ func (r *mutationResolver) IdentifySession(ctx context.Context, sessionID int, u
 	if !ok {
 		return nil, fmt.Errorf("error converting userObject interface type")
 	}
+	var os string
+	var browser string
+	// Parse the user-agent string
+	if userAgentString, ok := ctx.Value("userAgent").(string); ok {
+		ua := user_agent.New(userAgentString)
+		os = ua.OSInfo().Name
+		browser, _ = ua.Browser()
+	}
+
+	// TODO Cameron: Add more fields from user agent/location here  
 	fields := map[string]string{
 		"identifier": userIdentifier,
+		"os":         os,
+		"browser":    browser,
 	}
 	for k, v := range obj {
 		fields[k] = fmt.Sprintf("%v", v)
