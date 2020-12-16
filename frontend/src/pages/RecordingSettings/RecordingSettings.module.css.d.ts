dc0b0d67a6673286f388c992037cc6dbeb17f1ca
Jay Khatri <jaykhatrimail@gmail.com>
2020-12-15 20:22:01 -0500

    change

--- frontend/src/pages/RecordingSettings/RecordingSettings.module.css.d.ts
+++
@@ -1,5 +0,0 @@
-declare const styles: {
-  readonly "title": string;
-};
-export = styles;
-
--- frontend/src/pages/RecordingSettings/RecordingSettings.tsx
+++
@@ -1,7 +0,0 @@
-import React, { useEffect, useContext, useState } from 'react';
-import commonStyles from '../../Common.module.css';
-import styles from './RecordingSettings.module.css';
-
-export const RecordingSettings = () => {
-    return <p>hi</p>
-}
