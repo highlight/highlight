8147cdd69e987dc0b07b7243c4f66b0d31a45660
Jay Khatri <jaykhatrimail@gmail.com>
2021-01-10 11:47:19 -0500

    lets see what this does

--- frontend/src/pages/Sessions/SearchInputs/DateInput.tsx
+++ frontend/src/pages/Sessions/SearchInputs/DateInput.tsx
@@ -3,22 +3,25 @@ import React, { useContext } from 'react';
 import { SearchContext, SearchParams } from '../SearchContext/SearchContext';
 import inputStyles from './InputStyles.module.scss';
 
+const { RangePicker } = DatePicker;
+
 export const DateInput = () => {
     const { setSearchParams } = useContext(SearchContext);
     return (
         <div className={inputStyles.commonInputWrapper}>
-            <DatePicker.RangePicker
+            <RangePicker
                 className={inputStyles.datePicker}
-                onChange={(date: any, dateStrings: [string, string]) => {
-                    const start_date = dateStrings[0] ? new Date(dateStrings[0]) : undefined;
-                    const end_date = dateStrings[1] ? new Date(dateStrings[1]) : undefined;
-                    setSearchParams((params: SearchParams): SearchParams => {
-                        return {
-                            ...params,
-                            date_range: (!start_date || !end_date) ? undefined : { start_date, end_date },
-                        }
-                    })
-                }} />
+            // onChange={(date: any, dateStrings: [string, string]) => {
+            // const start_date = dateStrings[0] ? new Date(dateStrings[0]) : undefined;
+            // const end_date = dateStrings[1] ? new Date(dateStrings[1]) : undefined;
+            // setSearchParams((params: SearchParams): SearchParams => {
+            //     return {
+            //         ...params,
+            //         date_range: (!start_date || !end_date) ? undefined : { start_date, end_date },
+            //     }
+            // })
+            // }} 
+            />
         </div>
     );
 
