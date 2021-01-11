3cc85413e75e2b6527dd20f465dbf1c94a7921f8
Jay Khatri <jaykhatrimail@gmail.com>
2021-01-11 18:51:40 -0500

    change

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,6 +1,7 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useEffect, useContext, useState } from 'react';
 import { ReactComponent as HighlightLogoSmall } from '../../static/highlight-logo-small.svg';
 import { ReactComponent as Hamburger } from '../../static/hamburger.svg';
+import { ReactComponent as Close } from '../../static/close.svg';
 import { Link, withRouter } from 'react-router-dom';
 import { useParams, RouteComponentProps } from 'react-router-dom';
 import { UserDropdown } from './UserDropdown/UserDropdown';
@@ -11,11 +12,17 @@ import styles from './Header.module.scss';
 import { DemoContext } from '../../DemoContext';
 import { SidebarContext } from '../Sidebar/SidebarContext';
 import classNames from 'classnames/bind';
+import { Duration } from '../../util/time';
 
-const Head: React.FunctionComponent<RouteComponentProps> = ({ history }) => {
+type HeaderProps = {
+    trialTimeRemaining?: Duration;
+}
+
+const Head: React.FunctionComponent<RouteComponentProps & HeaderProps> = ({ history, ...props }) => {
     const { organization_id } = useParams<{ organization_id: string }>();
     const { demo } = useContext(DemoContext);
     const { setOpenSidebar, openSidebar } = useContext(SidebarContext);
+    const { trialTimeRemaining } = props;
 
     useEffect(() => {
         const keys = ['command+k', 'ctrl+k'];
@@ -35,47 +42,59 @@ const Head: React.FunctionComponent<RouteComponentProps> = ({ history }) => {
     return (
         <>
             <div className={styles.header}>
-                <div className={styles.logoWrapper}>
-                    <Hamburger
-                        className={styles.hamburger}
-                        onClick={() => {
-                            console.log('clicked');
-                            setOpenSidebar(!openSidebar)
-                        }}
-                        style={{
-                            transform: openSidebar
-                                ? 'rotate(-180deg)'
-                                : 'rotate(0deg)',
-                        }}
-                    />
-                    <Link
-                        className={styles.homeLink}
-                        to={demo ? '/' : `/${organization_id}/sessions`}
-                    >
-                        <HighlightLogoSmall className={styles.logo} />
-                        <span className={styles.logoText}>Highlight</span>
-                    </Link>
-                </div>
-                <div className={styles.searchWrapper}>
-                    <Link
-                        className={styles.searchBar}
-                        to={`/${organization_id}/sessions`}
-                    >
-                        <FaSearch style={{ marginRight: 8 }} />
-                        <span className={styles.dontSelect}>Search for sessions</span>
+                {trialTimeRemaining && <TrialBanner timeRemaining={trialTimeRemaining} />}
+                <div className={styles.headerContent}>
+                    <div className={styles.logoWrapper}>
+                        <Hamburger
+                            className={styles.hamburger}
+                            onClick={() => {
+                                console.log('clicked');
+                                setOpenSidebar(!openSidebar)
+                            }}
+                            style={{
+                                transform: openSidebar
+                                    ? 'rotate(-180deg)'
+                                    : 'rotate(0deg)',
+                            }}
+                        />
+                        <Link
+                            className={styles.homeLink}
+                            to={demo ? '/' : `/${organization_id}/sessions`}
+                        >
+                            <HighlightLogoSmall className={styles.logo} />
+                            <span className={styles.logoText}>Highlight</span>
+                        </Link>
+                    </div>
+                    <div className={styles.rightHeader}>
                         <div className={classNames(styles.commandWrapper, styles.dontSelect)}>
                             <div className={styles.commandContainer}>⌘</div>
                             <div className={styles.commandContainer}>K</div>
                         </div>
-                    </Link>
-                </div>
-                <div className={styles.rightHeader}>
-                    <UserDropdown />
+                        <UserDropdown />
+                    </div>
                 </div>
             </div>
             <div className={styles.headerPlaceholder} />
         </>
     );
 };
+
+
+
+const TrialBanner = ({ timeRemaining }: { timeRemaining: Duration }) => {
+    console.log(timeRemaining);
+    const { organization_id } = useParams<{ organization_id: string }>();
+    const [bannerPresent, setBannerPresent] = useState(true);
+    return (
+        bannerPresent ?
+            <div className={styles.trialWrapper} >
+                <div className={classNames(styles.trialTimeText)}>
+                    {timeRemaining.days}&nbsp;days left in your trial. Pick a plan <Link onClick={() => setBannerPresent(false)} className={styles.trialLink} to={`/${organization_id}/billing`}>here!</Link>
+                </div>
+                <Close className={styles.trialCloseStyle} onClick={() => setBannerPresent(false)} />
+            </div > :
+            <></>
+    )
+}
 
 export const Header = withRouter(Head);
