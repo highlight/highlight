8cb368cf93c4da2d45d0e27b54424d76f487828d
Jay Khatri <j@jaykhatri.com>
2021-01-11 21:49:49 -0500

    Add a trial banner, changes to search at the top. (#247)

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,21 +1,27 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useEffect, useContext, useState } from 'react';
 import { ReactComponent as HighlightLogoSmall } from '../../static/highlight-logo-small.svg';
 import { ReactComponent as Hamburger } from '../../static/hamburger.svg';
+import { ReactComponent as Close } from '../../static/close.svg';
 import { Link, withRouter } from 'react-router-dom';
 import { useParams, RouteComponentProps } from 'react-router-dom';
 import { UserDropdown } from './UserDropdown/UserDropdown';
-import { FaSearch } from 'react-icons/fa';
 import * as Mousetrap from 'mousetrap';
 
 import styles from './Header.module.scss';
 import { DemoContext } from '../../DemoContext';
 import { SidebarContext } from '../Sidebar/SidebarContext';
 import classNames from 'classnames/bind';
+import { Duration } from '../../util/time';
 
-const Head: React.FunctionComponent<RouteComponentProps> = ({ history }) => {
+type HeaderProps = {
+    trialTimeRemaining?: Duration;
+}
+
+const Head: React.FunctionComponent<RouteComponentProps & HeaderProps> = ({ history, ...props }) => {
     const { organization_id } = useParams<{ organization_id: string }>();
     const { demo } = useContext(DemoContext);
     const { setOpenSidebar, openSidebar } = useContext(SidebarContext);
+    const { trialTimeRemaining } = props;
 
     useEffect(() => {
         const keys = ['command+k', 'ctrl+k'];
@@ -35,47 +41,62 @@ const Head: React.FunctionComponent<RouteComponentProps> = ({ history }) => {
     return (
         <>
             <div className={styles.header}>
-                <div className={styles.logoWrapper}>
-                    <Hamburger
-                        className={styles.hamburger}
-                        onClick={() => {
-                            console.log('clicked');
-                            setOpenSidebar(!openSidebar)
-                        }}
-                        style={{
-                            transform: openSidebar
-                                ? 'rotate(-180deg)'
-                                : 'rotate(0deg)',
-                        }}
-                    />
-                    <Link
-                        className={styles.homeLink}
-                        to={demo ? '/' : `/${organization_id}/sessions`}
-                    >
-                        <HighlightLogoSmall className={styles.logo} />
-                        <span className={styles.logoText}>Highlight</span>
-                    </Link>
-                </div>
-                <div className={styles.searchWrapper}>
-                    <Link
-                        className={styles.searchBar}
-                        to={`/${organization_id}/sessions`}
-                    >
-                        <FaSearch style={{ marginRight: 8 }} />
-                        <span className={styles.dontSelect}>Search for sessions</span>
-                        <div className={classNames(styles.commandWrapper, styles.dontSelect)}>
+                {trialTimeRemaining && <TrialBanner timeRemaining={trialTimeRemaining} />}
+                <div className={styles.headerContent}>
+                    <div className={styles.logoWrapper}>
+                        <Hamburger
+                            className={styles.hamburger}
+                            onClick={() => {
+                                console.log('clicked');
+                                setOpenSidebar(!openSidebar)
+                            }}
+                            style={{
+                                transform: openSidebar
+                                    ? 'rotate(-180deg)'
+                                    : 'rotate(0deg)',
+                            }}
+                        />
+                        <Link
+                            className={styles.homeLink}
+                            to={demo ? '/' : `/${organization_id}/sessions`}
+                        >
+                            <HighlightLogoSmall className={styles.logo} />
+                            <span className={styles.logoText}>Highlight</span>
+                        </Link>
+                    </div>
+                    <div className={styles.rightHeader}>
+                        <div className={styles.searchPrompt} onClick={() => history.push(`/${organization_id}/sessions`)}>
+                            <div className={classNames(styles.commandWrapper, styles.dontSelect)}>
+                                Start a search with
                             <div className={styles.commandContainer}>⌘</div>
-                            <div className={styles.commandContainer}>K</div>
+                                <div className={styles.commandContainer}>K</div>
+                            </div>
                         </div>
-                    </Link>
-                </div>
-                <div className={styles.rightHeader}>
-                    <UserDropdown />
+                        <UserDropdown />
+                    </div>
                 </div>
             </div>
             <div className={styles.headerPlaceholder} />
         </>
     );
 };
+
+
+
+const TrialBanner = ({ timeRemaining }: { timeRemaining: Duration }) => {
+    console.log(timeRemaining);
+    const { organization_id } = useParams<{ organization_id: string }>();
+    const [bannerPresent, setBannerPresent] = useState(true);
+    return (
+        bannerPresent ?
+            <div className={styles.trialWrapper} >
+                <div className={classNames(styles.trialTimeText)}>
+                    {timeRemaining.days}&nbsp;days left in your trial. Pick a plan <Link onClick={() => setBannerPresent(false)} className={styles.trialLink} to={`/${organization_id}/billing`}>here!</Link>
+                </div>
+                <Close className={styles.trialCloseStyle} onClick={() => setBannerPresent(false)} />
+            </div > :
+            <></>
+    )
+}
 
 export const Header = withRouter(Head);
--- frontend/src/util/time.tsx
+++ frontend/src/util/time.tsx
@@ -14,3 +14,32 @@ export function MillisToMinutesAndSecondsVerbose(millis: any) {
     }
     return str + seconds + ' seconds';
 }
+
+export type Duration = {
+    days: number;
+    hours: number;
+    minutes: number;
+}
+
+
+export function MillisToDaysHoursMinSeconds(t: number): Duration {
+    var cd = 24 * 60 * 60 * 1000,
+        ch = 60 * 60 * 1000,
+        d = Math.floor(t / cd),
+        h = Math.floor((t - d * cd) / ch),
+        m = Math.round((t - d * cd - h * ch) / 60000),
+        pad = function (n: number) { return n < 10 ? 0 + n : n; };
+    if (m === 60) {
+        h++;
+        m = 0;
+    }
+    if (h === 24) {
+        d++;
+        h = 0;
+    }
+    return {
+        days: d,
+        hours: pad(h),
+        minutes: pad(m),
+    }
+}
