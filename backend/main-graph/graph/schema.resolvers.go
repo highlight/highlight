cbc7c5a735d4677141e6364a8e013efa9333cf40
Anthony <yihonganthony@gmail.com>
2021-01-14 16:36:02 -0600

    Added fields to player metabox

--- backend/main-graph/graph/schema.resolvers.go
+++ backend/main-graph/graph/schema.resolvers.go
@@ -227,11 +227,16 @@ func (r *mutationResolver) CreateCheckout(ctx context.Context, organizationID in
 }
 
 func (r *queryResolver) Session(ctx context.Context, id int) (*model.Session, error) {
-	session, err := r.isAdminSessionOwner(ctx, id)
-	if err != nil {
+	if _, err := r.isAdminSessionOwner(ctx, id); err != nil {
 		return nil, e.Wrap(err, "admin not session owner")
 	}
-	return session, nil
+	// r.DB.Preload("Fields")
+	sessionObj := &model.Session{}
+	res := r.DB.Preload("Fields").Where(&model.Session{Model: model.Model{ID: id}}).First(&sessionObj)
+	if res.Error != nil {
+		return nil, fmt.Errorf("error reading from session: %v", res.Error)
+	}
+	return sessionObj, nil
 }
 
 func (r *queryResolver) Events(ctx context.Context, sessionID int) ([]interface{}, error) {
--- frontend/src/pages/Player/MetadataBox/MetadataBox.tsx
+++ frontend/src/pages/Player/MetadataBox/MetadataBox.tsx
@@ -1,14 +1,22 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useQuery, gql } from '@apollo/client';
-import { Skeleton } from 'antd';
+import { Skeleton, Tag } from 'antd';
+import { UpOutlined, DownOutlined } from '@ant-design/icons';
 import { Avatar } from '../../../components/Avatar/Avatar';
 
 import styles from './MetadataBox.module.scss';
 import { DemoContext } from '../../../DemoContext';
 
+type Field = {
+    type: string;
+    name: string;
+    value: string;
+};
+
 export const MetadataBox = () => {
     const { session_id } = useParams<{ session_id: string }>();
+    const [expanded, setExpanded] = useState(false);
     const { demo } = useContext(DemoContext);
 
     const { loading, error, data } = useQuery<{
@@ -24,6 +32,7 @@ export const MetadataBox = () => {
             created_at: number;
             user_object: any;
             identifier: string;
+            fields: Array<Field>;
         };
     }>(
         gql`
@@ -40,6 +49,11 @@ export const MetadataBox = () => {
                     created_at
                     user_object
                     identifier
+                    fields {
+                        name
+                        value
+                        type
+                    }
                 }
             }
         `,
@@ -60,41 +74,89 @@ export const MetadataBox = () => {
             ) : error ? (
                 <p>{error?.toString()}</p>
             ) : (
-                        <>
-                            <div className={styles.userAvatarWrapper}>
-                                <Avatar
-                                    style={{ width: 75 }}
-                                    seed={data?.session.identifier ?? ''}
-                                />
-                            </div>
-                            <div className={styles.userContentWrapper}>
-                                <div className={styles.headerWrapper}>
-                                    <div>User#{data?.session.user_id}</div>
-                                    {data?.session.identifier && (
-                                        <div className={styles.userIdSubHeader}>
-                                            {data?.session.identifier}
-                                        </div>
-                                    )}
+                <>
+                    <div className={styles.userAvatarWrapper}>
+                        <Avatar
+                            style={{ width: 75 }}
+                            seed={data?.session.identifier ?? ''}
+                        />
+                    </div>
+                    <div className={styles.userContentWrapper}>
+                        <div className={styles.headerWrapper}>
+                            <div>User#{data?.session.user_id}</div>
+                            {data?.session.identifier && (
+                                <div className={styles.userIdSubHeader}>
+                                    {data?.session.identifier}
                                 </div>
-                                <div className={styles.userInfoWrapper}>
-                                    <div className={styles.userText}>
-                                        {data?.session.city ? data.session.city + ', ' : ''}
-                                        {data?.session.state ? data.session.state + ' ' : ''}
-                                        {data?.session.postal ? data.session.postal : ''}
-                                    </div>
-                                    <div className={styles.userText}>
-                                        {created.toUTCString()}
-                                    </div>
-                                    {data?.session.browser_name && (
-                                        <div className={styles.userText}>
-                                            {data?.session.os_name},&nbsp;
-                                            {data?.session.browser_name}&nbsp;-&nbsp;{data?.session.browser_version}
-                                        </div>
-                                    )}
-                                </div>
+                            )}
+                        </div>
+                        <div className={styles.tagDiv}>
+                            <div
+                                className={
+                                    expanded
+                                        ? styles.tagWrapperExpanded
+                                        : styles.tagWrapper
+                                }
+                            >
+                                {data?.session.fields
+                                    .filter(
+                                        (f) =>
+                                            f.type === 'user' &&
+                                            f.name !== 'identifier' &&
+                                            f.value.length
+                                    )
+                                    .map((f) => (
+                                        <Tag
+                                            color="#F2EEFB"
+                                            style={{
+                                                marginTop: '3px',
+                                            }}
+                                        >
+                                            <span
+                                                style={{
+                                                    color: 'black',
+                                                    fontWeight: 300,
+                                                }}
+                                                className={styles.tagEllipsis}
+                                            >
+                                                {f.name}:&nbsp;{f.value}
+                                            </span>
+                                        </Tag>
+                                    ))}
                             </div>
-                        </>
-                    )}
+                            <div
+                                className={styles.expandArrow}
+                                onClick={() => setExpanded(!expanded)}
+                            >
+                                {expanded ? <UpOutlined /> : <DownOutlined />}
+                            </div>
+                        </div>
+                        <div className={styles.userInfoWrapper}>
+                            <div className={styles.userText}>
+                                {data?.session.city
+                                    ? data.session.city + ', '
+                                    : ''}
+                                {data?.session.state
+                                    ? data.session.state + ' '
+                                    : ''}
+                                {data?.session.postal
+                                    ? data.session.postal
+                                    : ''}
+                            </div>
+                            <div className={styles.userText}>
+                                {created.toUTCString()}
+                            </div>
+                            {data?.session.browser_name && (
+                                <div className={styles.userText}>
+                                    {data?.session.os_name},&nbsp;
+                                    {data?.session.browser_name}&nbsp;-&nbsp;
+                                    {data?.session.browser_version}
+                                </div>
+                            )}
+                        </div>
+                    </div>
+                </>
+            )}
         </div>
     );
 };
