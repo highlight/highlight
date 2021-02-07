23b7b5b2ee8103915e495e6b724d66a385767088
John Pham <johnphammail@gmail.com>
2021-02-06 18:11:41 -0800

    Convert payload to string

--- frontend/src/pages/Player/StreamElement/StreamElement.tsx
+++ frontend/src/pages/Player/StreamElement/StreamElement.tsx
@@ -105,7 +105,7 @@ export const StreamElement = ({
                             }
                         >
                             <span className={styles.codeBlock}>
-                                {details.payload}
+                                {details.payload?.toString()}
                             </span>
                         </div>
                     )}
