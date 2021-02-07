34bf3eafa321d467987dfc5dbe6b13fc3e5c511e
Jay Khatri <jaykhatrimail@gmail.com>
2021-02-07 12:45:02 -0500

    lets see

---
+++ firstload/compare.py
@@ -0,0 +1,8 @@
+import subprocess
+
+local = subprocess.check_output("node -p -e \"require('./package.json').version\"", shell=True, text=True).replace('\r', '').replace('\n', '')
+remote = subprocess.check_output("npm show highlight.run version", shell=True, text=True).replace('\r', '').replace('\n', '')
+if local == remote:
+    print("Error: any changes to firstload require an updated package number, please run `yarn version`")
+    exit(1)
+exit(0)
