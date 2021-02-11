6dfc943f641d3ccfbd64593d2d0a05672680ad88
Jay Khatri <jaykhatrimail@gmail.com>
2021-02-10 20:59:58 -0500

    change

---
+++ client/src/listeners/segment-integration-listener.tsx
@@ -0,0 +1,18 @@
+export const SegmentIntegrationListener = (callback: (obj: any) => void) => {
+    callback(window.location.href);
+    var send = XMLHttpRequest.prototype.send;
+    XMLHttpRequest.prototype.send = function (data) {
+        setTimeout(() => {
+            var obj: any;
+            try {
+                obj = JSON.parse(data?.toString() ?? '');
+            } catch (e) {
+                return;
+            }
+            if (obj.type === 'track' || obj.type === 'identify') {
+                callback(obj);
+            }
+        }, 100);
+        send.call(this, data);
+    };
+};
