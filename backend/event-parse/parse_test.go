e435d56fe538ddad12971a68b37d98d2845f8d5a
Jay Khatri <jaykhatrimail@gmail.com>
2021-02-23 12:04:52 -0500

    run tests

--- backend/event-parse/parse_test.go
+++ backend/event-parse/parse_test.go
@@ -2,7 +2,6 @@ package parse
 
 import (
 	"encoding/json"
-	"strings"
 	"testing"
 	"time"
 
@@ -59,64 +58,6 @@ func TestEventsFromString(t *testing.T) {
 		want := *tt.wantEvents
 		if diff := deep.Equal(got, want); diff != nil {
 			t.Error(diff)
-		}
-	}
-}
-
-func TestInjectStyleSheets(t *testing.T) {
-	tables := []struct {
-		input string
-		want  string
-	}{
-		{
-			`
-			{
-				"node": {
-					"childNodes": [
-						{
-							"data": {"test": 5},
-							"timestamp": 0,
-							"type": 4
-						},
-						{
-							"href": "https://app.highlight.run/public.css",
-							"rel": "stylesheet"
-						}
-					]
-				}
-			}
-			`,
-			`
-			{
-				"node": {
-					"childNodes": [
-						{
-							"data": {"test": 5},
-							"timestamp": 0,
-							"type": 4
-						},
-						{
-							"_cssText": ".highlight {
-									color: black;
-								}"
-						}
-					]
-				}
-			}
-			`,
-		},
-	}
-	r := strings.NewReplacer("\n", "", "\t", "", " ", "")
-	for i, tt := range tables {
-		gotRaw, err := InjectStylsheets(json.RawMessage(tt.input))
-		if err != nil {
-			t.Errorf("[%v]: %v", i, err)
-			continue
-		}
-		got := r.Replace(string(gotRaw))
-		want := r.Replace(tt.want)
-		if got != want {
-			t.Errorf("[%v]: \n got \n%v \n want \n%v", i, got, want)
 		}
 	}
 }
