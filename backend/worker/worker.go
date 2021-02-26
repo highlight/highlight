8db6eac34e10cdcfd10cc62d2dad8a0e4b08b125
Jay Khatri <jaykhatrimail@gmail.com>
2021-02-26 12:18:37 -0500

    change

--- backend/worker/worker.go
+++ backend/worker/worker.go
@@ -5,7 +5,6 @@ import (
 	"time"
 
 	"github.com/jay-khatri/fullstory/backend/model"
-	"github.com/k0kubun/pp"
 	"github.com/pkg/errors"
 	"github.com/slack-go/slack"
 
@@ -20,7 +19,6 @@ type Worker struct {
 }
 
 func (w *Worker) processSession(s *model.Session) error {
-	pp.Println("processing")
 	// Set the session as processed; if any is error thrown after this, the session gets ignored.
 	if err := w.R.DB.Model(&model.Session{}).Where(
 		&model.Session{Model: model.Model{ID: s.ID}},
@@ -29,8 +27,6 @@ func (w *Worker) processSession(s *model.Session) error {
 	).Error; err != nil {
 		return errors.Wrap(err, "error updating session to processed status")
 	}
-
-	pp.Println("getting first events")
 
 	// Get the events with the earliest 'created_at' timestamp.
 	firstEvents := &model.EventsObject{}
@@ -42,8 +38,6 @@ func (w *Worker) processSession(s *model.Session) error {
 		return errors.Wrap(err, "error parsing first set of events")
 	}
 
-	pp.Println("getting last events")
-
 	// Get the events with the latest 'created_at' timestamp.
 	lastEvents := &model.EventsObject{}
 	if err := w.R.DB.Where(&model.EventsObject{SessionID: s.ID}).Order("created_at desc").First(lastEvents).Error; err != nil {
@@ -54,8 +48,6 @@ func (w *Worker) processSession(s *model.Session) error {
 		return errors.Wrap(err, "error parsing last set of events")
 	}
 
-	pp.Println("got events parsed")
-	pp.Println("calculation session length")
 	// Calcaulate total session length and write the length to the session.
 	diff := CalculateSessionLength(firstEventsParsed, lastEventsParsed)
 	if err := w.R.DB.Model(&model.Session{}).Where(
@@ -66,7 +58,6 @@ func (w *Worker) processSession(s *model.Session) error {
 		return errors.Wrap(err, "error updating session to processed status")
 	}
 
-	pp.Printf("id: %v, length: %v \n", s.ID, diff)
 	// Send a notification that the session was processed.
 	msg := slack.WebhookMessage{Text: fmt.Sprintf("```NEW SESSION \nid: %v\norg_id: %v\nuser_id: %v\nuser_object: %v\nurl: %v```",
 		s.ID,
