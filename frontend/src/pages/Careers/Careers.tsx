bbdd43d0652530b64cffb82339141326bccd9322
Jay Khatri <jaykhatrimail@gmail.com>
2021-03-01 18:01:14 -0500

    change

--- frontend/src/pages/Careers/Careers.tsx
+++ frontend/src/pages/Careers/Careers.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import styles from './Careers.module.scss';
 
-export const Careers = () => {
-    return (
-        <div className={styles.careersPage}>
-            <p className={styles.careersText}>
-                Highlight is hiring engineers. We're well funded, growing, and
-                have a very eng-centric culture. If that sounds interesting,
-                reach out at jay[at]highlight.run.
-            </p>
-        </div>
-    );
-};
+export const Careers = () => (
+    <div className={styles.careersPage}>
+        <p className={styles.careersText}>
+            Highlight is hiring engineers. We're well funded, growing, and have
+            a very eng-centric culture. If that sounds interesting, reach out at
+            jay[at]highlight.run.
+        </p>
+    </div>
+);
