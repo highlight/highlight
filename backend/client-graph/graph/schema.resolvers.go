294e7a8dab4e0d70e14e9d8668fd0b7d545f573d
Anthony <yihonganthony@gmail.com>
2021-03-09 13:22:28 -0600

    Added field suggestion for error event

--- backend/client-graph/graph/schema.resolvers.go
+++ backend/client-graph/graph/schema.resolvers.go
@@ -243,6 +243,7 @@ func (r *mutationResolver) PushPayload(ctx context.Context, sessionID int, event
 			metaFields = append(metaFields, &model.ErrorField{Name: "browser", Value: sessionObj.BrowserName})
 			metaFields = append(metaFields, &model.ErrorField{Name: "os_name", Value: sessionObj.OSName})
 			metaFields = append(metaFields, &model.ErrorField{Name: "visited_url", Value: errorToInsert.URL})
+			metaFields = append(metaFields, &model.ErrorField{Name: "event", Value: errorToInsert.Event})
 			if err := r.UpdateErrorGroup(*errorToInsert, v.Trace, metaFields); err != nil {
 				log.Errorf("Error updating error group: %v", errorToInsert)
 				continue
