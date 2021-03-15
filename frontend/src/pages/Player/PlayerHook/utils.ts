5c25b0247f03c0cc8b4ff704d5c8ad89d26f2a94
John Pham <john@highlight.run>
2021-03-15 10:41:02 -0700

    Move logic to method to debug

---
+++ frontend/src/pages/Player/PlayerHook/utils.ts
@@ -0,0 +1,51 @@
+import {
+    playerMetaData,
+    SessionInterval,
+} from '@highlight-run/rrweb/dist/types';
+
+const INACTIVE_THRESHOLD = 0.02;
+
+/**
+ * Calculates the active and inactive parts of a session.
+ * @param metadata The Replayer's metadata.
+ * @param allIntervals The intervals from Replayer. This is a Highlight-specific property.
+ * @returns A list of time durations with active/inactive annotations.
+ */
+export const getSessionIntervals = (
+    metadata: playerMetaData,
+    allIntervals: SessionInterval[]
+) => {
+    console.log(metadata, allIntervals);
+    // Preprocess and logic for player length with inactive sessions
+    const intervals = allIntervals.map((e) => ({
+        ...e,
+        startTime: e.startTime - metadata.startTime,
+        endTime: e.endTime - metadata.startTime,
+    }));
+    const { activeDuration, numInactive } = allIntervals.reduce(
+        (acc, interval) => ({
+            activeDuration: interval.active
+                ? acc.activeDuration + interval.duration
+                : acc.activeDuration,
+            numInactive: interval.active ? acc.numInactive : ++acc.numInactive,
+        }),
+        { activeDuration: 0, numInactive: 0 }
+    );
+    const inactiveSliceDuration = activeDuration
+        ? INACTIVE_THRESHOLD * activeDuration
+        : 1;
+    const totalDuration = activeDuration + inactiveSliceDuration * numInactive;
+    let currTime = 0;
+    const sliderIntervalMap = intervals.map((e) => {
+        const prevTime = currTime;
+        currTime = currTime + (e.active ? e.duration : inactiveSliceDuration);
+        return {
+            ...e,
+            startPercent: prevTime / totalDuration,
+            endPercent: currTime / totalDuration,
+        };
+    });
+    console.log('[Highlight] Session Intervals:', sliderIntervalMap);
+
+    return sliderIntervalMap;
+};
