0d1928deadfcdb75d56b0bf1e3b8f62b57547f76
nathanjbrockway <nathanjbrockway@gmail.com>
2021-03-24 13:30:24 -0500

    proper naming

--- backend/client-graph/graph/schema.resolvers.go
+++ backend/client-graph/graph/schema.resolvers.go
@@ -242,7 +242,7 @@ func (r *mutationResolver) PushPayload(ctx context.Context, sessionID int, event
 		metaFields = append(metaFields, &model.ErrorField{OrganizationID: organizationID, Name: "os_name", Value: sessionObj.OSName})
 		metaFields = append(metaFields, &model.ErrorField{OrganizationID: organizationID, Name: "visited_url", Value: errorToInsert.URL})
 		metaFields = append(metaFields, &model.ErrorField{OrganizationID: organizationID, Name: "event", Value: errorToInsert.Event})
-		group, err := r.UpdateErrorGroup(errorToInsert, v.Trace, metaFields)
+		group, err := r.HandleErrorAndGroup(errorToInsert, v.Trace, metaFields)
 		if err != nil {
 			log.Errorf("Error updating error group: %v", errorToInsert)
 			continue
