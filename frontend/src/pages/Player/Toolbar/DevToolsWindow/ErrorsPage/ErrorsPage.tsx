15fd8bf7abdbe8a3b81a28bd9840d0a413c66fe0
John Pham <john@highlight.run>
2021-03-25 16:49:04 -0700

    Remove unused import

--- frontend/src/pages/Player/Toolbar/DevToolsWindow/ErrorsPage/ErrorsPage.tsx
+++ frontend/src/pages/Player/Toolbar/DevToolsWindow/ErrorsPage/ErrorsPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
 import { useGetErrorsQuery } from '../../../../../graph/generated/hooks';
 import { DevToolsSelect } from '../Option/Option';
