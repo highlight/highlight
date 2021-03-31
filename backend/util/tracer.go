a49e1bea6d2cdb857efa790d30ff3c10814d2242
Jay Khatri <jaykhatrimail@gmail.com>
2021-03-31 12:07:22 -0400

    run traces on main and client

--- backend/util/tracer.go
+++ backend/util/tracer.go
@@ -9,23 +9,27 @@ import (
 	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
 )
 
-type Tracer struct{}
-
-var _ interface {
+type Tracer struct {
 	graphql.HandlerExtension
 	graphql.ResponseInterceptor
 	graphql.FieldInterceptor
-} = Tracer{}
 
-func (Tracer) ExtensionName() string {
+	serverName string
+}
+
+func NewTracer(backend string) Tracer {
+	return Tracer{serverName: backend}
+}
+
+func (t Tracer) ExtensionName() string {
 	return "HighlightTracer"
 }
 
-func (Tracer) Validate(graphql.ExecutableSchema) error {
+func (t Tracer) Validate(graphql.ExecutableSchema) error {
 	return nil
 }
 
-func (Tracer) InterceptField(ctx context.Context, next graphql.Resolver) (interface{}, error) {
+func (t Tracer) InterceptField(ctx context.Context, next graphql.Resolver) (interface{}, error) {
 	// taken from: https://docs.datadoghq.com/tracing/setup_overview/custom_instrumentation/go/#manually-creating-a-new-span
 	fc := graphql.GetFieldContext(ctx)
 	fieldSpan, ctx := tracer.StartSpanFromContext(ctx, "operation.field", tracer.ResourceName(fc.Field.Name))
@@ -41,13 +45,13 @@ func (Tracer) InterceptField(ctx context.Context, next graphql.Resolver) (interf
 	return next(ctx)
 }
 
-func (Tracer) InterceptResponse(ctx context.Context, next graphql.ResponseHandler) *graphql.Response {
+func (t Tracer) InterceptResponse(ctx context.Context, next graphql.ResponseHandler) *graphql.Response {
 	rc := graphql.GetOperationContext(ctx)
 	start := rc.Stats.OperationStart
 	// NOTE: This gets called for the first time at the highest level. Creates the 'tracing' value, calls the next handler
 	// and returns the response.
 	span, ctx := tracer.StartSpanFromContext(ctx, "graphql.operation", tracer.ResourceName(rc.Operation.Name))
-	span.SetTag("backend", "main-graph")
+	span.SetTag("backend", t.serverName)
 	defer span.Finish()
 	resp := next(ctx)
 	end := graphql.Now()
