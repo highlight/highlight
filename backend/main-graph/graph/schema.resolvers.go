cb773db55ff5c8695a4f4259909f3e338a7678d9
Anthony Teo <yihonganthony@gmail.com>
2021-04-01 11:17:09 -0500

    Update backend/main-graph/graph/schema.resolvers.go
    Co-authored-by: Jay Khatri <j@jaykhatri.com>

--- backend/main-graph/graph/schema.resolvers.go
+++ backend/main-graph/graph/schema.resolvers.go
@@ -1161,22 +1161,3 @@ type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
 type segmentResolver struct{ *Resolver }
 type sessionResolver struct{ *Resolver }
-
-// !!! WARNING !!!
-// The code below was going to be deleted when updating resolvers. It has been copied here so you have
-// one last chance to move it out of harms way if you want. There are two reasons this happens:
-//  - When renaming or deleting a resolver the old code will be put in here. You can safely delete
-//    it when you're done.
-//  - You have helper methods in this file. Move them out to keep these resolver files clean.
-func (r *sessionResolver) Viewed(ctx context.Context, obj *model.Session) (*bool, error) {
-	panic(fmt.Errorf("not implemented"))
-}
-func (r *sessionResolver) Starred(ctx context.Context, obj *model.Session) (*bool, error) {
-	panic(fmt.Errorf("not implemented"))
-}
-func (r *sessionResolver) FieldGroup(ctx context.Context, obj *model.Session) (*string, error) {
-	panic(fmt.Errorf("not implemented"))
-}
-func (r *sessionResolver) EnableStrictPrivacy(ctx context.Context, obj *model.Session) (*bool, error) {
-	panic(fmt.Errorf("not implemented"))
-}
