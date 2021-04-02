57d32d76565f00f2430479878ec46591aa67a878
Anthony Teo <yihonganthony@gmail.com>
2021-04-01 19:39:37 -0500

    Added tooltip

--- frontend/src/pages/Player/SessionLevelBar/CurrentUrlBar/CurrentUrlBar.tsx
+++ frontend/src/pages/Player/SessionLevelBar/CurrentUrlBar/CurrentUrlBar.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import styles from './CurrentUrlBar.module.scss';
 import { ReactComponent as SearchIcon } from '../../../../static/search.svg';
 import { FaCopy } from 'react-icons/fa';
-import { message } from 'antd';
+import { message, Tooltip } from 'antd';
 
 export const CurrentUrlBar = ({ url }: { url: string }) => {
     const [hover, setHover] = useState(false);
@@ -13,14 +13,16 @@ export const CurrentUrlBar = ({ url }: { url: string }) => {
             onMouseLeave={() => setHover(false)}
         >
             <SearchIcon className={styles.searchIcon} />
-            <a
-                className={styles.urlLink}
-                target="_blank"
-                href={url}
-                rel="noreferrer"
-            >
-                {url}
-            </a>
+            <Tooltip title={url}>
+                <a
+                    className={styles.urlLink}
+                    target="_blank"
+                    href={url}
+                    rel="noreferrer"
+                >
+                    {url}
+                </a>
+            </Tooltip>
             {hover && (
                 <FaCopy
                     className={styles.copyIcon}
