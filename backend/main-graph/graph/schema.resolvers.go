cd38ee62ee255500cbbcdecf41ebf23f0b6ecd32
Anthony Teo <yihonganthony@gmail.com>
2021-04-09 17:29:54 -0500

    Removed code

--- backend/main-graph/graph/schema.resolvers.go
+++ backend/main-graph/graph/schema.resolvers.go
@@ -1209,13 +1209,3 @@ type queryResolver struct{ *Resolver }
 type segmentResolver struct{ *Resolver }
 type sessionResolver struct{ *Resolver }
 type sessionCommentResolver struct{ *Resolver }
-
-// !!! WARNING !!!
-// The code below was going to be deleted when updating resolvers. It has been copied here so you have
-// one last chance to move it out of harms way if you want. There are two reasons this happens:
-//  - When renaming or deleting a resolver the old code will be put in here. You can safely delete
-//    it when you're done.
-//  - You have helper methods in this file. Move them out to keep these resolver files clean.
-func (r *sessionResolver) Processed(ctx context.Context, obj *model.Session) (*bool, error) {
-	panic(fmt.Errorf("not implemented"))
-}
