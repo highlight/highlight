687a971263cff2164a51ce18bc029d7c671f5384
Jay Khatri <jaykhatrimail@gmail.com>
2021-04-12 01:43:15 -0400

    change

---
+++ frontend/src/components/ErrorState/ErrorState.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { auth } from '../../util/auth';
+import { client } from '../../util/graph';
+import PrimaryButton from '../Button/PrimaryButton/PrimaryButton';
+import SecondaryButton from '../Button/SecondaryButton/SecondaryButton';
+
+import styles from './ErrorState.module.scss';
+
+export const ErrorState = ({ message }: { message: string }) => {
+    return (
+        <div className={styles.errorWrapper}>
+            <div className={styles.errorContent}>
+                <h2 className={styles.errorTitle}>Woops, something's wrong!</h2>
+                <p className={styles.errorBody}>{message}</p>
+                <a href={'https://app.highlight.run'}>
+                    <PrimaryButton>Create an Account</PrimaryButton>
+                </a>
+                <SecondaryButton
+                    style={{ marginLeft: 10 }}
+                    onClick={async () => {
+                        try {
+                            auth.signOut();
+                        } catch (e) {
+                            console.log(e);
+                        }
+                        client.cache.reset();
+                    }}
+                >
+                    Login as a different User
+                </SecondaryButton>
+            </div>
+        </div>
+    );
+};
