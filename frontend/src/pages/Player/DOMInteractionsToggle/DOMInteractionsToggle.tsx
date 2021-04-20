b4c9b01cb0f3ab6340bb6adba3c51acb9de4e6ae
John Pham <john@highlight.run>
2021-04-20 14:35:31 -0700

    New DevTools layout (#614)

--- frontend/src/pages/Player/DOMInteractionsToggle/DOMInteractionsToggle.tsx
+++ frontend/src/pages/Player/DOMInteractionsToggle/DOMInteractionsToggle.tsx
@@ -1,6 +1,7 @@
 import useLocalStorage from '@rehooks/local-storage';
 import { Switch } from 'antd';
 import React from 'react';
+import Tooltip from '../../../components/Tooltip/Tooltip';
 import styles from './DOMInteractionsToggle.module.scss';
 
 const DOMInteractionsToggle = () => {
@@ -10,15 +11,19 @@ const DOMInteractionsToggle = () => {
     );
 
     return (
-        <div className={styles.container}>
-            <Switch
-                checked={enableDOMInteractions}
-                onChange={() => {
-                    setEnableDOMInteractions(!enableDOMInteractions);
-                }}
-            />{' '}
-            DOM Interactions
-        </div>
+        <Tooltip
+            title={`Enabling DOM interactions allows you to inspect the DOM in the player. You can use the browser DevTools like Elements to debug layout and other CSS issues. Get started by right clicking in the player and click "Inspect".`}
+        >
+            <div className={styles.container}>
+                <Switch
+                    checked={enableDOMInteractions}
+                    onChange={() => {
+                        setEnableDOMInteractions(!enableDOMInteractions);
+                    }}
+                />{' '}
+                DOM interactions
+            </div>
+        </Tooltip>
     );
 };
 
