8a8dab038e3e4cea38fd1d559a77705b32076a4b
Anthony Teo <yihonganthony@gmail.com>
2021-04-23 09:24:33 -0500

    Process errror.event properly

--- backend/util/strings.go
+++ backend/util/strings.go
@@ -3,22 +3,9 @@ package util
 import "encoding/json"
 
 func JsonStringToStringArray(s string) []*string {
-	var eventInterface interface{}
+	var eventInterface []*string
 	if err := json.Unmarshal([]byte(s), &eventInterface); err != nil {
-		return nil
+		return []*string{&s}
 	}
-	// the event interface is either in the form 'string' or '[]string':
-	if val, ok := eventInterface.(string); ok {
-		return []*string{&val}
-	}
-	if val, ok := eventInterface.([]interface{}); ok {
-		ret := []*string{}
-		for _, v := range val {
-			if s, ok := v.(string); ok {
-				ret = append(ret, &s)
-			}
-		}
-		return ret
-	}
-	return nil
+	return eventInterface
 }
