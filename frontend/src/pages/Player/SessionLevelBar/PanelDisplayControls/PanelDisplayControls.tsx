cc97b15e57b44e8e62f2ddb39f23d77079aafd7d
John Pham <john@highlight.run>
2021-04-23 10:36:25 -0700

    Add tooltip to panel buttons

--- frontend/src/pages/Player/SessionLevelBar/PanelDisplayControls/PanelDisplayControls.tsx
+++ frontend/src/pages/Player/SessionLevelBar/PanelDisplayControls/PanelDisplayControls.tsx
@@ -2,6 +2,7 @@ import useLocalStorage from '@rehooks/local-storage';
 import classNames from 'classnames';
 import React from 'react';
 import Button from '../../../../components/Button/Button/Button';
+import Tooltip from '../../../../components/Tooltip/Tooltip';
 import SvgPanelBottomIcon from '../../../../static/PanelBottomIcon';
 import SvgPanelRightIcon from '../../../../static/PanelRightIcon';
 import styles from './PanelDisplayControls.module.scss';
@@ -19,6 +20,7 @@ const PanelDisplayControls = () => {
     return (
         <div className={styles.buttonContainer}>
             <PanelButton
+                tooltipText="Bottom panel: View the DevTools to see console logs, errors, and network requests."
                 onClick={() => {
                     setOpenDevTools(!openDevTools);
                 }}
@@ -28,6 +30,7 @@ const PanelDisplayControls = () => {
                 />
             </PanelButton>
             <PanelButton
+                tooltipText="Right panel: View the user's metadata and session events."
                 onClick={() => {
                     setShowRightPanelPreference(!showRightPanelPreference);
                 }}
@@ -47,9 +50,12 @@ export default PanelDisplayControls;
 const PanelButton = (
     props: React.PropsWithChildren<{
         onClick: React.MouseEventHandler<HTMLElement>;
+        tooltipText: string;
     }>
 ) => (
-    <Button type="text" className={styles.button} onClick={props.onClick}>
-        {props.children}
-    </Button>
+    <Tooltip title={props.tooltipText} placement="bottomRight">
+        <Button type="text" className={styles.button} onClick={props.onClick}>
+            {props.children}
+        </Button>
+    </Tooltip>
 );
