d7b482f5670dff20c61d150948809edac5aea6ac
John Pham <john@highlight.run>
2021-04-30 17:34:04 -0700

    Add more styles

--- frontend/src/components/Changelog/Changelog.tsx
+++ frontend/src/components/Changelog/Changelog.tsx
@@ -1,4 +1,6 @@
 import { gql, useQuery } from '@apollo/client';
+import useLocalStorage from '@rehooks/local-storage';
+import classNames from 'classnames';
 import moment from 'moment';
 import React, { useState } from 'react';
 import Popover from '../Popover/Popover';
@@ -8,12 +10,14 @@ const Announcements_Query = gql`
     {
         viewer {
             project(id: "pro_eTOZitaohMUZN") {
-                announcements {
+                announcements(first: 10) {
                     edges {
                         node {
                             id
                             headline
-                            createdAt
+                            publishedAt
+                            publicPermalink
+                            excerpt
                         }
                     }
                 }
@@ -23,9 +27,11 @@ const Announcements_Query = gql`
 `;
 
 interface AnnouncementNode {
-    createdAt: string;
+    publishedAt: string;
     headline: string;
     id: string;
+    publicPermalink: string;
+    excerpt: string;
 }
 
 interface LaunchNotesResponse {
@@ -42,6 +48,12 @@ interface LaunchNotesResponse {
 
 const Changelog = () => {
     const [data, setData] = useState<AnnouncementNode[] | null>(null);
+    const [lastReadId, setLastReadId] = useLocalStorage(
+        'highlightChangelogLastReadId',
+        '0'
+    );
+    const [hasNewUpdates, setHasNewUpdates] = useState(false);
+
     const { loading } = useQuery<LaunchNotesResponse>(Announcements_Query, {
         context: {
             clientName: 'launchNotes',
@@ -55,11 +67,15 @@ const Changelog = () => {
                 );
 
                 setData(parsedData);
+                const latestChangelogItem = parsedData[0];
+                if (lastReadId !== latestChangelogItem.id) {
+                    setHasNewUpdates(true);
+                }
             }
         },
     });
 
-    if (loading || !data) {
+    if (loading || !data || data.length === 0) {
         return null;
     }
     return (
@@ -67,23 +83,58 @@ const Changelog = () => {
             <Popover
                 align={{ offset: [18, -12] }}
                 placement="rightBottom"
-                defaultVisible={true}
                 hasBorder
+                onVisibleChange={() => {
+                    setHasNewUpdates(false);
+                    setLastReadId(data[0].id);
+                }}
                 content={
                     <div className={styles.changelogContainer}>
-                        {data.map(({ createdAt, headline, id }) => (
-                            <div key={id} className={styles.changelogItem}>
-                                <p className={styles.date}>
-                                    {moment(createdAt).format('YYYY-MM-DD')}
-                                </p>
-                                <h3>{headline}</h3>
-                            </div>
-                        ))}
+                        {data.map(
+                            ({
+                                publishedAt,
+                                headline,
+                                id,
+                                publicPermalink,
+                                excerpt,
+                            }) => (
+                                <a
+                                    key={id}
+                                    className={styles.changelogItem}
+                                    href={publicPermalink}
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    <p className={styles.date}>
+                                        {moment(publishedAt).format(
+                                            'YYYY-MM-DD'
+                                        )}
+                                    </p>
+                                    <h3>{excerpt || headline}</h3>
+                                </a>
+                            )
+                        )}
+                    </div>
+                }
+                title={
+                    <div className={styles.popoverHeader}>
+                        <h3>Changelog</h3>
+                        <a
+                            href="https://highlight.launchnotes.io/"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            View all updates
+                        </a>
                     </div>
                 }
             >
-                <button className={styles.indicator}>
-                    <div className={styles.indicatorIcon} />
+                <button className={classNames(styles.indicator)}>
+                    <div
+                        className={classNames(styles.indicatorIcon, {
+                            [styles.active]: hasNewUpdates,
+                        })}
+                    />
                 </button>
             </Popover>
         </div>
