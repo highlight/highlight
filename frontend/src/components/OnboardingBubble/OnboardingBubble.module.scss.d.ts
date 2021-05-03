b87f3804837e5102ec0383e5b2c3d26ce2e11afc
John Pham <john@highlight.run>
2021-05-03 14:54:57 -0700

    Add onboarding bubble

--- frontend/src/components/OnboardingBubble/OnboardingBubble.module.scss.d.ts
+++ frontend/src/components/OnboardingBubble/OnboardingBubble.module.scss.d.ts
@@ -1,2 +1,6 @@
+export const checkIcon: string;
+export const checkIconCompleted: string;
 export const collapsed: string;
 export const container: string;
+export const onboardingBubblePopover: string;
+export const stepsContainer: string;
--- frontend/src/components/OnboardingBubble/OnboardingBubble.tsx
+++ frontend/src/components/OnboardingBubble/OnboardingBubble.tsx
@@ -1,20 +1,121 @@
 import classNames from 'classnames';
+import { H } from 'highlight.run';
 import React from 'react';
+import { useHistory } from 'react-router';
+import { useParams } from 'react-router-dom';
+import SvgCircleCheckIcon from '../../static/CircleCheckIcon';
+import Button from '../Button/Button/Button';
 import PillButton from '../Button/PillButton/PillButton';
+import Popover from '../Popover/Popover';
+import Progress from '../Progress/Progress';
 import styles from './OnboardingBubble.module.scss';
 
 interface Props {
     collapsed: boolean;
 }
 
+interface OnboardingStep {
+    displayName: string;
+    action: () => void;
+    completed: boolean;
+}
+
 const OnboardingBubble = ({ collapsed }: Props) => {
+    const history = useHistory();
+    const { organization_id } = useParams<{
+        organization_id: string;
+    }>();
+
+    const STEPS: OnboardingStep[] = [
+        {
+            displayName: 'Install the Highlight SDK',
+            action: () => {
+                history.push(`/${organization_id}/setup`);
+            },
+            completed: true,
+        },
+        {
+            displayName: 'Integrate with Slack',
+            action: () => {
+                history.push(`/${organization_id}/setup`);
+            },
+            completed: false,
+        },
+        {
+            displayName: 'Invite your team',
+            action: () => {
+                history.push(`/${organization_id}/team`);
+            },
+            completed: false,
+        },
+        {
+            displayName: 'View your first session',
+            action: () => {
+                history.push(`/${organization_id}/sessions`);
+            },
+            completed: false,
+        },
+        {
+            displayName: 'Create your first comment',
+            action: () => {
+                history.push(`/${organization_id}/sessions`);
+            },
+            completed: false,
+        },
+    ];
+
     return (
         <div
             className={classNames(styles.container, {
                 [styles.collapsed]: collapsed,
             })}
         >
-            <PillButton type="primary">Highlight setup</PillButton>
+            <Popover
+                align={{ offset: [0, -24] }}
+                placement="topLeft"
+                trigger={['click']}
+                onVisibleChange={(visible) => {
+                    if (visible) {
+                        H.track('Viewed onboarding bubble', {});
+                    }
+                }}
+                content={
+                    <>
+                        <div className={styles.onboardingBubblePopover}>
+                            <div>
+                                <h2>Account setup</h2>
+                                <p>You're almost done setting up Highlight.</p>
+
+                                <Progress
+                                    numerator={1}
+                                    denominator={6}
+                                    showInfo
+                                />
+                            </div>
+                        </div>
+                        <ul className={styles.stepsContainer}>
+                            {STEPS.map((step) => (
+                                <li key={step.displayName}>
+                                    <Button onClick={step.action} type="text">
+                                        <SvgCircleCheckIcon
+                                            className={classNames(
+                                                styles.checkIcon,
+                                                {
+                                                    [styles.checkIconCompleted]:
+                                                        step.completed,
+                                                }
+                                            )}
+                                        />
+                                        {step.displayName}
+                                    </Button>
+                                </li>
+                            ))}
+                        </ul>
+                    </>
+                }
+            >
+                <PillButton type="primary">Highlight setup</PillButton>
+            </Popover>
         </div>
     );
 };
