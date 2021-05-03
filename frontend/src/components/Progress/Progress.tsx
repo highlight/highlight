61330c1bebbbfdf18d5aa0d1a37d613a927245c3
John Pham <john@highlight.run>
2021-05-03 14:23:25 -0700

    Add Progress component

---
+++ frontend/src/components/Progress/Progress.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+// eslint-disable-next-line no-restricted-imports
+import { Progress as AntDesignProgress, ProgressProps } from 'antd';
+
+type Props = Pick<ProgressProps, 'showInfo'> & {
+    numerator: number;
+    denominator: number;
+};
+
+const Progress = ({ denominator, numerator, ...props }: Props) => {
+    return (
+        <AntDesignProgress
+            percent={Math.floor((numerator / denominator) * 100)}
+            strokeColor="#5629c6"
+            {...props}
+        />
+    );
+};
+
+export default Progress;
