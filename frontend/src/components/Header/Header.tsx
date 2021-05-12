b558a90f8c236829f734e1e78f0c4c1846011517
John Pham <john@highlight.run>
2021-05-12 16:34:06 -0700

    Add dark mode toggle

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -14,6 +14,7 @@ import { CommandBar } from './CommandBar/CommandBar';
 import Notifications from './Notifications/Notifications';
 import { useGetBillingDetailsQuery } from '../../graph/generated/hooks';
 import { PlanType } from '../../graph/generated/schemas';
+import ThemeToggle from './ThemeToggle/ThemeToggle';
 
 export const Header = () => {
     const { organization_id } = useParams<{ organization_id: string }>();
@@ -49,6 +50,7 @@ export const Header = () => {
                         </Link>
                     </div>
                     <div className={styles.rightHeader}>
+                        <ThemeToggle />
                         <Notifications />
                         <UserDropdown />
                     </div>
