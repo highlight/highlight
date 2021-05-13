073c4e1621306de64643ee555ac190924eea84b8
Jay Khatri <j@jaykhatri.com>
2021-05-13 19:08:32 -0400

    chjange (#738)

--- backend/main.go
+++ backend/main.go
@@ -86,12 +86,23 @@ func main() {
 
 	if env == "prod" {
 		// Connect to the datadog daemon.
-		_, err := statsd.New(statsdHost)
+		hostTagKey, hostTagValue := "host", os.Getenv("RENDER_SERVICE_NAME")
+		serviceTagKey, serviceTagValue := "service", os.Getenv("RENDER_SERVICE_TYPE")+"-"+os.Getenv("RENDER_INSTANCE_ID")
+		_, err := statsd.New(statsdHost, statsd.WithTags(
+			[]string{
+				hostTagKey + ":" + hostTagValue,
+				serviceTagKey + ":" + serviceTagValue,
+			},
+		))
 		if err != nil {
 			log.Fatalf("error connecting to statsd: %v", err)
 			return
 		}
-		tracer.Start(tracer.WithAgentAddr(apmHost))
+		tracer.Start(
+			tracer.WithAgentAddr(apmHost),
+			tracer.WithGlobalTag(hostTagKey, hostTagValue),
+			tracer.WithGlobalTag(serviceTagKey, serviceTagValue),
+		)
 		defer tracer.Stop()
 	}
 
