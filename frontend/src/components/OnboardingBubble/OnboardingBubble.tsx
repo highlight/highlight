cc103b2a8e8b7b323c7255620e08e478d09687c7
John Pham <john@highlight.run>
2021-05-13 13:10:46 -0700

    Fix onboarding showing 0 steps remaining

--- frontend/src/components/OnboardingBubble/OnboardingBubble.tsx
+++ frontend/src/components/OnboardingBubble/OnboardingBubble.tsx
@@ -48,7 +48,6 @@ const OnboardingBubble = () => {
         loading,
         startPolling,
         stopPolling,
-        called,
         data,
     } = useGetOnboardingStepsQuery({
         variables: {
@@ -114,17 +113,16 @@ const OnboardingBubble = () => {
             setStepsNotFinishedCount(stepsNotFinishedCount);
 
             // Don't show the onboarding bubble if all the steps are completed.
-            if (
-                !loading &&
-                called &&
-                stepsNotFinishedCount === 0 &&
-                hasStartedOnboarding
-            ) {
-                setRainConfetti(true);
-                message.success('You have finished onboarding 👏');
-                setTimeout(() => {
+            if (stepsNotFinishedCount === 0) {
+                if (hasStartedOnboarding) {
+                    setRainConfetti(true);
+                    message.success('You have finished onboarding 👏');
+                    setTimeout(() => {
+                        setHasFinishedOnboarding(true);
+                    }, 1000 * 10);
+                } else {
                     setHasFinishedOnboarding(true);
-                }, 1000 * 10);
+                }
                 stopPolling();
             } else {
                 startPolling(3000);
@@ -135,11 +133,9 @@ const OnboardingBubble = () => {
             stopPolling();
         };
     }, [
-        called,
         data,
         hasStartedOnboarding,
         history,
-        loading,
         organization_id,
         setHasFinishedOnboarding,
         startPolling,
