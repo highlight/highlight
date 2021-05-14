362e09cc4f2c002a497beddee8f8415ffef847dc
John Pham <john@highlight.run>
2021-05-14 10:35:53 -0700

    john/hig-538-small-ui-related-things (#740)
    * Fix styling on LimitedSessionsCard
    
    * Make trial banner wider
    
    * Update fonts on error pages

--- frontend/src/components/Upsell/LimitedSessionsCard/LimitedSessionsCard.module.scss.d.ts
+++ frontend/src/components/Upsell/LimitedSessionsCard/LimitedSessionsCard.module.scss.d.ts
@@ -3,5 +3,4 @@ export const closeButton: string;
 export const closeIcon: string;
 export const container: string;
 export const description: string;
-export const header: string;
 export const link: string;
