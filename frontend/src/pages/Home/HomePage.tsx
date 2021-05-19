1a2aad15599d1f96e65d70673dafc612d84c4995
John Pham <john@highlight.run>
2021-05-19 14:19:02 -0700

    Add more home page time range options up to a year

--- frontend/src/pages/Home/HomePage.tsx
+++ frontend/src/pages/Home/HomePage.tsx
@@ -129,6 +129,8 @@ const HomePage = () => {
 const timeFilter = [
     { label: 'Last 7 days', value: 7 },
     { label: 'Last 30 days', value: 30 },
+    { label: 'Last 90 days', value: 90 },
+    { label: 'This year', value: 30 * 12 },
 ] as const;
 
 const SessionCountGraph = () => {
