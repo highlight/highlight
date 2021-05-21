4599551277c7b2e34e063ab28023f34db8305102
John Pham <john@highlight.run>
2021-05-21 13:20:39 -0700

    Reduce number of active users fetched

--- backend/private-graph/graph/schema.resolvers.go
+++ backend/private-graph/graph/schema.resolvers.go
@@ -1160,7 +1160,7 @@ func (r *queryResolver) TopUsers(ctx context.Context, organizationID int, lookBa
 	FROM (SELECT identifier, active_length from sessions WHERE active_length IS NOT NULL AND organization_id=%d AND identifier <> '' AND created_at >= NOW() - INTERVAL '%d DAY' AND processed=true) q1
 	GROUP BY identifier
 	ORDER BY total_active_time DESC
-	LIMIT 200`, organizationID, lookBackPeriod, organizationID, lookBackPeriod)).Scan(&topUsersPayload).Error; err != nil {
+	LIMIT 50`, organizationID, lookBackPeriod, organizationID, lookBackPeriod)).Scan(&topUsersPayload).Error; err != nil {
 		return nil, e.Wrap(err, "error retrieving top users")
 	}
 	topUsersSpan.Finish()
