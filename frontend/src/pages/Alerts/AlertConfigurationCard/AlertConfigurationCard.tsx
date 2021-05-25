30ba6593f6234cc865682d23a6da8c776a06b35f
John Pham <john@highlight.run>
2021-05-25 11:28:36 -0700

    Add form

--- frontend/src/pages/Alerts/AlertConfigurationCard/AlertConfigurationCard.tsx
+++ frontend/src/pages/Alerts/AlertConfigurationCard/AlertConfigurationCard.tsx
@@ -1,3 +1,4 @@
+import { Form } from 'antd';
 import React from 'react';
 
 import Button from '../../../components/Button/Button/Button';
@@ -18,6 +19,12 @@ interface Props {
 export const AlertConfigurationCard = ({
     configuration: { name, canControlThreshold },
 }: Props) => {
+    const [form] = Form.useForm();
+
+    const onSubmit = (data: any) => {
+        console.log({ ...data, name });
+    };
+
     const channels = [
         {
             displayValue: '#boba',
@@ -59,62 +66,97 @@ export const AlertConfigurationCard = ({
         },
     ];
 
+    const onChannelsChange = (channels: string[]) => {
+        form.setFieldsValue({ channels });
+    };
+
+    const onExcludedEnvironmentsChange = (excludedEnvironments: string[]) => {
+        form.setFieldsValue({ excludedEnvironments });
+    };
+
     return (
         <Card>
             <h2>{name}</h2>
 
-            <section>
-                <h3>Channels to notify</h3>
-                <p>
-                    Pick Slack channels or people to message when an alert is
-                    created.
-                </p>
-                <Select
-                    className={styles.channelSelect}
-                    options={channels}
-                    mode="tags"
-                    placeholder={`Select a channel(s) or person(s) to send ${name} to.`}
-                    notFoundContent={
-                        <div>
-                            <h2>Not Found</h2>
-                            <Button>CLick me</Button>
-                        </div>
-                    }
-                />
-            </section>
-
-            <section>
-                <h3>Excluded environments</h3>
-                <p>
-                    Pick environments that should not create alerts. Some teams
-                    don't want to be woken up at 2AM if an alert is created from
-                    localhost.
-                </p>
-                <Select
-                    className={styles.channelSelect}
-                    options={environments}
-                    mode="multiple"
-                    placeholder={`Select a environment(s) that should not trigger alerts.`}
-                    notFoundContent={
-                        <div>
-                            <h2>Not Found</h2>
-                            <Button>CLick me</Button>
-                        </div>
-                    }
-                />
-            </section>
-
-            {canControlThreshold && (
+            <Form
+                onFinish={onSubmit}
+                form={form}
+                initialValues={{
+                    threshold: 1,
+                    channels: [],
+                    excludedEnvironments: [],
+                }}
+            >
                 <section>
-                    <h3>Threshold</h3>
-                    <p>Pick how often an alert should be created.</p>
-                    <InputNumber defaultValue={1} />
+                    <h3>Channels to notify</h3>
+                    <p>
+                        Pick Slack channels or people to message when an alert
+                        is created.
+                    </p>
+                    <Form.Item name="channels">
+                        <Select
+                            className={styles.channelSelect}
+                            options={channels}
+                            mode="tags"
+                            placeholder={`Select a channel(s) or person(s) to send ${name} to.`}
+                            onChange={onChannelsChange}
+                            notFoundContent={
+                                <div>
+                                    <h2>Not Found</h2>
+                                    <Button>CLick me</Button>
+                                </div>
+                            }
+                        />
+                    </Form.Item>
                 </section>
-            )}
 
-            <Button type="primary" className={styles.saveButton}>
-                Save
-            </Button>
+                <section>
+                    <h3>Excluded environments</h3>
+                    <p>
+                        Pick environments that should not create alerts. Some
+                        teams don't want to be woken up at 2AM if an alert is
+                        created from localhost.
+                    </p>
+                    <Form.Item name="excludedEnvironments">
+                        <Select
+                            className={styles.channelSelect}
+                            options={environments}
+                            mode="multiple"
+                            placeholder={`Select a environment(s) that should not trigger alerts.`}
+                            onChange={onExcludedEnvironmentsChange}
+                            notFoundContent={
+                                <div>
+                                    <h2>Not Found</h2>
+                                    <Button>CLick me</Button>
+                                </div>
+                            }
+                        />
+                    </Form.Item>
+                </section>
+
+                {canControlThreshold && (
+                    <section>
+                        <h3>Threshold</h3>
+                        <p>Pick how often an alert should be created.</p>
+                        <Form.Item name="threshold">
+                            <InputNumber />
+                        </Form.Item>
+                    </section>
+                )}
+
+                <Form.Item shouldUpdate>
+                    {() => (
+                        <Button
+                            type="primary"
+                            className={styles.saveButton}
+                            htmlType="submit"
+                            disabled={!form.isFieldsTouched(false)}
+                        >
+                            Save
+                        </Button>
+                    )}
+                </Form.Item>
+            </Form>
         </Card>
     );
 };
