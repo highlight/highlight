8f831da282244c21beb92b0982e1040ef4e022b9
John Pham <john@highlight.run>
2021-06-04 14:41:38 -0700

    Update logos to new one

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -23,6 +23,7 @@ const options: HighlightOptions = {
     manualStart: true,
     enableStrictPrivacy: Math.floor(Math.random() * 2) === 0,
 };
+const favicon = document.querySelector("link[rel~='icon']") as any;
 if (dev) {
     options.scriptUrl = 'http://localhost:8080/dist/index.js';
 
@@ -39,7 +40,13 @@ if (dev) {
         ]
     }-localhost`;
     window.document.title = `⚙️ DEV ${window.document.title}`;
+    if (favicon) {
+        favicon.href = `${process.env.PUBLIC_URL}/favicon-localhost.ico`;
+    }
 } else if (window.location.href.includes('onrender')) {
+    if (favicon) {
+        favicon.href = `${process.env.PUBLIC_URL}/favicon-pr.ico`;
+    }
     window.document.title = `📸 PR ${window.document.title}`;
     options.environment = 'Pull Request Preview';
 }
