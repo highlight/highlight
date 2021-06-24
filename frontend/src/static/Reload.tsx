98fd4b6fd52d43b3e6e1d3c3a93060ae735b0162
John Pham <john@highlight.run>
2021-06-24 10:20:14 -0700

    Update icon

--- frontend/src/static/Reload.tsx
+++ frontend/src/static/Reload.tsx
@@ -5,6 +5,7 @@ function SvgReload(props: React.SVGProps<SVGSVGElement>) {
         <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 64 64"
+            fill="currentColor"
             width="1em"
             height="1em"
             {...props}
