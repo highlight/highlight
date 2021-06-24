b7a05e7437dd401429447099b836d1e26c520f83
Cameron Brill <c00brill@gmail.com>
2021-06-24 11:07:00 +0900

    oops

--- backend/util/tests.go
+++ backend/util/tests.go
@@ -73,17 +73,3 @@ func ClearTablesInDB(db *gorm.DB) error {
 	}
 	return nil
 }
-
-func MakeIntPointer(v int) *int {
-	return &v
-}
-
-func MakeStringPointer(v string) *string {
-	return &v
-}
-
-func MakeStringPointerFromInterface(v interface{}) *string {
-	exampleErrorTraceBytes, _ := json.Marshal(&v)
-	exampleErrorTraceString := string(exampleErrorTraceBytes)
-	return &exampleErrorTraceString
-}
