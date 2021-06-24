96001cad36b5f3d3eac6a3083d9733e93e6489bd
John Pham <john@highlight.run>
2021-06-24 15:04:07 -0700

    Update styles

--- frontend/src/pages/Player/SearchPanel/SegmentPickerForPlayer/SegmentPickerForPlayer.tsx
+++ frontend/src/pages/Player/SearchPanel/SegmentPickerForPlayer/SegmentPickerForPlayer.tsx
@@ -1,8 +1,16 @@
+import _ from 'lodash';
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import TextTransition from 'react-text-transition';
 
+import Button from '../../../../components/Button/Button/Button';
 import Select from '../../../../components/Select/Select';
-import { useGetSegmentsQuery } from '../../../../graph/generated/hooks';
+import {
+    useCreateSegmentMutation,
+    useEditSegmentMutation,
+    useGetSegmentsQuery,
+} from '../../../../graph/generated/hooks';
+import SvgPlayIcon from '../../../../static/PlayIcon';
 import { gqlSanitize } from '../../../../util/gqlSanitize';
 import { useSearchContext } from '../../../Sessions/SearchContext/SearchContext';
 import { EmptySessionsSearchParams } from '../../../Sessions/SessionsPage';
@@ -17,6 +25,8 @@ const SegmentPickerForPlayer = () => {
         setSegmentName,
         setExistingParams,
         segmentName,
+        searchParams,
+        existingParams,
     } = useSearchContext();
     const { loading, data } = useGetSegmentsQuery({
         variables: { organization_id },
@@ -24,6 +34,17 @@ const SegmentPickerForPlayer = () => {
     const [selectedSegment, setSelectedSegment] = useState<
         { value: string; id: string } | undefined
     >(undefined);
+    const [paramsIsDifferent, setParamsIsDifferent] = useState(false);
+    const [editSegment, editSegmentOptions] = useEditSegmentMutation({
+        refetchQueries: ['GetSegments'],
+    });
+
+    const [
+        createSegment,
+        { loading: createSegmentLoading },
+    ] = useCreateSegmentMutation({
+        refetchQueries: ['GetSegments'],
+    });
 
     const currentSegment = data?.segments?.find(
         (s) => s?.id === selectedSegment?.id
@@ -40,39 +61,80 @@ const SegmentPickerForPlayer = () => {
         }
     }, [currentSegment, setExistingParams, setSearchParams, setSegmentName]);
 
+    useEffect(() => {
+        // Compares original params and current search params to check if they are different
+        // Removes undefined, null fields, and empty arrays when comparing
+        setParamsIsDifferent(
+            !_.isEqual(
+                _.omitBy(
+                    _.pickBy(searchParams, _.identity),
+                    (v) => Array.isArray(v) && v.length === 0
+                ),
+                _.omitBy(
+                    _.pickBy(existingParams, _.identity),
+                    (v) => Array.isArray(v) && v.length === 0
+                )
+            )
+        );
+    }, [searchParams, existingParams]);
+
     return (
-        <Select
-            value={segmentName}
-            onChange={(value, option) => {
-                let nextValue = undefined;
-                if (value && option) {
-                    nextValue = {
-                        value: value,
-                        id: (option as any).key,
-                    };
-                } else {
-                    setExistingParams(EmptySessionsSearchParams);
-                    setSearchParams(EmptySessionsSearchParams);
-                    setSegmentName(null);
-                }
-                setSelectedSegment(nextValue);
-            }}
-            className={styles.segmentSelect}
-            placeholder="Choose Segment"
-            allowClear
-            options={(data?.segments || [])
-                .map((segment) => ({
-                    displayValue: segment?.name || '',
-                    value: segment?.name || '',
-                    id: segment?.id || '',
-                }))
-                .sort((a, b) =>
-                    a.displayValue.toLowerCase() > b.displayValue.toLowerCase()
-                        ? 1
-                        : -1
-                )}
-            loading={loading}
-        />
+        <section className={styles.segmentPickerSection}>
+            <Select
+                value={segmentName}
+                onChange={(value, option) => {
+                    let nextValue = undefined;
+                    if (value && option) {
+                        nextValue = {
+                            value: value,
+                            id: (option as any).key,
+                        };
+                    } else {
+                        setExistingParams(EmptySessionsSearchParams);
+                        setSearchParams(EmptySessionsSearchParams);
+                        setSegmentName(null);
+                    }
+                    setSelectedSegment(nextValue);
+                }}
+                className={styles.segmentSelect}
+                placeholder="Choose Segment"
+                allowClear
+                options={(data?.segments || [])
+                    .map((segment) => ({
+                        displayValue: segment?.name || '',
+                        value: segment?.name || '',
+                        id: segment?.id || '',
+                    }))
+                    .sort((a, b) =>
+                        a.displayValue.toLowerCase() >
+                        b.displayValue.toLowerCase()
+                            ? 1
+                            : -1
+                    )}
+                loading={loading}
+                hasAccent
+            />
+            <Button
+                trackingId="CreateSessionSegment"
+                // onClick={() => setCreateClicked(true)}
+                type="ghost"
+                small
+                className={styles.segmentButton}
+            >
+                <SvgPlayIcon />
+                <span>
+                    <TextTransition
+                        text={
+                            paramsIsDifferent && segmentName
+                                ? 'Update'
+                                : 'Create'
+                        }
+                        inline
+                    />{' '}
+                    Segment
+                </span>
+            </Button>
+        </section>
     );
 };
 
