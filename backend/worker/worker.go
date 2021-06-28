06f108b638a60d0e6ebf0eeb63824493604c151f
Jay Khatri <jaykhatrimail@gmail.com>
2021-06-27 21:39:50 -0700

    change

--- backend/worker/worker.go
+++ backend/worker/worker.go
@@ -127,8 +127,8 @@ func (w *Worker) processSession(ctx context.Context, s *model.Session) error {
 	eventStringBytes := 0
 	for _, ee := range events {
 		eventStringBytes += len(ee.Events)
-		dd.StatsD.Histogram("worker.processSession.incrementalEventPayloadStringSize", float64(eventStringBytes), nil, 1) //nolint
 	}
+	dd.StatsD.Histogram("worker.processSession.eventPayloadStringSize", float64(eventStringBytes), nil, 1) //nolint
 
 	// Delete the session if there's no events.
 	if len(events) == 0 {
