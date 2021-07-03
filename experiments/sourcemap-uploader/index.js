0ab30f4ffac2420776a1be02c352e453cf67e4cc
Cameron Brill <c00brill@gmail.com>
2021-07-03 10:51:58 +0900

    if version is null, bump

--- experiments/sourcemap-uploader/index.js
+++ experiments/sourcemap-uploader/index.js
@@ -100,7 +100,12 @@ async function uploadFile(organizationId, apiKey, version, filePath, fileName) {
   const fileContent = readFileSync(filePath);
 
   // Setting up S3 upload parameters
-  const bucketPath = `${organizationId}/${version}/${fileName}`;
+  var bucketPath;
+  if (version === null || version === undefined || version === "") {
+    bucketPath = `${organizationId}/${fileName}`;
+  } else {
+    bucketPath = `${organizationId}/${version}/${fileName}`;
+  }
   const params = {
     Bucket: BUCKET_NAME,
     Key: bucketPath,
