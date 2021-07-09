2161af12b92716544f8db42e1da366b5dd730fb4
John Pham <john@highlight.run>
2021-07-09 14:41:46 -0700

    Add Highlighter component

---
+++ frontend/src/components/TextHighlighter/TextHighlighter.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Highlighter, { HighlighterProps } from 'react-highlight-words';
+
+import styles from './TextHighlighter.module.scss';
+
+type Props = HighlighterProps;
+
+const TextHighlighter = (props: Props) => {
+    return (
+        <Highlighter
+            {...props}
+            highlightClassName={styles.highlighterStyles}
+            autoEscape={true}
+        />
+    );
+};
+
+export default TextHighlighter;
