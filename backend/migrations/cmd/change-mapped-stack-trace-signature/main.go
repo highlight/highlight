46a5e134e2d4cf09821a99256c7f8a94a3056e15
Cameron Brill <c00brill@gmail.com>
2021-07-13 08:25:35 +0900

    [migration] update all error groups to have new `mapped_stack_trace` json signature (#987)
    * fix mapping bug on errors page
    
    * as stack_trace
    
    * change stack trace signature
    
    * frontend changes
    
    * Revert "fix mapping bug on errors page"
    
    This reverts commit c72d01b5a964181605ac2da98a7d4c3f61cf8f14.
    
    * create migration

---
+++ backend/migrations/cmd/change-mapped-stack-trace-signature/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"encoding/json"
+	"os"
+
+	e "github.com/pkg/errors"
+	log "github.com/sirupsen/logrus"
+
+	"github.com/highlight-run/highlight/backend/model"
+	modelInputs "github.com/highlight-run/highlight/backend/private-graph/graph/model"
+)
+
+func main() {
+	log.Info("setting up db")
+	db, err := model.SetupDB(os.Getenv("PSQL_DB"))
+	if err != nil {
+		log.Fatalf("error setting up db: %+v", err)
+	}
+	var errorGroups []model.ErrorGroup
+	if err := db.Debug().Model(&model.ErrorGroup{}).Where("mapped_stack_trace IS NOT NULL").Scan(&errorGroups).Error; err != nil {
+		log.Error(e.Wrap(err, "error fetching error groups"))
+		return
+	}
+	for i := range errorGroups {
+		if errorGroups[i].MappedStackTrace == nil || *errorGroups[i].MappedStackTrace == "" {
+			continue
+		}
+		var oldStackTrace []*struct {
+			FileName     *string `json:"file_name"`
+			LineNumber   *int    `json:"line_number"`
+			FunctionName *string `json:"function_name"`
+			ColumnNumber *int    `json:"column_number"`
+		}
+		if err := json.Unmarshal([]byte(*errorGroups[i].MappedStackTrace), &oldStackTrace); err != nil {
+			log.Error(e.Wrapf(err, "error unmarshalling MappedStackTrace, id: %d", errorGroups[i].ID))
+			continue
+		}
+		var newStackTrace []*modelInputs.ErrorTrace
+		for _, t := range oldStackTrace {
+			val := &modelInputs.ErrorTrace{
+				FileName:     t.FileName,
+				LineNumber:   t.LineNumber,
+				FunctionName: t.FunctionName,
+				ColumnNumber: t.ColumnNumber,
+			}
+			newStackTrace = append(newStackTrace, val)
+		}
+		newStackTraceBytes, err := json.Marshal(&newStackTrace)
+		if err != nil {
+			log.Error(e.Wrapf(err, "error marshalling MappedStackTrace, id: %d", errorGroups[i].ID))
+			continue
+		}
+		newStackTraceString := string(newStackTraceBytes)
+		if err := db.Debug().Where(&model.ErrorGroup{Model: model.Model{ID: errorGroups[i].ID}}).Updates(&model.ErrorGroup{MappedStackTrace: &newStackTraceString}).Error; err != nil {
+			log.Error(e.Wrapf(err, "error saving MappedStackTrace, id: %d", errorGroups[i].ID))
+			continue
+		}
+	}
+}
