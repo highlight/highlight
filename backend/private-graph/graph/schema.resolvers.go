2c82d2b2d2dd3d3a9615fa03609ad5facf2e16ce
Cameron Brill <c00brill@gmail.com>
2021-07-13 05:29:50 +0900

    Get rid of trial banner (#988)

--- backend/private-graph/graph/schema.resolvers.go
+++ backend/private-graph/graph/schema.resolvers.go
@@ -1723,10 +1723,16 @@ func (r *queryResolver) BillingDetails(ctx context.Context, organizationID int) 
 	if err := g.Wait(); err != nil {
 		return nil, e.Wrap(err, "error querying session data for billing details")
 	}
+
+	quota := pricing.TypeToQuota(planType)
+	// use monthly session limit if it exists
+	if org.MonthlySessionLimit != nil {
+		quota = *org.MonthlySessionLimit
+	}
 	details := &modelInputs.BillingDetails{
 		Plan: &modelInputs.Plan{
 			Type:  modelInputs.PlanType(planType.String()),
-			Quota: pricing.TypeToQuota(planType),
+			Quota: quota,
 		},
 		Meter:              meter,
 		SessionsOutOfQuota: queriedSessionsOutOfQuota,
