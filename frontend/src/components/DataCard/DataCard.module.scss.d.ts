bb1edb65e7d4f1a95f53c6a6fcb6200c9bbb1a81
John Pham <john@highlight.run>
2021-07-14 12:31:47 -0700

    Move padding to DataCard

--- frontend/src/components/DataCard/DataCard.module.scss.d.ts
+++ frontend/src/components/DataCard/DataCard.module.scss.d.ts
@@ -1,1 +1,3 @@
 export const card: string;
+export const content: string;
+export const fullWidth: string;
--- frontend/src/components/DataCard/DataCard.tsx
+++ frontend/src/components/DataCard/DataCard.tsx
@@ -1,16 +1,23 @@
+import classNames from 'classnames';
 import React from 'react';
 
 import styles from './DataCard.module.scss';
 
 interface Props {
     title: string | React.ReactNode;
+    /** Should the card span the full available width. */
+    fullWidth?: boolean;
 }
 
-const DataCard: React.FC<Props> = ({ children, title }) => {
+const DataCard: React.FC<Props> = ({ children, title, fullWidth = false }) => {
     return (
-        <article className={styles.card}>
+        <article
+            className={classNames(styles.card, {
+                [styles.fullWidth]: fullWidth,
+            })}
+        >
             <h2>{title}</h2>
-            {children}
+            <main className={styles.content}>{children}</main>
         </article>
     );
 };
