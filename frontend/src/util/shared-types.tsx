d15635b92614ff93412ed7a7b97617e7f5e09911
John Pham <john@highlight.run>
2021-07-14 11:10:12 -0700

    Remove unused types

--- frontend/src/util/shared-types.tsx
+++ frontend/src/util/shared-types.tsx
@@ -19,30 +19,3 @@ export type ErrorMessage = {
     timestamp: string;
     payload?: string;
 };
-
-export type NetworkResourceContent = {
-    endTime?: number;
-    request?: HookRequest;
-    response?: HookResponse;
-};
-
-export type HookRequest = {
-    method: string;
-    url: string;
-    body: string;
-    headers: string;
-    timeout: number;
-    type: string;
-    withCredentials: string;
-};
-
-export type HookResponse = {
-    status: string;
-    statusText: string;
-    text: string;
-    headers: string;
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    xml: any;
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    data: any;
-};
