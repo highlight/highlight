eaacef02b394150fa33aaf266ff198ba6dcd5d69
John Pham <john@highlight.run>
2021-07-28 16:04:10 -0700

    Remove console log

--- client/src/listeners/network-listener/utils/xhr-listener.ts
+++ client/src/listeners/network-listener/utils/xhr-listener.ts
@@ -34,7 +34,6 @@ export const XHRListener = (
         this._shouldRecordHeaderAndBody = !urlBlocklist.some((blockedUrl) =>
             url.toLowerCase().includes(blockedUrl)
         );
-        console.log(this._shouldRecordHeaderAndBody, url, urlBlocklist);
 
         // @ts-expect-error
         return originalOpen.apply(this, arguments);
