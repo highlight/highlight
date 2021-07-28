62788df47634beddf6dfd5c8b69098fd68a5b17a
Cameron Brill <c00brill@gmail.com>
2021-07-29 02:54:12 +0900

    Enhance graphql error logging (#1087)

--- backend/util/logging.go
+++ backend/util/logging.go
@@ -3,6 +3,8 @@ package util
 import (
 	"context"
 
+	"github.com/99designs/gqlgen/graphql"
+	"github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
 	"github.com/vektah/gqlparser/v2/gqlerror"
 )
@@ -11,8 +13,15 @@ func GraphQLErrorPresenter(service string) func(ctx context.Context, e error) *g
 	return func(ctx context.Context, e error) *gqlerror.Error {
 		log.WithFields(log.Fields{
 			"error": e,
+			"path":  graphql.GetPath(ctx),
 		}).Errorf("%s graphql request failed", service)
 		err := gqlerror.Errorf(e.Error())
 		return err
+	}
+}
+
+func GraphQLRecoverFunc() func(ctx context.Context, err interface{}) error {
+	return func(ctx context.Context, err interface{}) error {
+		return errors.Errorf("panic {error: %+v}", err)
 	}
 }
