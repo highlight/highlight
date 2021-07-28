8d931fc85375a9accbc91b03022e34a102c1aa53
Cameron Brill <c00brill@gmail.com>
2021-07-28 10:47:41 +0900

    log path

--- backend/util/logging.go
+++ backend/util/logging.go
@@ -3,6 +3,7 @@ package util
 import (
 	"context"
 
+	"github.com/99designs/gqlgen/graphql"
 	log "github.com/sirupsen/logrus"
 	"github.com/vektah/gqlparser/v2/gqlerror"
 )
@@ -11,6 +12,7 @@ func GraphQLErrorPresenter(service string) func(ctx context.Context, e error) *g
 	return func(ctx context.Context, e error) *gqlerror.Error {
 		log.WithFields(log.Fields{
 			"error": e,
+			"path":  graphql.GetPath(ctx),
 		}).Errorf("%s graphql request failed", service)
 		err := gqlerror.Errorf(e.Error())
 		return err
