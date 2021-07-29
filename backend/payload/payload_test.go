1ebfdb9d1e06d0352b08d5b3e563d76055994e1f
Cameron Brill <c00brill@gmail.com>
2021-07-29 14:50:30 +0900

    payload test

--- backend/payload/payload_test.go
+++ backend/payload/payload_test.go
@@ -1,14 +1,13 @@
 package payload
 
 import (
+	"io"
 	"os"
 	"testing"
-
-	"log"
 )
 
 func TestPayloadReader(t *testing.T) {
-	f, err := os.Open("/tmp/15289.events.txt")
+	f, err := os.Open("./test-one.events.txt")
 	if err != nil {
 		t.Fatalf("error creating file: %v", err)
 	}
@@ -16,23 +15,31 @@ func TestPayloadReader(t *testing.T) {
 	r := NewPayloadReadWriter(f)
 	reader := r.Reader()
 	s, err := reader.Next()
-	if s != nil {
-		log.Println("one", len(*s))
+	if err != nil {
+		t.Errorf("error not nil: %v", err)
 	}
-	log.Printf("err: %v\n", err)
+	if s != nil && len(*s) != 5964255 {
+		t.Error("first event not the right length :hmm:")
+	}
 	s, err = reader.Next()
-	if s != nil {
-		log.Println("two", len(*s))
+	if err != nil {
+		t.Errorf("error not nil: %v", err)
 	}
-	log.Printf("err: %v\n", err)
+	if s != nil && len(*s) != 355572 {
+		t.Error("second event not the right length :hmm:")
+	}
 	s, err = reader.Next()
-	if s != nil {
-		log.Println("three", len(*s))
+	if err != nil {
+		t.Errorf("error not nil: %v", err)
 	}
-	log.Printf("err: %v\n", err)
+	if s != nil && len(*s) != 73227 {
+		t.Error("third event not the right length :hmm:")
+	}
 	s, err = reader.Next()
-	if s != nil {
-		log.Println("four", len(*s))
+	if s != nil && len(*s) != 0 {
+		t.Error("fourth event not the right length :hmm:")
 	}
-	log.Printf("err: %v\n", err)
+	if err != io.EOF {
+		t.Error("err not EOF")
+	}
 }
