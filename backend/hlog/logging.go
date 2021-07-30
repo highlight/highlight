a7c36b05041138f5080c36a3766f660b0c955c58
Richard Hua <rich@rkhua.com>
2021-07-29 17:49:49 -0700

    Fix broken function calls

--- backend/hlog/logging.go
+++ backend/hlog/logging.go
@@ -22,17 +22,17 @@ func logIfError(name string, err error) {
 
 // Histogram tracks the statistical distribution of a set of values on each host.
 func Histogram(name string, value float64, tags []string, rate float64) {
-	_logIfError(name, dd.StatsD.Histogram(name, value, tags, rate))
+	logIfError(name, dd.StatsD.Histogram(name, value, tags, rate))
 }
 
 // Timing sends timing information, it is an alias for TimeInMilliseconds
 // It is flushed by statsd with percentiles, mean and other info (https://github.com/etsy/statsd/blob/master/docs/metric_types.md#timing)
 func Timing(name string, value time.Duration, tags []string, rate float64) {
-	_logIfError(name, dd.StatsD.Timing(name, value, tags, rate))
+	logIfError(name, dd.StatsD.Timing(name, value, tags, rate))
 }
 
 // Incr is just Count of 1
 // Count tracks how many times something happened per second.
 func Incr(name string, tags []string, rate float64) {
-	_logIfError(name, dd.StatsD.Incr(name, tags, rate))
+	logIfError(name, dd.StatsD.Incr(name, tags, rate))
 }
