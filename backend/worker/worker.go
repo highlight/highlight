9103fdb63098bf45b6c530d014fcb5eccaf9a3f1
Richard Hua <rich@rkhua.com>
2021-07-29 18:02:33 -0700

    Log more info about sessions query and log logging errors (#1103)
    The trace logging of the db query is not working, Jay has an email thread to resolve that. In the meantime log in other ways.
    
    Also create a logging library that autologs errors, but only in prod.

--- backend/worker/worker.go
+++ backend/worker/worker.go
@@ -17,9 +17,8 @@ import (
 	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
 	"gorm.io/gorm/clause"
 
-	dd "github.com/highlight-run/highlight/backend/datadog"
-
 	parse "github.com/highlight-run/highlight/backend/event-parse"
+	"github.com/highlight-run/highlight/backend/hlog"
 	"github.com/highlight-run/highlight/backend/model"
 	storage "github.com/highlight-run/highlight/backend/object-storage"
 	mgraph "github.com/highlight-run/highlight/backend/private-graph/graph"
@@ -55,7 +54,7 @@ func (w *Worker) pushToObjectStorageAndWipe(ctx context.Context, s *model.Sessio
 	for _, ee := range resourcesObject {
 		payloadStringSize += len(ee.Resources)
 	}
-	dd.StatsD.Histogram("worker.processSession.payloadStringSize", float64(payloadStringSize), []string{fmt.Sprintf("session_id:%d", s.ID)}, 1) //nolint
+	hlog.Histogram("worker.processSession.payloadStringSize", float64(payloadStringSize), []string{fmt.Sprintf("session_id:%d", s.ID)}, 1) //nolint
 	resourcePayloadSize, err := w.S3Client.PushResourcesToS3(s.ID, s.OrganizationID, resourcesObject)
 	if err != nil {
 		return errors.Wrap(err, "error pushing network payload to s3")
@@ -68,7 +67,7 @@ func (w *Worker) pushToObjectStorageAndWipe(ctx context.Context, s *model.Sessio
 	for _, mm := range messagesObj {
 		payloadStringSize += len(mm.Messages)
 	}
-	dd.StatsD.Histogram("worker.processSession.payloadStringSize", float64(payloadStringSize), []string{fmt.Sprintf("session_id:%d", s.ID)}, 1) //nolint
+	hlog.Histogram("worker.processSession.payloadStringSize", float64(payloadStringSize), []string{fmt.Sprintf("session_id:%d", s.ID)}, 1) //nolint
 	messagePayloadSize, err := w.S3Client.PushMessagesToS3(s.ID, s.OrganizationID, messagesObj)
 	if err != nil {
 		return errors.Wrap(err, "error pushing network payload to s3")
@@ -94,7 +93,7 @@ func (w *Worker) pushToObjectStorageAndWipe(ctx context.Context, s *model.Sessio
 		return errors.Wrap(err, "error updating session to storage enabled")
 	}
 
-	dd.StatsD.Histogram("worker.pushToObjectStorageAndWipe.payloadSize", float64(totalPayloadSize), []string{fmt.Sprintf("session_id:%d", s.ID)}, 1) //nolint
+	hlog.Histogram("worker.pushToObjectStorageAndWipe.payloadSize", float64(totalPayloadSize), []string{fmt.Sprintf("session_id:%d", s.ID)}, 1) //nolint
 
 	// Delete all the events_objects in the DB.
 	if len(events) > 0 {
@@ -230,7 +229,7 @@ func (w *Worker) processSession(ctx context.Context, s *model.Session) error {
 	if err != nil {
 		log.Errorf(errors.Wrap(err, "error scanning session payload").Error())
 	} else {
-		dd.StatsD.Histogram("worker.processSession.scannedSessionPayload", float64(*size), []string{fmt.Sprintf("session_id:%d", s.ID)}, 1) //nolint
+		hlog.Histogram("worker.processSession.scannedSessionPayload", float64(*size), []string{fmt.Sprintf("session_id:%d", s.ID)}, 1) //nolint
 	}
 
 	// load all events
@@ -239,13 +238,13 @@ func (w *Worker) processSession(ctx context.Context, s *model.Session) error {
 		return errors.Wrap(err, "retrieving events")
 	}
 
-	dd.StatsD.Histogram("worker.processSession.numEventsRowsQueried", float64(len(events)), []string{fmt.Sprintf("session_id:%d", s.ID)}, 1) //nolint
+	hlog.Histogram("worker.processSession.numEventsRowsQueried", float64(len(events)), []string{fmt.Sprintf("session_id:%d", s.ID)}, 1) //nolint
 
 	payloadStringBytes := 0
 	for _, ee := range events {
 		payloadStringBytes += len(ee.Events)
 	}
-	dd.StatsD.Histogram("worker.processSession.payloadStringSize", float64(payloadStringBytes), []string{fmt.Sprintf("session_id:%d", s.ID)}, 1) //nolint
+	hlog.Histogram("worker.processSession.payloadStringSize", float64(payloadStringBytes), []string{fmt.Sprintf("session_id:%d", s.ID)}, 1) //nolint
 
 	// Delete the session if there's no events.
 	if len(events) == 0 {
@@ -515,7 +514,7 @@ func (w *Worker) Start() {
 			sessions[i], sessions[j] = sessions[j], sessions[i]
 		})
 		// Sends a "count" metric to datadog so that we can see how many sessions are being queried.
-		dd.StatsD.Histogram("worker.sessionsQuery.sessionCount", float64(len(sessions)), nil, 1) //nolint
+		hlog.Histogram("worker.sessionsQuery.sessionCount", float64(len(sessions)), nil, 1) //nolint
 		sessionsSpan.Finish()
 		type SessionLog struct {
 			SessionID      int
