114a5aa2930c696aee8d843a7bd42ad75eefa5b5
Cameron Brill <c00brill@gmail.com>
2021-08-04 11:51:13 +0900

    remove

--- backend/private-graph/graph/schema.resolvers.go
+++ backend/private-graph/graph/schema.resolvers.go
@@ -2333,13 +2333,3 @@ type segmentResolver struct{ *Resolver }
 type sessionResolver struct{ *Resolver }
 type sessionAlertResolver struct{ *Resolver }
 type sessionCommentResolver struct{ *Resolver }
-
-// !!! WARNING !!!
-// The code below was going to be deleted when updating resolvers. It has been copied here so you have
-// one last chance to move it out of harms way if you want. There are two reasons this happens:
-//  - When renaming or deleting a resolver the old code will be put in here. You can safely delete
-//    it when you're done.
-//  - You have helper methods in this file. Move them out to keep these resolver files clean.
-func (r *mutationResolver) UpdateDefaultSlackChannels(ctx context.Context, organizationID int, slackChannels []*modelInputs.SanitizedSlackChannelInput) (*int, error) {
-	panic(fmt.Errorf("not implemented"))
-}
