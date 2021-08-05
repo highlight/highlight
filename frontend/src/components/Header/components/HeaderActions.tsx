f7dcaee77db18ba7232a2d274f037323c6b83474
John Pham <john@highlight.run>
2021-08-04 18:06:07 -0700

    Update header actions

---
+++ frontend/src/components/Header/components/HeaderActions.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { useAuthContext } from '../../../AuthContext';
+import HelpMenu from '../HelpMenu/HelpMenu';
+import Notifications from '../Notifications/Notifications';
+import styles from './HeaderActions.module.scss';
+
+const HeaderActions = () => {
+    const { isLoggedIn } = useAuthContext();
+
+    return (
+        <div className={styles.headerActions}>
+            <div>{isLoggedIn && <Notifications />}</div>
+            <div>
+                <HelpMenu />
+            </div>
+        </div>
+    );
+};
+
+export default HeaderActions;
