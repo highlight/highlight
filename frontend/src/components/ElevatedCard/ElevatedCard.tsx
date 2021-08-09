b95dc24e6939aaa51a3cd00b06fd97a563f2084a
John Pham <john@highlight.run>
2021-08-09 13:12:12 -0700

    Add actions region to ElevatedCard

--- frontend/src/components/ElevatedCard/ElevatedCard.tsx
+++ frontend/src/components/ElevatedCard/ElevatedCard.tsx
@@ -6,12 +6,15 @@ import styles from './ElevatedCard.module.scss';
 interface Props {
     title?: string;
     animation?: React.ReactNode;
+    /** Buttons or action elements for the card. These are rendered at the bottom of the card. */
+    actions?: React.ReactNode;
 }
 
 const ElevatedCard = ({
     title,
     children,
     animation,
+    actions,
 }: PropsWithChildren<Props>) => {
     return (
         <div
@@ -22,6 +25,7 @@ const ElevatedCard = ({
             {animation && <div className={styles.animation}>{animation}</div>}
             {title && <h2>{title}</h2>}
             <div className={styles.content}>{children}</div>
+            {actions && <div className={styles.actions}>{actions}</div>}
         </div>
     );
 };
