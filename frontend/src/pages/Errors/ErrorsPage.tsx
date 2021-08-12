239ba9285f0e1a68b084dfc953c06ea4d3056212
John Pham <john@highlight.run>
2021-08-12 13:25:50 -0700

    Delete errorspage

--- frontend/src/pages/Errors/ErrorsPage.tsx
+++ frontend/src/pages/Errors/ErrorsPage.tsx
@@ -1,13 +1,5 @@
-import React from 'react';
-
 import { Complete } from '../../util/types';
-import { IntegrationCard } from '../Sessions/IntegrationCard/IntegrationCard';
-import { FeedNavigation } from '../Sessions/SearchSidebar/FeedNavigation/FeedNavigation';
-import { ErrorFeed } from './ErrorFeed/ErrorFeed';
 import { ErrorSearchParams } from './ErrorSearchContext/ErrorSearchContext';
-import { ErrorSearchSidebar } from './ErrorSearchSidebar/ErrorSearchSidebar';
-import { ErrorSegmentSidebar } from './ErrorSegmentSidebar/ErrorSegmentSidebar';
-import styles from './ErrorsPage.module.scss';
 
 export const EmptyErrorsSearchParams: Complete<ErrorSearchParams> = {
     browser: undefined,
@@ -17,25 +9,3 @@ export const EmptyErrorsSearchParams: Complete<ErrorSearchParams> = {
     os: undefined,
     visited_url: undefined,
 };
-
-const ErrorsPage = ({ integrated }: { integrated: boolean }) => {
-    return (
-        <div className={styles.errorsBody}>
-            <div className={styles.leftPanel}>
-                <FeedNavigation />
-                <ErrorSegmentSidebar />
-            </div>
-            <div className={styles.centerPanel}>
-                <div className={styles.errorsSection}>
-                    <ErrorFeed />
-                </div>
-            </div>
-            <div className={styles.rightPanel}>
-                <ErrorSearchSidebar />
-            </div>
-            {!integrated && <IntegrationCard />}
-        </div>
-    );
-};
-
-export default ErrorsPage;
