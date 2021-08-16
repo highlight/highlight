269c9f1d943ecc8c62bf70fb17379c64626736c7
John Pham <john@highlight.run>
2021-08-16 16:30:58 -0700

    Disable highlight

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,14 +3,13 @@ import './index.scss';
 import '@highlight-run/rrweb/dist/index.css';
 
 import { ApolloProvider } from '@apollo/client';
-import { H, HighlightOptions } from 'highlight.run';
+import { H } from 'highlight.run';
 import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 import { SkeletonTheme } from 'react-loading-skeleton';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { QueryParamProvider } from 'use-query-params';
 
-import packageJson from '../package.json';
 import {
     AuthContextProvider,
     AuthRole,
@@ -28,53 +27,53 @@ import { client } from './util/graph';
 import { SimpleErrorBoundary } from './util/simpleErrorBoundary';
 
 const dev = process.env.NODE_ENV === 'development' ? true : false;
-const options: HighlightOptions = {
-    debug: { clientInteractions: true, domRecording: true },
-    manualStart: true,
-    enableStrictPrivacy: Math.floor(Math.random() * 2) === 0,
-    version: packageJson['version'],
-    networkRecording: {
-        enabled: true,
-        recordHeadersAndBody: true,
-    },
-    integrations: {
-        mixpanel: {
-            projectToken: 'e70039b6a5b93e7c86b8afb02b6d2300',
-        },
-    },
-};
-const favicon = document.querySelector("link[rel~='icon']") as any;
-if (dev) {
-    options.scriptUrl = 'http://localhost:8080/dist/index.js';
-    options.backendUrl = 'http://localhost:8082/public';
+// const options: HighlightOptions = {
+//     debug: { clientInteractions: true, domRecording: true },
+//     manualStart: true,
+//     enableStrictPrivacy: Math.floor(Math.random() * 2) === 0,
+//     version: packageJson['version'],
+//     networkRecording: {
+//         enabled: true,
+//         recordHeadersAndBody: true,
+//     },
+//     integrations: {
+//         mixpanel: {
+//             projectToken: 'e70039b6a5b93e7c86b8afb02b6d2300',
+//         },
+//     },
+// };
+// const favicon = document.querySelector("link[rel~='icon']") as any;
+// if (dev) {
+//     options.scriptUrl = 'http://localhost:8080/dist/index.js';
+//     options.backendUrl = 'http://localhost:8082/public';
 
-    options.integrations = undefined;
+//     options.integrations = undefined;
 
-    const sampleEnvironmentNames = [
-        'john',
-        'jay',
-        'anthony',
-        'cameron',
-        'boba',
-    ];
-    options.environment = `${
-        sampleEnvironmentNames[
-            Math.floor(Math.random() * sampleEnvironmentNames.length)
-        ]
-    }-localhost`;
-    window.document.title = `⚙️ ${window.document.title}`;
-    if (favicon) {
-        favicon.href = `${process.env.PUBLIC_URL}/favicon-localhost.ico`;
-    }
-} else if (window.location.href.includes('onrender')) {
-    if (favicon) {
-        favicon.href = `${process.env.PUBLIC_URL}/favicon-pr.ico`;
-    }
-    window.document.title = `📸 ${window.document.title}`;
-    options.environment = 'Pull Request Preview';
-}
-H.init(process.env.REACT_APP_FRONTEND_ORG ?? 1, options);
-H.start();
+//     const sampleEnvironmentNames = [
+//         'john',
+//         'jay',
+//         'anthony',
+//         'cameron',
+//         'boba',
+//     ];
+//     options.environment = `${
+//         sampleEnvironmentNames[
+//             Math.floor(Math.random() * sampleEnvironmentNames.length)
+//         ]
+//     }-localhost`;
+//     window.document.title = `⚙️ ${window.document.title}`;
+//     if (favicon) {
+//         favicon.href = `${process.env.PUBLIC_URL}/favicon-localhost.ico`;
+//     }
+// } else if (window.location.href.includes('onrender')) {
+//     if (favicon) {
+//         favicon.href = `${process.env.PUBLIC_URL}/favicon-pr.ico`;
+//     }
+//     window.document.title = `📸 ${window.document.title}`;
+//     options.environment = 'Pull Request Preview';
+// }
+// H.init(process.env.REACT_APP_FRONTEND_ORG ?? 1, options);
+// H.start();
 
 window.Intercom('boot', {
     app_id: 'gm6369ty',
