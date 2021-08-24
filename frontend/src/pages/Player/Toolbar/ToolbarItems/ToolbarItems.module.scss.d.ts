412915ec432b5d9c4378e607c24526f22978ba52
John Pham <john@highlight.run>
2021-08-23 19:41:19 -0700

    Add toolbar item animation

--- frontend/src/pages/Player/Toolbar/ToolbarItems/ToolbarItems.module.scss.d.ts
+++ frontend/src/pages/Player/Toolbar/ToolbarItems/ToolbarItems.module.scss.d.ts
@@ -1,5 +1,7 @@
 export const devToolsActive: string;
 export const devToolsButton: string;
 export const devToolsIcon: string;
+export const grow: string;
 export const menuItemContainer: string;
 export const pinButton: string;
+export const toolbarItem: string;
