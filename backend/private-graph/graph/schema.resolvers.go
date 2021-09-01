e33b82f5e1e300aefe976283ea3f5c7cf5c1ff73
Cameron Brill <c00brill@gmail.com>
2021-09-01 10:00:04 -0700

    properly enrich error

--- backend/private-graph/graph/schema.resolvers.go
+++ backend/private-graph/graph/schema.resolvers.go
@@ -727,7 +727,7 @@ func (r *mutationResolver) CreateSessionComment(ctx context.Context, organizatio
 
 			err := r.SendEmailAlert(tos, authorName, viewLink, textForEmail, SendGridSessionCommentEmailTemplateID, sessionImage)
 			if err != nil {
-				log.Errorf(e.Wrap(err, "error notifying tagged admins in session comment").Error())
+				log.Error(e.Wrap(err, "error notifying tagged admins in session comment"))
 			}
 		}()
 
@@ -738,7 +738,7 @@ func (r *mutationResolver) CreateSessionComment(ctx context.Context, organizatio
 
 			err := r.SendPersonalSlackAlert(&org, admin, adminIds, viewLink, sessionComment.Text, "session")
 			if err != nil {
-				log.Errorf(e.Wrap(err, "error notifying tagged admins in session comment").Error())
+				log.Error(e.Wrap(err, "error notifying tagged admins in session comment"))
 			}
 		}()
 	}
