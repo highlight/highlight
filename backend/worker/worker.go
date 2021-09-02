c56178e79b8986905db455e15c24a333985afc86
Cameron Brill <c00brill@gmail.com>
2021-09-02 10:46:41 -0700

    increase workerpool size from 4 to 10

--- backend/worker/worker.go
+++ backend/worker/worker.go
@@ -538,7 +538,7 @@ func (w *Worker) Start() {
 		}
 
 		// process 4 sessions at a time. this number was chosen arbitrarily.
-		wp := workerpool.New(4)
+		wp := workerpool.New(10)
 		for _, session := range sessions {
 			session := session
 			wp.Submit(func() {
