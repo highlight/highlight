da949aa275fb9f2accd13ce4911440050be3e62e
Cameron Brill <c00brill@gmail.com>
2021-09-04 04:49:23 +0900

    Apply suggestions from code review
    Co-authored-by: John Pham <john@highlight.run>

--- frontend/src/pages/Error/ErrorPage.tsx
+++ frontend/src/pages/Error/ErrorPage.tsx
@@ -268,7 +268,7 @@ export const ErrorFrequencyGraph: React.FC<FrequencyGraphProps> = ({
 
     useGetDailyErrorFrequencyQuery({
         variables: {
-            organization_id: '' + errorGroup?.organization_id,
+            organization_id: `${errorGroup?.organization_id}`,
             error_group_id: '' + errorGroup?.id,
             date_offset: dateRangeLength - 1,
         },
