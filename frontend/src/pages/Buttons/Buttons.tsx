1ab69e8441f7fb2ce688127b92fb0d158235d28b
John Pham <john@highlight.run>
2021-09-08 17:18:34 -0700

    Preview error boundary

--- frontend/src/pages/Buttons/Buttons.tsx
+++ frontend/src/pages/Buttons/Buttons.tsx
@@ -1,3 +1,5 @@
+import { useAuthContext } from '@authentication/AuthContext';
+import { ErrorBoundary } from '@highlight-run/react';
 import { H } from 'highlight.run';
 import React, { useState } from 'react';
 
@@ -7,117 +9,145 @@ import styles from './Buttons.module.scss';
 import { CustomError, DefaultError } from './ButtonsHelper';
 export const Buttons = () => {
     const [hasError, setHasError] = useState(false);
+    const { admin } = useAuthContext();
+    const [showTest, setShowTest] = useState(false);
     const [sendEmail, { loading }] = useSendEmailSignupMutation();
     if (hasError) {
         throw new Error('got an error');
     }
     return (
-        <div className={styles.buttonBody}>
-            <div>
-                <button
-                    className={commonStyles.submitButton}
-                    onClick={() => {
-                        sendEmail({
-                            variables: {
-                                email: 'anothernewemail@newemail.com',
-                            },
-                        });
-                    }}
-                >
-                    {loading ? 'loading...' : 'Send an email'}
-                </button>
-                <button
-                    className={commonStyles.submitButton}
-                    onClick={() => {
-                        DefaultError();
-                    }}
-                >
-                    Throw an Error
-                </button>
-                <button
-                    className={commonStyles.submitButton}
-                    onClick={() => {
-                        console.error('boba');
-                    }}
-                >
-                    Console Error
-                </button>
-                <button
-                    className={commonStyles.submitButton}
-                    onClick={() => {
-                        setHasError(true);
-                    }}
-                >
-                    H.consumeError()
-                </button>
-                <button
-                    className={commonStyles.submitButton}
-                    onClick={() => {
-                        H.error('Highlight H.error');
-                    }}
-                >
-                    H.error()
-                </button>
-                <button
-                    className={commonStyles.submitButton}
-                    onClick={() => {
-                        CustomError();
-                    }}
-                >
-                    Throw a custom Error
-                </button>
-                <button
-                    className={commonStyles.submitButton}
-                    onClick={() => {
-                        H.stop();
-                    }}
-                >
-                    Stop Recording
-                </button>
-            </div>
+        <ErrorBoundary
+            showDialog
+            dialogOptions={{
+                user: {
+                    email: admin?.email,
+                    name: admin?.name,
+                },
+            }}
+        >
+            <button
+                onClick={() => {
+                    setShowTest(true);
+                }}
+            >
+                Show Error Boundary
+            </button>
+            {showTest && <Test />}
+            <div className={styles.buttonBody}>
+                <div>
+                    <button
+                        className={commonStyles.submitButton}
+                        onClick={() => {
+                            sendEmail({
+                                variables: {
+                                    email: 'anothernewemail@newemail.com',
+                                },
+                            });
+                        }}
+                    >
+                        {loading ? 'loading...' : 'Send an email'}
+                    </button>
+                    <button
+                        className={commonStyles.submitButton}
+                        onClick={() => {
+                            DefaultError();
+                        }}
+                    >
+                        Throw an Error
+                    </button>
+                    <button
+                        className={commonStyles.submitButton}
+                        onClick={() => {
+                            console.error('boba');
+                        }}
+                    >
+                        Console Error
+                    </button>
+                    <button
+                        className={commonStyles.submitButton}
+                        onClick={() => {
+                            setHasError(true);
+                        }}
+                    >
+                        H.consumeError()
+                    </button>
+                    <button
+                        className={commonStyles.submitButton}
+                        onClick={() => {
+                            H.error('Highlight H.error');
+                        }}
+                    >
+                        H.error()
+                    </button>
+                    <button
+                        className={commonStyles.submitButton}
+                        onClick={() => {
+                            CustomError();
+                        }}
+                    >
+                        Throw a custom Error
+                    </button>
+                    <button
+                        className={commonStyles.submitButton}
+                        onClick={() => {
+                            H.stop();
+                        }}
+                    >
+                        Stop Recording
+                    </button>
+                </div>
 
-            <div>
-                <button
-                    onClick={() => {
-                        fetch('https://pokeapi.co/api/v2/pokemon/ditto', {
-                            headers: {
-                                'Content-Type': 'application/json',
-                            },
-                        });
-                    }}
-                >
-                    GET fetch('https://pokeapi.co/api/v2/pokemon/ditto')
-                </button>
-                <button
-                    onClick={() => {
-                        fetch('https://pokeapi.co/api/v2/pokemon/ditto', {
-                            headers: {
-                                'Content-Type': 'application/json',
-                                Authorization: 'Basic YWxhZGRpbjpvcGVuc2VzYW1l',
-                                Cookie:
-                                    'PHPSESSID=298zf09hf012fh2; csrftoken=u32t4o3tb3gg43; _gat=1',
-                                'Proxy-Authorization':
-                                    'Basic YWxhZGRpbjpvcGVuc2VzYW1l',
-                            },
-                        });
-                    }}
-                >
-                    GET fetch('https://pokeapi.co/api/v2/pokemon/ditto') with
-                    sensitive headers
-                </button>
-                <button
-                    onClick={() => {
-                        fetch('https://pokeapi.co/api/v2/pokemon/ditto', {
-                            headers: {
-                                'Content-Type': 'application/json',
-                            },
-                            method: 'POST',
-                        });
-                    }}
-                >
-                    POST fetch('https://pokeapi.co/api/v2/pokemon/ditto')
-                </button>
+                <div>
+                    <button
+                        onClick={() => {
+                            fetch('https://pokeapi.co/api/v2/pokemon/ditto', {
+                                headers: {
+                                    'Content-Type': 'application/json',
+                                },
+                            });
+                        }}
+                    >
+                        GET fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+                    </button>
+                    <button
+                        onClick={() => {
+                            fetch('https://pokeapi.co/api/v2/pokemon/ditto', {
+                                headers: {
+                                    'Content-Type': 'application/json',
+                                    Authorization:
+                                        'Basic YWxhZGRpbjpvcGVuc2VzYW1l',
+                                    Cookie:
+                                        'PHPSESSID=298zf09hf012fh2; csrftoken=u32t4o3tb3gg43; _gat=1',
+                                    'Proxy-Authorization':
+                                        'Basic YWxhZGRpbjpvcGVuc2VzYW1l',
+                                },
+                            });
+                        }}
+                    >
+                        GET fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+                        with sensitive headers
+                    </button>
+                    <button
+                        onClick={() => {
+                            fetch('https://pokeapi.co/api/v2/pokemon/ditto', {
+                                headers: {
+                                    'Content-Type': 'application/json',
+                                },
+                                method: 'POST',
+                            });
+                        }}
+                    >
+                        POST fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+                    </button>
+                </div>
             </div>
-        </div>
+        </ErrorBoundary>
     );
 };
+
+const Test = () => (
+    <h2>
+        {/* @ts-expect-error*/}
+        {a}
+    </h2>
+);
