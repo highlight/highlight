ca675ca14b08acdda69ad36aada6824f2875b70a
jay-khatri <khatri.florida@gmail.com>
2021-09-10 13:09:12 -0700

    fix

--- backend/util/environment.go
+++ backend/util/environment.go
@@ -3,19 +3,12 @@ package util
 import "os"
 
 var (
-	env      = os.Getenv("ENVIRONMENT")
-	DEV_ENV  = "dev"
-	TEST_ENV = "test"
-	ON_PREM  = os.Getenv("REACT_APP_ONPREM")
+	env            = os.Getenv("ENVIRONMENT")
+	DEV_ENV        = "dev"
+	TEST_ENV       = "test"
+	ON_PREM        = os.Getenv("REACT_APP_ONPREM")
+	DOPPLER_CONFIG = os.Getenv("DOPPLER_CONFIG")
 )
-
-func IsDevEnv() bool {
-	return env == DEV_ENV
-}
-
-func IsTestEnv() bool {
-	return env == TEST_ENV
-}
 
 func IsDevOrTestEnv() bool {
 	return env == DEV_ENV || env == TEST_ENV
@@ -23,4 +16,8 @@ func IsDevOrTestEnv() bool {
 
 func IsOnPrem() bool {
 	return ON_PREM == "true"
+}
+
+func IsOnRender() bool {
+	return ON_RENDER == "prod_aws_render"
 }
