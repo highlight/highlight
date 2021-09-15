51ec25af1a962b55d0137307422636decb8863c0
Cameron Brill <c00brill@gmail.com>
2021-09-15 12:02:54 -0700

    create script

---
+++ backend/migrations/cmd/create-session-feedback-alerts/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"os"
+
+	log "github.com/sirupsen/logrus"
+
+	"github.com/highlight-run/highlight/backend/model"
+)
+
+func main() {
+	log.Info("setting up db")
+	db, err := model.SetupDB(os.Getenv("PSQL_DB"))
+	if err != nil {
+		log.Fatalf("error setting up db: %+v", err)
+	}
+
+	var orgs []model.Organization
+	if err := db.Model(&model.Organization{}).Scan(&orgs).Error; err != nil {
+		log.Fatalf("error getting organizations: %v", err)
+	}
+
+	thresholdWindow := 30
+	var alerts []model.SessionAlert
+	for _, o := range orgs {
+		alerts = append(alerts, model.SessionAlert{
+			Alert:           model.Alert{
+				OrganizationID:       o.ID,
+				ExcludedEnvironments: "[]",
+				CountThreshold:       1,
+				ThresholdWindow:      &thresholdWindow,
+				ChannelsToNotify:     "[]",
+				Type:                 &model.AlertType.SESSION_FEEDBACK,
+			},
+		})
+	}
+	if err := db.Create(&alerts).Error; err != nil {
+		log.Fatalf("error creating alerts: %v", err)
+	}
+}
