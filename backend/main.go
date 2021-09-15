14e2fce4488744839bfa8373bcfe1da945ba7489
John Pham <john@highlight.run>
2021-09-15 15:25:05 -0700

    Allow all headers

--- backend/main.go
+++ backend/main.go
@@ -163,7 +163,7 @@ func main() {
 	r.Use(cors.New(cors.Options{
 		AllowOriginRequestFunc: validateOrigin,
 		AllowCredentials:       true,
-		AllowedHeaders:         []string{"Content-Type", "Token", "Sentry-Trace", "Request-Id"},
+		AllowedHeaders:         []string{"*"},
 	}).Handler)
 	r.MethodFunc(http.MethodGet, "/health", healthRouter(runtimeParsed))
 
