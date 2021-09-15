a5487eb483e4c5b99ecbf67459866311028bf84c
John Pham <john@highlight.run>
2021-09-15 15:02:26 -0700

    Allow "Request-Id" as a request header to backends

--- backend/main.go
+++ backend/main.go
@@ -163,7 +163,7 @@ func main() {
 	r.Use(cors.New(cors.Options{
 		AllowOriginRequestFunc: validateOrigin,
 		AllowCredentials:       true,
-		AllowedHeaders:         []string{"Content-Type", "Token", "Sentry-Trace"},
+		AllowedHeaders:         []string{"Content-Type", "Token", "Sentry-Trace", "Request-Id"},
 	}).Handler)
 	r.MethodFunc(http.MethodGet, "/health", healthRouter(runtimeParsed))
 
