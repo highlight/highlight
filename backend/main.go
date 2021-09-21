ee8ebfb076c5d88aa242eb50525ac7a977dae7e5
Cameron Brill <c00brill@gmail.com>
2021-09-22 02:47:12 +0900

    HIG-1172: allow posts to /health (#1394)
    * allow posts to /health
    
    * `HandleFunc`

--- backend/main.go
+++ backend/main.go
@@ -166,8 +166,7 @@ func main() {
 		AllowCredentials:       true,
 		AllowedHeaders:         []string{"*"},
 	}).Handler)
-	r.MethodFunc(http.MethodGet, "/health", healthRouter(runtimeParsed))
-
+	r.HandleFunc("/health", healthRouter(runtimeParsed))
 	/*
 		Selectively turn on backends depending on the input flag
 		If type is 'all', we run public-graph on /public and private-graph on /private
