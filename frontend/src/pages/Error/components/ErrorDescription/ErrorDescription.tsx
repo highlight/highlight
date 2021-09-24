5853d8352821135af2a5afda411de734d358f87d
John Pham <john@highlight.run>
2021-09-24 14:11:45 -0700

    Render JSON tree if error description is JSON

--- frontend/src/pages/Error/components/ErrorDescription/ErrorDescription.tsx
+++ frontend/src/pages/Error/components/ErrorDescription/ErrorDescription.tsx
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+import Card from '@components/Card/Card';
+import React, { useEffect, useState } from 'react';
+import ReactJson from 'react-json-view';
 import LinesEllipsis from 'react-lines-ellipsis';
 
 import { ErrorGroup, Maybe } from '../../../../graph/generated/schemas';
@@ -12,27 +14,57 @@ interface Props {
 const ErrorDescription = ({ errorGroup }: Props) => {
     const [showExpandButton, setShowExpandButton] = useState(true);
     const [eventLineExpand, setEventLineExpand] = useState(false);
+    const [textAsJson, setTextAsJson] = useState<null | any>(false);
 
     const text = parseErrorDescription(errorGroup?.event);
+
+    useEffect(() => {
+        if (text) {
+            try {
+                console.log(JSON.parse(text));
+                const json = JSON.parse(text);
+                setTextAsJson(json);
+            } catch {
+                setTextAsJson(null);
+            }
+        }
+    }, [text]);
+
     return (
         <>
-            <LinesEllipsis
-                text={text}
-                maxLine={eventLineExpand ? Number.MAX_SAFE_INTEGER : 2}
-                style={{ display: 'inline' }}
-                onReflow={(c) => {
-                    setShowExpandButton(!(c.text === text));
-                }}
-                className={styles.eventText}
-            />
-            {showExpandButton && (
-                <span
-                    className={styles.expandButton}
-                    onClick={() => setEventLineExpand(true)}
-                >
-                    {' '}
-                    show more
-                </span>
+            {textAsJson === null ? (
+                <>
+                    <LinesEllipsis
+                        text={text}
+                        maxLine={eventLineExpand ? Number.MAX_SAFE_INTEGER : 2}
+                        style={{ display: 'inline' }}
+                        onReflow={(c) => {
+                            setShowExpandButton(!(c.text === text));
+                        }}
+                        className={styles.eventText}
+                    />
+                    {showExpandButton && (
+                        <span
+                            className={styles.expandButton}
+                            onClick={() => setEventLineExpand(true)}
+                        >
+                            {' '}
+                            show more
+                        </span>
+                    )}
+                </>
+            ) : (
+                <Card>
+                    <ReactJson
+                        src={textAsJson}
+                        collapsed={1}
+                        displayDataTypes={false}
+                        collapseStringsAfterLength={100}
+                        iconStyle="square"
+                        quotesOnKeys={false}
+                        name={null}
+                    />
+                </Card>
             )}
         </>
     );
