216d05f7f6b88d2765d71c9bc4efad943ccdd3d2
John Pham <john@highlight.run>
2021-09-28 12:13:37 -0700

    Fix error date deep linking

--- frontend/src/pages/Errors/ErrorSearchInputs/DateInput.tsx
+++ frontend/src/pages/Errors/ErrorSearchInputs/DateInput.tsx
@@ -1,44 +1,19 @@
-import { DatePicker, message } from 'antd';
+import { DatePicker } from 'antd';
 import moment from 'moment';
-import React, { useEffect } from 'react';
-import { useHistory, useLocation } from 'react-router-dom';
+import React from 'react';
 
 import SvgCalendarIcon from '../../../static/CalendarIcon';
-import { SessionPageSearchParams } from '../../Player/utils/utils';
 import { getDateRangeForDateInput } from '../../Sessions/SearchInputs/DateInput';
 import inputStyles from '../../Sessions/SearchInputs/InputStyles.module.scss';
 import {
     ErrorSearchParams,
     useErrorSearchContext,
 } from '../ErrorSearchContext/ErrorSearchContext';
-import { EmptyErrorsSearchParams } from '../ErrorsPage';
 
 const { RangePicker } = DatePicker;
 
 export const DateInput = () => {
     const { searchParams, setSearchParams } = useErrorSearchContext();
-    const location = useLocation();
-    const history = useHistory();
-    const dateFromSearchParams = new URLSearchParams(location.search).get(
-        SessionPageSearchParams.date
-    );
-
-    useEffect(() => {
-        if (dateFromSearchParams) {
-            const start_date = moment(dateFromSearchParams);
-            const end_date = moment(dateFromSearchParams);
-
-            setSearchParams(() => ({
-                // We are explicitly clearing any existing search params so the only applied search param is the date range.
-                ...EmptyErrorsSearchParams,
-                date_range: getDateRangeForDateInput(start_date, end_date),
-            }));
-            message.success(
-                `Showing errors that were thrown on ${dateFromSearchParams}`
-            );
-            history.replace({ search: '' });
-        }
-    }, [history, dateFromSearchParams, setSearchParams]);
 
     return (
         <div>
