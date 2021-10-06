12866cda45980b326e24f9138ed5462f5bbd8db5
John Pham <john@highlight.run>
2021-10-06 12:08:14 -0700

    Allow detailed panel to be independent from Player Right Panel

--- frontend/src/pages/Player/Toolbar/DevToolsWindow/DetailPanel/DetailPanel.module.scss.d.ts
+++ frontend/src/pages/Player/Toolbar/DevToolsWindow/DetailPanel/DetailPanel.module.scss.d.ts
@@ -2,3 +2,4 @@ export const contentContainer: string;
 export const detailPanel: string;
 export const header: string;
 export const title: string;
+export const toggleButton: string;
--- frontend/src/pages/Player/Toolbar/DevToolsWindow/DetailPanel/DetailPanel.tsx
+++ frontend/src/pages/Player/Toolbar/DevToolsWindow/DetailPanel/DetailPanel.tsx
@@ -1,11 +1,13 @@
+import PanelToggleButton from '@pages/Player/components/PanelToggleButton/PanelToggleButton';
 import { usePlayerUIContext } from '@pages/Player/context/PlayerUIContext';
+import classNames from 'classnames';
 import { AnimatePresence, motion } from 'framer-motion';
 import React from 'react';
 
 import styles from './DetailPanel.module.scss';
 
 const DetailPanel = () => {
-    const { detailedPanel } = usePlayerUIContext();
+    const { detailedPanel, setDetailedPanel } = usePlayerUIContext();
 
     return (
         <AnimatePresence>
@@ -13,10 +15,18 @@ const DetailPanel = () => {
                 <motion.div
                     key="detailPanel"
                     className={styles.detailPanel}
-                    initial={{ transform: 'translateX(105%)' }}
+                    initial={{ transform: 'translateX(110%)' }}
                     animate={{ transform: 'translateX(0%)' }}
-                    exit={{ transform: 'translateX(105%)' }}
+                    exit={{ transform: 'translateX(110%)' }}
                 >
+                    <PanelToggleButton
+                        className={classNames(styles.toggleButton)}
+                        direction="right"
+                        isOpen={true}
+                        onClick={() => {
+                            setDetailedPanel(undefined);
+                        }}
+                    />
                     {!detailedPanel.options?.noHeader && (
                         <div className={styles.header}>
                             <h3 className={styles.title}>
