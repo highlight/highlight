8c54b4b5682dffdccadf6c1ea4d1e5fe4586eb63
John Pham <john@highlight.run>
2021-10-06 12:49:29 -0700

    Get resizing to work

--- frontend/src/pages/Player/Toolbar/DevToolsWindow/DetailPanel/DetailPanel.tsx
+++ frontend/src/pages/Player/Toolbar/DevToolsWindow/DetailPanel/DetailPanel.tsx
@@ -2,6 +2,7 @@ import PanelToggleButton from '@pages/Player/components/PanelToggleButton/PanelT
 import { usePlayerUIContext } from '@pages/Player/context/PlayerUIContext';
 import classNames from 'classnames';
 import { AnimatePresence, motion } from 'framer-motion';
+import { Resizable } from 're-resizable';
 import React from 'react';
 
 import styles from './DetailPanel.module.scss';
@@ -12,33 +13,52 @@ const DetailPanel = () => {
     return (
         <AnimatePresence>
             {!detailedPanel ? null : (
-                <motion.div
-                    key="detailPanel"
-                    className={styles.detailPanel}
-                    initial={{ transform: 'translateX(110%)' }}
-                    animate={{ transform: 'translateX(0%)' }}
-                    exit={{ transform: 'translateX(110%)' }}
+                <Resizable
+                    enable={{ left: true }}
+                    className={styles.resizeContainer}
+                    defaultSize={{
+                        width: '350',
+                        height: '100%',
+                    }}
+                    minWidth="300"
+                    maxWidth="90vw"
+                    handleComponent={{
+                        left: (
+                            <div
+                                className={classNames(styles.dragHandle)}
+                            ></div>
+                        ),
+                    }}
+                    handleWrapperClass={classNames(styles.dragHandleWrapper)}
                 >
-                    <PanelToggleButton
-                        className={classNames(styles.toggleButton)}
-                        direction="right"
-                        isOpen={true}
-                        onClick={() => {
-                            setDetailedPanel(undefined);
-                        }}
-                    />
-                    {!detailedPanel.options?.noHeader && (
-                        <div className={styles.header}>
-                            <h3 className={styles.title}>
-                                {detailedPanel.title}
-                            </h3>
-                        </div>
-                    )}
+                    <motion.div
+                        key="detailPanel"
+                        className={styles.detailPanel}
+                        initial={{ transform: 'translateX(110%)' }}
+                        animate={{ transform: 'translateX(0%)' }}
+                        exit={{ transform: 'translateX(110%)' }}
+                    >
+                        <PanelToggleButton
+                            className={classNames(styles.toggleButton)}
+                            direction="right"
+                            isOpen={true}
+                            onClick={() => {
+                                setDetailedPanel(undefined);
+                            }}
+                        />
+                        {!detailedPanel.options?.noHeader && (
+                            <div className={styles.header}>
+                                <h3 className={styles.title}>
+                                    {detailedPanel.title}
+                                </h3>
+                            </div>
+                        )}
 
-                    <div className={styles.contentContainer}>
-                        {detailedPanel.content}
-                    </div>
-                </motion.div>
+                        <div className={styles.contentContainer}>
+                            {detailedPanel.content}
+                        </div>
+                    </motion.div>
+                </Resizable>
             )}
         </AnimatePresence>
     );
