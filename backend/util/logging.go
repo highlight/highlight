5c65d6d63076e96002a7ba839167aa0c85ed5ad9
Cameron Brill <c00brill@gmail.com>
2021-11-01 13:28:39 -0400

    cast to *gqlerror.Error

--- backend/util/logging.go
+++ backend/util/logging.go
@@ -17,13 +17,20 @@ func GraphQLErrorPresenter(service string) func(ctx context.Context, e error) *g
 			"error": e,
 			"path":  graphql.GetPath(ctx),
 		}).Errorf("%s graphql request failed", service)
+
+		var gqlerr *gqlerror.Error
+		switch t := e.(type) {
+		case *gqlerror.Error:
+			gqlerr = t
+			e = gqlerr.Unwrap()
+		}
+
 		err := highlight.ConsumeError(ctx, e)
 		if err != nil &&
 			err.Error() != "context does not contain highlightSessionSecureID; context must have injected values from highlight.InterceptRequest" &&
 			err.Error() != "context does not contain highlightRequestSecureID; context must have injected values from highlight.InterceptRequest" {
 			log.WithError(err).Error("[highlight-go] error consuming error")
 		}
-		gqlerr := gqlerror.Errorf(e.Error())
 		return gqlerr
 	}
 }
