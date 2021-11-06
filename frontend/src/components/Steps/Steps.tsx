cbd5b79e1fa1e4306a33b417742f436a39e56c3b
Cameron Brill <c00brill@gmail.com>
2021-11-05 17:20:29 -0700

    Create steps component

---
+++ frontend/src/components/Steps/Steps.tsx
@@ -0,0 +1,29 @@
+import {
+    StepProps as AntDesignStepProps,
+    // Disabling here because we are using this file as a proxy.
+    // eslint-disable-next-line no-restricted-imports
+    Steps as AntDesignSteps,
+    StepsProps as AntDesignStepsProps,
+} from 'antd';
+import React from 'react';
+
+type StepsProps = Pick<AntDesignStepsProps, 'current'>;
+
+interface StepsType extends React.FC<StepsProps> {
+    Step: React.ClassicComponentClass<AntDesignStepProps>;
+}
+
+/**
+ * A proxy for Ant Design's steps. This component should be used instead of directly using Ant Design's.
+ */
+const Steps: StepsType = ({ current, children, ...props }) => {
+    return (
+        <AntDesignSteps progressDot current={current} {...props}>
+            {children}
+        </AntDesignSteps>
+    );
+};
+
+Steps.Step = AntDesignSteps.Step;
+
+export default Steps;
