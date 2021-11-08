0774533f02287d5f03ad11893ac231770591ad0f
John Pham <john@highlight.run>
2021-11-08 14:55:21 -0800

    Add skeleton to detailed error panel

--- frontend/src/pages/Player/Toolbar/DevToolsWindow/ErrorsPage/components/ErrorModal/ErrorModal.module.scss.d.ts
+++ frontend/src/pages/Player/Toolbar/DevToolsWindow/ErrorsPage/components/ErrorModal/ErrorModal.module.scss.d.ts
@@ -3,4 +3,5 @@ export const alertContainer: string;
 export const container: string;
 export const errorDescriptionContainer: string;
 export const loadingBarContainer: string;
+export const stackTraceLoadingWrapper: string;
 export const titleContainer: string;
