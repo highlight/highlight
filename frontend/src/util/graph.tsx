88da74d0b1c30ad61dd0b05b8ae1150a2a83ecca
John Pham <john@highlight.run>
2021-11-12 12:50:15 -0800

    Use GraphCDN to cache home page queries

--- frontend/src/util/graph.tsx
+++ frontend/src/util/graph.tsx
@@ -1,7 +1,14 @@
 import 'firebase/auth';
 
-import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
+import {
+    ApolloClient,
+    ApolloLink,
+    createHttpLink,
+    HttpLink,
+    InMemoryCache,
+} from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
+import { namedOperations } from '@graph/operations';
 import { isOnPrem } from '@util/onPrem/onPremUtils';
 import * as firebase from 'firebase/app';
 
@@ -13,6 +20,9 @@ const highlightGraph = createHttpLink({
     credentials: 'include',
 });
 
+const graphCdnGraph = new HttpLink({
+    uri: 'https://graphcdn.highlight.run',
+});
 if (isOnPrem) {
     console.log('Private Graph URI: ', uri);
 }
@@ -26,8 +36,39 @@ const authLink = setContext((_, { headers }) => {
     });
 });
 
+const { Query } = namedOperations;
+/**
+ * These are the queries that should be routed to GraphCDN instead of private graph.
+ * We use GraphCDN for expensive queries.
+ * */
+const GraphCDNOperations = [
+    Query.GetKeyPerformanceIndicators,
+    Query.GetDailyErrorFrequency,
+    Query.GetDailySessionsCount,
+    Query.GetReferrersCount,
+    Query.GetTopUsers,
+    Query.GetRageClicksForProject,
+] as const;
+
 export const client = new ApolloClient({
-    link: authLink.concat(highlightGraph),
+    link: ApolloLink.split(
+        (operation) => {
+            // Don't query GraphCDN for localhost.
+            // GraphCDN only caches production data.
+            if (process.env.NODE_ENV === 'development') {
+                return false;
+            }
+
+            // Check to see if the operation is one that we should send to GraphCDN instead of private graph.
+            // @ts-expect-error
+            if (GraphCDNOperations.includes(operation.operationName)) {
+                return true;
+            }
+            return false;
+        },
+        graphCdnGraph,
+        authLink.concat(highlightGraph)
+    ),
     cache: new InMemoryCache({
         typePolicies: {
             Session: {
