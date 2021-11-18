2126ed4333305ceea523d05b9318b3fcc63a96ed
John Pham <john@highlight.run>
2021-11-18 15:49:45 -0800

    Add start of query builder

--- frontend/src/routers/InternalRouter/InternalRouter.tsx
+++ frontend/src/routers/InternalRouter/InternalRouter.tsx
@@ -1,11 +1,25 @@
-import React, { lazy, Suspense } from 'react';
+import { useAppLoadingContext } from '@context/AppLoadingContext';
+import React, { lazy, Suspense, useEffect } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 const InternalRouter = () => {
+    const { setIsLoading } = useAppLoadingContext();
+
+    useEffect(() => {
+        setIsLoading(false);
+    }, [setIsLoading]);
+
     return (
         <Switch>
             <Suspense fallback={<></>}>
                 <Route
+                    path="/_internal/query-builder"
+                    component={lazy(
+                        () => import('../../pages/Internal/QueryBuilderPage')
+                    )}
+                />
+                <Route
+                    exact
                     path="/_internal"
                     component={lazy(
                         () => import('../../pages/Internal/InternalPage')
