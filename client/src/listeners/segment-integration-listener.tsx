cae7be8451bf4cf96f928d97cf960fb2fba4357a
John Pham <john@highlight.run>
2021-11-18 13:52:52 -0800

    Revert "Add fetch listener to segment integration (#1829)"
    This reverts commit a9917601152f14799bc2e44d31d35ad4b7348389.

--- client/src/listeners/segment-integration-listener.tsx
+++ client/src/listeners/segment-integration-listener.tsx
@@ -1,7 +1,3 @@
-import { HighlightFetchWindow } from '../listeners/network-listener/utils/fetch-listener';
-
-declare var window: HighlightFetchWindow & Window;
-
 export const SegmentIntegrationListener = (callback: (obj: any) => void) => {
     callback(window.location.href);
     var send = XMLHttpRequest.prototype.send;
@@ -20,30 +16,7 @@ export const SegmentIntegrationListener = (callback: (obj: any) => void) => {
         send.call(this, data);
     };
 
-    const fetchProxy = window._fetchProxy;
-    window._fetchProxy = (input, init) => {
-        const url =
-            (typeof input === 'object' && input.url) || (input as string);
-        const isSegmentRequest = url.includes('api.segment.io');
-
-        if (isSegmentRequest && init && 'body' in init) {
-            try {
-                const body = init.body?.toString();
-                if (body) {
-                    const object = JSON.parse(body);
-                    if (object.type === 'track' || object.type === 'identify') {
-                        callback(object);
-                    }
-                }
-            } catch (e) {
-                return fetchProxy.call(this, input, init);
-            }
-        }
-        return fetchProxy.call(this, input, init);
-    };
-
     return () => {
         XMLHttpRequest.prototype.send = send;
-        window.fetch = fetchProxy;
     };
 };
