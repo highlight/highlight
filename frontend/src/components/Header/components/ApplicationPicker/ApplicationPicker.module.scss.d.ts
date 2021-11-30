b0c7105ca45ce3139d0b01a974f93927bc51ce53
John Pham <john@highlight.run>
2021-11-29 16:33:42 -0800

    Update applicationpicker

--- frontend/src/components/Header/components/ApplicationPicker/ApplicationPicker.module.scss.d.ts
+++ frontend/src/components/Header/components/ApplicationPicker/ApplicationPicker.module.scss.d.ts
@@ -1,4 +1,7 @@
+export const applicationPicker: string;
 export const createNewProjectOption: string;
 export const existingProjectOption: string;
+export const headerContainer: string;
 export const selected: string;
 export const subTitle: string;
+export const workspaceIcon: string;
