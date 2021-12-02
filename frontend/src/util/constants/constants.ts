9d760b6a6dd3ea6d6fe9af7933bb498a9f73eecb
Cameron Brill <c00brill@gmail.com>
2021-12-03 08:55:51 +0900

    Update frontend/src/util/constants/constants.ts
    Co-authored-by: John Pham <john@highlight.run>

--- frontend/src/util/constants/constants.ts
+++ frontend/src/util/constants/constants.ts
@@ -1,2 +1,2 @@
-export const DEMO_WORKSPACE_NAME = 'Demo Workspace';
-export const DEMO_PROJECT_NAME = 'Pied Piper Inc.';
+export const DEMO_WORKSPACE_NAME = 'Pied Piper Inc.';
+export const DEMO_PROJECT_NAME = 'Frontend';
