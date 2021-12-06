031b643ffbf18e7ae79d745d74d9d89f0f42148c
Cameron Brill <c00brill@gmail.com>
2021-12-07 03:00:54 +0900

    Show Switch Workspace option unconditionally (#1916)
    * del
    
    * remove unused

--- frontend/src/components/Header/components/ApplicationPicker/ApplicationPicker.tsx
+++ frontend/src/components/Header/components/ApplicationPicker/ApplicationPicker.tsx
@@ -28,7 +28,6 @@ const ApplicationPicker = () => {
         allProjects,
         currentProject,
         currentWorkspace,
-        workspaces,
     } = useApplicationContext();
     const { workspace_id, project_id } = useParams<{
         workspace_id: string;
@@ -121,7 +120,6 @@ const ApplicationPicker = () => {
                                 action: () => {
                                     history.push(`/switch`);
                                 },
-                                hidden: workspaces.length <= 1,
                             },
                             {
                                 displayName: 'Workspace Settings',
