66ebdc0cd581a6b088df2c84a1eded5cc633aa76
John Pham <john@highlight.run>
2021-12-13 15:59:25 -0800

    Monkey patch localStorage.setItem to listen to on page changes

--- client/src/listeners/segment-integration-listener.tsx
+++ client/src/listeners/segment-integration-listener.tsx
@@ -22,7 +22,7 @@ export const SegmentIntegrationListener = (callback: (obj: any) => void) => {
         send.call(this, data);
     };
 
-    const localStorageHandler = (e: StorageEvent) => {
+    const localStorageHandler = (e: Pick<StorageEvent, 'key'>) => {
         if (
             e.key === SEGMENT_LOCAL_STORAGE_KEYS['USER_ID'] ||
             e.key === SEGMENT_LOCAL_STORAGE_KEYS['ANONYMOUS_ID'] ||
@@ -64,6 +64,18 @@ export const SegmentIntegrationListener = (callback: (obj: any) => void) => {
 
     window.addEventListener('storage', localStorageHandler);
 
+    // `window.addEventListener('storage', localStorageHandler)` only gets called when the storage
+    // is changed on a different window/tab. Same-page changes do not cause an event to get created.
+    // This breaks our use case here since Segment sets the localStorage values on the same tab that
+    // Highlight is running on. Without this, we won't be able to read the Segment identify values.
+    monkeyPatchLocalStorage(({ keyName }) => {
+        const mockStorageEvent = {
+            key: keyName,
+        };
+
+        localStorageHandler(mockStorageEvent);
+    });
+
     return () => {
         XMLHttpRequest.prototype.send = send;
     };
@@ -84,5 +96,26 @@ const getLocalStorageValues = () => {
         userId,
         userTraits,
         anonymousId,
+    };
+};
+
+const monkeyPatchLocalStorage = (
+    onSetItemHandler: ({
+        keyName,
+        keyValue,
+    }: {
+        keyName: string;
+        keyValue: string;
+    }) => void
+) => {
+    const originalSetItem = window.localStorage.setItem;
+
+    window.localStorage.setItem = function () {
+        const [keyName, keyValue] = (arguments as unknown) as [
+            key: string,
+            value: string
+        ];
+        onSetItemHandler({ keyName, keyValue });
+        originalSetItem.apply(this, [keyName, keyValue]);
     };
 };
