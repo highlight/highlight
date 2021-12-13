362b26cd179e38cc3e925f86001e20a01f0f8478
Cameron Brill <c00brill@gmail.com>
2021-12-14 07:56:20 +0900

    Update backend/private-graph/graph/schema.resolvers.go

--- backend/private-graph/graph/schema.resolvers.go
+++ backend/private-graph/graph/schema.resolvers.go
@@ -4096,13 +4096,3 @@ type sessionResolver struct{ *Resolver }
 type sessionAlertResolver struct{ *Resolver }
 type sessionCommentResolver struct{ *Resolver }
 type subscriptionResolver struct{ *Resolver }
-
-// !!! WARNING !!!
-// The code below was going to be deleted when updating resolvers. It has been copied here so you have
-// one last chance to move it out of harms way if you want. There are two reasons this happens:
-//  - When renaming or deleting a resolver the old code will be put in here. You can safely delete
-//    it when you're done.
-//  - You have helper methods in this file. Move them out to keep these resolver files clean.
-func (r *errorAlertResolver) Cadence(ctx context.Context, obj *model.ErrorAlert) (int, error) {
-	panic(fmt.Errorf("not implemented"))
-}
