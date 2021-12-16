4ef59139fc20d6364ea5afc022239178650fda1b
John Pham <john@highlight.run>
2021-12-16 11:16:18 -0800

    Update Slack copy (#1987)

--- frontend/src/components/Header/components/PersonalNotificationButton/PersonalNotificationButton.tsx
+++ frontend/src/components/Header/components/PersonalNotificationButton/PersonalNotificationButton.tsx
@@ -81,7 +81,7 @@ const PersonalNotificationButton = ({
                 }
             }}
         >
-            {text || 'Get Comment Notifications'}
+            {text || 'Tag Slack Channels in Comments'}
         </Button>
     );
 };
