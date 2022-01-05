2b93defe645e5fd358065afbecbafc448b2696c1
John Pham <john@highlight.run>
2022-01-05 15:54:06 -0800

    Improve formatting

--- frontend/src/pages/Error/components/ErrorDescription/ErrorDescription.tsx
+++ frontend/src/pages/Error/components/ErrorDescription/ErrorDescription.tsx
@@ -2,16 +2,36 @@ import JsonOrTextCard from '@pages/Error/components/JsonOrTextCard/JsonOrTextCar
 import React from 'react';
 
 import { ErrorGroup, Maybe } from '../../../../graph/generated/schemas';
-import { parseErrorDescription } from './utils/utils';
 
 interface Props {
     errorGroup: Maybe<Pick<ErrorGroup, 'event'>> | undefined;
 }
 
 const ErrorDescription = ({ errorGroup }: Props) => {
-    const text = parseErrorDescription(errorGroup?.event);
-
-    return <JsonOrTextCard jsonOrText={text} title={'Error Body'} />;
+    const jsonOrText = formatErrorDescription(errorGroup?.event);
+    return (
+        <JsonOrTextCard
+            jsonOrText={jsonOrText.toString()}
+            title={'Error Body'}
+        />
+    );
 };
 
 export default ErrorDescription;
+
+const formatErrorDescription = (event: any) => {
+    const textToRender = `[${event}]`;
+    let isJson = true;
+
+    try {
+        JSON.parse(textToRender);
+    } catch {
+        isJson = false;
+    }
+
+    if (isJson) {
+        return textToRender;
+    }
+
+    return event || '';
+};
