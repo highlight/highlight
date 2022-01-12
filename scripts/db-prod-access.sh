24732a2436e609f8d21d9449ac35624c7bb6a537
Richard Hua <rich@rkhua.com>
2022-01-12 15:14:26 -0800

    Change db script to use auto vertical output

--- scripts/db-prod-access.sh
+++ scripts/db-prod-access.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-doppler run -c prod_aws --command "PGPASSWORD=\$PSQL_PASSWORD pgcli -h \$PSQL_HOST -U \$PSQL_USER \$PSQL_DB"
+doppler run -c prod_aws --command "PGPASSWORD=\$PSQL_PASSWORD pgcli --auto-vertical-output -h \$PSQL_HOST -U \$PSQL_USER \$PSQL_DB"
