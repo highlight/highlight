5a8113c32ad89c7832b0fff789d6e3e5b5687e2a
mayberryzane <86132398+mayberryzane@users.noreply.github.com>
2022-01-19 16:41:23 -0500

    [HIG-1752] readonly query picker in segment modals (#2110)
    * [HIG-1752] readonly query picker in segment modals
    
    * PR feedback - tooltip for disabled buttons, add gate, don't render if empty

--- frontend/src/pages/Errors/ErrorSegmentSidebar/SegmentButtons/CreateErrorSegmentModal.tsx
+++ frontend/src/pages/Errors/ErrorSegmentSidebar/SegmentButtons/CreateErrorSegmentModal.tsx
@@ -1,5 +1,10 @@
+import {
+    queryBuilderEnabled,
+    useAuthContext,
+} from '@authentication/AuthContext';
 import Input from '@components/Input/Input';
 import { namedOperations } from '@graph/operations';
+import ErrorQueryBuilder from '@pages/Error/components/ErrorQueryBuilder/ErrorQueryBuilder';
 import { useParams } from '@util/react-router/useParams';
 import { message } from 'antd';
 import React, { useState } from 'react';
@@ -33,6 +38,9 @@ const CreateErrorSegmentModal = ({
         project_id: string;
         segment_id: string;
     }>();
+    const { isHighlightAdmin } = useAuthContext();
+    const isQueryBuilder = queryBuilderEnabled(isHighlightAdmin, project_id);
+
     const { searchParams, setExistingParams } = useErrorSearchContext();
     const history = useHistory();
 
@@ -80,6 +88,11 @@ const CreateErrorSegmentModal = ({
                         Creating a segment allows you to save search queries
                         that target a specific set of errors.
                     </p>
+                    {isQueryBuilder && (
+                        <div className={styles.queryBuilderContainer}>
+                            <ErrorQueryBuilder readonly />
+                        </div>
+                    )}
                     <Input
                         name="name"
                         value={newSegmentName}
