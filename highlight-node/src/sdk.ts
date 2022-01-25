8b9bc7f7ddf744838664e5d76a22a8511c1b674c
eightants <yihonganthony@gmail.com>
2022-01-25 10:55:58 -0600

    Added highlight-node package

---
+++ highlight-node/src/sdk.ts
@@ -0,0 +1,5 @@
+import { NodeOptions } from './types';
+
+export function init(options: NodeOptions = {}): void {
+    console.log('package imported');
+}
---
+++ highlight-node/src/types.ts
@@ -0,0 +1,3 @@
+export interface NodeOptions {
+    sessionId?: string;
+}
