ba8db8ed529424c37f47566ce3dbef5d60b510fd
Anthony Teo <yihonganthony@gmail.com>
2022-02-02 13:21:58 -0600

    Changed bg to white on no workspace access (#2136)

--- frontend/src/routers/OrgRouter/OrgRouter.tsx
+++ frontend/src/routers/OrgRouter/OrgRouter.tsx
@@ -311,6 +311,7 @@ export const ProjectRouter = () => {
                                     'ProjectRouter Error: ' +
                                     JSON.stringify(error)
                                 }
+                                shownWithHeader
                             />
                         ) : (
                             <>
