a4a9d357c3489f7913d92673f743365770656337
John Pham <john@highlight.run>
2022-02-02 14:14:13 -0800

    Add basic responsive grid

--- frontend/src/pages/Dashboards/pages/Dashboard/DashboardPage.module.scss.d.ts
+++ frontend/src/pages/Dashboards/pages/Dashboard/DashboardPage.module.scss.d.ts
@@ -1,3 +1,3 @@
 export const dateRangePicker: string;
 export const dateRangePickerContainer: string;
-export const grid: string;
+export const gridContainer: string;
--- frontend/src/pages/Dashboards/pages/Dashboard/DashboardPage.tsx
+++ frontend/src/pages/Dashboards/pages/Dashboard/DashboardPage.tsx
@@ -1,12 +1,18 @@
+import 'react-grid-layout/css/styles.css';
+import 'react-resizable/css/styles.css';
+
 import { StandardDropdown } from '@components/Dropdown/StandardDropdown/StandardDropdown';
 import DashboardCard from '@pages/Dashboards/components/DashboardCard/DashboardCard';
 import { useDashboardsContext } from '@pages/Dashboards/DashboardsContext/DashboardsContext';
 import { useParams } from '@util/react-router/useParams';
 import moment from 'moment';
 import React, { useEffect, useState } from 'react';
+import { Layouts, Responsive, WidthProvider } from 'react-grid-layout';
 import { useHistory } from 'react-router-dom';
 
 import styles from './DashboardPage.module.scss';
+
+const ResponsiveGridLayout = WidthProvider(Responsive);
 
 const SUPPORTED_WEB_VITALS = ['CLS', 'FCP', 'FID', 'LCP', 'TTFB'] as const;
 
@@ -25,6 +31,7 @@ const DashboardPage = () => {
     const [dateRangeLength, setDateRangeLength] = useState<number>(
         timeFilter[2].value
     );
+    const [layout, setLayout] = useState<Layouts>(DEFAULT_WEB_VITALS_LAYOUT);
 
     useEffect(() => {
         const dashboard = dashboards.find((d) => d.id === parseInt(id, 10));
@@ -43,25 +50,102 @@ const DashboardPage = () => {
                     className={styles.dateRangePicker}
                 />
             </div>
-            <div className={styles.grid}>
-                {SUPPORTED_WEB_VITALS.map((webVital) => (
-                    <DashboardCard
-                        webVitalName={webVital}
-                        key={webVital}
-                        dateRange={{
-                            startDate: moment(new Date())
-                                .subtract(dateRangeLength, 'days')
-                                .startOf('day')
-                                .toISOString(),
-                            endDate: moment(new Date())
-                                .endOf('day')
-                                .toISOString(),
-                        }}
-                    />
-                ))}
+            <div className={styles.gridContainer}>
+                <ResponsiveGridLayout
+                    layouts={layout}
+                    cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
+                    breakpoints={{
+                        lg: 1200,
+                        md: 996,
+                        sm: 768,
+                        xs: 480,
+                        xxs: 0,
+                    }}
+                    containerPadding={[0, 0]}
+                    rowHeight={155}
+                    resizeHandles={['e']}
+                    onDragStop={(layout) => {
+                        setLayout({
+                            lg: layout,
+                        });
+                    }}
+                    onResizeStop={(layout) => {
+                        setLayout({
+                            lg: layout,
+                        });
+                    }}
+                >
+                    {SUPPORTED_WEB_VITALS.map((webVital, index) => (
+                        <div key={index.toString()}>
+                            <DashboardCard
+                                webVitalName={webVital}
+                                key={webVital}
+                                dateRange={{
+                                    startDate: moment(new Date())
+                                        .subtract(dateRangeLength, 'days')
+                                        .startOf('day')
+                                        .toISOString(),
+                                    endDate: moment(new Date())
+                                        .endOf('day')
+                                        .toISOString(),
+                                }}
+                            />
+                        </div>
+                    ))}
+                </ResponsiveGridLayout>
             </div>
         </>
     );
 };
 
 export default DashboardPage;
+
+const DEFAULT_WEB_VITALS_LAYOUT = {
+    lg: [
+        {
+            w: 6,
+            h: 2,
+            x: 0,
+            y: 0,
+            i: '0',
+            minW: 2,
+            static: false,
+        },
+        {
+            w: 6,
+            h: 2,
+            x: 6,
+            y: 0,
+            i: '1',
+            minW: 2,
+            static: false,
+        },
+        {
+            w: 6,
+            h: 2,
+            x: 0,
+            y: 2,
+            i: '2',
+            minW: 2,
+            static: false,
+        },
+        {
+            w: 6,
+            h: 2,
+            x: 6,
+            y: 2,
+            i: '3',
+            minW: 2,
+            static: false,
+        },
+        {
+            w: 6,
+            h: 2,
+            x: 0,
+            y: 4,
+            i: '4',
+            minW: 2,
+            static: false,
+        },
+    ],
+};
