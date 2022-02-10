50a5c2c5bf2bcb12c9c176b86a6b311ccca107ec
Zane Mayberry <mayberryzane@gmail.com>
2022-02-10 16:48:33 -0500

    add migration script

---
+++ scripts/migrations/email_fields.sql
@@ -0,0 +1,36 @@
+drop table if exists temp_email_identifiers;
+
+create temp table temp_email_identifiers
+as select created_at, updated_at, value, project_id, id
+from fields a
+where a.type = 'user'
+and a.name = 'identifier'
+and a.value ilike '%@%.%'
+and not exists (
+	select 1
+	from fields b
+	where b.type = 'user'
+	and b.name = 'email'
+	and b.value = a.value
+);
+
+insert into fields
+(created_at, updated_at, type, name, value, project_id)
+select NOW(), NOW(), 'user', 'email', value, project_id
+from temp_email_identifiers;
+
+insert into session_fields
+(field_id, session_id)
+select email_id as field_id, session_id from session_fields sf
+inner join
+(
+	select a.id as identifier_id, b.id as email_id
+	from fields a
+	inner join fields b
+	on a.value = b.value
+	where a.type = 'user'
+	and a.name = 'identifier'
+	and b.type = 'user'
+	and b.name = 'email') c
+on sf.field_id = c.identifier_id
+on conflict do nothing;
