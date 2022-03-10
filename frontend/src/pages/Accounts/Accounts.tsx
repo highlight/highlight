8bf844228c458eb7caf3ed52b6ebef9eed302801
Jay Khatri <khatri.florida@gmail.com>
2022-03-10 14:58:05 -0700

    More edits to the account view (#2362)
    * progress
    
    * change
    
    * changes

--- frontend/src/pages/Accounts/Accounts.tsx
+++ frontend/src/pages/Accounts/Accounts.tsx
@@ -2,43 +2,73 @@ import {
     AppLoadingState,
     useAppLoadingContext,
 } from '@context/AppLoadingContext';
+import { useParams } from '@util/react-router/useParams';
+import { Table } from 'antd';
 import React, { useEffect } from 'react';
+import { Route, Switch, useHistory } from 'react-router-dom';
 
 import { useGetAccountsQuery } from '../../graph/generated/hooks';
 
-export const Accounts = () => {
+export const AccountsPage = () => {
     const { setLoadingState } = useAppLoadingContext();
-    const { data: accountData, loading } = useGetAccountsQuery({
-        fetchPolicy: 'network-only',
-    });
 
     useEffect(() => {
         setLoadingState(AppLoadingState.LOADED);
     }, [setLoadingState]);
 
     return (
-        <div>
+        <Switch>
+            <Route path="/accounts/:account_id">
+                <Account />
+            </Route>
+            <Route path="/accounts">
+                <Accounts />
+            </Route>
+        </Switch>
+    );
+};
+
+export const Account = () => {
+    const { account_id } = useParams<{ account_id: string }>();
+    return <div>{account_id}</div>;
+};
+
+export const Accounts = () => {
+    const history = useHistory();
+    const { data: accountData, loading } = useGetAccountsQuery({
+        fetchPolicy: 'network-only',
+    });
+
+    return (
+        <div style={{ padding: 50 }}>
             {loading ? (
                 'loading...'
             ) : (
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>ID</th>
-                            <th>Last Month Count</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {accountData?.accounts?.map((a, i) => (
-                            <tr key={i}>
-                                <td>{a?.name}</td>
-                                <td>{a?.id}</td>
-                                <td>{a?.session_count}</td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
+                <Table
+                    onRow={(record) => {
+                        return {
+                            onClick: () => {
+                                history.push(`/accounts/${record.id}`);
+                            },
+                        };
+                    }}
+                    columns={[
+                        { title: 'Name', dataIndex: 'name' },
+                        { title: 'ID', dataIndex: 'id' },
+                        {
+                            title: 'Last Month Count',
+                            dataIndex: 'session_count',
+                        },
+                    ]}
+                    dataSource={accountData?.accounts?.map((a, i) => {
+                        return {
+                            key: i,
+                            name: a?.name,
+                            id: a?.id,
+                            session_count: a?.session_count,
+                        };
+                    })}
+                />
             )}
         </div>
     );
