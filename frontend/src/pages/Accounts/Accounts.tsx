7c33785c895b9e2a68d1762e4cf670bc3920a27c
jay-khatri <khatri.florida@gmail.com>
2022-03-10 10:56:03 -0700

    change

--- frontend/src/pages/Accounts/Accounts.tsx
+++ frontend/src/pages/Accounts/Accounts.tsx
@@ -1,37 +1,46 @@
-import { useAppLoadingContext } from '@context/AppLoadingContext';
+import {
+    AppLoadingState,
+    useAppLoadingContext,
+} from '@context/AppLoadingContext';
 import React, { useEffect } from 'react';
 
 import { useGetAccountsQuery } from '../../graph/generated/hooks';
 
 export const Accounts = () => {
-    const { setIsLoading } = useAppLoadingContext();
-    const { data: accountData } = useGetAccountsQuery({
+    const { setLoadingState } = useAppLoadingContext();
+    const { data: accountData, loading } = useGetAccountsQuery({
         fetchPolicy: 'network-only',
     });
 
     useEffect(() => {
-        setIsLoading(false);
-    }, [setIsLoading]);
+        setLoadingState(AppLoadingState.LOADED);
+    }, [setLoadingState]);
 
     return (
-        <table>
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>ID</th>
-                    <th>Last Month Count</th>
-                </tr>
-            </thead>
-            <tbody>
-                {accountData?.accounts?.map((a, i) => (
-                    <tr key={i}>
-                        <td>{a?.name}</td>
-                        <td>{a?.id}</td>
-                        <td>{a?.session_count}</td>
-                    </tr>
-                ))}
-            </tbody>
-        </table>
+        <div>
+            {loading ? (
+                'loading...'
+            ) : (
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>ID</th>
+                            <th>Last Month Count</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {accountData?.accounts?.map((a, i) => (
+                            <tr key={i}>
+                                <td>{a?.name}</td>
+                                <td>{a?.id}</td>
+                                <td>{a?.session_count}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            )}
+        </div>
     );
 };
 
