795b397df91634d9b68833de9316261816d160e0
Zane Mayberry <mayberryzane@gmail.com>
2022-04-11 15:43:20 -0700

    fix gate for loading chunks

--- frontend/src/pages/Player/PlayerHook/PlayerHook.tsx
+++ frontend/src/pages/Player/PlayerHook/PlayerHook.tsx
@@ -191,6 +191,7 @@ export const usePlayer = (): ReplayerContextInterface => {
 
     const { data: eventChunksData } = useGetEventChunksQuery({
         variables: { secure_id: session_secure_id },
+        skip: !CHUNKING_ENABLED_PROJECTS.includes(project_id),
     });
 
     const [
