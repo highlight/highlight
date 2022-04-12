a402b5870b420be61668de425e7e76b358d04a3e
Vadim Korolik <vkorolik@gmail.com>
2022-04-12 15:00:00 -0700

    [HIG-2160] track click events as session fields

--- client/src/listeners/click-listener/click-listener.ts
+++ client/src/listeners/click-listener/click-listener.ts
@@ -1,12 +1,12 @@
 import { getElementSelector } from '../../utils/dom';
 
-export const ClickListener = (callback: (targetSelector: string) => void) => {
+export const ClickListener = (callback: (targetSelector: string, event: MouseEvent) => void) => {
     const recordClick = (event: MouseEvent) => {
         if (event.target) {
             const element = event.target as Element;
             const targetSelector = getElementSelector(element);
 
-            callback(targetSelector);
+            callback(targetSelector, event);
         }
     };
     window.addEventListener('click', recordClick);
