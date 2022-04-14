8c7791bb675066ae9f41734e9b0c0729f7eaf92e
Vadim Korolik <vkorolik@gmail.com>
2022-04-14 00:47:18 -0700

    refactor consumer/producer into separate files
    fix producer count

--- backend/kafka-queue/kafkaqueue.go
+++ backend/kafka-queue/kafkaqueue.go
@@ -1,23 +1,14 @@
 package kafka_queue
 
 import (
-	"bytes"
-	"encoding/json"
-	"github.com/andybalholm/brotli"
 	"github.com/confluentinc/confluent-kafka-go/kafka"
-	"github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
-	"io"
-	"os"
 	"sync"
 	"time"
 )
 
-// LocalConsumerPrefetch Number of Messages to retrieve per worker (populating local consumerQueue)
-const LocalConsumerPrefetch = 1024
-
-// ConsumerWorkers Number of kafka Consumer workers.
-const ConsumerWorkers = 8
+// WorkerPrefetch Number of Messages to retrieve per worker (populating local consumerQueue)
+const WorkerPrefetch = 1024
 
 // KafkaOperationTimeoutMs How long to wait for Kafka operations before polling again.
 const KafkaOperationTimeoutMs = 15 * 1000
@@ -31,7 +22,6 @@ type Queue struct {
 	topic         string
 	producerQueue chan *PartitionMessage
 	consumerQueue chan *Message
-	workers       int
 	partitions    int
 	run           bool
 
@@ -54,51 +44,12 @@ type MessageQueue interface {
 	LogStats()
 }
 
-func MakeConsumer() (*kafka.Consumer, error) {
-	kafkaC, err := kafka.NewConsumer(&kafka.ConfigMap{
-		"sasl.mechanism":                  "SCRAM-SHA-512",
-		"security.protocol":               "sasl_ssl",
-		"bootstrap.servers":               os.Getenv("KAFKA_SERVERS"),
-		"sasl.username":                   os.Getenv("KAFKA_SASL_USERNAME"),
-		"sasl.password":                   os.Getenv("KAFKA_SASL_PASSWORD"),
-		"group.id":                        "group-default",
-		"auto.offset.reset":               "smallest",
-		"go.application.rebalance.enable": true,
-		"queued.min.messages":             LocalConsumerPrefetch * ConsumerWorkers,
-		"statistics.interval.ms":          5000,
-		"fetch.message.max.bytes":         prefetchSizeBytes,
-		"message.max.bytes":               messageSizeBytes,
-		"receive.message.max.bytes":       messageSizeBytes + 1*1024*1024,
-	})
-	return kafkaC, err
-}
-
-func MakeProducer() (*kafka.Producer, error) {
-	kafkaProducerID, err := os.Hostname()
-	if err != nil {
-		kafkaProducerID = "public-unknown"
-	}
-
-	kafkaP, err := kafka.NewProducer(&kafka.ConfigMap{
-		"sasl.mechanism":         "SCRAM-SHA-512",
-		"security.protocol":      "sasl_ssl",
-		"bootstrap.servers":      os.Getenv("KAFKA_SERVERS"),
-		"sasl.username":          os.Getenv("KAFKA_SASL_USERNAME"),
-		"sasl.password":          os.Getenv("KAFKA_SASL_PASSWORD"),
-		"client.id":              kafkaProducerID,
-		"message.max.bytes":      messageSizeBytes,
-		"queue.buffering.max.ms": 100,
-		"acks":                   1})
-	return kafkaP, err
-}
-
 func New(topic string, producer *kafka.Producer, consumer *kafka.Consumer) *Queue {
 	pool := &Queue{
-		topic:   topic,
-		workers: ConsumerWorkers,
-		run:     true,
-		kafkaP:  producer,
-		kafkaC:  consumer,
+		topic:  topic,
+		run:    true,
+		kafkaP: producer,
+		kafkaC: consumer,
 	}
 
 	var metadata *kafka.Metadata
@@ -123,10 +74,10 @@ func New(topic string, producer *kafka.Producer, consumer *kafka.Consumer) *Queu
 	log.Infof("established kafka topic %s with %d partitions", pool.topic, pool.partitions)
 
 	if consumer != nil {
-		pool.consumerQueue = make(chan *Message, pool.workers*LocalConsumerPrefetch)
-		pool.consumerWg.Add(pool.workers)
-		pool.consumers = make([]queueConsumer, pool.workers)
-		for i := 0; i < pool.workers; i++ {
+		pool.consumerQueue = make(chan *Message, ConsumerWorkers*WorkerPrefetch)
+		pool.consumerWg.Add(ConsumerWorkers)
+		pool.consumers = make([]queueConsumer, ConsumerWorkers)
+		for i := 0; i < ConsumerWorkers; i++ {
 			pool.consumers[i] = queueConsumer{
 				run:           &pool.run,
 				kafkaC:        pool.kafkaC,
@@ -137,10 +88,10 @@ func New(topic string, producer *kafka.Producer, consumer *kafka.Consumer) *Queu
 		}
 	}
 	if producer != nil {
-		pool.producerQueue = make(chan *PartitionMessage, pool.workers*LocalConsumerPrefetch)
-		pool.producerWg.Add(pool.workers)
-		pool.producers = make([]queueProducer, pool.workers)
-		for i := 0; i < pool.workers; i++ {
+		pool.producerQueue = make(chan *PartitionMessage, ProducerWorkers*WorkerPrefetch)
+		pool.producerWg.Add(ProducerWorkers)
+		pool.producers = make([]queueProducer, ProducerWorkers)
+		for i := 0; i < ProducerWorkers; i++ {
 			pool.producers[i] = queueProducer{
 				topic:         pool.topic,
 				kafkaP:        pool.kafkaP,
@@ -223,171 +174,4 @@ func (p *Queue) LogStats() {
 		}
 		log.Infof("Kafka Consumer Stats: %d consumed, %d received, %s consumer blocked, %.2f%% of loop", numConsumed, p.numReceived, consumerQueueBlocked, blockPercent)
 	}
-}
-
-func (p *Queue) rebalance(c *kafka.Consumer, event kafka.Event) (err error) {
-	switch e := event.(type) {
-	case kafka.AssignedPartitions:
-		log.Infof("Assigned Partition %v", e)
-		err = c.Assign(e.Partitions)
-	case kafka.RevokedPartitions:
-		log.Infof("Revoked Partition %v", e)
-		err = c.Unassign()
-	case nil:
-	default:
-		log.Infof("Ignored %v", e)
-	}
-
-	if err != nil {
-		log.Errorf("Kafka consumer encountered error on rebalance %v", err)
-	}
-	return
-}
-
-type queueProducer struct {
-	kafkaP *kafka.Producer
-	topic  string
-	run    *bool
-
-	buff     bytes.Buffer
-	brWriter brotli.Writer
-
-	producerQueue chan *PartitionMessage
-
-	numDelivered int
-	producerWg   *sync.WaitGroup
-}
-
-func (p *queueProducer) runProducer() {
-	go func() {
-		for e := range p.kafkaP.Events() {
-			if !*p.run && p.kafkaP.Len() == 0 {
-				break
-			}
-			switch ev := e.(type) {
-			case *kafka.Message:
-				if ev.TopicPartition.Error != nil {
-					log.Errorf("Failed to deliver message: %v", ev.TopicPartition)
-				} else {
-					p.numDelivered += 1
-				}
-			}
-		}
-	}()
-	for {
-		partitionMessage := <-p.producerQueue
-		// channel is closed via the Stop() operation
-		if partitionMessage == nil {
-			break
-		}
-		msg, err := p.serializeMessage(partitionMessage.Message)
-		if err != nil {
-			log.Errorf("an error occured while adding message to producer queue %+v", err)
-		}
-		err = p.kafkaP.Produce(&kafka.Message{
-			TopicPartition: kafka.TopicPartition{Topic: &p.topic, Partition: partitionMessage.Partition},
-			Value:          []byte(msg)}, nil,
-		)
-		if err != nil {
-			log.Errorf("an error occured while adding message to producer queue %+v", err)
-		}
-	}
-	p.producerWg.Done()
-}
-
-func (p *queueProducer) serializeMessage(msg *Message) (compressed []byte, err error) {
-	b, err := json.Marshal(&msg)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to marshall json")
-	}
-
-	p.buff.Reset()
-	p.brWriter.Reset(&p.buff)
-	if _, err = p.brWriter.Write(b); err != nil {
-		return nil, errors.Wrap(err, "failed to write to brotli writer")
-	}
-	if err = p.brWriter.Flush(); err != nil {
-		return nil, errors.Wrap(err, "failed to flush brotli writer")
-	}
-
-	return p.buff.Bytes(), nil
-}
-
-type queueConsumer struct {
-	run    *bool
-	kafkaC *kafka.Consumer
-
-	buff     bytes.Buffer
-	brReader brotli.Reader
-
-	consumerQueue chan *Message
-
-	numConsumed          int
-	consumerQueueBlocked time.Duration
-	consumerLoopTime     time.Duration
-
-	consumerWg *sync.WaitGroup
-}
-
-func (p *queueConsumer) runConsumer() {
-	for *p.run {
-		loopStart := time.Now()
-		var err error
-		ev := p.kafkaC.Poll(KafkaOperationTimeoutMs)
-		switch e := ev.(type) {
-		case kafka.AssignedPartitions:
-			log.Infof("Assigned Partition %v", e)
-			err = p.kafkaC.Assign(e.Partitions)
-		case kafka.RevokedPartitions:
-			log.Infof("Revoked Partition %v", e)
-			err = p.kafkaC.Unassign()
-		case *kafka.Message:
-			var msg *Message
-			msg, err = p.deserializeMessage(e.Value)
-			if err != nil {
-				log.Errorf("error deserializing msg %v", err)
-				continue
-			}
-			start := time.Now()
-			p.consumerQueue <- msg
-			p.numConsumed += 1
-			p.consumerQueueBlocked += time.Since(start)
-		case kafka.PartitionEOF:
-			log.Infof("Reached %v", e)
-		case kafka.Error:
-			log.Errorf("Error: %v", e)
-			time.Sleep(time.Second)
-		case *kafka.Stats:
-			var stats map[string]interface{}
-			_ = json.Unmarshal([]byte(e.String()), &stats)
-			log.Infof("Kafka Consumer librdkafka Stats: rxmsgs %+v, rxmsg_bytes %+v, replyq %+v", stats["rxmsgs"], stats["rxmsg_bytes"], stats["replyq"])
-		}
-		if err != nil {
-			log.Errorf("Kafka consumer encountered error %v", err)
-			err = nil
-		}
-		p.consumerLoopTime += time.Since(loopStart)
-	}
-	p.consumerWg.Done()
-}
-
-func (p *queueConsumer) deserializeMessage(compressed []byte) (*Message, error) {
-	p.buff.Reset()
-	_ = p.brReader.Reset(&p.buff)
-
-	if _, err := p.buff.Write(compressed); err != nil {
-		return nil, errors.Wrap(err, "failed to write compressed buffer")
-	}
-
-	out := new(bytes.Buffer)
-	if _, err := io.Copy(out, &p.brReader); err != nil {
-		return nil, errors.Wrap(err, "failed to read decompressed from reader")
-	}
-
-	var msg Message
-	if err := json.Unmarshal(out.Bytes(), &msg); err != nil {
-		return nil, errors.Wrap(err, "failed to unmarshall decompressed msg")
-	}
-
-	return &msg, nil
 }
