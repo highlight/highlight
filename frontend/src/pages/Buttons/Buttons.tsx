cec9ee04f1be0ecda3b1dbc530c23e2a7b6632e0
Zane Mayberry <mayberryzane@gmail.com>
2022-04-21 16:16:04 -0700

    catch abort

--- frontend/src/pages/Buttons/Buttons.tsx
+++ frontend/src/pages/Buttons/Buttons.tsx
@@ -150,7 +150,9 @@ export const Buttons = () => {
                             {
                                 signal: controller.signal,
                             }
-                        );
+                        ).catch(() => {
+                            console.log('abort caught!');
+                        });
                     }}
                 >
                     Abort!
