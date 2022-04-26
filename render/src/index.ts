1d144d15e7da5bfc769ac42d4e047f4a2766aa62
Vadim Korolik <Vadman97@users.noreply.github.com>
2022-04-25 21:56:50 -0700

    [HIG-2197] backend screenshot rendering (#2499)
    Creates a new lambda function (IAM protected https://zbty37wu02.execute-api.us-east-2.amazonaws.com/default/session-screenshots?project=1&session=33618264&ts=1000)
    which can be called for a `project` id, `session` id, and timestamp `ts` to render a single screenshot.
    Currently only exposes the single-screenshot functionality though there is
    code to generate a set of screenshots in parallel.

---
+++ render/src/index.ts
@@ -0,0 +1,37 @@
+import { APIGatewayEvent } from 'aws-lambda';
+import { serialRender } from './serial';
+import { readFileSync } from 'fs';
+
+interface Args {
+    project?: string;
+    session?: string;
+    ts?: string;
+}
+
+export const handler = async (event?: APIGatewayEvent) => {
+    const args = (event?.queryStringParameters as unknown) as Args | undefined;
+    const files = await serialRender(
+        Number(args?.project),
+        Number(args?.session),
+        Number(args?.ts)
+    );
+    const file = readFileSync(files[0]);
+    return {
+        statusCode: 200,
+        isBase64Encoded: true,
+        body: new Buffer(file).toString('base64'),
+        headers: {
+            'content-type': 'image/png',
+        },
+    };
+};
+
+if (process.env.DEV?.length) {
+    handler(({
+        queryStringParameters: {
+            project: '1',
+            session: '33618264',
+            ts: '1000',
+        },
+    } as unknown) as APIGatewayEvent).then(console.info);
+}
---
+++ render/src/render.ts
@@ -0,0 +1,121 @@
+import { mkdtemp, readFileSync } from 'fs';
+import { promisify } from 'util';
+import path from 'path';
+import { tmpdir } from 'os';
+import chromium from 'chrome-aws-lambda';
+
+const getHtml = (): string => {
+    return `<html lang="en"><head><title></title><style>
+.rrwebPlayerWrapper {
+    align-items: center;
+    box-sizing: border-box;
+    display: flex;
+    flex-grow: 1;
+    justify-content: center;
+    position: relative;
+    width: 1920px;
+    height: 1080px;
+}
+.rrwebPlayerDiv {
+    height: 100%;
+    position: relative;
+}
+</style></head><body style="padding: 0; margin: 0">
+  <div id="player" class="rrwebPlayerWrapper">
+    <div id="player" class="rrwebPlayerDiv"></div>
+  </div>
+</body></html>
+    `;
+};
+
+export async function render(
+    events: string,
+    worker: number,
+    workers: number,
+    fps?: number,
+    ts?: number,
+    dir?: string
+) {
+    if (!ts && !fps) {
+        throw new Error('timestamp or fps must be provided');
+    }
+    events = events.replace(/\\/g, '\\\\');
+    if (!dir?.length) {
+        const prefix = path.join(tmpdir(), 'render_');
+        dir = await promisify(mkdtemp)(prefix);
+    }
+
+    const browser = await chromium.puppeteer.launch({
+        headless: chromium.headless,
+        ignoreHTTPSErrors: true,
+        defaultViewport: {
+            width: 1920,
+            height: 1080,
+        },
+        args: chromium.args,
+        executablePath: await chromium.executablePath,
+    });
+
+    const page = await browser.newPage();
+    await page.goto('about:blank');
+    await page.setContent(getHtml());
+
+    const jsPath = path.join(
+        path.dirname(__dirname),
+        'node_modules',
+        '@highlight-run',
+        'rrweb',
+        'dist',
+        'rrweb.js'
+    );
+    const js = readFileSync(jsPath, 'utf8');
+    await page.evaluate(js);
+    await page.evaluate(
+        `
+        const playerMountingRoot = document.getElementById(
+            'player'
+        );
+        const events = JSON.parse(` +
+            '`' +
+            events +
+            '`' +
+            `);
+        const r = new rrweb.Replayer(events, {
+            root: playerMountingRoot,
+            triggerFocus: true,
+            mouseTail: false,
+            UNSAFE_replayCanvas: true,
+            liveMode: true,
+        });
+        r.pause(0)
+        meta = r.getMetaData()
+        loaded = true
+    `
+    );
+    await page.waitForFunction('loaded');
+    const meta = (await page.evaluate('meta')) as {
+        startTime: number;
+        endTime: number;
+        totalTime: number;
+    };
+
+    let interval = 1000;
+    let start = ts || meta.startTime;
+    let end = ts || meta.endTime;
+    if (fps) {
+        interval = Math.round(1000 / fps);
+        start = Math.floor((meta.totalTime / workers) * worker);
+        end = Math.floor((meta.totalTime / workers) * (worker + 1));
+    }
+
+    const files: string[] = [];
+    for (let i = start; i <= end; i += interval) {
+        const file = path.join(dir, `${i}.png`);
+        await page.evaluate(`r.pause(${i})`);
+        await page.screenshot({ path: file });
+        files.push(file);
+    }
+
+    await browser.close();
+    return files;
+}
