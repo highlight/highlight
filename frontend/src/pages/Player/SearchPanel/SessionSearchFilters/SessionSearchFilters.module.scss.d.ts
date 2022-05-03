c97db7457463916bd5ffbf8adc4e76b24e7d524e
Richard Hua <rich@rkhua.com>
2022-05-03 14:14:50 -0700

    Remove now-unused sessionssearch

--- frontend/src/pages/Player/SearchPanel/SessionSearchFilters/SessionSearchFilters.module.scss.d.ts
+++
@@ -1,7 +0,0 @@
-export const clearFilters: string;
-export const clearFiltersContainer: string;
-export const contentContainer: string;
-export const groupContainer: string;
-export const hasFilters: string;
-export const inputContainer: string;
-export const popover: string;
