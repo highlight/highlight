7227e678bc7084e16170dcf2258f90acaf420a91
Vadim Korolik <vkorolik@gmail.com>
2022-05-12 11:53:52 -0700

    [HIG-2273] fix opensearch pagination limit
    Show a tooltip on the pagination component if we exceed opensearch
    offset limits.

--- frontend/src/components/Pagination/Pagination.tsx
+++ frontend/src/components/Pagination/Pagination.tsx
@@ -1,5 +1,6 @@
 import Button from '@components/Button/Button/Button';
 import styles from '@components/Pagination/Pagination.module.scss';
+import Tooltip from '@components/Tooltip/Tooltip';
 import SvgFastForwardIcon from '@icons/FastForwardIcon';
 import SvgRewindIcon from '@icons/RewindIcon';
 import { Pagination as AntdPagination } from 'antd';
@@ -7,6 +8,8 @@ import React from 'react';
 
 export const PAGE_SIZE = 10;
 export const STARTING_PAGE = 1;
+const OPENSEARCH_MAX_RESULTS = 10000;
+const MAX_PAGES = Math.floor(OPENSEARCH_MAX_RESULTS / PAGE_SIZE) - 1;
 
 enum PageDirection {
     Forward,
@@ -22,9 +25,10 @@ export const Pagination = ({
     setPage: React.Dispatch<React.SetStateAction<number | undefined>>;
     totalPages: React.MutableRefObject<number>;
 }) => {
+    const maxPage = Math.min(MAX_PAGES, totalPages.current);
     const changePage = (dir: PageDirection) => {
         if (dir === PageDirection.Forward) {
-            if ((page || STARTING_PAGE) < totalPages.current) {
+            if ((page || STARTING_PAGE) < maxPage) {
                 setPage((p) => (p || STARTING_PAGE) + 1);
             }
         } else if (dir === PageDirection.Backward) {
@@ -33,20 +37,31 @@ export const Pagination = ({
             }
         }
     };
-    if (!totalPages.current) return null;
+    if (!maxPage) return null;
     return (
         <>
             <div className={styles.pageButtonsRow}>
-                <div className={styles.container}>
-                    <AntdPagination
-                        showSizeChanger={false}
-                        size={'small'}
-                        pageSize={PAGE_SIZE}
-                        total={PAGE_SIZE * (totalPages.current + 1)}
-                        current={page || STARTING_PAGE}
-                        onChange={setPage}
-                    />
-                </div>
+                <Tooltip
+                    mouseEnterDelay={0.3}
+                    placement={'top'}
+                    align={{ offset: [76, 0] }}
+                    title={
+                        (page || STARTING_PAGE) >= MAX_PAGES
+                            ? 'There are more pages than we can load. Please narrow the search query.'
+                            : ''
+                    }
+                >
+                    <div className={styles.container}>
+                        <AntdPagination
+                            showSizeChanger={false}
+                            size={'small'}
+                            pageSize={PAGE_SIZE}
+                            total={PAGE_SIZE * (maxPage + 1)}
+                            current={page || STARTING_PAGE}
+                            onChange={setPage}
+                        />
+                    </div>
+                </Tooltip>
                 <div className={styles.pageButtonsContainer} hidden>
                     <Button
                         className={styles.btn}
@@ -60,7 +75,7 @@ export const Pagination = ({
                     </Button>
                     <Button
                         className={styles.btn}
-                        disabled={(page || STARTING_PAGE) >= totalPages.current}
+                        disabled={(page || STARTING_PAGE) >= maxPage}
                         trackingId={'SessionsFeedNextPage'}
                         onClick={() => {
                             changePage(PageDirection.Forward);
