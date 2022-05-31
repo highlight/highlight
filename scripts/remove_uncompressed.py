2f1c51cada082b36f83947be32b16233adee0b53
Vadim Korolik <vkorolik@gmail.com>
2022-05-31 10:06:39 -0700

    cleanup script

--- scripts/remove_uncompressed.py
+++ scripts/remove_uncompressed.py
@@ -1,25 +1,24 @@
+import argparse
 import os
 
 import boto3
 
-BUCKET = 'highlight-session-s3-test'
-PREFIX = '1/'
-
 HIGHLIGHT_FILES = {'session-contents', 'console-messages', 'network-resources'}
 
 s3 = boto3.Session().resource('s3')
-bucket = s3.Bucket(BUCKET)
 
 
-def process():
+def process(bucket, prefix, do_archive=False):
+    b = s3.Bucket(bucket)
     last = {'project': 0, 'session': 0}
     has_compressed = {k: False for k in HIGHLIGHT_FILES}
+    to_archive = []
 
     def process_if_compressed():
         if all(has_compressed.values()):
-            process_uncompressed(**last)
+            process_uncompressed(b, to_archive, **last, do_archive=do_archive)
 
-    for idx, f in enumerate(bucket.objects.filter(Prefix=PREFIX)):
+    for idx, f in enumerate(b.objects.filter(Prefix=prefix)):
         try:
             p, s, obj = str(f.key).split(os.path.sep)
         except ValueError:
@@ -30,17 +29,20 @@ def process():
             process_if_compressed()
             last = {'project': p, 'session': s}
             has_compressed = {k: False for k in HIGHLIGHT_FILES}
+            to_archive = []
 
         for k in HIGHLIGHT_FILES:
             if f'{k}-compressed' == obj:
                 has_compressed[k] = True
                 break
+        else:
+            to_archive.append(f)
 
     process_if_compressed()
 
 
-def process_uncompressed(project=0, session=0):
-    for f in bucket.objects.filter(Prefix='/'.join(map(str, [project, session]))):
+def process_uncompressed(b, files_to_archive, project=0, session=0, do_archive=False):
+    for f in files_to_archive:
         if 'compressed' in f.key:
             continue
         if f.storage_class == 'DEEP_ARCHIVE':
@@ -50,16 +52,27 @@ def process_uncompressed(project=0, session=0):
             # r = f.restore_object(Bucket=BUCKET, Key=f.key, RestoreRequest={'Days': 1})
             # print(project, session, f, 'already', f.storage_class, 'RESTORED', r)
             continue
+
+        if not do_archive:
+            print('dry run, not actually archiving', project, session, f)
+            continue
         print('ARCHIVING', project, session, f)
-        bucket.copy(
-            {'Bucket': BUCKET, 'Key': f.key}, f.key,
+        b.copy(
+            {'Bucket': b.name, 'Key': f.key}, f.key,
             ExtraArgs={
                 'StorageClass': 'DEEP_ARCHIVE',
                 'MetadataDirective': 'COPY'
             }
         )
-    exit(0)
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Description of your program')
+    parser.add_argument('-b', '--bucket', help='the s3 bucket to process', default='highlight-session-s3-test')
+    parser.add_argument('-p', '--prefix', help='the bucket prefix to process', default='1/')
+    parser.add_argument('--do-archive', help='confirm archive files', default=False, action='store_true')
+    process(**vars(parser.parse_args()))
 
 
 if __name__ == '__main__':
-    process()
+    main()
