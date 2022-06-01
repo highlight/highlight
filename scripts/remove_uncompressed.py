191b34908aae0b2a3d38422f4479fb3be660acf6
Vadim Korolik <vkorolik@gmail.com>
2022-06-01 12:53:43 -0700

    add s3 uncompressed object archival script (#2653)
    take uncompressed objects and migrate them to glacier deep archive storage tier for sessions that have
    compressed objects for all file types.
    
    the script is multiprocessed to allow one process to find sessions with uncompressed and compressed files (to archive the uncompressed ones) and feeds these to a pool of subprocesses workers.
    
    we would run this on highlight sessions only to start. the glacier `DEEP_ARCHIVE` storage class is 5% of the standard storage price, and we can later delete the archived uncompressed data. glacier archived files have to be unarchived to make them accessible again and takes up to 12 hours.

---
+++ scripts/remove_uncompressed.py
@@ -0,0 +1,88 @@
+import argparse
+import multiprocessing.pool
+import os
+import threading
+
+import boto3
+
+ARCHIVE_STORAGE_CLASS = 'DEEP_ARCHIVE'
+HIGHLIGHT_FILES = {'session-contents', 'console-messages', 'network-resources'}
+
+
+def init_bucket(bucket):
+    s3 = boto3.Session().resource('s3')
+    return s3.Bucket(bucket)
+
+
+def process(bucket, prefix, do_archive=False):
+    pool = multiprocessing.pool.Pool()
+    b = init_bucket(bucket)
+    last = {'project': '0', 'session': '0'}
+    has_compressed = {k: False for k in HIGHLIGHT_FILES}
+
+    def process_if_compressed():
+        if all(has_compressed.values()):
+            pool.apply_async(process_uncompressed, args=(bucket,), kwds={'do_archive': do_archive, **last})
+
+    for idx, f in enumerate(b.objects.filter(Prefix=prefix)):
+        try:
+            p, s, obj = str(f.key).split(os.path.sep)
+        except ValueError:
+            continue
+
+        if p != last['project'] or s != last['session']:
+            process_if_compressed()
+            last = {'project': p, 'session': s}
+            has_compressed = {k: False for k in HIGHLIGHT_FILES}
+
+        for k in HIGHLIGHT_FILES:
+            if f'{k}-compressed' == obj:
+                has_compressed[k] = True
+                break
+
+    process_if_compressed()
+    print('thread pool closed')
+    pool.close()
+    print('waiting for thread pool to finish...')
+    pool.join()
+    print('done!')
+
+
+def process_uncompressed(bucket, project='0', session='0', do_archive=False):
+    local = threading.local()
+    if not getattr(local, 'b', None):
+        local.b = init_bucket(bucket)
+    for f in local.b.objects.filter(Prefix='/'.join(map(str, [project, session]))):
+        if 'compressed' in f.key:
+            continue
+        if f.storage_class == ARCHIVE_STORAGE_CLASS:
+            print(project, session, f, 'already', ARCHIVE_STORAGE_CLASS, 'skipping')
+
+            # In case the object needs to be restored:
+            # r = f.restore_object(Bucket=BUCKET, Key=f.key, RestoreRequest={'Days': 1})
+            # print(project, session, f, 'already', f.storage_class, 'RESTORED', r)
+            continue
+
+        if not do_archive:
+            print('dry run, not actually archiving', project, session, f)
+            continue
+        print('ARCHIVING', project, session, f)
+        local.b.copy(
+            {'Bucket': local.b.name, 'Key': f.key}, f.key,
+            ExtraArgs={
+                'StorageClass': ARCHIVE_STORAGE_CLASS,
+                'MetadataDirective': 'COPY'
+            }
+        )
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Description of your program')
+    parser.add_argument('-b', '--bucket', help='the s3 bucket to process', default='highlight-session-s3-test')
+    parser.add_argument('-p', '--prefix', help='the bucket prefix to process', default='1/')
+    parser.add_argument('--do-archive', help='confirm archive files', default=False, action='store_true')
+    process(**vars(parser.parse_args()))
+
+
+if __name__ == '__main__':
+    main()
---
+++ scripts/test_remove_uncompressed.py
@@ -0,0 +1,42 @@
+from unittest import mock
+from unittest.mock import call
+
+import pytest
+
+from remove_uncompressed import process, HIGHLIGHT_FILES, process_uncompressed
+
+
+def create_file(project, session, k):
+    f = mock.MagicMock()
+    f.key = f'{project}/{session}/{k}'
+    return f
+
+
+@pytest.mark.parametrize('session_compressed', [False, True])
+@pytest.mark.parametrize('console_compressed', [False, True])
+@pytest.mark.parametrize('network_compressed', [False, True])
+def test(mocker, session_compressed, console_compressed, network_compressed):
+    files = set(HIGHLIGHT_FILES)
+    if session_compressed:
+        files.add('session-contents-compressed')
+    if console_compressed:
+        files.add('console-messages-compressed')
+    if network_compressed:
+        files.add('network-resources-compressed')
+
+    mock_pool = mocker.patch('remove_uncompressed.multiprocessing.pool.Pool')
+    p = mock_pool.return_value.apply_async
+    mocker.patch('remove_uncompressed.boto3')
+    create_bucket = mocker.patch('remove_uncompressed.init_bucket')
+    create_bucket.return_value.objects.filter.return_value = [
+        create_file(p, s, k)
+        for p in range(1, 10) for s in range(1, 10) for k in files
+    ]
+    process('mock-bucket', '1/')
+    if session_compressed and console_compressed and network_compressed:
+        p.assert_has_calls(calls=[
+            call(process_uncompressed, args=('mock-bucket',), kwds={'project': str(p), 'session': str(s), 'do_archive': False})
+            for p in range(1, 10) for s in range(1, 10)
+        ])
+    else:
+        p.assert_not_called()
