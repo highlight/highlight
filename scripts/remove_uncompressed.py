4b580669456c25b8def2a9ef185a528bfb61b9a4
Vadim Korolik <vkorolik@gmail.com>
2022-06-02 14:09:57 -0700

    tuning

--- scripts/remove_uncompressed.py
+++ scripts/remove_uncompressed.py
@@ -13,8 +13,8 @@ from typing import List
 import boto3
 import brotli
 
-WORKER_PREFETCH = 16
-WORKERS = cpu_count() * 2
+WORKER_PREFETCH = 4
+WORKERS = cpu_count() * 4
 MAX_TASKS_WAITING = WORKERS * WORKER_PREFETCH
 
 ARCHIVE_STORAGE_CLASS = 'DEEP_ARCHIVE'
@@ -45,7 +45,7 @@ def init_bucket(bucket):
 def process(bucket, prefix, do_compress=False, do_archive=False, debug=False):
     pool = None
     if not debug:
-        pool = BoundedPool(processes=WORKERS, maxtasksperchild=WORKER_PREFETCH)
+        pool = BoundedPool(processes=WORKERS)
 
     b = init_bucket(bucket)
     last = {'project': '0', 'session': '0'}
