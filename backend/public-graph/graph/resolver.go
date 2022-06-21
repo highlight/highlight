02a09bba9ac3ea8ee9a905cb02172e24292f2d6f
Vadim Korolik <vkorolik@gmail.com>
2022-06-20 22:08:33 -0700

    [HIG-2371] kafkaify markBackendSetup (#2690)
    * kafkaify markBackendSetup
    
    though we don't need kafka for this public graph with regards to db performance (not many calls to this graph at all),
    we need it to ensure correct ordering between
    session creation and backend setup so that we
    have the session in the db when this graph
    is called.
    
    * retry failed kafka tasks
    
    allow defining a retry count of our worker tasks to requeue tasks that error.

--- backend/public-graph/graph/resolver.go
+++ backend/public-graph/graph/resolver.go
@@ -1101,6 +1101,19 @@ func (r *Resolver) InitializeSessionImplementation(sessionID int, ip string) (*m
 	return session, nil
 }
 
+func (r *Resolver) MarkBackendSetupImpl(projectID int) error {
+	var backendSetupCount int64
+	if err := r.DB.Model(&model.Project{}).Where("id = ? AND backend_setup=true", projectID).Count(&backendSetupCount).Error; err != nil {
+		return e.Wrap(err, "error querying backend_setup flag")
+	}
+	if backendSetupCount < 1 {
+		if err := r.DB.Model(&model.Project{}).Where("id = ?", projectID).Updates(&model.Project{BackendSetup: &model.T}).Error; err != nil {
+			return e.Wrap(err, "error updating backend_setup flag")
+		}
+	}
+	return nil
+}
+
 func (r *Resolver) IdentifySessionImpl(_ context.Context, sessionID int, userIdentifier string, userObject interface{}) error {
 	obj, ok := userObject.(map[string]interface{})
 	if !ok {
@@ -1695,7 +1708,7 @@ func (r *Resolver) ProcessBackendPayloadImpl(ctx context.Context, sessionSecureI
 	}
 }
 
-func (r *Resolver) ProcessPayload(ctx context.Context, sessionID int, events customModels.ReplayEventsInput, messages string, resources string, errors []*customModels.ErrorObjectInput, isBeacon bool, hasSessionUnloaded bool, highlightLogs *string) {
+func (r *Resolver) ProcessPayload(ctx context.Context, sessionID int, events customModels.ReplayEventsInput, messages string, resources string, errors []*customModels.ErrorObjectInput, isBeacon bool, hasSessionUnloaded bool, highlightLogs *string) error {
 	querySessionSpan, _ := tracer.StartSpanFromContext(ctx, "public-graph.pushPayload", tracer.ResourceName("db.querySession"))
 	querySessionSpan.SetTag("sessionID", sessionID)
 	querySessionSpan.SetTag("messagesLength", len(messages))
@@ -1714,8 +1727,7 @@ func (r *Resolver) ProcessPayload(ctx context.Context, sessionID int, events cus
 	if err := r.DB.Where(&model.Session{Model: model.Model{ID: sessionID}}).First(&sessionObj).Error; err != nil {
 		retErr := e.Wrapf(err, "error reading from session %v", sessionID)
 		querySessionSpan.Finish(tracer.WithError(retErr))
-		log.Error(retErr)
-		return
+		return retErr
 	}
 	querySessionSpan.SetTag("project_id", sessionObj.ProjectID)
 	querySessionSpan.Finish()
@@ -1955,8 +1967,7 @@ func (r *Resolver) ProcessPayload(ctx context.Context, sessionID int, events cus
 	})
 
 	if err := g.Wait(); err != nil {
-		log.Error(err)
-		return
+		return err
 	}
 
 	now := time.Now()
@@ -1987,14 +1998,14 @@ func (r *Resolver) ProcessPayload(ctx context.Context, sessionID int, events cus
 			Select("PayloadUpdatedAt", "BeaconTime", "HasUnloaded", "Processed", "ObjectStorageEnabled", "Excluded", "HasErrors").
 			Updates(&fieldsToUpdate).Error; err != nil {
 			log.Error(e.Wrap(err, "error updating session payload time and beacon time with errors"))
-			return
+			return err
 		}
 	} else {
 		if err := r.DB.Model(&model.Session{Model: model.Model{ID: sessionID}}).
 			Select("PayloadUpdatedAt", "BeaconTime", "HasUnloaded", "Processed", "ObjectStorageEnabled", "Excluded").
 			Updates(&fieldsToUpdate).Error; err != nil {
 			log.Error(e.Wrap(err, "error updating session payload time and beacon time"))
-			return
+			return err
 		}
 	}
 
@@ -2007,7 +2018,7 @@ func (r *Resolver) ProcessPayload(ctx context.Context, sessionID int, events cus
 			"has_errors": sessionHasErrors,
 		}); err != nil {
 			log.Error(e.Wrap(err, "error updating session in opensearch"))
-			return
+			return err
 		}
 	}
 
@@ -2016,9 +2027,10 @@ func (r *Resolver) ProcessPayload(ctx context.Context, sessionID int, events cus
 			"has_errors": true,
 		}); err != nil {
 			log.Error(e.Wrap(err, "error setting has_errors on session in opensearch"))
-			return
+			return err
 		}
 	}
+	return nil
 }
 
 func (r *Resolver) submitFrontendNetworkMetric(ctx context.Context, sessionObj *model.Session, resources []NetworkResource) error {
