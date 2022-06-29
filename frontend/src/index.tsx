a0d473c8310abf1947cd86979be2cd45b56ba1de
Vadim Korolik <vkorolik@gmail.com>
2022-06-28 17:32:40 -0700

    remove accidental change

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -58,9 +58,6 @@ const options: HighlightOptions = {
         mixpanel: {
             projectToken: 'e70039b6a5b93e7c86b8afb02b6d2300',
         },
-        amplitude: {
-            apiKey: '',
-        },
     },
     sessionShortcut: 'alt+1,command+`,alt+esc',
 };
