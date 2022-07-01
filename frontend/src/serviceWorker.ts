f1bf2d59d2c5012605c9a0e63363f79a27976c1d
Vadim Korolik <vkorolik@gmail.com>
2022-07-01 14:34:50 -0700

    update frontend rrweb css fix frontend uri

--- frontend/src/serviceWorker.ts
+++ frontend/src/serviceWorker.ts
@@ -29,7 +29,7 @@ export function register(config?: Config) {
     if (process.env.NODE_ENV === 'prod' && 'serviceWorker' in navigator) {
         // The URL constructor is available in all browsers that support SW.
         const publicUrl = new URL(
-            process.env.REACT_APP_PUBLIC_URL || '',
+            process.env.REACT_APP_FRONTEND_URI || '',
             window.location.href
         );
         if (publicUrl.origin !== window.location.origin) {
@@ -40,7 +40,7 @@ export function register(config?: Config) {
         }
 
         window.addEventListener('load', () => {
-            const swUrl = `${process.env.REACT_APP_PUBLIC_URL}/service-worker.js`;
+            const swUrl = `${process.env.REACT_APP_FRONTEND_URI}/service-worker.js`;
 
             if (isLocalhost) {
                 // This is running on localhost. Let's check if a service worker still exists or not.
