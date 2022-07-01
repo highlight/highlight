71779ebcb0ddb086d39dc386b2d0474a51cd9fc1
Vadim Korolik <vkorolik@gmail.com>
2022-07-01 16:38:08 -0700

    [HIG-2400] add project setting to set backend domains
    Expose the project setting for network request recording matching backend domains.

--- frontend/src/pages/ProjectSettings/ProjectSettings.tsx
+++ frontend/src/pages/ProjectSettings/ProjectSettings.tsx
@@ -3,6 +3,7 @@ import Tabs from '@components/Tabs/Tabs';
 import { DangerForm } from '@pages/ProjectSettings/DangerForm/DangerForm';
 import { ErrorSettingsForm } from '@pages/ProjectSettings/ErrorSettingsForm/ErrorSettingsForm';
 import { ExcludedUsersForm } from '@pages/ProjectSettings/ExcludedUsersForm/ExcludedUsersForm';
+import { NetworkRecordingForm } from '@pages/ProjectSettings/NetworkRecordingForm/NetworkRecordingForm';
 import { RageClicksForm } from '@pages/ProjectSettings/RageClicksForm/RageClicksForm';
 import SourcemapSettings from '@pages/WorkspaceSettings/SourcemapSettings/SourcemapSettings';
 import React from 'react';
@@ -47,6 +48,7 @@ const ProjectSettings = () => {
                                                     <>
                                                         <ExcludedUsersForm />
                                                         <RageClicksForm />
+                                                        <NetworkRecordingForm />
                                                     </>
                                                 ),
                                             },
