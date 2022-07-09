3003db2cf45b913d1335decad82738a0aff7e096
Vadim Korolik <vkorolik@gmail.com>
2022-07-08 18:36:56 -0700

    fix webpack revert

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -39,7 +39,7 @@ import packageJson from '../package.json';
 import LoginForm, { AuthAdminRouter } from './pages/Login/Login';
 import * as serviceWorker from './serviceWorker';
 
-const dev = process.env.NODE_ENV === 'dev';
+const dev = process.env.NODE_ENV === 'development';
 let commitSHA = process.env.REACT_APP_COMMIT_SHA || '';
 if (commitSHA.length > 8) {
     commitSHA = commitSHA.substring(0, 8);
