149417c601e672a1767cec1bc5ef06fd20816c1b
Vadim Korolik <vkorolik@gmail.com>
2022-08-19 15:57:04 -0700

    upgrade to vite 3

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -64,7 +64,7 @@ export default defineConfig({
         sourcemap: true,
     },
     server: {
-        https: true,
+        https: false,
         port: 3000,
         host: '0.0.0.0',
         cors: true,
