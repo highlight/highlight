63d35f885e5e102be7a5a61073822e4c7c4e8472
Sasha Aptlin <17913919+aptlin@users.noreply.github.com>
2022-08-26 08:38:28 -0700

    fix: issue with json raw msg serde

--- backend/public-graph/graph/resolver.go
+++ backend/public-graph/graph/resolver.go
@@ -2064,31 +2064,35 @@ func (r *Resolver) AddTrackProperties(ctx context.Context, sessionID int, events
 	for _, event := range events.Events {
 		if event.Type == parse.Custom {
 			dataObject := map[string]json.RawMessage{}
+			if err := json.Unmarshal([]byte(event.Data), &dataObject); err != nil {
+				return e.New("error deserializing custom event properties")
+			}
+
 			tag, tagExists := dataObject["tag"]
 			tagStr := string(tag)
+
 			if !tagExists || !strings.Contains(tagStr, "Track") {
 				continue
 			}
-			if err := json.Unmarshal(event.Data, &dataObject); err != nil {
-				return e.New("error deserializing custom event properties")
-			}
+
 			payload, payloadExists := dataObject["payload"]
 			if !payloadExists {
 				return e.New("error reading raw payload from track event")
 			}
 
+			var payloadStr string
+			if err := json.Unmarshal(payload, &payloadStr); err != nil {
+				return e.Wrap(err, "error deserializing track event payload into a string")
+
+			}
+
 			propertiesObject := make(map[string]interface{})
-			if err := json.Unmarshal(payload, &propertiesObject); err != nil {
+			if err := json.Unmarshal([]byte(payloadStr), &propertiesObject); err != nil {
 				return e.Wrap(err, "error deserializing track event properties")
 			}
 
-			log.Warnf("%v", propertiesObject)
 			for k, v := range propertiesObject {
 				formattedVal := fmt.Sprintf("%.*v", SESSION_FIELD_MAX_LENGTH, v)
-				strOrJson, err := strconv.Unquote(formattedVal)
-				if err != nil {
-					formattedVal = strOrJson
-				}
 				if len(formattedVal) > 0 {
 					fields[k] = formattedVal
 				}
