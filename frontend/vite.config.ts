c508ccfe795603affa6d2b7d29f3273372bd5323
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-08-30 18:31:11 -0500

    add vitest

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,4 +1,5 @@
-import { defineConfig } from 'vite';
+/// <reference types="vitest/globals" />
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import svgr from 'vite-plugin-svgr';
@@ -16,5 +17,10 @@ export default defineConfig({
     },
     build: {
         outDir: 'build',
+    },
+    test: {
+        globals: true,
+        environment: 'happy-dom',
+        setupFiles: ['./src/setupTests.ts'],
     },
 });
