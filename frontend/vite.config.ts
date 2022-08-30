ade831c3af12b33ead768e7cb4e58189fbf5913e
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-08-30 17:51:15 -0500

    add vite; it builds

---
+++ frontend/vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import svgr from 'vite-plugin-svgr';
+
+export default defineConfig({
+    plugins: [react(), tsconfigPaths(), svgr()],
+});
