5ed76fe303c6e8202caba1ba212dd34b304b244d
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-08-30 19:00:00 -0500

    handle env manually

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,15 +3,24 @@ import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import svgr from 'vite-plugin-svgr';
-import envCompatible from 'vite-plugin-env-compatible';
+
+const reactAppEnv = {};
+for (const key in process.env) {
+    if (key.startsWith('REACT_APP_')) {
+        reactAppEnv[`process.env.${key}`] = JSON.stringify(process.env[key]);
+    }
+}
 
 export default defineConfig({
-    plugins: [
-        react(),
-        tsconfigPaths(),
-        svgr(),
-        envCompatible({ prefix: 'REACT_APP' }),
-    ],
+    plugins: [react(), tsconfigPaths(), svgr()],
+    define: {
+        ...reactAppEnv,
+        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+        'process.env.DEV': JSON.stringify(
+            process.env.NODE_ENV === 'development'
+        ),
+        'process.env.PUBLIC_URL': JSON.stringify('/'),
+    },
     server: {
         port: 3000,
     },
