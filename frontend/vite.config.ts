d409fd70e197e5dc7dde0d8d09d4999f01d83c8e
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-09-01 13:39:46 -0500

    use ssl cert from backend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,24 @@
 /// <reference types="vite/client" />
 /// <reference types="vitest/globals" />
-import basicSsl from '@vitejs/plugin-basic-ssl';
 import react from '@vitejs/plugin-react';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'url';
 import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vitest/config';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export default defineConfig({
-    plugins: [react(), tsconfigPaths(), svgr(), basicSsl()],
+    plugins: [react(), tsconfigPaths(), svgr()],
     envPrefix: 'REACT_APP_',
     server: {
         port: 3000,
-        https: true,
+        https: {
+            key: join(__dirname, '../backend/localhostssl/server.key'),
+            cert: join(__dirname, '../backend/localhostssl/server.crt'),
+        },
     },
     build: {
         outDir: 'build',
