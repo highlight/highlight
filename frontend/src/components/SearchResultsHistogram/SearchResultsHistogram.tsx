4b8e23a1c6a558126ddb3b375e816628797b194d
Richard Hua <rich@rkhua.com>
2022-09-01 16:17:13 -0700

    Fix test failure

--- frontend/src/components/SearchResultsHistogram/SearchResultsHistogram.tsx
+++ frontend/src/components/SearchResultsHistogram/SearchResultsHistogram.tsx
@@ -1,10 +1,98 @@
 import Histogram, { Series } from '@components/Histogram/Histogram'
-import { DateHistogramBucketSize } from '@graph/schemas'
+import {
+	DateHistogramBucketSize,
+	OpenSearchCalendarInterval,
+} from '@graph/schemas'
 import moment from 'moment'
 import React, { useCallback } from 'react'
 import Skeleton from 'react-loading-skeleton'
 
 import styles from './SearchResultsHistogram.module.css'
+
+// Returns a calendar interval as described in
+// https://www.elastic.co/guide/en/elasticsearch/reference/current/search-aggregations-bucket-datehistogram-aggregation.html#calendar_intervals
+// If multiples of these units are desired, we can have the client manually combine buckets
+export function GetHistogramBucketSize(
+	timeRange: moment.Duration,
+): DateHistogramBucketSize {
+	const TARGET_BUCKETS = 35 // Return as many buckets as possible, up to TARGET_BUCKETS
+	const POSSIBLE_BUCKET_SIZES: DateHistogramBucketSize[] = [
+		{
+			calendar_interval: OpenSearchCalendarInterval.Minute,
+			multiple: 1,
+		},
+		{
+			calendar_interval: OpenSearchCalendarInterval.Minute,
+			multiple: 5,
+		},
+		{
+			calendar_interval: OpenSearchCalendarInterval.Minute,
+			multiple: 10,
+		},
+		{
+			calendar_interval: OpenSearchCalendarInterval.Minute,
+			multiple: 30,
+		},
+		{
+			calendar_interval: OpenSearchCalendarInterval.Hour,
+			multiple: 1,
+		},
+		{
+			calendar_interval: OpenSearchCalendarInterval.Hour,
+			multiple: 3,
+		},
+		{
+			calendar_interval: OpenSearchCalendarInterval.Hour,
+			multiple: 12,
+		},
+		{
+			calendar_interval: OpenSearchCalendarInterval.Day,
+			multiple: 1,
+		},
+		{
+			calendar_interval: OpenSearchCalendarInterval.Week,
+			multiple: 1,
+		},
+		{
+			calendar_interval: OpenSearchCalendarInterval.Month,
+			multiple: 1,
+		},
+		{
+			calendar_interval: OpenSearchCalendarInterval.Quarter,
+			multiple: 1,
+		},
+	]
+	for (const bucketSize of POSSIBLE_BUCKET_SIZES) {
+		let numCalendarIntervals = Number.MAX_VALUE
+		switch (bucketSize.calendar_interval) {
+			case OpenSearchCalendarInterval.Minute:
+				numCalendarIntervals = timeRange.asMinutes()
+				break
+			case OpenSearchCalendarInterval.Hour:
+				numCalendarIntervals = timeRange.asHours()
+				break
+			case OpenSearchCalendarInterval.Day:
+				numCalendarIntervals = timeRange.asDays()
+				break
+			case OpenSearchCalendarInterval.Week:
+				numCalendarIntervals = timeRange.asWeeks()
+				break
+			case OpenSearchCalendarInterval.Month:
+				numCalendarIntervals = timeRange.asMonths()
+				break
+			case OpenSearchCalendarInterval.Quarter:
+				numCalendarIntervals = timeRange.asMonths() / 3
+				break
+			case OpenSearchCalendarInterval.Year:
+				numCalendarIntervals = timeRange.asYears()
+				break
+		}
+		if (numCalendarIntervals / bucketSize.multiple <= TARGET_BUCKETS) {
+			return bucketSize
+		}
+	}
+	return POSSIBLE_BUCKET_SIZES.at(-1)!
+}
 
 export const SearchResultsHistogram = React.memo(
 	({
