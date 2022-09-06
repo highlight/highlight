acb42ec47144cb033695843699bd96b5cd9ba16a
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-09-06 18:51:43 -0500

    write up custom ResizePanel

--- frontend/src/pages/Player/Toolbar/DevToolsWindow/DevToolsWindow.tsx
+++ frontend/src/pages/Player/Toolbar/DevToolsWindow/DevToolsWindow.tsx
@@ -1,6 +1,7 @@
+import { useHTMLElementEvent } from '@hooks/useHTMLElementEvent'
+import { useWindowEvent } from '@hooks/useWindowEvent'
 import PerformancePage from '@pages/Player/Toolbar/DevToolsWindow/PerformancePage/PerformancePage'
 import React from 'react'
-import ResizePanel from 'react-resize-panel-ts'
 
 import Tabs, { TabItem } from '../../../../components/Tabs/Tabs'
 import SvgXIcon from '../../../../static/XIcon'
@@ -45,20 +46,23 @@ export const DevToolsWindow = React.memo(
 		]
 
 		return (
-			<ResizePanel
-				direction="n"
-				containerClass={styles.resizeContainer}
-				handleClass={styles.resizeHandle}
-				borderClass={styles.resizeBorder}
-			>
-				<div className={styles.devToolsWrapper}>
-					<DevToolsTabs
-						closeDevToolsHandler={() => {
-							setOpenDevTools(false)
-						}}
-						tabs={TABS}
-					/>
-				</div>
+			<ResizePanel>
+				{({ panelRef, handleRef }) => (
+					<div className={styles.devToolsWrapper} ref={panelRef}>
+						<button
+							className="flex cursor-ns-resize justify-center border-none bg-transparent p-2 outline-none"
+							ref={handleRef}
+						>
+							<div className="relative h-2 w-10 rounded-full bg-gray-200" />
+						</button>
+						<DevToolsTabs
+							closeDevToolsHandler={() => {
+								setOpenDevTools(false)
+							}}
+							tabs={TABS}
+						/>
+					</div>
+				)}
 			</ResizePanel>
 		)
 	},
@@ -88,3 +92,40 @@ const DevToolsTabs = React.memo(({ closeDevToolsHandler, tabs }: Props) => {
 		/>
 	)
 })
+
+function ResizePanel({
+	children,
+}: {
+	children: (props: {
+		panelRef: (element: HTMLElement | null) => void
+		handleRef: (element: HTMLElement | null) => void
+	}) => React.ReactNode
+}) {
+	const [panel, panelRef] = React.useState<HTMLElement | null>()
+	const [handle, handleRef] = React.useState<HTMLElement | null>()
+	const [dragging, setDragging] = React.useState(false)
+
+	useHTMLElementEvent(handle, 'pointerdown', (event) => {
+		if (handle && event.composedPath().includes(handle)) {
+			setDragging(true)
+		}
+	})
+
+	useWindowEvent(
+		'pointermove',
+		(event) => {
+			if (dragging && panel) {
+				const panelRect = panel.getBoundingClientRect()
+				const newHeight = panelRect.height - event.movementY
+				panel.style.height = `${newHeight}px`
+			}
+		},
+		{ passive: true },
+	)
+
+	useWindowEvent('pointerup', () => {
+		setDragging(false)
+	})
+
+	return <>{children({ panelRef, handleRef })}</>
+}
