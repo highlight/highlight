192c16f1b76c58cce45b6583ee2e43a930dd021f
Zane Mayberry <mayberryzane@gmail.com>
2022-09-13 11:55:27 -0700

    wip test

--- backend/lambda-functions/deleteSessions/handlers/handlers_test.go
+++ backend/lambda-functions/deleteSessions/handlers/handlers_test.go
@@ -1,6 +1,9 @@
 package handlers
 
 import (
+	"context"
+	"fmt"
+	"log"
 	"testing"
 
 	"github.com/highlight-run/highlight/backend/lambda-functions/deleteSessions/utils"
@@ -9,11 +12,15 @@ import (
 func TestHandlers(t *testing.T) {
 	h := NewHandlers()
 	input := utils.QuerySessionsInput{
-		ProjectId: 1,
-		Email: "zane@highlight.io",
-		FirstName: "Zane", 
-		Query: "",
-		SessionCount:,
+		ProjectId:    1,
+		Email:        "zane@highlight.io",
+		FirstName:    "Zane",
+		Query:        "{\"bool\":{\"must\":[{\"bool\":{\"should\":[{\"term\":{\"processed\":\"false\"}}]}},{\"bool\":{\"should\":[{\"range\":{\"created_at\":{\"gte\":\"2022-07-14T16:59:11.000Z\",\"lte\":\"2022-08-13T16:59:18.000Z\"}}}]}}]}}",
+		SessionCount: 256,
 	}
-	h.GetSessionIdsByQuery()
+	out, err := h.GetSessionIdsByQuery(context.TODO(), input)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(out)
 }
