a2cadef74e7ecf4ba848ccc53b21a49a613a6f8d
Zane Mayberry <mayberryzane@gmail.com>
2022-09-15 16:05:15 -0700

    use v8 of go-redis client

--- backend/object-storage/storage.go
+++ backend/object-storage/storage.go
@@ -13,7 +13,7 @@ import (
 	"strings"
 	"time"
 
-	"github.com/go-redis/redis"
+	"github.com/go-redis/redis/v8"
 	"github.com/google/uuid"
 	"github.com/openlyinc/pointy"
 	"golang.org/x/sync/errgroup"
