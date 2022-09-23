c7171eedc4ecbf4e30cb64516adf340267abb163
Chris Schmitz <ccschmitz@gmail.com>
2022-09-23 10:54:27 -0500

    Disable video

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,7 @@ module.exports = defineConfig({
 	e2e: {
 		baseUrl: 'https://127.0.0.1:3000',
 		pageLoadTimeout: 1200000,
+		video: false,
 		setupNodeEvents(on, config) {
 			// implement node event listeners here
 		},
