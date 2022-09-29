bbfc38554f313fd81579094ac9a596befd8ffe18
Vadim Korolik <vkorolik@gmail.com>
2022-09-29 09:39:43 -0700

    remove rrweb

--- packages/rrweb/src/entries/all.ts
+++
@@ -1,6 +0,0 @@
-export * from '../index'
-export * from '../packer'
-export * from '../plugins/console/record'
-export * from '../plugins/console/replay'
-export { getRecordSequentialIdPlugin } from '../plugins/sequential-id/record'
-export { getReplaySequentialIdPlugin } from '../plugins/sequential-id/replay'
