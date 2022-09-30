fe311ec46fe0a9ffeb5b5a2416cb69c03a6af8a5
Eric Thomas <eric.l.m.thomas@gmail.com>
2022-09-29 18:35:55 -0600

    add a couple more REACT_ env vars

--- frontend/src/env.d.ts
+++ frontend/src/env.d.ts
@@ -1,7 +1,11 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
+	readonly REACT_APP_FIREBASE_CONFIG_OBJECT: string
 	readonly REACT_APP_FRONT_INTEGRATION_CLIENT_ID: string
+	readonly REACT_APP_FRONTEND_ORG: string
+	readonly REACT_APP_PRIVATE_GRAPH_URI: string
+
 	readonly LINEAR_CLIENT_ID: string
 	readonly SLACK_CLIENT_ID: string
 }
