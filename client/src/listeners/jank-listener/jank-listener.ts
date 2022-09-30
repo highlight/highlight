eb8386d5080213b3e2d6c6368875478b17186f2f
Vadim Korolik <vkorolik@gmail.com>
2022-09-30 14:18:39 -0700

    feedback and cleanup jank listener

--- client/src/listeners/jank-listener/jank-listener.ts
+++ client/src/listeners/jank-listener/jank-listener.ts
@@ -1,11 +1,7 @@
-// Browsers call back to `requestAnimationFrame` 60 times per second
-// under smooth rendering conditions.
-// TODO(vkorolik) what about macs that run on 120fps
 import { getSimpleSelector } from '../../utils/dom'
 
-const EXPECTED_RAF_DURATION = 1000.0 / 60.0
-// if a frame is deplayed more than 16 ms and causes a skipped frame, track it
-const JANK_THRESHOLD = 16
+// samples taken to calculate expected RAF
+const RAF_SAMPLES = 30
 
 export interface JankPayload {
 	relativeTimestamp: number
@@ -14,14 +10,34 @@ export interface JankPayload {
 	newLocation?: string
 }
 
+const getRAFDuration = () => {
+	return new Promise<number>((resolve) =>
+		requestAnimationFrame((t1) =>
+			requestAnimationFrame((t2) => resolve(t2 - t1)),
+		),
+	)
+}
+
+const getExpectedRAFDuration = async () => {
+	const rafs: number[] = []
+	for (let i = 0; i < RAF_SAMPLES; i++) {
+		rafs.push(await getRAFDuration())
+	}
+	return rafs.reduce((a, b) => a + b, 0) / rafs.length
+}
+
 export const JankListener = (
 	callback: (payload: JankPayload) => void,
 	recordingStartTime: number,
 ) => {
 	let jankState: { event?: UIEvent; location?: string; timerStart?: number } =
 		{}
+	// use 60 fps refresh rate as a default until we measure expected
+	let expectedRAFDuration = 1000 / 60
+	getExpectedRAFDuration().then((raf) => (expectedRAFDuration = raf))
 
 	const listener = (e: MouseEvent | KeyboardEvent) => {
+		// only one event listener at a time
 		if (jankState?.event) return
 		jankState = {
 			event: e,
@@ -33,14 +49,13 @@ export const JankListener = (
 
 	const onAnimationFrame = () => {
 		if (!jankState?.timerStart) return
+		// the amount of time (ms) that a frame is delayed due to JS execution.
 		const jankAmount =
 			window.performance.now() -
 			jankState.timerStart -
-			EXPECTED_RAF_DURATION
+			expectedRAFDuration
 
-		if (jankAmount > JANK_THRESHOLD) {
-			reportJank(jankAmount)
-		}
+		reportJank(jankAmount)
 
 		// Reset jankState so we capture the next event
 		jankState = {}
