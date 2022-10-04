38079e41653f03cbffab9d62411ee0ebf982ff44
Zane Mayberry <mayberryzane@gmail.com>
2022-10-04 08:59:17 -0700

    wip

--- highlight-next/src/util/withHighlightConfig.ts
+++ highlight-next/src/util/withHighlightConfig.ts
@@ -8,35 +8,49 @@ import { H, HIGHLIGHT_REQUEST_HEADER, NodeOptions } from '@highlight-run/node'
 import { instrumentServer } from './instrumentServer'
 
 export const withHighlightConfig = (config: NextConfig): NextConfig => {
-	let newRewrites = config.rewrites
-	if (newRewrites) {
-		newRewrites = async () => {
-			const re = await config.rewrites!()
+	const newRewrites = async () => {
+		let re
+		if (!config.rewrites) {
+			re = []
+		} else {
+			re = await config.rewrites()
+		}
 
-			if (Array.isArray(re)) {
-				return {
-					beforeFiles: [
-						{
-							source: '/:path*.map',
-							destination: '/404',
-						},
-					],
-					afterFiles: re,
-					fallback: [],
-				}
-			} else {
-				return {
-					beforeFiles: re.beforeFiles,
-					afterFiles: re.afterFiles,
-					fallback: re.fallback,
-				}
+		if (Array.isArray(re)) {
+			return {
+				beforeFiles: [
+					{
+						source: '/:path*.map',
+						destination: '/404',
+					},
+				],
+				afterFiles: re,
+				fallback: [],
+			}
+		} else {
+			return {
+				beforeFiles: re.beforeFiles,
+				afterFiles: re.afterFiles,
+				fallback: re.fallback,
 			}
 		}
 	}
 
 	let newWebpack = config.webpack
 	if (newWebpack) {
-		newWebpack = undefined
+		newWebpack = (config, opts) => {
+			const originalConfig = config.webpack(config, opts)
+			originalConfig.plugins
+				.push
+				// new CopyPlugin([
+				// 	{
+				// 		from: './node_modules/@pdftron/webviewer/public',
+				// 		to: './public/webviewer',
+				// 	},
+				// ]),
+				()
+			return originalConfig
+		}
 	}
 
 	return { ...config, rewrites: newRewrites, webpack: newWebpack }
