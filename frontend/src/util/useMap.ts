77cd60dc8472dd00680ac4577ea402a89f9df3ab
Vadim Korolik <vkorolik@gmail.com>
2022-10-04 11:48:00 -0700

    instrument chunk map

--- frontend/src/util/useMap.ts
+++ frontend/src/util/useMap.ts
@@ -1,3 +1,4 @@
+import { timedCall } from '@util/perf/instrument'
 import { useCallback, useState } from 'react'
 
 export type MapOrEntries<K, V> = Map<K, V> | [K, V][]
@@ -16,37 +17,51 @@ function useMap<K, V>(
 	const [map, setMap] = useState(new Map(initialState))
 
 	const set = useCallback((key: K, value: V) => {
-		setMap((prev) => {
-			const copy = new Map(prev)
-			copy.set(key, value)
-			return copy
+		timedCall('useMap/set', () => {
+			setMap((prev) => {
+				return timedCall('useMap/set/copy', () => {
+					const copy = new Map(prev)
+					copy.set(key, value)
+					return copy
+				})
+			})
 		})
 	}, [])
 
 	const setMulti = useCallback((entries: [K, V | undefined][]) => {
-		setMap((prev) => {
-			const copy = new Map(prev)
-			for (const [k, v] of entries) {
-				if (v === undefined) {
-					copy.delete(k)
-				} else {
-					copy.set(k, v)
-				}
-			}
-			return copy
+		timedCall('useMap/setMulti', () => {
+			setMap((prev) => {
+				return timedCall('useMap/setMulti/copy', () => {
+					const copy = new Map(prev)
+					for (const [k, v] of entries) {
+						if (v === undefined) {
+							copy.delete(k)
+						} else {
+							copy.set(k, v)
+						}
+					}
+					return copy
+				})
+			})
 		})
 	}, [])
 
 	const remove = useCallback((key: K) => {
-		setMap((prev) => {
-			const copy = new Map(prev)
-			copy.delete(key)
-			return copy
+		timedCall('useMap/remove', () => {
+			setMap((prev) => {
+				return timedCall('useMap/remove/copy', () => {
+					const copy = new Map(prev)
+					copy.delete(key)
+					return copy
+				})
+			})
 		})
 	}, [])
 
 	const reset = useCallback(() => {
-		setMap(() => new Map())
+		timedCall('useMap/reset', () => {
+			setMap(() => new Map())
+		})
 	}, [])
 
 	return [map, set, setMulti, remove, reset]
