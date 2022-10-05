68c61c69658ea5366a21dc40f825697e9806d28d
Zane Mayberry <mayberryzane@gmail.com>
2022-10-04 22:11:09 -0700

    working with cjs

--- highlight-next/src/util/highlightWebpackPlugin.ts
+++ highlight-next/src/util/highlightWebpackPlugin.ts
@@ -2,12 +2,11 @@ import { uploadSourcemaps } from '@highlight-run/sourcemap-uploader'
 
 export default class HighlightWebpackPlugin {
 	apply(compiler: any) {
-		console.log('hi')
 		compiler.hooks.afterEmit.tap('HighlightWebpackPlugin', () => {
 			uploadSourcemaps({
 				apiKey: '',
 				appVersion: '',
-				path: './.next/static/chunks/',
+				path: '',
 			})
 		})
 		return compiler
