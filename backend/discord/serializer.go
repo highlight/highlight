dc775181305138e77fef88ab542008942c5ad97f
Eric Thomas <eric.l.m.thomas@gmail.com>
2022-10-06 11:29:10 -0600

    remove oauth stuff

--- backend/discord/serializer.go
+++ backend/discord/serializer.go
@@ -1,44 +1,8 @@
 package discord
 
 import (
-	"os"
-
 	"github.com/highlight-run/highlight/backend/model"
-	"github.com/pkg/errors"
-
-	"golang.org/x/oauth2"
 )
-
-var Endpoint = oauth2.Endpoint{
-	AuthURL:   "https://discord.com/api/oauth2/authorize",
-	TokenURL:  "https://discord.com/api/oauth2/token",
-	AuthStyle: oauth2.AuthStyleInParams,
-}
-
-func oauthConfig() (*oauth2.Config, error) {
-	var (
-		ok                  bool
-		DiscordClientID     string
-		DiscordClientSecret string
-		FrontendUri         string
-	)
-	if DiscordClientID, ok = os.LookupEnv("DISCORD_CLIENT_ID"); !ok || DiscordClientID == "" {
-		return nil, errors.New("DISCORD_CLIENT_ID not set")
-	}
-	if DiscordClientSecret, ok = os.LookupEnv("DISCORD_CLIENT_SECRET"); !ok || DiscordClientSecret == "" {
-		return nil, errors.New("DISCORD_CLIENT_SECRET not set")
-	}
-	if FrontendUri, ok = os.LookupEnv("REACT_APP_FRONTEND_URI"); !ok || FrontendUri == "" {
-		return nil, errors.New("REACT_APP_FRONTEND_URI not set")
-	}
-
-	return &oauth2.Config{
-		ClientID:     DiscordClientID,
-		ClientSecret: DiscordClientSecret,
-		Endpoint:     Endpoint,
-		RedirectURL:  FrontendUri + "/callback/discord",
-	}, nil
-}
 
 func SerializeModelToGQL(channels model.DiscordChannels) []*model.DiscordChannel {
 	ret := []*model.DiscordChannel{}
