4dea4a1911e70a9088da6d643c4e7e7c53ae65cd
Chris Schmitz <ccschmitz@gmail.com>
2022-10-10 08:26:21 -0500

    Mock import for consts:publicGraphURI

--- firstload/vitest.config.js
+++ firstload/vitest.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
 	test: {
-		globals: true,
 		environment: 'jsdom',
+		globals: true,
+		setupFiles: ['./src/__tests__/setup.ts'],
 	},
 })
