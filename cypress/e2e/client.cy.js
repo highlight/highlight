5c1d2710b886fb899b46f87f3984541f68c14868
Vadim Korolik <vkorolik@gmail.com>
2022-10-11 23:28:26 -0700

    simplify test

--- cypress/e2e/client.cy.js
+++ cypress/e2e/client.cy.js
@@ -8,13 +8,14 @@ describe('client recording spec', () => {
 
 	it('fetch requests are recorded', () => {
 		cy.visit('/')
-		cy.wait('@PushPayload')
-		cy.wait(4000)
-		cy.wait('@PushPayload')
 		// ensure pushPayload buffer is cleared
 
 		// make fetch requests
 		cy.window().then((win) => {
+			cy.wait('@PushPayload')
+				.its('request.body.variables.resources')
+				.should('have.property', 'resources')
+
 			win.eval(`fetch(new URL('https://localhost:3000/index.html'))`)
 			win.eval(
 				`fetch(new URL('https://localhost:3000/index.html'), {method: 'POST'})`,
@@ -23,25 +24,10 @@ describe('client recording spec', () => {
 			win.eval(
 				`fetch('https://localhost:3000/index.html', {method: 'POST'})`,
 			)
-			cy.wait(1000)
-			cy.wait(60000)
 
-			// Ensure client network resources are recorded
-			const pp = cy
-				.wait('@PushPayload')
+			cy.wait('@PushPayload')
 				.its('request.body.variables.resources')
-			for (let i = 0; i < 4; i++) {
-				pp.should(
-					'have.deep.property',
-					`resources[${i * 4}].initiatorType`,
-					'fetch',
-				)
-				pp.should(
-					'have.deep.property',
-					`resources[${i * 4}].name`,
-					'https://localhost:3000/index.html',
-				)
-			}
+				.should('have.property', 'resources')
 		})
 	})
 })
