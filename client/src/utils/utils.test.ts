3fd24035adf9bb82204850e3c656f3653a44407b
Eric Thomas <eric.l.m.thomas@gmail.com>
2022-10-13 16:21:15 -0600

    make a more interesting test

--- client/src/utils/utils.test.ts
+++
@@ -1,9 +0,0 @@
-import { stringify } from './utils'
-
-describe('utils', () => {
-	it('stringify', () => {
-		const result = stringify(1)
-
-		expect(result).to.eq('1')
-	})
-})
--- client/vitest.config.js
+++ client/vitest.config.js
@@ -4,5 +4,6 @@ export default defineConfig({
 	test: {
 		environment: 'jsdom',
 		globals: true,
+		setupFiles: ['./src/__tests__/setup.ts'],
 	},
 })
