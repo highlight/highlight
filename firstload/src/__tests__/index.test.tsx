e5d8ae140c76323b406d36ddb13a52d4bf8fe3db
Eric Thomas <eric.l.m.thomas@gmail.com>
2022-10-19 08:50:34 -0600

    remove jest from client app

--- firstload/src/__tests__/index.test.tsx
+++ firstload/src/__tests__/index.test.tsx
@@ -2,7 +2,6 @@ import { H } from 'highlight.run'
 import { HighlightPublicInterface } from '@highlight-run/client'
 
 // Don't run tests for now. Need to move code from firstload to client for backend errors.
-// TODO - delete
 describe.skip('should work outside of the browser in unit test', () => {
 	let highlight: HighlightPublicInterface
 
