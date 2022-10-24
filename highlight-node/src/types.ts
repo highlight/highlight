1c1dd1373815138530b712182c3fb3eadc63e9d2
Zane Mayberry <mayberryzane@gmail.com>
2022-10-24 15:09:48 -0700

    add helper to parse request headers

--- highlight-node/src/types.ts
+++ highlight-node/src/types.ts
@@ -17,3 +17,21 @@ export interface NodeOptions extends HighlightOptions {
 	 */
 	errorSourceContextCacheSizeMB?: number
 }
+
+export interface HighlightRequestInfo {
+	/**
+	 * Disables source code context lines for error reporting.
+	 * This may be useful for performance if your source
+	 * files are particularly large or memory is limited.
+	 * @default false
+	 */
+	disableErrorSourceContext?: boolean
+	/**
+	 * Source files are cached in memory to speed up error reporting
+	 * and avoid costly disk access. The default cache size is 10MB,
+	 * but this can be overridden.
+	 * Specifying a value <= 0 removes all cache size limits.
+	 * @default 10
+	 */
+	errorSourceContextCacheSizeMB?: number
+}
