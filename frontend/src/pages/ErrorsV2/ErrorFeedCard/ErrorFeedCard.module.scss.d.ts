b116eb9e95fb167c0e3c771be0d793340a4403fb
Sasha Aptlin <17913919+aptlin@users.noreply.github.com>
2022-10-26 16:40:23 -0700

    fix: resolved status icon

--- frontend/src/pages/ErrorsV2/ErrorFeedCard/ErrorFeedCard.module.scss.d.ts
+++ frontend/src/pages/ErrorsV2/ErrorFeedCard/ErrorFeedCard.module.scss.d.ts
@@ -2,5 +2,6 @@ export const badge: string
 export const errorCard: string
 export const errorCardSelected: string
 export const icon: string
+export const resolvedIcon: string
 export const separator: string
 export const title: string
