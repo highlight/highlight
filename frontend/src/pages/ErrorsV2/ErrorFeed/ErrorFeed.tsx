ec97e6ce4f5c7766d84eb6a03d559482208f26e2
Sasha Aptlin <17913919+aptlin@users.noreply.github.com>
2022-10-27 11:27:52 -0700

    refactor: extract histogram to a separate component

--- frontend/src/pages/ErrorsV2/ErrorFeed/ErrorFeed.tsx
+++ frontend/src/pages/ErrorsV2/ErrorFeed/ErrorFeed.tsx
@@ -1,31 +1,18 @@
-import { Series } from '@components/Histogram/Histogram'
 import { Pagination, STARTING_PAGE } from '@components/Pagination/Pagination'
 import { SearchEmptyState } from '@components/SearchEmptyState/SearchEmptyState'
-import { SearchResultsHistogram } from '@components/SearchResultsHistogram/SearchResultsHistogram'
 import { Skeleton } from '@components/Skeleton/Skeleton'
-import {
-	useGetErrorGroupsOpenSearchQuery,
-	useGetErrorsHistogramQuery,
-} from '@graph/hooks'
-import {
-	DateHistogramBucketSize,
-	ErrorGroup,
-	ErrorResults,
-	Maybe,
-} from '@graph/schemas'
+import { useGetErrorGroupsOpenSearchQuery } from '@graph/hooks'
+import { ErrorGroup, ErrorResults, Maybe } from '@graph/schemas'
 import { Box } from '@highlight-run/ui'
-import ErrorQueryBuilder, {
-	TIME_RANGE_FIELD,
-} from '@pages/Error/components/ErrorQueryBuilder/ErrorQueryBuilder'
+import ErrorQueryBuilder from '@pages/Error/components/ErrorQueryBuilder/ErrorQueryBuilder'
 import SegmentPickerForErrors from '@pages/Error/components/SegmentPickerForErrors/SegmentPickerForErrors'
 import { useErrorSearchContext } from '@pages/Errors/ErrorSearchContext/ErrorSearchContext'
 import { ErrorFeedCard } from '@pages/ErrorsV2/ErrorFeedCard/ErrorFeedCard'
-import { updateQueriedTimeRange } from '@pages/Sessions/SessionsFeedV2/components/QueryBuilder/QueryBuilder'
+import ErrorFeedHistogram from '@pages/ErrorsV2/ErrorFeedHistogram/ErrorFeedHistogram'
 import useLocalStorage from '@rehooks/local-storage'
 import { gqlSanitize } from '@util/gqlSanitize'
 import { useParams } from '@util/react-router/useParams'
-import { serializeAbsoluteTimeRange } from '@util/time'
-import { useCallback, useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import * as style from './ErrorFeed.css'
 
@@ -56,8 +43,6 @@ const ErrorFeed = () => {
 	const totalPages = Math.ceil(errorCount / PAGE_SIZE)
 
 	const useCachedErrors = errorCount > PAGE_SIZE
-
-	const histogram = useHistogram(projectId, useCachedErrors)
 
 	const [fetchedData, setFetchedData] = useState<ErrorResults>({
 		error_groups: [],
@@ -97,7 +82,7 @@ const ErrorFeed = () => {
 					paddingBottom="tiny"
 					px="medium"
 				>
-					{histogram}
+					<ErrorFeedHistogram useCachedErrors={useCachedErrors} />
 				</Box>
 			)}
 			<Box padding="small" cssClass={style.content}>
@@ -144,84 +129,6 @@ const ErrorFeed = () => {
 				/>
 			</Box>
 		</>
-	)
-}
-
-const useHistogram = (projectID: string, useCachedErrors: boolean) => {
-	const { backendSearchQuery, searchParams, setSearchParams } =
-		useErrorSearchContext()
-	const [histogram, setHistogram] = useState<{
-		seriesList: Series[]
-		bucketTimes: number[]
-	}>({
-		seriesList: [],
-		bucketTimes: [],
-	})
-	const { loading } = useGetErrorsHistogramQuery({
-		variables: {
-			query: backendSearchQuery?.childSearchQuery as string,
-			project_id: projectID,
-			histogram_options: {
-				bucket_size:
-					backendSearchQuery?.histogramBucketSize as DateHistogramBucketSize,
-				time_zone:
-					Intl.DateTimeFormat().resolvedOptions().timeZone ?? 'UTC',
-				bounds: {
-					start_date:
-						backendSearchQuery?.startDate.toISOString() as string,
-					end_date:
-						backendSearchQuery?.endDate.toISOString() as string,
-				},
-			},
-		},
-		onCompleted: (r) => {
-			let seriesList: Series[] = []
-			let bucketTimes: number[] = []
-			const histogramData = r?.errors_histogram
-			if (backendSearchQuery && histogramData) {
-				bucketTimes = histogramData.bucket_times.map((startTime) =>
-					new Date(startTime).valueOf(),
-				)
-				seriesList = [
-					{
-						label: 'Errors logged',
-						color: '--color-purple',
-						counts: histogramData.error_objects,
-					},
-				]
-			}
-			setHistogram({
-				seriesList,
-				bucketTimes,
-			})
-		},
-		skip: !backendSearchQuery?.childSearchQuery,
-		fetchPolicy: useCachedErrors ? 'cache-first' : 'no-cache',
-	})
-
-	const updateTimeRange = useCallback(
-		(newStartTime: Date, newEndTime: Date) => {
-			const newSearchParams = {
-				...searchParams,
-				query: updateQueriedTimeRange(
-					searchParams.query || '',
-					TIME_RANGE_FIELD,
-					serializeAbsoluteTimeRange(newStartTime, newEndTime),
-				),
-			}
-			setSearchParams(newSearchParams)
-		},
-		[searchParams, setSearchParams],
-	)
-
-	return (
-		<SearchResultsHistogram
-			seriesList={histogram.seriesList}
-			bucketTimes={histogram.bucketTimes}
-			bucketSize={backendSearchQuery?.histogramBucketSize}
-			loading={loading}
-			updateTimeRange={updateTimeRange}
-		/>
 	)
 }
 
