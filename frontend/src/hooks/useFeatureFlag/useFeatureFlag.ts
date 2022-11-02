548131ce043b84912087535fcb7f6b2df7f4302b
Vadim Korolik <vadim@highlight.io>
2022-11-02 15:50:48 -0700

    roll out histogram timeline to 25% of workspaces (#3261)
    ## Summary
    
    Adds `useFeature` hook and uses it to roll out new timeline to 25% of users.
    
    ## How did you test this change?
    
    Local deploy with testing of different percentiles with breakpoint. Frontend preview showing track-properties call: https://frontend-pr-3261.onrender.com/1/sessions/eBewvuTPIO9G0WjsJWus0lTe6VO7
    
    <img width="321" alt="Screenshot 2022-11-01 at 1 24 09 PM" src="https://user-images.githubusercontent.com/1351531/199333756-ab39681c-ca3a-4b65-8cab-bb59bb00858b.png">
    
    
    ## Are there any deployment considerations?
    
    Will be rolled out after the latest timeline tweaks are released: https://github.com/highlight-run/highlight/pull/3258

--- frontend/src/hooks/useFeatureFlag/useFeatureFlag.ts
+++ frontend/src/hooks/useFeatureFlag/useFeatureFlag.ts
@@ -1,33 +1,96 @@
+import { useAuthContext } from '@authentication/AuthContext'
+import { useGetProjectQuery } from '@graph/hooks'
+import { useParams } from '@util/react-router/useParams'
+import { H } from 'highlight.run'
 import { useEffect, useState } from 'react'
 
-import { useGetAdminQuery } from '../../graph/generated/hooks'
-
-const useFeatureFlag = (feature: Feature) => {
-	const { data } = useGetAdminQuery({
-		skip: false,
-	})
-	const [hasAccessToFeature, setHasAccessToFeature] = useState(false)
-
-	useEffect(() => {
-		if (data) {
-			const flight = Flights[feature]
-
-			if (flight.includes(data.admin?.id || '')) {
-				setHasAccessToFeature(true)
-			} else {
-				setHasAccessToFeature(false)
-			}
-		}
-	}, [data, feature])
-
-	return { hasAccessToFeature }
+interface Config {
+	project?: boolean
+	workspace?: boolean
+	percent: number
 }
 
-export default useFeatureFlag
+export enum Feature {
+	HistogramTimelineV2,
+}
 
-export enum Feature {}
+// configures the criteria and percentage of population for which the feature is active.
+// can configure to rollout by project, workspace, or admin
+export const FeatureConfig: { [key: number]: Config } = {
+	[Feature.HistogramTimelineV2]: {
+		workspace: true,
+		percent: 25,
+	},
+}
 
-/**
- * A mapping between features and users that have access to them. Users are identified with their Admin ID.
- */
-const Flights: { [key in Feature]: string[] } = {}
+const isActive = async function (
+	feature: Feature,
+	key: string,
+	percent: number,
+) {
+	const text = `${Feature[feature]}-${key}`
+	const textAsBuffer = new TextEncoder().encode(text)
+	const hashBuffer = await window.crypto.subtle.digest(
+		'SHA-256',
+		textAsBuffer,
+	)
+	const hashArray = Array.from(new Uint8Array(hashBuffer))
+	const digest = hashArray.reduce((a, b) => a + b, 0)
+
+	return digest % Math.ceil(100 / percent) === 0
+}
+
+const isFeatureOn = async function (
+	feature: Feature,
+	projectId?: string,
+	workspaceId?: string,
+	adminId?: string,
+) {
+	const config = FeatureConfig[feature]
+	return isActive(
+		feature,
+		(config.project
+			? projectId
+			: config.workspace
+			? workspaceId
+			: adminId) ?? 'demo',
+		config.percent,
+	)
+}
+
+// use to roll out a feature to a subset of users.
+const useFeature = (feature: Feature, override?: boolean) => {
+	const { admin } = useAuthContext()
+	const { project_id } = useParams<{
+		project_id: string
+	}>()
+	const { data: project } = useGetProjectQuery({
+		variables: { id: project_id },
+		skip: !project_id,
+	})
+
+	const [isOn, setIsOn] = useState<boolean>(!!override)
+
+	useEffect(() => {
+		isFeatureOn(
+			feature,
+			project?.project?.id,
+			project?.workspace?.id,
+			admin?.id,
+		).then((_isOn) => {
+			const on = override ?? _isOn
+			setIsOn(on)
+			H.track(`Feature-${Feature[feature]}`, { on })
+		})
+	}, [
+		admin?.id,
+		feature,
+		override,
+		project?.project?.id,
+		project?.workspace?.id,
+	])
+
+	return isOn
+}
+
+export default useFeature
