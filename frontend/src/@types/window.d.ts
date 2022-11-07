425176041bdc448992c10ff46a7e13e074eb8986
Chris Schmitz <ccschmitz@gmail.com>
2022-11-07 12:34:06 -0600

    Revert "Add project/workspace IDs and user properties in Mixpanel (#3284)"
    This reverts commit 7221014061d3a09f71e4c2d4c0c00f7d2c5a6c42.

--- frontend/src/@types/window.d.ts
+++ frontend/src/@types/window.d.ts
@@ -1,9 +1,10 @@
-// eslint-disable @typescript-eslint/no-explicit-any
 interface Window {
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
 	analytics: any
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
 	H: any
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
 	Intercom: any
-	mixpanel: any
 	_highlightFirebaseConfig: any
 	_highlightFirebaseConfigString: any
 }
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -26,7 +26,6 @@ import {
 	useGetAdminLazyQuery,
 	useGetAdminRoleByProjectLazyQuery,
 	useGetAdminRoleLazyQuery,
-	useGetProjectLazyQuery,
 } from '@graph/hooks'
 import { Admin } from '@graph/schemas'
 import { ErrorBoundary } from '@highlight-run/react'
@@ -193,7 +192,6 @@ const App = () => {
 const AuthenticationRoleRouter = () => {
 	const workspaceId = /^\/w\/(\d+)\/.*$/.exec(window.location.pathname)?.pop()
 	const projectId = /^\/(\d+)\/.*$/.exec(window.location.pathname)?.pop()
-	const [getProjectQuery] = useGetProjectLazyQuery()
 	const [
 		getAdminWorkspaceRoleQuery,
 		{
@@ -340,29 +338,6 @@ const AuthenticationRoleRouter = () => {
 			)
 		}
 	}, [authRole, setLoadingState])
-
-	useEffect(() => {
-		// Wait until auth is finished loading otherwise this request can fail.
-		if (!window.mixpanel || !projectId || isAuthLoading(authRole)) {
-			return
-		}
-
-		getProjectQuery({
-			variables: {
-				id: projectId,
-			},
-			onCompleted: (data) => {
-				if (!data.project) {
-					return
-				}
-
-				window.mixpanel.register({
-					'Project ID': data.project?.id,
-					'Workspace ID': data.workspace?.id,
-				})
-			},
-		})
-	}, [getProjectQuery, projectId, authRole])
 
 	const [enableStaffView] = useLocalStorage(
 		`highlight-enable-staff-view`,
