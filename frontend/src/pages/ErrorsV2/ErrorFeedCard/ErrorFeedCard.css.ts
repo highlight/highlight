d5cbd096c7cef236ef0d9e66fb9d773eeef8586b
Sasha Aptlin <17913919+aptlin@users.noreply.github.com>
2022-11-08 10:18:01 -0800

    fix: css props

--- frontend/src/pages/ErrorsV2/ErrorFeedCard/ErrorFeedCard.css.ts
+++ frontend/src/pages/ErrorsV2/ErrorFeedCard/ErrorFeedCard.css.ts
@@ -1,20 +1,14 @@
 import { colors } from '@highlight-run/ui/src/css/colors'
-import { ERROR_FEED_SCROLLBAR_WIDTH } from '@pages/ErrorsV2/ErrorFeed/ErrorFeed.css'
-import { ERROR_FEED_LEFT_PANEL_WIDTH } from '@pages/ErrorsV2/SearchPanel/SearchPanel.css'
 import { style } from '@vanilla-extract/css'
 
 export const ERROR_CARD_PX = 12
 
 export const errorCardTitle = style({
 	height: 20,
-	width:
-		ERROR_FEED_LEFT_PANEL_WIDTH -
-		2 * ERROR_CARD_PX -
-		ERROR_FEED_SCROLLBAR_WIDTH,
 })
 
 export const errorCardTitleText = style({
-	overflowX: 'hidden',
+	maxWidth: '100%',
 })
 
 export const errorCard = style({
