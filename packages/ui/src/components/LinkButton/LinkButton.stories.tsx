115253fa5118eb147ace0e36e083f79e5ac5d016
Chris Schmitz <ccschmitz@gmail.com>
2022-11-14 12:02:02 -0800

    LinkButton component

---
+++ packages/ui/src/components/LinkButton/LinkButton.stories.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { ComponentMeta } from '@storybook/react'
+
+import { LinkButton } from './LinkButton'
+
+export default {
+	title: 'Components/LinkButton',
+	component: LinkButton,
+} as ComponentMeta<typeof LinkButton>
+
+export const Basic = () => (
+	<>
+		<LinkButton href="www.example.com">Hello! 👋</LinkButton>
+		<LinkButton kind="primary" emphasis="high" href="www.example.com">
+			Hello! 👋
+		</LinkButton>
+	</>
+)
