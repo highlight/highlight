c5b8ba8c46a3ee8a3a85ef3ec3892f651344d41f
Chris Schmitz <ccschmitz@gmail.com>
2022-11-16 14:57:00 -0800

    Fix type errors in Stack

--- packages/ui/src/components/Stack/Stack.tsx
+++ packages/ui/src/components/Stack/Stack.tsx
@@ -1,59 +1,40 @@
 import React from 'react'
-import {
-	mapResponsiveValue,
-	OptionalResponsiveObject,
-} from '../../css/sprinkles.css'
 import { Box, BoxProps } from '../Box/Box'
-
-const directionToFlexDirectionLookup = {
-	horizontal: 'row',
-	vertical: 'column',
-} as const
-
-const wrapFlexWrapLookup = {
-	true: 'wrap',
-	false: 'nowrap',
-} as const
 
 type Props = React.PropsWithChildren & {
 	as?: BoxProps['as']
 	align?: BoxProps['alignItems']
-	direction?: OptionalResponsiveObject<'horizontal' | 'vertical'>
+	direction?: BoxProps['flexDirection']
 	flex?: BoxProps['flex']
 	gap?: BoxProps['gap']
 	justify?: BoxProps['justifyContent']
-	wrap?: OptionalResponsiveObject<true | false>
+	wrap?: boolean | BoxProps['flexWrap']
 }
 
 export const Stack: React.FC<Props> = ({
 	as = 'div',
 	align,
 	children,
-	direction = 'vertical',
+	direction,
 	flex,
 	gap = '16',
 	justify,
 	wrap,
 }) => {
-	const flexDirection = mapResponsiveValue(
-		direction,
-		(value) => directionToFlexDirectionLookup[value],
-	)
-	const flexWrap: BoxProps['flexWrap'] = mapResponsiveValue(
-		wrap,
-		(value) => wrapFlexWrapLookup[String(value)],
-	)
+	if (typeof wrap === 'boolean') {
+		wrap = 'wrap'
+	}
 
 	return (
 		<Box
 			as={as}
 			alignItems={align}
 			display="flex"
-			flexDirection={flexDirection}
+			flexDirection={direction}
 			flex={flex}
 			justifyContent={justify}
 			gap={gap}
-			flexWrap={flexWrap}
+			flexWrap={wrap}
 		>
 			{children}
 		</Box>
