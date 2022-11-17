044a0274540f512dfafcf6d680021a224190c801
Vadim Korolik <vadim@highlight.io>
2022-11-17 13:31:26 -0800

    optimize data fetching for error pages using influxdb (#3316)
    ## Summary
    
    Creates a new set of influx buckets and measurements for error metrics.
    Creates continuous tasks to aggregate the data hourly for fast querying.
    
    <img width="1158" alt="Screenshot 2022-11-15 at 12 04 41 PM"
    src="https://user-images.githubusercontent.com/1351531/202014845-fa11b768-2e38-4395-b32a-dda0d9a017a7.png">
    
    Adds a query graphql operation to return aggregated data for any time
    window size.
    
    ## How did you test this change?
    
    Local graphql requests
    
    <img width="1366" alt="Screenshot 2022-11-15 at 12 05 09 PM"
    src="https://user-images.githubusercontent.com/1351531/202014919-a3f6cf87-da55-45e6-80e7-1eac7e09a0c2.png">
    
    
    ## Are there any deployment considerations?
    
    New graphql operation. Will monitor the deployment of new influx tasks
    and their cost. Since the new tasks run hourly, expecting their impact
    to be much less.

--- backend/private-graph/graph/schema.resolvers.go
+++ backend/private-graph/graph/schema.resolvers.go
@@ -4035,6 +4035,55 @@ func (r *queryResolver) ErrorDistribution(ctx context.Context, projectID int, er
 	return errorDistribution, nil
 }
 
+// ErrorGroupFrequencies is the resolver for the errorGroupFrequencies field.
+func (r *queryResolver) ErrorGroupFrequencies(ctx context.Context, projectID int, errorGroupSecureIds []string, params modelInputs.ErrorGroupFrequenciesParamsInput) ([]*modelInputs.ErrorDistributionItem, error) {
+	var errorGroupIDs []string
+	for _, egSecureID := range errorGroupSecureIds {
+		errorGroup, err := r.canAdminViewErrorGroup(ctx, egSecureID, false)
+		if err != nil {
+			return nil, e.Wrap(err, "admin not error group owner")
+		}
+		errorGroupIDs = append(errorGroupIDs, strconv.Itoa(errorGroup.ID))
+	}
+
+	bucket, measurement := r.TDB.GetSampledMeasurement(r.TDB.GetBucket(strconv.Itoa(projectID), timeseries.Errors), timeseries.Errors, params.DateRange.EndDate.Sub(params.DateRange.StartDate))
+	query := fmt.Sprintf(`
+      from(bucket: "%[1]s")
+		|> range(start: %[2]s, stop: %[3]s)
+		|> filter(fn: (r) => r._measurement == "%[4]s")
+		|> filter(fn: (r) => contains(value: r.ErrorGroupID, set: %[5]q))
+		|> aggregateWindow(every: %[6]dh, fn: sum, createEmpty: true)
+	`, bucket, params.DateRange.StartDate.Format(time.RFC3339), params.DateRange.EndDate.Format(time.RFC3339), measurement, errorGroupIDs, params.ResolutionHours)
+	span, _ := tracer.StartSpanFromContext(ctx, "tdb.errorGroupFrequencies")
+	span.SetTag("projectID", projectID)
+	span.SetTag("errorGroupIDs", errorGroupSecureIds)
+	results, err := r.TDB.Query(ctx, query)
+	if err != nil {
+		return nil, e.Wrap(err, "failed to perform tdb query for error group frequencies")
+	}
+	var response []*modelInputs.ErrorDistributionItem
+	for _, r := range results {
+		field := r.Values["_field"]
+		if field != nil {
+			var value int64
+			if r.Value != nil {
+				value = r.Value.(int64)
+			}
+			var id string
+			if r.Values["ErrorGroupID"] != nil {
+				id = r.Values["ErrorGroupID"].(string)
+			}
+			response = append(response, &modelInputs.ErrorDistributionItem{
+				ErrorGroupID: id,
+				Date:         r.Time,
+				Name:         field.(string),
+				Value:        value,
+			})
+		}
+	}
+	return response, nil
+}
+
 // Referrers is the resolver for the referrers field.
 func (r *queryResolver) Referrers(ctx context.Context, projectID int, lookBackPeriod int) ([]*modelInputs.ReferrerTablePayload, error) {
 	if _, err := r.isAdminInProjectOrDemoProject(ctx, projectID); err != nil {
@@ -5637,7 +5686,7 @@ func (r *queryResolver) SuggestedMetrics(ctx context.Context, projectID int, pre
 		  |> group(columns: ["_field"])
 		  |> distinct(column: "_field")
 		  |> yield(name: "distinct")
-	`, r.TDB.GetBucket(strconv.Itoa(projectID)), timeseries.Metrics, filter)
+	`, r.TDB.GetBucket(strconv.Itoa(projectID), timeseries.Metrics), timeseries.Metrics, filter)
 	tdbQuerySpan, _ := tracer.StartSpanFromContext(ctx, "tdb.querySuggestedMetrics")
 	tdbQuerySpan.SetTag("projectID", projectID)
 	results, err := r.TDB.Query(ctx, query)
@@ -5660,7 +5709,7 @@ func (r *queryResolver) MetricTags(ctx context.Context, projectID int, metricNam
 	query := fmt.Sprintf(`
 		import "influxdata/influxdb/schema"
 		schema.tagKeys(bucket: "%s", predicate: (r) => r["_measurement"] == "%s" and r["_field"] == "%s")
-	`, r.TDB.GetBucket(strconv.Itoa(projectID)), timeseries.Metrics, metricName)
+	`, r.TDB.GetBucket(strconv.Itoa(projectID), timeseries.Metrics), timeseries.Metrics, metricName)
 	tdbQuerySpan, _ := tracer.StartSpanFromContext(ctx, "tdb.queryMetricTags")
 	tdbQuerySpan.SetTag("projectID", projectID)
 	tdbQuerySpan.SetTag("metricName", metricName)
@@ -5690,7 +5739,7 @@ func (r *queryResolver) MetricTagValues(ctx context.Context, projectID int, metr
 	query := fmt.Sprintf(`
 		import "influxdata/influxdb/schema"
 		schema.tagValues(bucket: "%s", tag: "%s", predicate: (r) => r["_measurement"] == "%s" and r["_field"] == "%s")
-	`, r.TDB.GetBucket(strconv.Itoa(projectID)), tagName, timeseries.Metrics, metricName)
+	`, r.TDB.GetBucket(strconv.Itoa(projectID), timeseries.Metrics), tagName, timeseries.Metrics, metricName)
 	tdbQuerySpan, _ := tracer.StartSpanFromContext(ctx, "tdb.queryMetricTagValues")
 	tdbQuerySpan.SetTag("projectID", projectID)
 	tdbQuerySpan.SetTag("metricName", metricName)
@@ -5720,7 +5769,7 @@ func (r *queryResolver) MetricsHistogram(ctx context.Context, projectID int, met
 		return nil, err
 	}
 
-	bucket, measurement := r.TDB.GetSampledMeasurement(r.TDB.GetBucket(strconv.Itoa(projectID)), timeseries.Metrics, params.DateRange.EndDate.Sub(*params.DateRange.StartDate))
+	bucket, measurement := r.TDB.GetSampledMeasurement(r.TDB.GetBucket(strconv.Itoa(projectID), timeseries.Metrics), timeseries.Metrics, params.DateRange.EndDate.Sub(*params.DateRange.StartDate))
 	div := CalculateMetricUnitConversion(MetricOriginalUnits(metricName), params.Units)
 	tagFilters := GetTagFilters(params.Filters)
 	if params.MinValue == nil || params.MaxValue == nil {
@@ -5857,7 +5906,7 @@ func (r *queryResolver) NetworkHistogram(ctx context.Context, projectID int, par
 		  |> sort(desc: true)
           |> limit(n: 10)
 		  |> yield(name: "count")
-	`, r.TDB.GetBucket(strconv.Itoa(projectID)), days, timeseries.Metrics, extraFiltersStr, params.Attribute.String())
+	`, r.TDB.GetBucket(strconv.Itoa(projectID), timeseries.Metrics), days, timeseries.Metrics, extraFiltersStr, params.Attribute.String())
 	networkHistogramSpan, _ := tracer.StartSpanFromContext(ctx, "tdb.queryTimeline")
 	networkHistogramSpan.SetTag("projectID", projectID)
 	networkHistogramSpan.SetTag("attribute", params.Attribute.String())
