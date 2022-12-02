ba925088f46ed2480dcf809d5d8d08a0894eeb52
SpennyNDaJets <Spencer22309@gmail.com>
2022-12-02 16:37:37 -0500

    A third test

--- frontend/src/pages/ErrorsV2/ErrorMetrics/multiple_environment_test_data.ts
+++ frontend/src/pages/ErrorsV2/ErrorMetrics/multiple_environment_test_data.ts
@@ -496,6 +496,254 @@ export const TEST_DATA = {
 				"label": "staging",
 				"value": 2,
 				"__typename": "ErrorDistributionItem"
+			},
+			{
+				"error_group_id": "37",
+				"date": "2022-12-01T17:36:00Z",
+				"name": "environmentCount",
+				"label": "demo",
+				"value": 13,
+				"__typename": "ErrorDistributionItem"
+			},
+			{
+				"error_group_id": "37",
+				"date": "2022-12-01T18:24:00Z",
+				"name": "environmentCount",
+				"label": "demo",
+				"value": 0,
+				"__typename": "ErrorDistributionItem"
+			},
+			{
+				"error_group_id": "37",
+				"date": "2022-12-01T19:12:00Z",
+				"name": "environmentCount",
+				"label": "demo",
+				"value": 7,
+				"__typename": "ErrorDistributionItem"
+			},
+			{
+				"error_group_id": "37",
+				"date": "2022-12-01T20:00:00Z",
+				"name": "environmentCount",
+				"label": "demo",
+				"value": 12,
+				"__typename": "ErrorDistributionItem"
+			},
+			{
+				"error_group_id": "37",
+				"date": "2022-12-01T20:48:00Z",
+				"name": "environmentCount",
+				"label": "demo",
+				"value": 4,
+				"__typename": "ErrorDistributionItem"
+			},
+			{
+				"error_group_id": "37",
+				"date": "2022-12-01T21:36:00Z",
+				"name": "environmentCount",
+				"label": "demo",
+				"value": 8,
+				"__typename": "ErrorDistributionItem"
+			},
+			{
+				"error_group_id": "37",
+				"date": "2022-12-01T22:24:00Z",
+				"name": "environmentCount",
+				"label": "demo",
+				"value": 0,
+				"__typename": "ErrorDistributionItem"
+			},
+			{
+				"error_group_id": "37",
+				"date": "2022-12-01T23:12:00Z",
+				"name": "environmentCount",
+				"label": "demo",
+				"value": 0,
+				"__typename": "ErrorDistributionItem"
+			},
+			{
+				"error_group_id": "37",
+				"date": "2022-12-02T00:00:00Z",
+				"name": "environmentCount",
+				"label": "demo",
+				"value": 0,
+				"__typename": "ErrorDistributionItem"
+			},
+			{
+				"error_group_id": "37",
+				"date": "2022-12-02T00:48:00Z",
+				"name": "environmentCount",
+				"label": "demo",
+				"value": 2,
+				"__typename": "ErrorDistributionItem"
+			},
+			{
+				"error_group_id": "37",
+				"date": "2022-12-02T01:36:00Z",
+				"name": "environmentCount",
+				"label": "demo",
+				"value": 1,
+				"__typename": "ErrorDistributionItem"
+			},
+			{
+				"error_group_id": "37",
+				"date": "2022-12-02T02:24:00Z",
+				"name": "environmentCount",
+				"label": "demo",
+				"value": 0,
+				"__typename": "ErrorDistributionItem"
+			},
+			{
+				"error_group_id": "37",
+				"date": "2022-12-02T03:12:00Z",
+				"name": "environmentCount",
+				"label": "demo",
+				"value": 1,
+				"__typename": "ErrorDistributionItem"
+			},
+			{
+				"error_group_id": "37",
+				"date": "2022-12-02T04:00:00Z",
+				"name": "environmentCount",
+				"label": "demo",
+				"value": 2,
+				"__typename": "ErrorDistributionItem"
+			},
+			{
+				"error_group_id": "37",
+				"date": "2022-12-02T04:48:00Z",
+				"name": "environmentCount",
+				"label": "demo",
+				"value": 0,
+				"__typename": "ErrorDistributionItem"
+			},
+			{
+				"error_group_id": "37",
+				"date": "2022-12-02T05:36:00Z",
+				"name": "environmentCount",
+				"label": "demo",
+				"value": 1,
+				"__typename": "ErrorDistributionItem"
+			},
+			{
+				"error_group_id": "37",
+				"date": "2022-12-02T06:24:00Z",
+				"name": "environmentCount",
+				"label": "demo",
+				"value": 0,
+				"__typename": "ErrorDistributionItem"
+			},
+			{
+				"error_group_id": "37",
+				"date": "2022-12-02T07:12:00Z",
+				"name": "environmentCount",
+				"label": "demo",
+				"value": 3,
+				"__typename": "ErrorDistributionItem"
+			},
+			{
+				"error_group_id": "37",
+				"date": "2022-12-02T08:00:00Z",
+				"name": "environmentCount",
+				"label": "demo",
+				"value": 0,
+				"__typename": "ErrorDistributionItem"
+			},
+			{
+				"error_group_id": "37",
+				"date": "2022-12-02T08:48:00Z",
+				"name": "environmentCount",
+				"label": "demo",
+				"value": 0,
+				"__typename": "ErrorDistributionItem"
+			},
+			{
+				"error_group_id": "37",
+				"date": "2022-12-02T09:36:00Z",
+				"name": "environmentCount",
+				"label": "demo",
+				"value": 1,
+				"__typename": "ErrorDistributionItem"
+			},
+			{
+				"error_group_id": "37",
+				"date": "2022-12-02T10:24:00Z",
+				"name": "environmentCount",
+				"label": "demo",
+				"value": 0,
+				"__typename": "ErrorDistributionItem"
+			},
+			{
+				"error_group_id": "37",
+				"date": "2022-12-02T11:12:00Z",
+				"name": "environmentCount",
+				"label": "demo",
+				"value": 0,
+				"__typename": "ErrorDistributionItem"
+			},
+			{
+				"error_group_id": "37",
+				"date": "2022-12-02T12:00:00Z",
+				"name": "environmentCount",
+				"label": "demo",
+				"value": 0,
+				"__typename": "ErrorDistributionItem"
+			},
+			{
+				"error_group_id": "37",
+				"date": "2022-12-02T12:48:00Z",
+				"name": "environmentCount",
+				"label": "demo",
+				"value": 0,
+				"__typename": "ErrorDistributionItem"
+			},
+			{
+				"error_group_id": "37",
+				"date": "2022-12-02T13:36:00Z",
+				"name": "environmentCount",
+				"label": "demo",
+				"value": 12,
+				"__typename": "ErrorDistributionItem"
+			},
+			{
+				"error_group_id": "37",
+				"date": "2022-12-02T14:24:00Z",
+				"name": "environmentCount",
+				"label": "demo",
+				"value": 1,
+				"__typename": "ErrorDistributionItem"
+			},
+			{
+				"error_group_id": "37",
+				"date": "2022-12-02T15:12:00Z",
+				"name": "environmentCount",
+				"label": "demo",
+				"value": 0,
+				"__typename": "ErrorDistributionItem"
+			},
+			{
+				"error_group_id": "37",
+				"date": "2022-12-02T16:00:00Z",
+				"name": "environmentCount",
+				"label": "demo",
+				"value": 0,
+				"__typename": "ErrorDistributionItem"
+			},
+			{
+				"error_group_id": "37",
+				"date": "2022-12-02T16:48:00Z",
+				"name": "environmentCount",
+				"label": "demo",
+				"value": 1,
+				"__typename": "ErrorDistributionItem"
+			},
+			{
+				"error_group_id": "37",
+				"date": "2022-12-02T17:24:00Z",
+				"name": "environmentCount",
+				"label": "demo",
+				"value": 1,
+				"__typename": "ErrorDistributionItem"
 			}
 		]
 	}
