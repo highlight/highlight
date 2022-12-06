750c5b620a1aa2536defa8281d26dd15bd17fb66
Chris Schmitz <chris@highlight.io>
2022-12-06 14:19:46 -0600

    Error page updates for unauthenticated users (#3396)

--- frontend/src/pages/Error/utils/ErrorPageUIConfiguration.ts
+++ frontend/src/pages/Error/utils/ErrorPageUIConfiguration.ts
@@ -1,18 +1,28 @@
+import { useAuthContext } from '@authentication/AuthContext'
 import useLocalStorage from '@rehooks/local-storage'
+
+const DEFAULT_STATE = {
+	showLeftPanel: false,
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	setShowLeftPanel: (_: boolean) => {},
+}
 
 /**
  * Gets configuration for the Error Page.
  */
-const useErrorPageConfiguration = () => {
+const useErrorPageConfiguration = (): typeof DEFAULT_STATE => {
+	const { isLoggedIn } = useAuthContext()
 	const [showLeftPanel, setShowLeftPanel] = useLocalStorage<boolean>(
 		'highlightErrorPageShowLeftPanel',
 		false,
 	)
 
-	return {
-		showLeftPanel,
-		setShowLeftPanel,
-	}
+	return isLoggedIn
+		? {
+				showLeftPanel,
+				setShowLeftPanel,
+		  }
+		: DEFAULT_STATE
 }
 
 export default useErrorPageConfiguration
