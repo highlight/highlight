6399088534bc79356d3eb9884630ef4576a160ab
Chris Schmitz <ccschmitz@gmail.com>
2022-12-22 07:57:24 -0600

    Revert tooltip change

--- packages/ui/src/components/Tooltip/Tooltip.tsx
+++ packages/ui/src/components/Tooltip/Tooltip.tsx
@@ -37,7 +37,7 @@ const TooltipRenderer: React.FC<React.PropsWithChildren> = ({ children }) => {
 	return (
 		<Box
 			backgroundColor="white"
-			border="primary"
+			border="secondary"
 			p="4"
 			borderRadius="6"
 			shadow="small"
