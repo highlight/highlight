8169c3c276de64a2a0c66ca57f28be94b528356a
Eric Thomas <eric@highlight.io>
2023-01-05 14:00:33 -0700

    lint

--- frontend/src/pages/IntegrationsPage/components/common/useIsProjectIntegratedWith.ts
+++ frontend/src/pages/IntegrationsPage/components/common/useIsProjectIntegratedWith.ts
@@ -3,29 +3,28 @@ import { IntegrationType } from '@graph/schemas'
 import { useApplicationContext } from '@routers/OrgRouter/ApplicationContext'
 
 const useIsProjectIntegratedWith = (integrationType: IntegrationType) => {
-    const { currentProject } = useApplicationContext()
+	const { currentProject } = useApplicationContext()
 	const projectIdStr = currentProject?.id ?? ''
 
-    const { data, loading }  = useGetProjectIntegratedWithQuery({
+	const { data, loading } = useGetProjectIntegratedWithQuery({
 		variables: {
 			project_id: projectIdStr,
-			integration_type: integrationType
+			integration_type: integrationType,
 		},
 		skip: !currentProject,
 	})
 
-    if (loading) {
-        return {
-            loading: true,
-            isIntegrated: undefined
-        }
-    } else {
-        return {
-            loading: false,
-            isIntegrated: data?.is_project_integrated_with ?? false
-        }
-
-    }
+	if (loading) {
+		return {
+			loading: true,
+			isIntegrated: undefined,
+		}
+	} else {
+		return {
+			loading: false,
+			isIntegrated: data?.is_project_integrated_with ?? false,
+		}
+	}
 }
 
 export { useIsProjectIntegratedWith }
