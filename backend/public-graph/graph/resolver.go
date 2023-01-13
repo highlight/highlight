1cbe5b1aa104a604f0ebabd5077a94f8364082af
Zane Mayberry <mayberryzane@gmail.com>
2023-01-13 11:46:56 -0800

    set AvoidPostgresStorage = true

--- backend/public-graph/graph/resolver.go
+++ backend/public-graph/graph/resolver.go
@@ -1193,7 +1193,7 @@ func (r *Resolver) InitializeSessionImpl(ctx context.Context, input *kafka_queue
 		ClientID:                       input.ClientID,
 		Excluded:                       &model.T, // A session is excluded by default until it receives events
 		ProcessWithRedis:               true,
-		AvoidPostgresStorage:           false,
+		AvoidPostgresStorage:           true,
 	}
 
 	// determine if session is within billing quota
