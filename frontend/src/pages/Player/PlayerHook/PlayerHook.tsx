1cc57172cbb510721d1d540bb843780b1ce89651
Vadim Korolik <vkorolik@gmail.com>
2023-01-19 15:24:28 -0800

    fix session playback getting stuck on chunk boundaries

--- frontend/src/pages/Player/PlayerHook/PlayerHook.tsx
+++ frontend/src/pages/Player/PlayerHook/PlayerHook.tsx
@@ -297,26 +297,24 @@ export const usePlayer = (): ReplayerContextInterface => {
 				: startIdx
 			if (forceLoadNext) endIdx += 1
 
-			// whether we need to pause the player because we need to load the destination chunk
-			let blockingLoad = false
 			const promises = []
 			log(
-				'PlayerHook.tsx',
+				'PlayerHook.tsx:ensureChunksLoaded',
 				'checking chunk loaded status range',
 				startIdx,
 				endIdx,
 			)
 			for (let i = startIdx; i <= endIdx; i++) {
 				log(
-					'PlayerHook.tsx',
+					'PlayerHook.tsx:ensureChunksLoaded',
 					'hasChunk',
 					i,
 					chunkEventsRef.current.has(i),
 				)
 				if (loadingChunks.current.has(i)) {
 					log(
-						'PlayerHook.tsx',
-						'ensureChunksLoaded waiting for loading chunk',
+						'PlayerHook.tsx:ensureChunksLoaded',
+						'waiting for loading chunk',
 						i,
 					)
 				} else if (!chunkEventsRef.current.has(i)) {
@@ -327,11 +325,10 @@ export const usePlayer = (): ReplayerContextInterface => {
 					if (!promises.length) {
 						if (action || i == startIdx) {
 							log(
-								'PlayerHook.tsx',
-								'ensureChunksLoaded needs blocking load for chunk',
+								'PlayerHook.tsx:ensureChunksLoaded',
+								'needs blocking load for chunk',
 								i,
 							)
-							blockingLoad = true
 							dispatch({
 								type: PlayerActionType.startChunksLoad,
 							})
@@ -353,7 +350,11 @@ export const usePlayer = (): ReplayerContextInterface => {
 										await chunkResponse.json(),
 									),
 								)
-								log('PlayerHook.tsx', 'set data for chunk', _i)
+								log(
+									'PlayerHook.tsx:ensureChunksLoaded',
+									'set data for chunk',
+									_i,
+								)
 							} catch (e: any) {
 								H.consumeError(
 									e,
@@ -376,7 +377,7 @@ export const usePlayer = (): ReplayerContextInterface => {
 				if (currentChunkIdx.current) {
 					toRemove.delete(currentChunkIdx.current)
 				}
-				log('PlayerHook.tsx', 'getChunksToRemove', {
+				log('PlayerHook.tsx:ensureChunksLoaded', 'getChunksToRemove', {
 					after: chunkEventsRef.current,
 					toRemove,
 				})
@@ -386,10 +387,9 @@ export const usePlayer = (): ReplayerContextInterface => {
 				await Promise.all(promises)
 				// check that the target chunk has not moved since we started the loading.
 				// eg. if we start loading, then someone clicks to a new spot, we should cancel first action.
-				if (blockingLoad && startTime === targetTime.current) {
+				if (startTime === targetTime.current) {
 					log(
-						'PlayerHook.tsx',
-						'ensureChunksLoaded',
+						'PlayerHook.tsx:ensureChunksLoaded',
 						'calling dispatchAction due to loading',
 						{
 							time: lastTimeRef.current,
@@ -402,8 +402,7 @@ export const usePlayer = (): ReplayerContextInterface => {
 					targetTime.current = undefined
 				} else {
 					log(
-						'PlayerHook.tsx',
-						'ensureChunksLoaded',
+						'PlayerHook.tsx:ensureChunksLoaded',
 						'canceling dispatchAction',
 						{
 							startTime,
@@ -414,8 +413,7 @@ export const usePlayer = (): ReplayerContextInterface => {
 				}
 			} else if (!loadingChunks.current.has(startIdx) && action) {
 				log(
-					'PlayerHook.tsx',
-					'ensureChunksLoaded',
+					'PlayerHook.tsx:ensureChunksLoaded',
 					'calling dispatchAction due to action',
 					{
 						startTime,
