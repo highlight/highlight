fd59e3dbfe63b2bc995613fd9c739e349ad0d5db
Vadim Korolik <vkorolik@gmail.com>
2023-01-24 19:17:57 -0800

    bundle startup into docker compose

--- docker/run.sh
+++ docker/run.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
 
 # startup the entire app
-doppler run -- docker compose up -d --build --remove-orphans
-
-# configure influxdb
-docker compose exec influxdb bash -c 'influx setup --skip-verify --bucket dev-bucket --org dev-org --username dev --password devdevdevdev --retention 0 --token not-a-secure-token --force'
-
-# configure elasticsearch indexes
-doppler run -- docker compose run backend bash -c '/bin/backend -runtime=worker -worker-handler=init-opensearch'
+doppler run -- docker compose up -d --build
