0244d3d46a57fd1ceb9cde6a46ba41a8d261137e
Eric Thomas <eric@highlight.io>
2023-01-25 15:01:50 -0700

    use less parsing in time test

--- backend/clickhouse/logs_test.go
+++ backend/clickhouse/logs_test.go
@@ -2,7 +2,6 @@ package clickhouse
 
 import (
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -29,7 +28,7 @@ func TestMakeLogRow(t *testing.T) {
 	assert.Equal(t, uint32(projectID), logRow.ProjectId)
 	assert.Equal(t, secureSessionID, logRow.SecureSessionID)
 	assert.Equal(t, "info", logRow.SeverityText)
-	assert.Equal(t, time.Time(time.Date(2023, time.January, 25, 14, 24, 24, 244000000, time.Local)), logRow.Timestamp)
+	assert.Equal(t, int64(1674681864244), logRow.Timestamp.UnixMilli())
 	assert.Equal(t,
 		"Sending: 120 events, 2 messages, 0 network resources, 0 errors \nTo: https://localhost:8082/public\nOrg: 1\nSessionSecureID: 0L4pKSh2LaG51f1Nq3jYb17OCiaM",
 		logRow.Body,
