edbeb4b39045538a806eaf217d8a58905ec99bb1
Vadim Korolik <vkorolik@gmail.com>
2023-01-24 16:06:44 -0800

    fix otel collector

--- backend/otel/otel.go
+++ backend/otel/otel.go
@@ -2,24 +2,36 @@ package otel
 
 import (
 	"github.com/go-chi/chi"
+	private "github.com/highlight-run/highlight/backend/private-graph/graph"
 	log "github.com/sirupsen/logrus"
 	"io"
 	"net/http"
 )
 
-const OTELPort = "4318"
+const OTELPort = "4319"
 
-// HandleTokenRequest will process a request for oauth /token command per the RFC spec.
-func Handle(w http.ResponseWriter, r *http.Request) {
+func HandleTrace(w http.ResponseWriter, r *http.Request) {
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 	}
-	log.Infof("otel %s", body)
+	log.Infof("otel trace %s", body)
+}
+
+func HandleLog(w http.ResponseWriter, r *http.Request) {
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+	log.Infof("otel log %s", body)
 }
 
 func Listen() {
 	r := chi.NewMux()
-	r.HandleFunc("/", Handle)
+	r.Route("/v1", func(r chi.Router) {
+		r.Use(private.PrivateMiddleware)
+		r.HandleFunc("/traces", HandleTrace)
+		r.HandleFunc("/logs", HandleLog)
+	})
 	log.Fatal(http.ListenAndServe(":"+OTELPort, r))
 }
