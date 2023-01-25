23623ad8fd1189addbcbaa6a942f3cd4907df9cd
Vadim Korolik <vkorolik@gmail.com>
2023-01-24 18:45:28 -0800

    tweaks

--- docker/run.sh
+++ docker/run.sh
@@ -4,7 +4,7 @@
 doppler run -- docker compose up -d --build --remove-orphans
 
 # configure influxdb
-docker compose exec influxdb bash -c 'influx setup --skip-verify --bucket dev_bucket --org dev_org --username dev --password devdevdevdev --retention 0 --token not_a_secure_token --force'
+docker compose exec influxdb bash -c 'influx setup --skip-verify --bucket dev-bucket --org dev-org --username dev --password devdevdevdev --retention 0 --token not-a-secure-token --force'
 
 # configure elasticsearch indexes
 doppler run -- docker compose run backend bash -c '/bin/backend -runtime=worker -worker-handler=init-opensearch'
