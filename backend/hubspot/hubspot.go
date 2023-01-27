6fb911f2e0ec044915d2f75cbfe17639b52edb7f
Chris Schmitz <ccschmitz@gmail.com>
2023-01-27 15:33:33 -0600

    Update Hubspot referral property on contacts

--- backend/hubspot/hubspot.go
+++ backend/hubspot/hubspot.go
@@ -74,8 +74,8 @@ func (h *HubspotApi) CreateContactForAdmin(adminID int, email string, userDefine
 				Value:    phone,
 			},
 			{
-				Property: "referral",
-				Name:     "referral",
+				Property: "referral_url",
+				Name:     "referral_url",
 				Value:    phone,
 			},
 		},
