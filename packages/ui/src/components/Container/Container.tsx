1dc3fc2a7f4aaaca56efc5ceb8d6f2f77df9cde1
Chris Schmitz <chris@highlight.io>
2023-02-03 16:05:01 -0600

    Fix inconsistent page width on errors (#4139)

--- packages/ui/src/components/Container/Container.tsx
+++ packages/ui/src/components/Container/Container.tsx
@@ -3,20 +3,25 @@ import { Box, BoxProps } from '../Box/Box'
 
 import * as styles from './styles.css'
 
-type Props = React.PropsWithChildren & BoxProps & { centered?: boolean }
+type Props = React.PropsWithChildren &
+	BoxProps & { centered?: boolean; padded?: boolean }
 
 export const Container: React.FC<Props> = ({
 	children,
 	centered = true,
+	padded = true,
 	...props
 }) => {
 	return (
-		<Box
-			cssClass={styles.container}
-			{...props}
-			mx={centered ? 'auto' : undefined}
-		>
-			{children}
+		<Box cssClass={styles.container}>
+			<Box
+				cssClass={styles.content}
+				px={padded ? '20' : undefined}
+				mx={centered ? 'auto' : undefined}
+				{...props}
+			>
+				{children}
+			</Box>
 		</Box>
 	)
 }
