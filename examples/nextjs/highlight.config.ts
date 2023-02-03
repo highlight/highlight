8a015019c9e52ae07388a40388cffde293d3b491
Vadim Korolik <vkorolik@gmail.com>
2023-02-02 16:44:22 -0800

    update backend url

--- examples/nextjs/highlight.config.ts
+++ examples/nextjs/highlight.config.ts
@@ -3,5 +3,6 @@ import { Highlight } from '@highlight-run/next'
 export const withHighlight = Highlight({
 	projectID: '1jdkoe52',
 	debug: true,
+	backendUrl: 'http://localhost:8082/public',
 	otlpEndpoint: 'http://localhost:4318',
 })
