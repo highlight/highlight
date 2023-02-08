2ebc1ecc2f89f1222189a10750c0c693977eb713
Sasha Aptlin <17913919+aptlin@users.noreply.github.com>
2023-02-08 09:59:16 -0800

    fix: detailed panel cleanup

--- frontend/src/pages/Player/context/PlayerUIContext.ts
+++ frontend/src/pages/Player/context/PlayerUIContext.ts
@@ -4,19 +4,6 @@ import { NetworkResource } from '@pages/Player/Toolbar/DevToolsWindowV2/utils'
 import { createContext } from '@util/context/context'
 import React from 'react'
 
-interface DetailedPanelOptions {
-	noHeader?: boolean
-	noPadding?: boolean
-}
-
-export interface DetailedPanel {
-	title: string | React.ReactNode
-	content: React.ReactNode
-	options?: DetailedPanelOptions
-	/** The ID of the object that is being showed details for. */
-	id: string
-}
-
 export enum RightPanelView {
 	Error = 'ERROR',
 	NetworkResource = 'NETWORK_RESOURCE',
