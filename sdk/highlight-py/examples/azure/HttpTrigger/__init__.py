91a7473e5119eae7bf06488e219cc71a26c9b4d9
Vadim Korolik <vkorolik@gmail.com>
2023-02-13 15:05:21 -0800

    switch to v1 example

---
+++ sdk/highlight-py/examples/azure/HttpTrigger/__init__.py
@@ -0,0 +1,34 @@
+import logging
+
+import azure.functions as func
+
+import highlight_io
+from highlight_io.integrations.azure import observe_handler
+
+H = highlight_io.H("TODO-PROJECT-ID", record_logs=True)
+
+
+@observe_handler
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    x = 2
+    if x + 2 == 4:
+        raise ValueError('oh no!')
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    else:
+        return func.HttpResponse(
+            "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+            status_code=200
+        )
