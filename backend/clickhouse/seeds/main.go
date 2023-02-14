7a6090f5f5d54768aaddafb46fce52b678a5bf08
Eric Thomas <eric@highlight.io>
2023-02-13 23:02:28 -0700

    add seed script

---
+++ backend/clickhouse/seeds/main.go
@@ -0,0 +1,138 @@
+package main
+
+import (
+	"context"
+	"math/rand"
+	"strconv"
+	"time"
+
+	"github.com/highlight-run/highlight/backend/clickhouse"
+	log "github.com/sirupsen/logrus"
+)
+
+func makeRandTime() time.Time {
+	now := time.Now()
+
+	randomTimes := [3]time.Time{
+		now.Add(-time.Hour * 72),
+		now.Add(-time.Hour * 144),
+		now,
+	}
+
+	return randomTimes[rand.Intn(len(randomTimes))]
+}
+
+func makeRandProjectId() uint32 {
+	projectIDs := make([]uint32, 0)
+	projectIDs = append(projectIDs, 1, 2, 3, 4, 5)
+	return projectIDs[rand.Intn(len(projectIDs))]
+}
+
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func makeRandomBody() string {
+	b := make([]rune, 30)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
+}
+
+func makeRandLogAttributes() map[string]string {
+	randomKeys := [20]string{
+		"key1",
+		"key2",
+		"key3",
+		"key4",
+		"key5",
+		"key6",
+		"key7",
+		"key8",
+		"key9",
+		"key10",
+		"key11",
+		"key12",
+		"key13",
+		"key14",
+		"key15",
+		"key16",
+		"key17",
+		"key18",
+		"key19",
+		"key20",
+	}
+
+	randomVals := [20]string{
+		"val1",
+		"val2",
+		"val3",
+		"val4",
+		"val5",
+		"val6",
+		"val7",
+		"val8",
+		"val9",
+		"val10",
+		"val11",
+		"val12",
+		"val13",
+		"val14",
+		"val15",
+		"val16",
+		"val17",
+		"val18",
+		"val19",
+		"val20",
+	}
+
+	randomServices := [6]string{
+		"backend",
+		"frontend",
+		"middleware",
+		"logger",
+		"imageparser",
+		"flounder",
+	}
+
+	logAttributes := map[string]string{}
+
+	randomKey := randomKeys[rand.Intn(len(randomKeys))]
+	randomVal := randomVals[rand.Intn(len(randomVals))]
+	randomService := randomServices[rand.Intn(len(randomServices))]
+
+	logAttributes["workspace_id"] = strconv.Itoa(rand.Intn(100))
+	logAttributes["user_id"] = strconv.Itoa(rand.Intn(100))
+	logAttributes["service_name"] = randomService
+	logAttributes[randomKey] = randomVal
+
+	return logAttributes
+}
+
+// Run via
+// `doppler run -- go run backend/clickhouse/seeds/main.go“
+func main() {
+	client, err := clickhouse.NewClient(clickhouse.PrimaryDatabase)
+
+	if err != nil {
+		log.Fatal("could not connect to clickhouse db")
+	}
+
+	logRows := []*clickhouse.LogRow{}
+
+	for i := 1; i < 10000; i++ {
+		for j := 1; j < 10000; j++ {
+			logRows = append(logRows, &clickhouse.LogRow{
+				Timestamp:     makeRandTime(),
+				ProjectId:     makeRandProjectId(),
+				Body:          makeRandomBody(),
+				LogAttributes: makeRandLogAttributes(),
+			})
+		}
+
+		err = client.BatchWriteLogRows(context.Background(), logRows)
+
+		if err != nil {
+			log.Fatalf("failed to write log row data: %v", err)
+		}
+	}
+}
