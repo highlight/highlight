c6687dc8cec9812daa2e63dce4634df9b778ffdc
Eric Thomas <et@users.noreply.github.com>
2023-02-14 20:51:52 -0700

    add a datepicker to ui package library (#4245)
    ## Summary
    
    <!--
    Ideally, there is an attached Linear ticket that will describe the
    "why".
    
    If relevant, use this section to call out any additional information
    you'd like to _highlight_ to the reviewer.
    -->
    
    This adds a datepicker component to our UI library. You'll see that that
    there are several top level components here, but the top level
    component, `PreviousDateRangePicker`, handles showing a dropdown with
    quick time ranges (e.g. Last 4 hours):
    
    ![Screenshot 2023-02-14 at 2 50 58
    PM](https://user-images.githubusercontent.com/58678/218871286-1e4c537b-0400-4dc7-94c1-9e079fbb926f.png)
    
    along with the ability to select a custom date:
    ![Screenshot 2023-02-14 at 2 51 38
    PM](https://user-images.githubusercontent.com/58678/218871385-ccb97d89-6fc5-4e21-a79e-62ddb86a40e0.png)
    
    We need to do a bit more polish on this but it's a good start to
    creating a timepicker that has no dependency to another UI kit (e.g.
    antd).
    
    ## How did you test this change?
    
    <!--
    Frontend - Leave a screencast or a screenshot to visually describe the
    changes.
    -->
    
    Did a general click test. See loom:
    
    https://www.loom.com/share/cf84da862684450b8491d4c9346ad760
    
    ## Are there any deployment considerations?
    
    <!--
     Backend - Do we need to consider migrations or backfilling data?
    -->
    
    N/A

---
+++ packages/ui/src/components/DatePicker/PreviousDateRangePicker.test.tsx
@@ -0,0 +1,38 @@
+import { Preset, getLabel } from './PreviousDateRangePicker'
+import { subtractDays, subtractHours } from './utils'
+
+const now = new Date(1676410246855) // Feb 14 2023
+const fourHoursAgo = subtractHours(now, 4)
+
+const presets: Preset[] = [
+	{
+		label: 'Last 4 hours',
+		startDate: fourHoursAgo,
+	},
+	{
+		label: 'Last 12 hours',
+		startDate: subtractHours(now, 12),
+	},
+	{
+		label: 'Last 24 hours',
+		startDate: subtractHours(now, 24),
+	},
+	{
+		label: 'Last 3 days',
+		startDate: subtractDays(now, 3),
+	},
+]
+
+describe('getLabel', () => {
+	it('finds the matching preset', async () => {
+		const selectedDates = [fourHoursAgo, now]
+		const label = getLabel({ selectedDates, presets })
+		expect(label).toEqual('Last 4 hours')
+	})
+
+	it('handles when the preset does not match', async () => {
+		const selectedDates = [subtractDays(now, 2), now]
+		const label = getLabel({ selectedDates, presets })
+		expect(label).toEqual('Sun Feb 12 2023 - Tue Feb 14 2023')
+	})
+})
---
+++ packages/ui/src/components/DatePicker/utils.ts
@@ -0,0 +1,13 @@
+const subtractDays = (date: Date, days: number) => {
+	const offset = 24 * 60 * 60 * 1000 * days
+	return new Date(date.getTime() - offset)
+}
+
+const subtractHours = (date: Date, hours: number) => {
+	const newDate = new Date(date.getTime())
+	newDate.setHours(newDate.getHours() - hours)
+
+	return newDate
+}
+
+export { subtractDays, subtractHours }
