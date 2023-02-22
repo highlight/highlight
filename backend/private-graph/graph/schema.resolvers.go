b2528c1a454caf31f91ebf09af8d662c9449ef4b
Vadim Korolik <vkorolik@gmail.com>
2023-02-21 18:51:22 -0800

    fix prod bugs

--- backend/private-graph/graph/schema.resolvers.go
+++ backend/private-graph/graph/schema.resolvers.go
@@ -982,11 +982,6 @@ func (r *mutationResolver) ChangeAdminRole(ctx context.Context, workspaceID int,
 		return false, e.New("A admin tried changing their own role.")
 	}
 
-	// TODO(vkorolik) workspace admins table
-	if err := r.DB.Model(&model.Admin{Model: model.Model{ID: adminID}}).Update("Role", newRole).Error; err != nil {
-		return false, e.Wrap(err, "error updating admin role")
-	}
-
 	if err := r.DB.Model(&model.WorkspaceAdmin{AdminID: adminID, WorkspaceID: workspaceID}).Update("Role", newRole).Error; err != nil {
 		return false, e.Wrap(err, "error updating workspace_admin role")
 	}
@@ -3964,11 +3959,11 @@ func (r *queryResolver) ErrorInstance(ctx context.Context, errorGroupSecureID st
 		Order("id desc")
 
 	if errorObjectID == nil {
-		// TODO(vkorolik) needs limit on elements here
 		var sessionIds []int
 		if err := r.DB.Model(&errorObject).
 			Where(&model.ErrorObject{ErrorGroupID: errorGroup.ID}).
 			Where("session_id is not null").
+			Limit(100).
 			Pluck("session_id", &sessionIds).
 			Error; err != nil {
 			return nil, e.Wrap(err, "error reading session ids")
