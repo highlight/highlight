93bfea864440a1976ac945ba2b40a34cf3b53479
Vadim Korolik <vadim@highlight.io>
2023-02-22 22:24:17 -0800

    add a fastapi middleware (#4368)
    ## Summary
    
    Adds a new fastapi middleware for tracing requests and capturing errors.
    
    Basic usage:
    ```python
    
    from fastapi import FastAPI
    
    import highlight_io
    from highlight_io.integrations.fastapi import FastAPIMiddleware
    
    H = highlight_io.H("YOUR_PROJECT_ID", record_logs=True)
    app = FastAPI()
    app.add_middleware(FastAPIMiddleware)
    
    @app.get("/")
    async def root():
        return {"message": "Hello World"}
    
    
    ```
    
    ## How did you test this change?
    
    New unit test, new e2e flaskapi app (locally deployed sending errors).
    
    ## Are there any deployment considerations?
    
    New python SDK version 0.3.0

---
+++ sdk/highlight-py/tests/test_fastapi.py
@@ -0,0 +1,47 @@
+from contextlib import nullcontext
+
+from fastapi import Request, Response
+
+import pytest
+
+from highlight_io import H
+from highlight_io.integrations.fastapi import FastAPIMiddleware
+
+
+@pytest.fixture(params=[False, True])
+def highlight_setup(request):
+    H._instance = None
+    try:
+        if request.param:
+            yield H(
+                project_id="1",
+                record_logs=True,
+                integrations=[],
+                otlp_endpoint="http://localhost:4318",
+            )
+        else:
+            yield
+    finally:
+        H._instance = None
+
+
+@pytest.mark.parametrize("exception", [False, True])
+@pytest.mark.asyncio
+async def test_fastapi(mocker, highlight_setup, exception):
+    app = mocker.MagicMock()
+    middleware = FastAPIMiddleware(app)
+    request = mocker.MagicMock()
+
+    async def call_next(_: Request) -> Response:
+        if exception:
+            raise ValueError()
+        return Response()
+
+    ctx = nullcontext()
+    if not highlight_setup:
+        ctx = pytest.raises(NotImplementedError)
+    elif exception:
+        ctx = pytest.raises(ValueError)
+
+    with ctx:
+        await middleware.dispatch(request, call_next)
