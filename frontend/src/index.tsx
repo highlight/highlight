c459954a12c565b11ae98d628caf01efe20c2d0e
Vadim Korolik <vadim@highlight.io>
2023-02-23 14:30:06 -0800

    fix docker ci (#4382)
    ## Summary
    
    Fix docker github action.
    
    ## How did you test this change?
    
    Local docker.yml invocation.
    ![Screenshot 2023-02-23 at 1 25 39
    PM](https://user-images.githubusercontent.com/1351531/221033810-c3b19e60-f2da-4ddd-9c8d-018e1ec63e6e.png)
    
    ## Are there any deployment considerations?
    
    No. Will allow manually triggering `docker` github action in PRs.

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -41,16 +41,17 @@ import { showIntercom } from '@util/window'
 import { H, HighlightOptions } from 'highlight.run'
 import { parse, stringify } from 'query-string'
 import React, { useEffect, useState } from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import { Helmet } from 'react-helmet'
 import { SkeletonTheme } from 'react-loading-skeleton'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { QueryParamProvider } from 'use-query-params'
 import { ReactRouter6Adapter } from 'use-query-params/adapters/react-router-6'
-import { createRoot } from 'react-dom/client'
 
 analytics.initialize()
-const dev = import.meta.env.DEV
+const dev =
+	import.meta.env.DEV ||
+	import.meta.env.REACT_APP_FRONTEND_URI?.indexOf('localhost') !== -1
 const options: HighlightOptions = {
 	debug: { clientInteractions: true, domRecording: true },
 	manualStart: true,
@@ -91,7 +92,7 @@ const options: HighlightOptions = {
 const favicon = document.querySelector("link[rel~='icon']") as any
 if (dev) {
 	options.scriptUrl = 'http://localhost:8080/dist/index.js'
-	options.backendUrl = 'https://localhost:8082/public'
+	options.backendUrl = import.meta.env.REACT_APP_PUBLIC_GRAPH_URI
 
 	options.integrations = undefined
 
