199dd873d8d769f34e6e82a459d5042502ed29e6
Eric Thomas <eric@highlight.io>
2023-02-24 16:21:55 -0700

    use clickhouse default for bloom_filter

--- backend/clickhouse/migrations/000004_create_new_logs_table.up.sql
+++ backend/clickhouse/migrations/000004_create_new_logs_table.up.sql
@@ -13,12 +13,12 @@ CREATE TABLE logs
     LogAttributes      Map(LowCardinality(String), String) CODEC (ZSTD(1)),
     ProjectId          UInt32 CODEC (ZSTD(1)),
     SecureSessionId    String CODEC (ZSTD(1)),
-    INDEX idx_trace_id          TraceId TYPE bloom_filter(0.001) GRANULARITY 1,
-    INDEX idx_secure_session_id SecureSessionId TYPE bloom_filter(0.001) GRANULARITY 1,
-    INDEX idx_res_attr_key      mapKeys(ResourceAttributes) TYPE bloom_filter(0.01) GRANULARITY 1,
-    INDEX idx_res_attr_value    mapValues(ResourceAttributes) TYPE bloom_filter(0.01) GRANULARITY 1,
-    INDEX idx_log_attr_key      mapKeys(LogAttributes) TYPE bloom_filter(0.01) GRANULARITY 1,
-    INDEX idx_log_attr_value    mapValues(LogAttributes) TYPE bloom_filter(0.01) GRANULARITY 1,
+    INDEX idx_trace_id          TraceId TYPE bloom_filter GRANULARITY 1,
+    INDEX idx_secure_session_id SecureSessionId TYPE bloom_filter GRANULARITY 1,
+    INDEX idx_res_attr_key      mapKeys(ResourceAttributes) TYPE bloom_filter GRANULARITY 1,
+    INDEX idx_res_attr_value    mapValues(ResourceAttributes) TYPE bloom_filter GRANULARITY 1,
+    INDEX idx_log_attr_key      mapKeys(LogAttributes) TYPE bloom_filter GRANULARITY 1,
+    INDEX idx_log_attr_value    mapValues(LogAttributes) TYPE bloom_filter GRANULARITY 1,
     INDEX idx_body              Body TYPE tokenbf_v1(32768, 3, 0) GRANULARITY 1
 )
     ENGINE = MergeTree
