1dc64766eacbc06425236e3c16b3f0a9c70711e9
Vadim Korolik <vkorolik@gmail.com>
2023-02-27 14:48:48 -0800

    add support for next.js generateBuildId

--- e2e/nextjs/next.config.js
+++ e2e/nextjs/next.config.js
@@ -2,6 +2,7 @@ const { withHighlightConfig } = require('@highlight-run/next')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	generateBuildId: () => nextBuildId({ dir: __dirname }),
 	experimental: {
 		appDir: true,
 	},
