c7fb24a969c2883b7612822271ab763418c9c0ba
Vadim Korolik <vkorolik@gmail.com>
2023-03-02 11:24:37 -0800

    better lambda example

--- sdk/highlight-py/e2e/highlight_aws/lambda_function.py
+++ sdk/highlight-py/e2e/highlight_aws/lambda_function.py
@@ -5,14 +5,17 @@ from datetime import datetime
 import highlight_io
 from highlight_io.integrations.aws import observe_handler
 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
 H = highlight_io.H("1", record_logs=True)
 
 
 @observe_handler
 def lambda_handler(event, context):
     start = datetime.now()
-    logging.info("Python HTTP trigger function processed a request.")
-    logging.info(
+    logger.info(f"Python AWS Lambda request {event}, {context}")
+    logger.info(
         "Python AWS Lambda hello handler",
         {"customer": event.get("customer") or "unknown"},
     )
@@ -20,15 +23,15 @@ def lambda_handler(event, context):
     if random.random() < 0.2:
         raise ValueError("oh no!")
 
-    name = event.get("name")
-
-    logging.info(
+    name = event.get("name") or 'unknown'
+    customer = event.get("customer") or "unknown"
+    logger.info(
         "Python AWS Lambda got name",
         {
-            "customer": event.get("customer") or "unknown",
+            "customer": customer,
             "name": name,
             "float": 1.2345,
-            "duration": datetime.now() - start,
+            "duration": (datetime.now() - start).total_seconds(),
         },
     )
 
