highlight/highlight

commit 29f8114b54620d5ede14d6c3b2e47e7366161798

Log permalinking (#4535)

frontend/src/pages/LogsPage/LogsPage.tsx
@@ -1,60 +1,87 @@
-import { useGetLogsQuery } from '@graph/hooks'
+import { CircularSpinner } from '@components/Loading/Loading'
 import { LogLevel } from '@graph/schemas'
 import { Box } from '@highlight-run/ui'
 import {
 	fifteenMinutesAgo,
-	FORMAT,
 	now,
 	PRESETS,
 	thirtyDaysAgo,
+	TIME_MODE,
 } from '@pages/LogsPage/constants'
 import LogsCount from '@pages/LogsPage/LogsCount/LogsCount'
 import LogsHistogram from '@pages/LogsPage/LogsHistogram/LogsHistogram'
 import { LogsTable } from '@pages/LogsPage/LogsTable/LogsTable'
+import { NoLogsFound } from '@pages/LogsPage/LogsTable/NoLogsFound'
 import { SearchForm } from '@pages/LogsPage/SearchForm/SearchForm'
+import { useGetLogs } from '@pages/LogsPage/useGetLogs'
 import { useParams } from '@util/react-router/useParams'
-import moment from 'moment'
-import React, { useRef, useState } from 'react'
+import React, { useRef } from 'react'
 import { Helmet } from 'react-helmet'
 import {
 	DateTimeParam,
+	QueryParamConfig,
 	StringParam,
 	useQueryParam,
 	withDefault,
 } from 'use-query-params'
 
 const QueryParam = withDefault(StringParam, '')
-const StartDateParam = withDefault(DateTimeParam, fifteenMinutesAgo)
+const FixedRangeStartDateParam = withDefault(DateTimeParam, fifteenMinutesAgo)
+const PermalinkStartDateParam = withDefault(DateTimeParam, thirtyDaysAgo)
 const EndDateParam = withDefault(DateTimeParam, now.toDate())
 
 const LogsPage = () => {
+	const { log_cursor } = useParams<{
+		log_cursor: string
+	}>()
+
+	const timeMode = log_cursor !== undefined ? 'permalink' : 'fixed-range'
+	const startDateDefault =
+		timeMode === 'permalink'
+			? PermalinkStartDateParam
+			: FixedRangeStartDateParam
+
+	return (
+		<LogsPageInner
+			logCursor={log_cursor}
+			timeMode={timeMode}
+			startDateDefault={startDateDefault}
+		/>
+	)
+}
+
+type Props = {
+	timeMode: TIME_MODE
+	logCursor: string | undefined
+	startDateDefault: QueryParamConfig<Date | null | undefined, Date>
+}
+
+const LogsPageInner = ({ timeMode, logCursor, startDateDefault }: Props) => {
 	const { project_id } = useParams<{
 		project_id: string
 	}>()
 	const [query, setQuery] = useQueryParam('query', QueryParam)
 	const [startDate, setStartDate] = useQueryParam(
 		'start_date',
-		StartDateParam,
+		startDateDefault,
 	)
 
 	const tableContainerRef = useRef<HTMLDivElement>(null)
 
 	const [endDate, setEndDate] = useQueryParam('end_date', EndDateParam)
 
-	const [loadingAfter, setLoadingAfter] = useState(false)
-	const { data, loading, fetchMore } = useGetLogsQuery({
-		variables: {
-			project_id: project_id!,
-			params: {
-				query,
-				date_range: {
-					start_date: moment(startDate).format(FORMAT),
-					end_date: moment(endDate).format(FORMAT),
-				},
-			},
-		},
-		skip: !project_id,
-		fetchPolicy: 'cache-and-network',
+	const {
+		logEdges,
+		loading,
+		loadingAfter,
+		fetchMoreForward,
+		fetchMoreBackward,
+	} = useGetLogs({
+		query,
+		project_id,
+		logCursor,
+		startDate,
+		endDate,
 	})
 
 	const handleFormSubmit = (value: string) => {
@@ -74,39 +101,20 @@ const LogsPage = () => {
 		setQuery(`level:${String(level).toLowerCase()}`)
 	}
 
-	const fetchMoreOnBottomReached = React.useCallback(
+	const fetchMoreWhenScrolled = React.useCallback(
 		(containerRefElement?: HTMLDivElement | null) => {
 			if (containerRefElement) {
 				const { scrollHeight, scrollTop, clientHeight } =
 					containerRefElement
 				//once the user has scrolled within 100px of the bottom of the table, fetch more data if there is any
 				if (scrollHeight - scrollTop - clientHeight < 100) {
-					const pageInfo = data?.logs.pageInfo
-
-					if (pageInfo && pageInfo.hasNextPage) {
-						setLoadingAfter(true)
-						fetchMore({
-							variables: {
-								project_id: project_id!,
-								params: {
-									query,
-									date_range: {
-										start_date:
-											moment(startDate).format(FORMAT),
-										end_date:
-											moment(endDate).format(FORMAT),
-									},
-								},
-								after: pageInfo.endCursor,
-							},
-						}).finally(() => {
-							setLoadingAfter(false)
-						})
-					}
+					fetchMoreForward()
+				} else if (scrollTop === 0) {
+					fetchMoreBackward()
 				}
 			}
 		},
-		[data?.logs.pageInfo, endDate, fetchMore, project_id, query, startDate],
+		[fetchMoreForward, fetchMoreBackward],
 	)
 
 	return (
@@ -138,6 +146,7 @@ const LogsPage = () => {
 						onDatesChange={handleDatesChange}
 						presets={PRESETS}
 						minDate={thirtyDaysAgo}
+						timeMode={timeMode}
 					/>
 					<LogsHistogram
 						query={query}
@@ -159,17 +168,40 @@ const LogsPage = () => {
 						pb="12"
 						overflowY="scroll"
 						onScroll={(e) =>
-							fetchMoreOnBottomReached(e.target as HTMLDivElement)
+							fetchMoreWhenScrolled(e.target as HTMLDivElement)
 						}
 						ref={tableContainerRef}
 					>
-						<LogsTable
-							data={data}
-							loading={loading}
-							loadingAfter={loadingAfter}
-							query={query}
-							tableContainerRef={tableContainerRef}
-						/>
+						{loading && (
+							<Box
+								display="flex"
+								flexGrow={1}
+								alignItems="center"
+								justifyContent="center"
+							>
+								<CircularSpinner />
+							</Box>
+						)}
+						{logEdges.length === 0 && (
+							<Box
+								display="flex"
+								flexGrow={1}
+								alignItems="center"
+								justifyContent="center"
+							>
+								<NoLogsFound />
+							</Box>
+						)}
+						{/** Pagination smooth scrolling does not work without this guard. */}
+						{logEdges.length > 0 && (
+							<LogsTable
+								logEdges={logEdges}
+								loadingAfter={loadingAfter}
+								query={query}
+								tableContainerRef={tableContainerRef}
+								selectedCursor={logCursor}
+							/>
+						)}
 					</Box>
 				</Box>
 			</Box>

frontend/src/pages/LogsPage/LogsTable/LogsTable.tsx
@@ -1,5 +1,3 @@
-import { CircularSpinner } from '@components/Loading/Loading'
-import { GetLogsQuery } from '@graph/operations'
 import { LogLevel as LogLevelType } from '@graph/schemas'
 import { LogEdge } from '@graph/schemas'
 import {
@@ -13,7 +11,6 @@ import { LogDetails } from '@pages/LogsPage/LogsTable/LogDetails'
 import { LogLevel } from '@pages/LogsPage/LogsTable/LogLevel'
 import { LogMessage } from '@pages/LogsPage/LogsTable/LogMessage'
 import { LogTimestamp } from '@pages/LogsPage/LogsTable/LogTimestamp'
-import { NoLogsFound } from '@pages/LogsPage/LogsTable/NoLogsFound'
 import {
 	ColumnDef,
 	ExpandedState,
@@ -24,24 +21,24 @@ import {
 } from '@tanstack/react-table'
 import { useVirtualizer } from '@tanstack/react-virtual'
 import clsx from 'clsx'
-import React, { Fragment, useEffect, useMemo, useState } from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
 
 import * as styles from './LogsTable.css'
 
 type Props = {
-	loading: boolean
 	loadingAfter: boolean
-	data: GetLogsQuery | undefined
+	logEdges: LogEdge[]
 	query: string
 	tableContainerRef: React.RefObject<HTMLDivElement>
+	selectedCursor: string | undefined
 }
 
 export const LogsTable = ({
-	data,
-	loading,
+	logEdges,
 	loadingAfter,
 	query,
 	tableContainerRef,
+	selectedCursor,
 }: Props) => {
 	const [expanded, setExpanded] = useState<ExpandedState>({})
 
@@ -94,12 +91,6 @@ export const LogsTable = ({
 		[query],
 	)
 
-	let logEdges: LogEdge[] = useMemo(() => [], [])
-
-	if (data?.logs?.edges) {
-		logEdges = data.logs.edges
-	}
-
 	const table = useReactTable({
 		data: logEdges,
 		columns,
@@ -134,31 +125,20 @@ export const LogsTable = ({
 		table.toggleAllRowsExpanded(false)
 	}, [logEdges, table])
 
-	if (loading) {
-		return (
-			<Box
-				display="flex"
-				flexGrow={1}
-				alignItems="center"
-				justifyContent="center"
-			>
-				<CircularSpinner />
-			</Box>
+	useEffect(() => {
+		const foundRow = rows.find(
+			(row) => row.original.cursor === selectedCursor,
 		)
-	}
 
-	if (logEdges.length === 0) {
-		return (
-			<Box
-				display="flex"
-				flexGrow={1}
-				alignItems="center"
-				justifyContent="center"
-			>
-				<NoLogsFound />
-			</Box>
-		)
-	}
+		if (foundRow) {
+			rowVirtualizer.scrollToIndex(foundRow.index, {
+				align: 'start',
+				behavior: 'smooth',
+			})
+		}
+		// Only run when the component mounts
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [])
 
 	return (
 		<div style={{ height: `${totalSize}px`, position: 'relative' }}>
@@ -190,6 +170,14 @@ export const LogsTable = ({
 									</Fragment>
 								)
 							})}
+
+							{/* TODO(et) - Wire this up with react-table's expanded state
+							    Currently, it seems non-trivial and we may have to possibly manage the state using `manualExpanding
+								https://tanstack.com/table/v8/docs/api/features/expanding?from=reactTableV7&original=https://react-table-v7.tanstack.com/docs/api/useExpanded#manualexpanding
+							*/}
+							{selectedCursor === row.original.cursor && (
+								<>Selected</>
+							)}
 						</Stack>
 
 						<LogDetails row={row} />

backend/clickhouse/cursor.go
@@ -21,26 +21,30 @@ func getLogsConnection(edges []*modelInputs.LogEdge, pagination Pagination) *mod
 	)
 
 	if pagination.At != nil && len(*pagination.At) > 1 {
-		if len(edges) == LogsLimit+3 { // has forward and backwards
-			hasNextPage = true
-			hasPreviousPage = true
+		idx := getCursorIdx(edges, *pagination.At)
 
-			edges = edges[1 : len(edges)-1] // remove first and last
-		} else if len(edges) == LogsLimit+2 { // has forward pagination (not backwards)
+		beforeCount := idx
+		afterCount := len(edges) - idx - 1
+
+		if beforeCount == LogsLimit/2+1 { // has backwards pagination
+			hasPreviousPage = true
+			edges = edges[1:] // remove first
+		}
+
+		if afterCount == LogsLimit/2+1 { // has forward pagination
 			hasNextPage = true
 			edges = edges[:len(edges)-1] // remove last
 		}
-
 	} else if pagination.After != nil && len(*pagination.After) > 1 {
 		hasPreviousPage = true // implicitly true because the passed in cursor should match
 		if len(edges) == LogsLimit+1 {
-			hasNextPage = len(edges) == LogsLimit+1
+			hasNextPage = true
 			edges = edges[:len(edges)-1]
 		}
 	} else if pagination.Before != nil && len(*pagination.Before) > 1 {
 		hasNextPage = true // implicitly true because the passed in cursor should match
 		if len(edges) == LogsLimit+1 {
-			hasPreviousPage = len(edges) == LogsLimit+1
+			hasPreviousPage = true
 			edges = edges[1 : len(edges)-1]
 		}
 	} else {
@@ -64,6 +68,15 @@ func getLogsConnection(edges []*modelInputs.LogEdge, pagination Pagination) *mod
 			StartCursor:     startCursor,
 		},
 	}
+}
+
+func getCursorIdx(edges []*modelInputs.LogEdge, cursor string) int {
+	for idx, edge := range edges {
+		if edge.Cursor == cursor {
+			return idx
+		}
+	}
+	return -1
 }
 
 func encodeCursor(t time.Time, uuid string) string {

backend/clickhouse/cursor_test.go
@@ -126,76 +126,6 @@ func TestGetConnectionBefore(t *testing.T) {
 	}, connection)
 }
 
-func TestGetConnectionAt(t *testing.T) {
-	zeroEdges := []*modelInputs.LogEdge{}
-	forwardAndBackwardsEdges := []*modelInputs.LogEdge{
-		{
-			Cursor: "cursor",
-		},
-	}
-
-	for i := 1; i <= LogsLimit+2; i++ {
-		forwardAndBackwardsEdges = append(forwardAndBackwardsEdges, &modelInputs.LogEdge{
-			Cursor: "cursor",
-		})
-	}
-
-	forwardEdgesOnly := []*modelInputs.LogEdge{
-		{
-			Cursor: "cursor",
-		},
-	}
-
-	for i := 1; i <= LogsLimit+1; i++ {
-		forwardEdgesOnly = append(forwardEdgesOnly, &modelInputs.LogEdge{
-			Cursor: "cursor",
-		})
-	}
-
-	connection := getLogsConnection(zeroEdges, Pagination{
-		At: ptr.String("cursor"),
-	})
-
-	assert.Equal(t, &modelInputs.LogsConnection{
-		Edges: zeroEdges,
-		PageInfo: &modelInputs.PageInfo{
-			HasNextPage:     false,
-			HasPreviousPage: false,
-			StartCursor:     "",
-			EndCursor:       "",
-		},
-	}, connection)
-
-	connection = getLogsConnection(forwardAndBackwardsEdges, Pagination{
-		At: ptr.String("cursor"),
-	})
-
-	assert.Equal(t, &modelInputs.LogsConnection{
-		Edges: forwardAndBackwardsEdges[1:102],
-		PageInfo: &modelInputs.PageInfo{
-			HasNextPage:     true,
-			HasPreviousPage: true,
-			StartCursor:     "cursor",
-			EndCursor:       "cursor",
-		},
-	}, connection)
-
-	connection = getLogsConnection(forwardEdgesOnly, Pagination{
-		At: ptr.String("cursor"),
-	})
-
-	assert.Equal(t, &modelInputs.LogsConnection{
-		Edges: forwardEdgesOnly[0:101],
-		PageInfo: &modelInputs.PageInfo{
-			HasNextPage:     true,
-			HasPreviousPage: false,
-			StartCursor:     "cursor",
-			EndCursor:       "cursor",
-		},
-	}, connection)
-
-}
-
 func TestGetConnectionNoPagination(t *testing.T) {
 	zeroEdges := []*modelInputs.LogEdge{}
 	manyEdges := []*modelInputs.LogEdge{}

backend/clickhouse/logs.go
@@ -82,9 +82,10 @@ const OrderBackward = "Timestamp ASC, UUID ASC"
 const OrderForward = "Timestamp DESC, UUID DESC"
 
 type Pagination struct {
-	After  *string
-	Before *string
-	At     *string
+	After     *string
+	Before    *string
+	At        *string
+	CountOnly bool
 }
 
 func (client *Client) ReadLogs(ctx context.Context, projectID int, params modelInputs.LogsParamsInput, pagination Pagination) (*modelInputs.LogsConnection, error) {
@@ -175,7 +176,7 @@ func (client *Client) ReadLogs(ctx context.Context, projectID int, params modelI
 }
 
 func (client *Client) ReadLogsTotalCount(ctx context.Context, projectID int, params modelInputs.LogsParamsInput) (uint64, error) {
-	sb, err := makeSelectBuilder("COUNT(*)", projectID, params, Pagination{})
+	sb, err := makeSelectBuilder("COUNT(*)", projectID, params, Pagination{CountOnly: true})
 	if err != nil {
 		return 0, err
 	}
@@ -478,6 +479,11 @@ func makeSelectBuilder(selectStr string, projectID int, params modelInputs.LogsP
 	} else {
 		sb.Where(sb.LessEqualThan("toUInt64(toDateTime(Timestamp))", uint64(params.DateRange.EndDate.Unix()))).
 			Where(sb.GreaterEqualThan("toUInt64(toDateTime(Timestamp))", uint64(params.DateRange.StartDate.Unix())))
+
+		if !pagination.CountOnly { // count queries can't be ordered because we don't include Timestamp in the select
+			sb.OrderBy(OrderForward)
+		}
+
 	}
 
 	filters := makeFilters(params.Query)

backend/clickhouse/logs_test.go
@@ -39,6 +39,23 @@ func makeDateWithinRange(now time.Time) *modelInputs.DateRangeRequiredInput {
 	}
 }
 
+func assertCursorsOutput(t *testing.T, edges []*modelInputs.LogEdge, expectedCursor string) {
+	allCursorsUnique := make(map[string]struct{})
+	for _, edge := range edges {
+		_, ok := allCursorsUnique[edge.Cursor]
+		if ok {
+			assert.Fail(t, "Cursors are not unique")
+		} else {
+			allCursorsUnique[edge.Cursor] = struct{}{}
+		}
+	}
+
+	_, ok := allCursorsUnique[expectedCursor]
+	if !ok {
+		assert.Fail(t, "Expected cursor is not in the returned output")
+	}
+}
+
 func TestReadLogsWithTimeQuery(t *testing.T) {
 	ctx := context.Background()
 	client, teardown := setupTest(t)
@@ -75,6 +92,30 @@ func TestReadLogsWithTimeQuery(t *testing.T) {
 	assert.NoError(t, err)
 
 	assert.Len(t, payload.Edges, 1)
+}
+
+func TestReadLogsTotalCount(t *testing.T) {
+	ctx := context.Background()
+	client, teardown := setupTest(t)
+	defer teardown(t)
+
+	now := time.Now()
+	rows := []*LogRow{
+		{
+			LogRowPrimaryAttrs: LogRowPrimaryAttrs{
+				Timestamp: now,
+				ProjectId: 1,
+			},
+		},
+	}
+
+	assert.NoError(t, client.BatchWriteLogRows(ctx, rows))
+
+	count, err := client.ReadLogsTotalCount(ctx, 1, modelInputs.LogsParamsInput{
+		DateRange: makeDateWithinRange(now),
+	})
+	assert.NoError(t, err)
+	assert.Equal(t, uint64(1), count)
 }
 
 func TestReadLogsHistogram(t *testing.T) {
@@ -403,42 +444,54 @@ func TestReadLogsAtCursor(t *testing.T) {
 
 	assert.NoError(t, client.BatchWriteLogRows(ctx, rows))
 
-	connection, err := client.ReadLogs(ctx, 1, modelInputs.LogsParamsInput{
+	// Load the initial page with no pagination
+	originalConnection, err := client.ReadLogs(ctx, 1, modelInputs.LogsParamsInput{
 		DateRange: makeDateWithinRange(now),
 	}, Pagination{})
 	assert.NoError(t, err)
 
-	assert.Len(t, connection.Edges, 100)
+	assert.Len(t, originalConnection.Edges, 100)
 
-	middleLog := connection.Edges[49]
-
-	connection, err = client.ReadLogs(ctx, 1, modelInputs.LogsParamsInput{
+	// Permalink the log in the middle ensuring there is a previous and next page
+	permalink := originalConnection.Edges[51]
+	connection, err := client.ReadLogs(ctx, 1, modelInputs.LogsParamsInput{
 		DateRange: makeDateWithinRange(now),
 	}, Pagination{
-		At: ptr.String(middleLog.Cursor),
+		At: ptr.String(permalink.Cursor),
 	})
 	assert.NoError(t, err)
 
 	assert.Len(t, connection.Edges, 101) // 50 before + 50 after + the permalinked log
-	assert.True(t, connection.PageInfo.HasPreviousPage, true)
-	assert.True(t, connection.PageInfo.HasNextPage, true)
+	assert.Equal(t, connection.Edges[50], permalink)
+	assert.True(t, connection.PageInfo.HasPreviousPage)
+	assert.True(t, connection.PageInfo.HasNextPage)
+	assertCursorsOutput(t, connection.Edges, permalink.Cursor)
 
-	allCursorsUnique := make(map[string]struct{})
+	// Permalink the log before the middle ensuring there is not a previous page but has a next page
+	permalink = originalConnection.Edges[50]
+	connection, err = client.ReadLogs(ctx, 1, modelInputs.LogsParamsInput{
+		DateRange: makeDateWithinRange(now),
+	}, Pagination{
+		At: ptr.String(permalink.Cursor),
+	})
 
-	for _, edge := range connection.Edges {
-		_, ok := allCursorsUnique[edge.Cursor]
-		if ok {
-			assert.Fail(t, "Cursors are not unique")
-		} else {
-			allCursorsUnique[edge.Cursor] = struct{}{}
-		}
-	}
+	assert.NoError(t, err)
+	assert.False(t, connection.PageInfo.HasPreviousPage)
+	assert.True(t, connection.PageInfo.HasNextPage)
+	assertCursorsOutput(t, connection.Edges, permalink.Cursor)
 
-	_, ok := allCursorsUnique[middleLog.Cursor]
-	if !ok {
-		assert.Fail(t, "Middle cursor is not in the returned output")
+	// Permalink the log after the middle ensuring there is a previous page but no next page
+	permalink = originalConnection.Edges[52]
+	connection, err = client.ReadLogs(ctx, 1, modelInputs.LogsParamsInput{
+		DateRange: makeDateWithinRange(now),
+	}, Pagination{
+		At: ptr.String(permalink.Cursor),
+	})
 
-	}
+	assert.NoError(t, err)
+	assert.True(t, connection.PageInfo.HasPreviousPage)
+	assert.False(t, connection.PageInfo.HasNextPage)
+	assertCursorsOutput(t, connection.Edges, permalink.Cursor)
 }
 
 func TestReadLogsWithBodyFilter(t *testing.T) {

frontend/src/graph/generated/hooks.tsx
@@ -11555,8 +11555,20 @@ export type GetEmailOptOutsQueryResult = Apollo.QueryResult<
 	Types.GetEmailOptOutsQueryVariables
 >
 export const GetLogsDocument = gql`
-	query GetLogs($project_id: ID!, $params: LogsParamsInput!, $after: String) {
-		logs(project_id: $project_id, params: $params, after: $after) {
+	query GetLogs(
+		$project_id: ID!
+		$params: LogsParamsInput!
+		$after: String
+		$before: String
+		$at: String
+	) {
+		logs(
+			project_id: $project_id
+			params: $params
+			after: $after
+			before: $before
+			at: $at
+		) {
 			edges {
 				cursor
 				node {
@@ -11571,6 +11583,8 @@ export const GetLogsDocument = gql`
 			}
 			pageInfo {
 				hasNextPage
+				hasPreviousPage
+				startCursor
 				endCursor
 			}
 		}
@@ -11592,6 +11606,8 @@ export const GetLogsDocument = gql`
  *      project_id: // value for 'project_id'
  *      params: // value for 'params'
  *      after: // value for 'after'
+ *      before: // value for 'before'
+ *      at: // value for 'at'
  *   },
  * });
  */

frontend/src/graph/generated/operations.tsx
@@ -3967,6 +3967,8 @@ export type GetLogsQueryVariables = Types.Exact<{
 	project_id: Types.Scalars['ID']
 	params: Types.LogsParamsInput
 	after?: Types.Maybe<Types.Scalars['String']>
+	before?: Types.Maybe<Types.Scalars['String']>
+	at?: Types.Maybe<Types.Scalars['String']>
 }>
 
 export type GetLogsQuery = { __typename?: 'Query' } & {
@@ -3987,7 +3989,7 @@ export type GetLogsQuery = { __typename?: 'Query' } & {
 		>
 		pageInfo: { __typename?: 'PageInfo' } & Pick<
 			Types.PageInfo,
-			'hasNextPage' | 'endCursor'
+			'hasNextPage' | 'hasPreviousPage' | 'startCursor' | 'endCursor'
 		>
 	}
 }

commit 5c481284a35a03eb814ed35bf701367c130acc1a

fix empty state flashing (#4547)
## Summary

<!--
Ideally, there is an attached Linear ticket that will describe the
"why".

If relevant, use this section to call out any additional information
you'd like to _highlight_ to the reviewer.
-->

Regression from #4535. It's more noticeable on prod where the initial
logs request takes longer than development.

Fixes #4546

## How did you test this change?

<!--
Frontend - Leave a screencast or a screenshot to visually describe the
changes.
-->

Confirmed on render preview that flashing doesn't occur.

## Are there any deployment considerations?

<!--
 Backend - Do we need to consider migrations or backfilling data?
-->

N/A

frontend/src/pages/LogsPage/LogsPage.tsx
@@ -1,4 +1,3 @@
-import { CircularSpinner } from '@components/Loading/Loading'
 import { LogLevel } from '@graph/schemas'
 import { Box } from '@highlight-run/ui'
 import {
@@ -11,7 +10,6 @@ import {
 import LogsCount from '@pages/LogsPage/LogsCount/LogsCount'
 import LogsHistogram from '@pages/LogsPage/LogsHistogram/LogsHistogram'
 import { LogsTable } from '@pages/LogsPage/LogsTable/LogsTable'
-import { NoLogsFound } from '@pages/LogsPage/LogsTable/NoLogsFound'
 import { SearchForm } from '@pages/LogsPage/SearchForm/SearchForm'
 import { useGetLogs } from '@pages/LogsPage/useGetLogs'
 import { useParams } from '@util/react-router/useParams'
@@ -172,36 +170,14 @@ const LogsPageInner = ({ timeMode, logCursor, startDateDefault }: Props) => {
 						}
 						ref={tableContainerRef}
 					>
-						{loading && (
-							<Box
-								display="flex"
-								flexGrow={1}
-								alignItems="center"
-								justifyContent="center"
-							>
-								<CircularSpinner />
-							</Box>
-						)}
-						{logEdges.length === 0 && (
-							<Box
-								display="flex"
-								flexGrow={1}
-								alignItems="center"
-								justifyContent="center"
-							>
-								<NoLogsFound />
-							</Box>
-						)}
-						{/** Pagination smooth scrolling does not work without this guard. */}
-						{logEdges.length > 0 && (
-							<LogsTable
-								logEdges={logEdges}
-								loadingAfter={loadingAfter}
-								query={query}
-								tableContainerRef={tableContainerRef}
-								selectedCursor={logCursor}
-							/>
-						)}
+						<LogsTable
+							logEdges={logEdges}
+							loading={loading}
+							loadingAfter={loadingAfter}
+							query={query}
+							tableContainerRef={tableContainerRef}
+							selectedCursor={logCursor}
+						/>
 					</Box>
 				</Box>
 			</Box>

frontend/src/pages/LogsPage/LogsTable/LogsTable.tsx
@@ -1,3 +1,4 @@
+import { CircularSpinner } from '@components/Loading/Loading'
 import { LogLevel as LogLevelType } from '@graph/schemas'
 import { LogEdge } from '@graph/schemas'
 import {
@@ -11,6 +12,7 @@ import { LogDetails } from '@pages/LogsPage/LogsTable/LogDetails'
 import { LogLevel } from '@pages/LogsPage/LogsTable/LogLevel'
 import { LogMessage } from '@pages/LogsPage/LogsTable/LogMessage'
 import { LogTimestamp } from '@pages/LogsPage/LogsTable/LogTimestamp'
+import { NoLogsFound } from '@pages/LogsPage/LogsTable/NoLogsFound'
 import {
 	ColumnDef,
 	ExpandedState,
@@ -26,6 +28,7 @@ import React, { Fragment, useEffect, useState } from 'react'
 import * as styles from './LogsTable.css'
 
 type Props = {
+	loading: boolean
 	loadingAfter: boolean
 	logEdges: LogEdge[]
 	query: string
@@ -33,7 +36,37 @@ type Props = {
 	selectedCursor: string | undefined
 }
 
-export const LogsTable = ({
+export const LogsTable = (props: Props) => {
+	if (props.loading) {
+		return (
+			<Box
+				display="flex"
+				flexGrow={1}
+				alignItems="center"
+				justifyContent="center"
+			>
+				<CircularSpinner />
+			</Box>
+		)
+	}
+
+	if (props.logEdges.length === 0) {
+		return (
+			<Box
+				display="flex"
+				flexGrow={1}
+				alignItems="center"
+				justifyContent="center"
+			>
+				<NoLogsFound />
+			</Box>
+		)
+	}
+
+	return <LogsTableInner {...props} />
+}
+
+const LogsTableInner = ({
 	logEdges,
 	loadingAfter,
 	query,