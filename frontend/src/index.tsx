e54cf3c2c65075ae6ff16ef130aa9420bdfabc1b
Chris Schmitz <chris@highlight.io>
2023-03-09 10:19:20 -0600

    Setup Sentry for JS error monitoring (#4316)

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -30,6 +30,8 @@ import { Admin } from '@graph/schemas'
 import { ErrorBoundary } from '@highlight-run/react'
 import useLocalStorage from '@rehooks/local-storage'
 import { AppRouter } from '@routers/AppRouter/AppRouter'
+import * as Sentry from '@sentry/react'
+import { BrowserTracing } from '@sentry/tracing'
 import analytics from '@util/analytics'
 import { getAttributionData, setAttributionData } from '@util/attribution'
 import { auth } from '@util/auth'
@@ -118,6 +120,14 @@ analytics.track('attribution', getAttributionData())
 if (!isOnPrem) {
 	H.start()
 	showIntercom({ hideMessage: true })
+
+	if (!dev) {
+		Sentry.init({
+			dsn: 'https://e8052ada7c10490b823e0f939c519903@o4504696930631680.ingest.sentry.io/4504697059934208',
+			integrations: [new BrowserTracing()],
+			tracesSampleRate: 1.0,
+		})
+	}
 }
 
 showHiringMessage()
