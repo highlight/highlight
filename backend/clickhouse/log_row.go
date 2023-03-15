e8051468d3812196aa6133c7230ee5ebee6b0915
Eric Thomas <et@users.noreply.github.com>
2023-03-15 16:13:09 -0600

    skip writing log attributes if these are internal attributes (#4597)
    ## Summary
    
    <!--
    Ideally, there is an attached Linear ticket that will describe the
    "why".
    
    If relevant, use this section to call out any additional information
    you'd like to _highlight_ to the reviewer.
    -->
    
    `log.severity` and `log.message` aren't being excluded from
    `LogAttributes`.
    
    This PR fixes that. It also moves some data parsing logic down out of
    the OTEL code and into the logs code. This makes it easier to unit test
    and it sets up a better interface for building a log row.
    
    ## How did you test this change?
    
    <!--
    Frontend - Leave a screencast or a screenshot to visually describe the
    changes.
    -->
    
    Confirmed `log.severity` and `log.message` aren't present in log
    attributes anymore.
    
    <img width="1488" alt="Screenshot 2023-03-14 at 10 42 54 PM"
    src="https://user-images.githubusercontent.com/58678/225208807-f7f3c030-50dc-4a8b-8ab9-762f697f7325.png">
    
    
    ## Are there any deployment considerations?
    
    <!--
     Backend - Do we need to consider migrations or backfilling data?
    -->
    
    N/A

---
+++ backend/clickhouse/log_row.go
@@ -0,0 +1,104 @@
+package clickhouse
+
+import (
+	"strconv"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/highlight/highlight/sdk/highlight-go"
+	log "github.com/sirupsen/logrus"
+)
+
+type LogRowPrimaryAttrs struct {
+	Timestamp       time.Time
+	ProjectId       uint32
+	TraceId         string
+	SpanId          string
+	SecureSessionId string
+}
+
+type LogRow struct {
+	LogRowPrimaryAttrs
+	UUID           string
+	TraceFlags     uint32
+	SeverityText   string
+	SeverityNumber int32
+	ServiceName    string
+	Body           string
+	LogAttributes  map[string]string
+}
+
+func NewLogRow(attrs LogRowPrimaryAttrs, opts ...LogRowOption) *LogRow {
+	logRow := &LogRow{
+		LogRowPrimaryAttrs: LogRowPrimaryAttrs{
+			Timestamp:       attrs.Timestamp,
+			TraceId:         attrs.TraceId,
+			SpanId:          attrs.SpanId,
+			ProjectId:       attrs.ProjectId,
+			SecureSessionId: attrs.SecureSessionId,
+		},
+		UUID:           uuid.New().String(),
+		SeverityText:   "INFO",
+		SeverityNumber: int32(log.InfoLevel),
+	}
+
+	for _, opt := range opts {
+		opt(logRow)
+	}
+
+	return logRow
+}
+
+func (l *LogRow) Cursor() string {
+	return encodeCursor(l.Timestamp, l.UUID)
+}
+
+type LogRowOption func(*LogRow)
+
+func WithLogAttributes(resourceAttributes, eventAttributes map[string]any) LogRowOption {
+	return func(h *LogRow) {
+		h.LogAttributes = getAttributesMap(resourceAttributes, eventAttributes)
+	}
+}
+
+func cast[T string | int64 | float64](v interface{}, fallback T) T {
+	c, ok := v.(T)
+	if !ok {
+		return fallback
+	}
+	return c
+}
+
+func getAttributesMap(resourceAttributes, eventAttributes map[string]any) map[string]string {
+	attributesMap := make(map[string]string)
+	for _, m := range []map[string]any{resourceAttributes, eventAttributes} {
+		for k, v := range m {
+			shouldSkip := false
+
+			for _, attr := range highlight.InternalAttributes {
+				if k == attr {
+					shouldSkip = true
+					break
+				}
+			}
+
+			if shouldSkip {
+				continue
+			}
+
+			vStr := cast(v, "")
+			if vStr != "" {
+				attributesMap[k] = vStr
+			}
+			vInt := cast[int64](v, 0)
+			if vInt != 0 {
+				attributesMap[k] = strconv.FormatInt(vInt, 10)
+			}
+			vFlt := cast(v, 0.)
+			if vFlt > 0. {
+				attributesMap[k] = strconv.FormatFloat(vFlt, 'f', -1, 64)
+			}
+		}
+	}
+	return attributesMap
+}
