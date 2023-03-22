38ff2cf59379e858a86083b24ef3e26402accbe4
Vadim Korolik <vadim@highlight.io>
2023-03-21 17:23:57 -0700

    ensure error logs create error objects (#4618)
    ## Summary
    
    OTEL error level logs should create error objects, so that our UI can
    successfully link logs to errors.
    
    ## How did you test this change?
    
    Local deploy.
    
    ## Are there any deployment considerations?
    
    No

--- frontend/src/pages/ErrorsV2/ErrorLogCursor/ErrorLogCursorRedirect.tsx
+++ frontend/src/pages/ErrorsV2/ErrorLogCursor/ErrorLogCursorRedirect.tsx
@@ -1,18 +1,29 @@
 import { ErrorState } from '@components/ErrorState/ErrorState'
+import {
+	AppLoadingState,
+	useAppLoadingContext,
+} from '@context/AppLoadingContext'
 import { useGetErrorObjectForLogQuery } from '@graph/hooks'
 import { useParams } from '@util/react-router/useParams'
 import React from 'react'
 import { Navigate } from 'react-router'
 
 const ErrorLogCursorRedirect: React.FC = () => {
+	const { setLoadingState } = useAppLoadingContext()
 	const { cursor_id: logCursor } = useParams<{
 		cursor_id: string
 	}>() as { cursor_id: string }
 
-	const { loading, data, error } = useGetErrorObjectForLogQuery({
+	const { called, loading, data, error } = useGetErrorObjectForLogQuery({
 		variables: { log_cursor: logCursor! },
 		skip: !logCursor,
 	})
+
+	setLoadingState(
+		!called || loading
+			? AppLoadingState.EXTENDED_LOADING
+			: AppLoadingState.LOADED,
+	)
 
 	if (loading) {
 		return null
