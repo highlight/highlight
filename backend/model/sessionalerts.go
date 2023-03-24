39a1fe4eb15a0f0ca4577ade9be202e87ef13518
Vadim Korolik <vadim@highlight.io>
2023-03-24 15:51:13 -0700

    add backend webhook sending for alerts payloads (#4678)
    ## Summary
    
    Adds backend logic to store webhook destinations for alerts and send
    alerts payloads to set destinations as json via a retryable http client.
    
    Payload currently sends the full body of the `integrations.` payload
    objects, though as a v2 we could support customizing the formats of the
    payload body.
    
    Frontend to update webhook URLs coming in a separate PR. Implements
    backend for #4550
    
    ## How did you test this change?
    
    Local deploy with manually configured alert webhooks sending to
    webhook.site
    
    <img width="1566" alt="Screenshot 2023-03-23 at 1 03 17 AM"
    src="https://user-images.githubusercontent.com/1351531/227140230-f21ef4bd-6200-4ade-a79d-5662cf397fa0.png">
    
    ## Are there any deployment considerations?
    
    Will monitor alerts db migration.

--- backend/model/sessionalerts.go
+++ backend/model/sessionalerts.go
@@ -4,7 +4,6 @@ import (
 	"database/sql/driver"
 	"encoding/json"
 	"fmt"
-
 	"github.com/pkg/errors"
 )
 
@@ -30,6 +29,29 @@ func (dc DiscordChannels) Value() (driver.Value, error) {
 	return string(bytes), err
 }
 
+type WebhookDestination struct {
+	URL           string
+	Authorization string
+}
+
+type WebhookDestinations []*WebhookDestination
+
+// Scan scan value into Jsonb, implements sql.Scanner interface
+func (dc *WebhookDestinations) Scan(value interface{}) error {
+	bytes, ok := value.([]byte)
+	if !ok {
+		return errors.New(fmt.Sprint("Failed to unmarshal JSONB value:", value))
+	}
+	return json.Unmarshal(bytes, &dc)
+}
+
+// Value return json value, implement driver.Valuer interface
+func (dc WebhookDestinations) Value() (driver.Value, error) {
+	bytes, err := json.Marshal(dc)
+	return string(bytes), err
+}
+
 type AlertIntegrations struct {
-	DiscordChannelsToNotify DiscordChannels `gorm:"type:jsonb;default:'[]'" json:"discord_channels_to_notify"`
+	DiscordChannelsToNotify DiscordChannels     `gorm:"type:jsonb;default:'[]'" json:"discord_channels_to_notify"`
+	WebhookDestinations     WebhookDestinations `gorm:"type:jsonb;default:'[]'" json:"webhook_destinations"`
 }
