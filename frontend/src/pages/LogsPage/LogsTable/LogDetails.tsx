1dbcf51c0b2ffd42073b277f83c8dbea16a4b9ac
Vadim Korolik <vkorolik@gmail.com>
2023-03-27 12:36:10 -0700

    frontend wiring

--- frontend/src/pages/LogsPage/LogsTable/LogDetails.tsx
+++ frontend/src/pages/LogsPage/LogsTable/LogDetails.tsx
@@ -52,8 +52,16 @@ export const LogDetails = ({ row, queryTerms }: Props) => {
 	const { projectId } = useProjectId()
 	const navigate = useNavigate()
 	const [allExpanded, setAllExpanded] = useState(false)
-	const { traceID, spanID, secureSessionID, logAttributes, message, level } =
-		row.original.node
+	const {
+		traceID,
+		spanID,
+		secureSessionID,
+		logAttributes,
+		message,
+		level,
+		source,
+		serviceName,
+	} = row.original.node
 	const expanded = row.getIsExpanded()
 	const expandable = Object.values(logAttributes).some(
 		(v) => typeof v === 'object',
@@ -129,6 +137,24 @@ export const LogDetails = ({ row, queryTerms }: Props) => {
 					<LogValue
 						label="secure_session_id"
 						value={secureSessionID}
+						queryTerms={queryTerms}
+					/>
+				</Box>
+			)}
+			{source && (
+				<Box>
+					<LogValue
+						label="source"
+						value={source}
+						queryTerms={queryTerms}
+					/>
+				</Box>
+			)}
+			{serviceName && (
+				<Box>
+					<LogValue
+						label="service_name"
+						value={serviceName}
 						queryTerms={queryTerms}
 					/>
 				</Box>
