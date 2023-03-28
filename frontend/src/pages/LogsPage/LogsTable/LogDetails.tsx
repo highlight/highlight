f3242306112770a295c4a28283047bb258601c27
Chris Schmitz <ccschmitz@gmail.com>
2023-03-28 11:37:05 -0400

    Update types

--- frontend/src/pages/LogsPage/LogsTable/LogDetails.tsx
+++ frontend/src/pages/LogsPage/LogsTable/LogDetails.tsx
@@ -66,7 +66,7 @@ export const LogDetails = ({ row, queryTerms }: Props) => {
 		(v) => typeof v === 'object',
 	)
 	const reservedLogAttributes: {
-		[key in ReservedLogKey]: Maybe<string> | undefined
+		[key in ReservedLogKey]: Maybe<string> | string | undefined
 	} = {
 		level,
 		message,
