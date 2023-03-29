8d8bc674daf16001c869d1b22c2d6c89fed744bf
Jay Khatri <jaykhatrimail@gmail.com>
2023-03-29 12:58:01 -0700

    change

---
+++ highlight.io/components/Competitors/Competitors/competitors.ts
@@ -0,0 +1,120 @@
+import { StaticImageData } from 'next/image'
+
+export type ComparisonTableRow = {
+  feature: string
+  highlight: 0 | 0.5 | 1
+  competitor: 0 | 1
+  tooltip?: string
+}
+
+export type ComparisonTableSection = {
+  title: string
+  rows: ComparisonTableRow[]
+}
+
+export type Competitor = {
+  slug: string
+  name: string
+  header: string
+  subheader: string
+  subHeader2: string
+  logo?: StaticImageData
+  sections: ComparisonTableSection[]
+}
+
+export const COMPETITORS: { [k: string]: Competitor } = {
+  'log-rocket': {
+    slug: 'log-rocket',
+    name: 'LogRocket',
+    header: 'The Open Source Logrocket Alternative.',
+    subheader: 'Pixel-perfect video replay of your web application. Step into the shoes of your users.',
+    subHeader2: 'An Open Source, Fullstack Alternative to Logrocket. Get Started for free in minutes.',
+    sections: [
+      {
+        title: 'General',
+        rows: [
+          {
+            feature: 'Unlimited Team Members',
+            highlight: 1,
+            competitor: 1,
+          },
+          {
+            feature: 'Unlimited Team Members',
+            highlight: 1,
+            competitor: 0,
+          },
+          {
+            feature: 'Unlimited Team Members',
+            highlight: 1,
+            competitor: 1,
+          },
+          {
+            feature: 'Unlimited Team Members',
+            highlight: 0.5,
+            competitor: 1,
+          },
+          {
+            feature: 'Unlimited Team Members',
+            highlight: 1,
+            competitor: 1,
+          },
+        ],
+      },
+      {
+        title: 'Session Replay',
+        rows: [
+          {
+            feature: 'Commenting / Mentions',
+            highlight: 1,
+            competitor: 1,
+          },
+          {
+            feature: 'Heatmaps',
+            highlight: 1,
+            competitor: 0,
+          },
+          {
+            feature: 'Session Sharing',
+            highlight: 1,
+            competitor: 1,
+          },
+          {
+            feature: 'Privacy SDKs',
+            highlight: 0.5,
+            competitor: 0,
+          },
+          {
+            feature: 'Embedded Error Monitoring',
+            highlight: 1,
+            competitor: 1,
+          },
+          {
+            feature: 'Canvas & WebGL Recording',
+            highlight: 1,
+            competitor: 1,
+          },
+          {
+            feature: 'Session-level Debugging Data',
+            highlight: 1,
+            competitor: 1,
+          },
+        ],
+      },
+      {
+        title: 'Error Monitoring',
+        rows: [
+          {
+            feature: 'Embedded Session Replay',
+            highlight: 1,
+            competitor: 1,
+          },
+          {
+            feature: 'Support for Backend SDKs',
+            highlight: 1,
+            competitor: 0,
+          },
+        ],
+      },
+    ],
+  },
+}
