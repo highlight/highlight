c0ea3362e90ae498877ae342fc0464b320621308
Eric Thomas <eric@highlight.io>
2023-03-28 21:42:52 -0600

    lint

--- frontend/src/components/Link/styles.css.ts
+++ frontend/src/components/Link/styles.css.ts
@@ -1,7 +1,6 @@
 import { vars } from '@highlight-run/ui/src/css/vars'
 import { style } from '@vanilla-extract/css'
 
-
 export const link = style({
 	textDecoration: 'none',
 	color: vars.theme.interactive.fill.secondary.content.text,
