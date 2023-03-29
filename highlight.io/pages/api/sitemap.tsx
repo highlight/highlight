21b08d38cfa0f156c6e006331912c5099225b614
Vadim Korolik <vkorolik@gmail.com>
2023-03-28 20:23:32 -0700

    empty sitemap

--- highlight.io/pages/api/sitemap.tsx
+++ highlight.io/pages/api/sitemap.tsx
@@ -1,96 +1,97 @@
-import { gql, GraphQLClient } from 'graphql-request'
-import { H } from '@highlight-run/next'
-import { iProduct, PRODUCTS } from '../../components/Products/products'
-import { getGithubDocsPaths } from './docs/github'
-import { FEATURES, iFeature } from '../../components/Features/features'
+// import { gql, GraphQLClient } from 'graphql-request'
+// import { H } from '@highlight-run/next'
+// import { iProduct, PRODUCTS } from '../../components/Products/products'
+// import { getGithubDocsPaths } from './docs/github'
+// import { FEATURES, iFeature } from '../../components/Features/features'
 import { NextApiRequest, NextApiResponse } from 'next'
 
 async function generateXML(): Promise<string> {
-	const graphcms = new GraphQLClient(
-		'https://api-us-west-2.graphcms.com/v2/cl2tzedef0o3p01yz7c7eetq8/master',
-		{
-			headers: {
-				Authorization: `Bearer ${process.env.GRAPHCMS_TOKEN}`,
-			},
-		},
-	)
-
-	const start = global.performance.now()
-	const [{ posts }, { customers }, { changelogs }, docs] = await Promise.all([
-		await graphcms.request(gql`
-      query GetPosts() {
-        posts(orderBy: publishedAt_DESC) {
-          slug
-        }
-      }
-    `),
-		await graphcms.request(gql`
-      query GetCustomers() {
-        customers() {
-          slug
-        }
-      }
-    `),
-		await graphcms.request(gql`
-      query GetChangelogs() {
-        changelogs() {
-          slug
-        }
-      }
-    `),
-		await getGithubDocsPaths(),
-	])
-	H.metrics([
-		{
-			name: 'sitemap-gql-latency-ms',
-			value: global.performance.now() - start,
-			tags: [{ name: 'site', value: process.env.WEBSITE_URL || '' }],
-		},
-	])
-
-	const blogPages = posts.map((post: any) => `blog/${post.slug}`)
-	const customerPages = customers.map(
-		(customer: { slug: string }) => `customers/${customer.slug}`,
-	)
-	const changelogPages = changelogs.map(
-		(changelog: { slug: string }) => `changelog/${changelog.slug}`,
-	)
-	const docsPages = Array.from(docs.keys()).map(
-		(d) => `docs/${d.split('docs-content/').pop()}`,
-	)
-	const productPages = Object.values(PRODUCTS).map(
-		(product: iProduct) => `for/${product.slug}`,
-	)
-	const featurePages = Object.values(FEATURES).map(
-		(feature: iFeature) => `${feature.slug}`,
-	)
-
-	const staticPagePaths = process.env.staticPages?.split(', ') || []
-	const staticPages = staticPagePaths.map((path) => {
-		return `${path.replace('pages', '').replace('index.tsx', '')}`
-	})
-
-	const pages = [
-		...staticPages,
-		...blogPages,
-		...customerPages,
-		...changelogPages,
-		...docsPages,
-		...productPages,
-		...featurePages,
-	]
-
-	const addPage = (page: string) => {
-		return `    <url>
-      <loc>${`${process.env.WEBSITE_URL}/${page}`}</loc>
-      <changefreq>hourly</changefreq>
-    </url>`
-	}
-
-	return `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-${pages.map(addPage).join('\n')}
-  </urlset>`
+	return 'NOT IMPLEMENTED'
+	// 	const graphcms = new GraphQLClient(
+	// 		'https://api-us-west-2.graphcms.com/v2/cl2tzedef0o3p01yz7c7eetq8/master',
+	// 		{
+	// 			headers: {
+	// 				Authorization: `Bearer ${process.env.GRAPHCMS_TOKEN}`,
+	// 			},
+	// 		},
+	// 	)
+	//
+	// 	const start = global.performance.now()
+	// 	const [{ posts }, { customers }, { changelogs }, docs] = await Promise.all([
+	// 		await graphcms.request(gql`
+	//       query GetPosts() {
+	//         posts(orderBy: publishedAt_DESC) {
+	//           slug
+	//         }
+	//       }
+	//     `),
+	// 		await graphcms.request(gql`
+	//       query GetCustomers() {
+	//         customers() {
+	//           slug
+	//         }
+	//       }
+	//     `),
+	// 		await graphcms.request(gql`
+	//       query GetChangelogs() {
+	//         changelogs() {
+	//           slug
+	//         }
+	//       }
+	//     `),
+	// 		await getGithubDocsPaths(),
+	// 	])
+	// 	H.metrics([
+	// 		{
+	// 			name: 'sitemap-gql-latency-ms',
+	// 			value: global.performance.now() - start,
+	// 			tags: [{ name: 'site', value: process.env.WEBSITE_URL || '' }],
+	// 		},
+	// 	])
+	//
+	// 	const blogPages = posts.map((post: any) => `blog/${post.slug}`)
+	// 	const customerPages = customers.map(
+	// 		(customer: { slug: string }) => `customers/${customer.slug}`,
+	// 	)
+	// 	const changelogPages = changelogs.map(
+	// 		(changelog: { slug: string }) => `changelog/${changelog.slug}`,
+	// 	)
+	// 	const docsPages = Array.from(docs.keys()).map(
+	// 		(d) => `docs/${d.split('docs-content/').pop()}`,
+	// 	)
+	// 	const productPages = Object.values(PRODUCTS).map(
+	// 		(product: iProduct) => `for/${product.slug}`,
+	// 	)
+	// 	const featurePages = Object.values(FEATURES).map(
+	// 		(feature: iFeature) => `${feature.slug}`,
+	// 	)
+	//
+	// 	const staticPagePaths = process.env.staticPages?.split(', ') || []
+	// 	const staticPages = staticPagePaths.map((path) => {
+	// 		return `${path.replace('pages', '').replace('index.tsx', '')}`
+	// 	})
+	//
+	// 	const pages = [
+	// 		...staticPages,
+	// 		...blogPages,
+	// 		...customerPages,
+	// 		...changelogPages,
+	// 		...docsPages,
+	// 		...productPages,
+	// 		...featurePages,
+	// 	]
+	//
+	// 	const addPage = (page: string) => {
+	// 		return `    <url>
+	//       <loc>${`${process.env.WEBSITE_URL}/${page}`}</loc>
+	//       <changefreq>hourly</changefreq>
+	//     </url>`
+	// 	}
+	//
+	// 	return `<?xml version="1.0" encoding="UTF-8"?>
+	//   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+	// ${pages.map(addPage).join('\n')}
+	//   </urlset>`
 }
 
 export default async function handler(
