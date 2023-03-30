38303bdbee9951d373c5c1d82e2bdd221d52a96e
Eric Thomas <eric@highlight.io>
2023-03-30 12:50:46 -0600

    only include the year when date range spans a new year

--- packages/ui/src/components/DatePicker/PreviousDateRangePicker.test.tsx
+++ packages/ui/src/components/DatePicker/PreviousDateRangePicker.test.tsx
@@ -33,6 +33,12 @@ describe('getLabel', () => {
 	it('handles when the preset does not match', async () => {
 		const selectedDates = [subtractDays(now, 2), now]
 		const label = getLabel({ selectedDates, presets })
-		expect(label).toEqual('Feb 12, 2023, 09:30 PM - Feb 14, 2023, 09:30 PM')
+		expect(label).toEqual('Feb 12, 09:30 PM - Feb 14, 09:30 PM')
+	})
+
+	it('includes the year when the the range spans to the next year', async () => {
+		const selectedDates = [subtractDays(now, 60), now]
+		const label = getLabel({ selectedDates, presets })
+		expect(label).toEqual('Feb 12, 2023, 09:30 PM - Feb 14, 2022, 09:30 PM')
 	})
 })
