3ce4c06a869c01bf621b603ae0eaa1c172c54679
mayberryzane <86132398+mayberryzane@users.noreply.github.com>
2023-04-12 16:01:15 -0700

    add sync slack button to log alerts slack dropdown (#4954)
    ## Summary
    - need a way to refresh slack channels
    - basic logic copied over from other alerts
    <!--
    Ideally, there is an attached GitHub issue that will describe the "why".
    
    If relevant, use this section to call out any additional information
    you'd like to _highlight_ to the reviewer.
    -->
    
    ## How did you test this change?
    - click test locally
    <!--
    Frontend - Leave a screencast or a screenshot to visually describe the
    changes.
    -->
    
    ## Are there any deployment considerations?
    - no
    <!--
     Backend - Do we need to consider migrations or backfilling data?
    -->

--- frontend/src/pages/Alerts/LogAlert/LogAlertPage.tsx
+++ frontend/src/pages/Alerts/LogAlert/LogAlertPage.tsx
@@ -42,19 +42,21 @@ import {
 import LogsHistogram from '@pages/LogsPage/LogsHistogram/LogsHistogram'
 import { Search } from '@pages/LogsPage/SearchForm/SearchForm'
 import { useParams } from '@util/react-router/useParams'
-import { message } from 'antd'
+import { Divider, message } from 'antd'
 import { capitalize } from 'lodash'
 import moment from 'moment'
 import { useEffect, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { DateTimeParam, StringParam, useQueryParam } from 'use-query-params'
 
+import { getSlackUrl } from '@/components/Header/components/ConnectHighlightWithSlackButton/utils/utils'
 import LoadingBox from '@/components/LoadingBox'
 import { namedOperations } from '@/graph/generated/operations'
 import {
 	DiscordChannelInput,
 	SanitizedSlackChannelInput,
 } from '@/graph/generated/schemas'
+import SyncWithSlackButton from '@/pages/Alerts/AlertConfigurationCard/SyncWithSlackButton'
 
 import * as styles from './styles.css'
 
@@ -264,13 +266,32 @@ export const LogAlertPage = () => {
 							query: form.getValue(form.names.query),
 						}
 
-						if (!input.name) {
-							message.error(`Missing alert name!`)
-							return
-						}
+						const nameErr = !input.name
+						const thresholdErr = !input.count_threshold
+						if (nameErr || thresholdErr) {
+							const errs = []
+							if (nameErr) {
+								form.setError(
+									form.names.name,
+									'Name is required',
+								)
+								errs.push('name')
+							}
 
-						if (!input.count_threshold) {
-							message.error(`Missing alert threshold!`)
+							if (thresholdErr) {
+								form.setError(
+									form.names.threshold,
+									'Threshold is required',
+								)
+								errs.push('threshold')
+							}
+
+							message.error(
+								`Missing required field(s): ${errs.join(
+									', ',
+								)}.`,
+							)
+
 							return
 						}
 
@@ -470,6 +491,14 @@ const LogAlertForm = ({
 			id: email,
 		}))
 
+	const syncSlack = (
+		<SyncWithSlackButton
+			slackUrl={getSlackUrl(projectId ?? '')}
+			isSlackIntegrated={alertsPayload?.is_integrated_with_slack || false}
+			refetchQueries={[namedOperations.Query.GetLogAlertsPagePayload]}
+		/>
+	)
+
 	return (
 		<Box cssClass={styles.grid}>
 			<Stack gap="12">
@@ -520,6 +549,11 @@ const LogAlertForm = ({
 									label="Red"
 								/>
 							}
+							style={{
+								borderColor: form.errors.threshold
+									? 'var(--color-red-500)'
+									: undefined,
+							}}
 						/>
 					</Column>
 
@@ -566,6 +600,11 @@ const LogAlertForm = ({
 					type="text"
 					placeholder="Alert name"
 					label="Name"
+					style={{
+						borderColor: form.errors.name
+							? 'var(--color-red-500)'
+							: undefined,
+					}}
 				/>
 
 				<Form.NamedSection
@@ -617,15 +656,22 @@ const LogAlertForm = ({
 							)
 						}}
 						value={form.values.slackChannels}
-						notFoundContent={
-							slackChannels.length === 0 ? (
-								<Link to="/integrations">
-									Connect Highlight with Slack
-								</Link>
-							) : (
-								'Slack channel not found'
-							)
-						}
+						notFoundContent="Slack channel not found"
+						dropdownRender={(menu) => (
+							<div>
+								{menu}
+								{slackChannels.length > 0 && (
+									<>
+										<Divider
+											style={{
+												margin: '4px 0',
+											}}
+										/>
+										<Box mx="12">{syncSlack}</Box>
+									</>
+								)}
+							</div>
+						)}
 						className={styles.selectContainer}
 						mode="multiple"
 						labelInValue
