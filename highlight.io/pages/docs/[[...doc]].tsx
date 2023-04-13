5a03bf72d8fbc2979975cd1a7fbc3da00e797d28
Jay Khatri <jaykhatrimail@gmail.com>
2023-04-13 15:51:21 -0700

    force rebuild

--- highlight.io/pages/docs/[[...doc]].tsx
+++ highlight.io/pages/docs/[[...doc]].tsx
@@ -1347,7 +1347,7 @@ const QuickStart = (content: { content: QuickStartContent }) => {
 	)
 }
 
-const MissingFrameworkCopy = () => {
+const MissingFrameworkCopy = ({}) => {
 	return (
 		<Callout
 			content={`If there's a framework that's missing, feel free to [create an issue](https://github.com/highlight/highlight/issues/new?assignees=&labels=external+bug+%2F+request&template=feature_request.md&title=) or message us on [discord](https://highlight.io/community).`}
