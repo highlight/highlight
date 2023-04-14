60293849d766ed37fbb447c8378e990a5f7016e7
Vadim Korolik <vkorolik@gmail.com>
2023-04-13 17:05:15 -0700

    use native type

--- sdk/client/src/listeners/web-vitals-listener/web-vitals-listener.tsx
+++ sdk/client/src/listeners/web-vitals-listener/web-vitals-listener.tsx
@@ -1,9 +1,4 @@
-import { onCLS, onFCP, onFID, onLCP, onTTFB, onINP } from 'web-vitals'
-
-interface Metric {
-	name: string
-	value: number
-}
+import { onCLS, onFCP, onFID, onLCP, onTTFB, onINP, Metric } from 'web-vitals'
 
 export const WebVitalsListener = (callback: (metric: Metric) => void) => {
 	onCLS(callback)
