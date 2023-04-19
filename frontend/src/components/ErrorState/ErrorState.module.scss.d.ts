6d3f811eb6d3e627a28d8293f7a6cf20da85c2f5
Shayne O'Sullivan <shayneo@pm.me>
2023-04-19 09:14:25 -0500

    Convert errorBody display to details and summary (#4918)
    ## Summary
    
    Converts the ErrorState component from a clickable span to
    `<details><summary/></details>`
    [elements](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/details),
    which handles showing and hiding detailed information natively.
    
    ## How did you test this change?
    Tested with auth error. Validated in Chrome, Firefox, Safari.
    
    ### Before
    <img width="547" alt="Screenshot 2023-04-10 at 9 12 04 PM"
    src="https://user-images.githubusercontent.com/1341253/231039779-5adbda4d-3af7-4bfd-bd46-b54b638963f6.png">
    
    <img width="540" alt="Screenshot 2023-04-10 at 9 12 10 PM"
    src="https://user-images.githubusercontent.com/1341253/231039838-bf2cb95e-73da-43b9-9b53-6bf623abd0dd.png">
    
    ### After
    <img width="577" alt="Screenshot 2023-04-10 at 9 12 50 PM"
    src="https://user-images.githubusercontent.com/1341253/231039865-7ddff6c7-e68b-4c76-b8b1-e14db307015e.png">
    
    <img width="524" alt="Screenshot 2023-04-10 at 9 12 58 PM"
    src="https://user-images.githubusercontent.com/1341253/231039898-4a64723a-fbb5-4c2e-af0b-e1808fb249e5.png">
    
    
    
    ## Are there any deployment considerations?
    nope.

--- frontend/src/components/ErrorState/ErrorState.module.scss.d.ts
+++ frontend/src/components/ErrorState/ErrorState.module.scss.d.ts
@@ -1,6 +1,5 @@
 export const buttonGroup: string
 export const errorBody: string
 export const errorWrapper: string
-export const expandButton: string
 export const loggedInButtonGroup: string
 export const shownWithHeader: string
