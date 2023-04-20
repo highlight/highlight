c960e555fa1827f1d1611e672ed4f5a80b4fcb8d
Chris Esplin <chris@chrisesplin.com>
2023-04-20 08:13:06 -0600

    Inline styles for @highlight-run/react

--- sdk/highlight-react/src/components/index.ts
+++ sdk/highlight-react/src/components/index.ts
@@ -1,4 +1,4 @@
-import './fonts.css'
-import './styles.css'
+// import './fonts.css'
+// import './styles.css'
 export * from './ErrorBoundary/ErrorBoundary'
 export * from './SampleBuggyButton/SampleBuggyButton'
