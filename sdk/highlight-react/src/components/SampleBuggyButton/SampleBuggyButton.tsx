132eb635dd002b2e4c7786d21a3d2eb15ac8dea0
Chris Esplin <chris@chrisesplin.com>
2023-04-20 08:27:08 -0600

    SampleBuggyButton working

--- sdk/highlight-react/src/components/SampleBuggyButton/SampleBuggyButton.tsx
+++ sdk/highlight-react/src/components/SampleBuggyButton/SampleBuggyButton.tsx
@@ -4,9 +4,15 @@ export const SampleBuggyButton = ({
 	children,
 }: React.PropsWithChildren<{}>) => {
 	const [isError, setError] = React.useState(false)
-	if (isError) {
-		throw new Error('something bad happened - this is a sample test error')
-	}
+
+	React.useEffect(() => {
+		if (isError) {
+			setError(false)
+
+			throw new Error('SampleBuggyButton: Error!!!')
+		}
+	}, [isError])
+
 	return (
 		<button
 			type="button"
