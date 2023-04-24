3e06075fffa4a93b25e1debb0919a2e5cbacf719
Chris Esplin <chris@chrisesplin.com>
2023-04-24 10:46:53 -0600

    Removing e2e/nextjs

--- e2e/nextjs/highlight.config.ts
+++
@@ -1,8 +0,0 @@
-import { Highlight } from '@highlight-run/next'
-
-export const withHighlight = Highlight({
-	projectID: '1jdkoe52',
-	debug: true,
-	backendUrl: 'http://localhost:8082/public',
-	otlpEndpoint: 'http://localhost:4318',
-})
