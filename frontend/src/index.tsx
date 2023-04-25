d3ea91c17300a2ad4ea88802fcc61d60208cc2a5
Chris Esplin <chris@chrisesplin.com>
2023-04-25 13:59:14 -0600

    Exploring the ErrorState

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -240,6 +240,10 @@ const AuthenticationRoleRouter = () => {
 		refetch = sRefetch
 	}
 
+	adminError = 'asdf'
+
+	console.log({ workspaceId, projectId })
+
 	const { setLoadingState } = useAppLoadingContext()
 	const [getProjectQuery] = useGetProjectLazyQuery()
 
