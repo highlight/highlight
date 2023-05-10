9b67273f137aaf41560893828e10949cffa3d0fa
Chris Schmitz <ccschmitz@gmail.com>
2023-05-10 11:10:27 -0500

    Update UI of new comment form

--- packages/ui/src/components/Form/Form.tsx
+++ packages/ui/src/components/Form/Form.tsx
@@ -103,15 +103,28 @@ export const Submit = ({ ...props }: ButtonProps) => {
 	return <Button type="submit" {...props} />
 }
 
-export type InputProps = Omit<AriaKitFormInputProps, 'size'> &
+export type InputProps = Omit<AriaKitFormInputProps, 'as' | 'size'> &
 	Variants &
 	HasLabel & {
+		// Shouldn't need to override the `as` types. Should see if we can remove
+		// them in a future PR.
+		as?: 'input' | 'textarea'
 		cssClass?: ClassValue | ClassValue[]
 	}
 
 export const Input = forwardRef<HTMLInputElement, InputProps>(
 	(
-		{ label, cssClass, size, collapsed, truncate, outline, name, ...props },
+		{
+			label,
+			cssClass,
+			size,
+			collapsed,
+			truncate,
+			outline,
+			name,
+			as = 'input',
+			...props
+		},
 		ref,
 	) => {
 		const _ref = useRef<HTMLInputElement>(null)
@@ -128,6 +141,7 @@ export const Input = forwardRef<HTMLInputElement, InputProps>(
 		return (
 			<NamedSection label={label} name={name}>
 				<AriaKitFormInput
+					as={as}
 					ref={ref}
 					name={name}
 					className={clsx(
