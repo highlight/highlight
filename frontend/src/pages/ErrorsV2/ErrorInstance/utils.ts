46b9dfd32277a6cf61c4eec62142d5cd13ea0dde
Eric Thomas <eric@highlight.io>
2023-05-11 10:35:30 -0600

    remove utils

--- frontend/src/pages/ErrorsV2/ErrorInstance/utils.ts
+++
@@ -1,6 +0,0 @@
-import { ErrorObjectFragment } from "@/graph/generated/operations"
-
-export const isSessionAvailable = (errorObject?: ErrorObjectFragment) => {
-    return errorObject && errorObject.session?.excluded === false
-
-}
