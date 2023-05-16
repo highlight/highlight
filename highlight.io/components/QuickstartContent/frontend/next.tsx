671b7a78851bd862cb80b2d1c25f7eaa725ff053
Chris Esplin <chris@chrisesplin.com>
2023-05-16 07:24:57 -0600

    Cleaning up Next.js quickstart

--- highlight.io/components/QuickstartContent/frontend/next.tsx
+++ highlight.io/components/QuickstartContent/frontend/next.tsx
@@ -1,12 +1,13 @@
 import {
 	configureSourcemapsCI,
 	identifySnippet,
-	initializeSnippet,
 	verifySnippet,
 } from './shared-snippets'
 
-import { QuickStartContent } from '../QuickstartContent'
 import { siteUrl } from '../../../utils/urls'
+import { QuickStartContent } from '../QuickstartContent'
+
+const GUIDE_URL = siteUrl('/docs/getting-started/fullstack-frameworks/next-js')
 
 export const NextContent: QuickStartContent = {
 	title: 'Next.js',
@@ -21,52 +22,74 @@ export const NextContent: QuickStartContent = {
 			code: {
 				text: `
 # with npm
-npm install highlight.run @highlight-run/react
+npm install @highlight-run/next highlight.run @highlight-run/react
 
 # with yarn
-yarn add highlight.run @highlight-run/react
+yarn add @highlight-run/next highlight.run @highlight-run/react
 				`,
 				language: 'bash',
 			},
 		},
 		{
-			title: 'Using the `app` dir?',
-			content: `If you're using the \`app\` directory, you'll need to ensure that highlight.io gets intialized in a client-only context. See our full docs on [Next.js](${siteUrl(
-				'/docs/getting-started/fullstack-frameworks/next-js',
-			)}).`,
+			title: 'Initialize the client SDK.',
+			content: `Grab your project ID from [app.highlight.io/setup](https://app.highlight.io/setup). If you're using the original Next.js Page router, drop \`<HighlightInit />\` in your \`_app.tsx\` file. For the App Router, add it to your top-level \`layout.tsx\` file.`,
+			code: {
+				text: `
+// src/app/layout.tsx
+import { HighlightInit } from '@highlight-run/next/highlight-init'
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+	return (
+		<>
+			<HighlightInit
+				projectId={'<YOUR_PROJECT_ID>'}
+				tracingOrigins
+				networkRecording={{
+					enabled: true,
+					recordHeadersAndBody: true,
+					urlBlocklist: [],
+				}}
+			/>
+
+			<html lang="en">
+				<body>{children}</body>
+			</html>
+		</>
+	)
+}
+				`,
+				language: 'js',
+			},
 		},
-		initializeSnippet,
 		{
 			title: 'Add the ErrorBoundary component. (optional)',
 			content: `The ErrorBoundary component wraps your component tree and catches crashes/exceptions from your react app. When a crash happens, your users will be prompted with a modal to share details about what led up to the crash. Read more [here](${siteUrl(
 				'/docs/getting-started/client-sdk/replay-configuration',
 			)}).`,
 			code: {
-				text: `import { ErrorBoundary } from '@highlight-run/react';
+				text: `
+import { ErrorBoundary } from '@highlight-run/react';
 
-				export default function App({ Component, pageProps }: AppProps) {
-				
-					// other page level logic ...
-					
-					return (
-						<ErrorBoundary>
-							<Component {...pageProps} />
-						</ErrorBoundary>
-					);
-				}`,
+export default function App({ Component, pageProps }: AppProps) {
+
+	// other page level logic ...
+	
+	return (
+		<ErrorBoundary>
+			<Component {...pageProps} />
+		</ErrorBoundary>
+	);
+}
+			`,
 				language: 'js',
 			},
 		},
 		identifySnippet,
 		verifySnippet,
-		configureSourcemapsCI(
-			'/docs/getting-started/fullstack-frameworks/next-js',
-		),
+		configureSourcemapsCI(`${GUIDE_URL}#test-source-maps`),
 		{
 			title: 'More Next.js features?',
-			content: `With Next.js, we have an additional package (\`@highlight-run/next\`) which supports automatic sourcemap uploading, network proxying, and log destinations. Read more in our [Next.js docs](${siteUrl(
-				'/docs/getting-started/fullstack-frameworks/next-js',
-			)}).`,
+			content: `See our [fullstack Next.js guide](${GUIDE_URL}) for more information on how to use Highlight with Next.js.`,
 		},
 	],
 }
