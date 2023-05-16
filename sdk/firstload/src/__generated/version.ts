a7a4200a2321ade0a2ed63f5900a6933a9e5cd58
Vadim Korolik <vkorolik@gmail.com>
2023-05-16 11:21:22 -0700

    use consistent js extension for highlight.run

--- sdk/firstload/src/__generated/version.ts
+++ sdk/firstload/src/__generated/version.ts
@@ -1,1 +1,1 @@
-export default "6.4.2"
+export default "6.4.3"
--- sdk/firstload/vite.config.ts
+++ sdk/firstload/vite.config.ts
@@ -18,6 +18,7 @@ export default defineConfig({
 		rollupOptions: {
 			output: {
 				exports: 'named',
+				entryFileNames: '[name].[format].js',
 			},
 		},
 	},
