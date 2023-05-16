d00c7d10fee2117ebc20af7c4805c16d61d024e9
Lewis Liu <lewisl9029@users.noreply.github.com>
2023-05-16 14:27:06 -0700

    Use a single node & esbuild process for reflame scripts

--- frontend/scripts/build-scss-modules.mjs
+++ frontend/scripts/build-scss-modules.mjs
@@ -5,90 +5,94 @@ import * as fs from 'node:fs'
 import * as path_ from 'node:path'
 import readdirp from 'readdirp'
 
-const args = process.argv.slice(2)
-const watch = args.includes('--watch') || args.includes('-w')
+export const run = async () => {
+	const args = process.argv.slice(2)
+	const watch = args.includes('--watch') || args.includes('-w')
 
-const workingDirectory = path_.join(process.cwd(), './src')
-const outputDirectory = path_.join(workingDirectory, '__generated/scss')
+	const workingDirectory = path_.join(process.cwd(), './src')
+	const outputDirectory = path_.join(workingDirectory, '__generated/scss')
 
-const watchedFiles = ['**/**.scss']
+	const watchedFiles = ['**/**.scss']
 
-// uncomment for cleanup
-// await fs.promises.rm(outputDirectory, { recursive: true, force: true })
+	// uncomment for cleanup
+	// await fs.promises.rm(outputDirectory, { recursive: true, force: true })
 
-// TODO: also need a special case for /static/shared.scss where we need to update
-// every .scss file that potentially imports from it
-// Luckily that seems to be the only usage of scss imports
-const build = async (path) => {
-	console.log(new Date(), 'building scss module', path)
-	const result = await esbuild
-		.build({
-			entryPoints: [path],
-			outExtension: { '.js': '.scss.js' },
-			absWorkingDir: workingDirectory,
-			bundle: true,
-			// TODO: support user supplied sourcemaps
-			// sourcemap: true,
-			format: 'esm',
-			allowOverwrite: true,
-			platform: 'browser',
-			outdir: outputDirectory,
-			outbase: workingDirectory,
-			minify: true,
-			splitting: false,
-			target: 'esnext',
-			plugins: [
-				stylePlugin({
-					cssModulesOptions: {
-						// Can't do camelCaseOnly due to mentions.module.scss
-						// for @highlight-run/react-mentions
-						localsConvention: 'camelCase',
-					},
-				}),
-			],
-			external: [],
-			write: false,
-			logLevel: 'warning',
-		})
-		.catch((error) => {
-			console.error(error)
+	// TODO: also need a special case for /static/shared.scss where we need to update
+	// every .scss file that potentially imports from it
+	// Luckily that seems to be the only usage of scss imports
+	const build = async (path) => {
+		console.log(new Date(), 'building scss module', path)
+		const result = await esbuild
+			.build({
+				entryPoints: [path],
+				outExtension: { '.js': '.scss.js' },
+				absWorkingDir: workingDirectory,
+				bundle: true,
+				// TODO: support user supplied sourcemaps
+				// sourcemap: true,
+				format: 'esm',
+				allowOverwrite: true,
+				platform: 'browser',
+				outdir: outputDirectory,
+				outbase: workingDirectory,
+				minify: true,
+				splitting: false,
+				target: 'esnext',
+				plugins: [
+					stylePlugin({
+						cssModulesOptions: {
+							// Can't do camelCaseOnly due to mentions.module.scss
+							// for @highlight-run/react-mentions
+							localsConvention: 'camelCase',
+						},
+					}),
+				],
+				external: [],
+				write: false,
+				logLevel: 'warning',
+			})
+			.catch((error) => {
+				console.error(error)
+				return
+			})
+		if (!result) {
 			return
-		})
-	if (!result) {
+		}
+		const jsOutput = result.outputFiles.find(({ path }) =>
+			path.endsWith('.js'),
+		)
+		const outputPath = jsOutput.path
+		await fs.promises.mkdir(path_.dirname(outputPath), { recursive: true })
+		await fs.promises.writeFile(outputPath, jsOutput.contents)
+		console.log(new Date(), 'built scss module', path)
 		return
 	}
-	const jsOutput = result.outputFiles.find(({ path }) => path.endsWith('.js'))
-	const outputPath = jsOutput.path
-	await fs.promises.mkdir(path_.dirname(outputPath), { recursive: true })
-	await fs.promises.writeFile(outputPath, jsOutput.contents)
-	console.log(new Date(), 'built scss module', path)
-	return
-}
 
-const removeBuildOutput = async (path) => {
-	console.log(new Date(), 'removing scss module', path)
-	await fs.promises.rm(path_.join(outputDirectory, path + '.js'), {
-		force: true,
-	})
-}
-
-if (watch) {
-	chokidar
-		.watch(watchedFiles, {
-			cwd: workingDirectory,
-			ignored: ['**/__generated/**'],
+	const removeBuildOutput = async (path) => {
+		console.log(new Date(), 'removing scss module', path)
+		await fs.promises.rm(path_.join(outputDirectory, path + '.js'), {
+			force: true,
 		})
-		.on('add', build)
-		.on('change', build)
-		.on('unlink', removeBuildOutput)
-		.on('ready', () => console.log(new Date(), 'scss modules ready'))
-} else {
-	await Promise.all(
-		(
-			await readdirp.promise(workingDirectory, {
-				fileFilter: watchedFiles,
-				directoryFilter: ['!__generated'],
+	}
+
+	if (watch) {
+		chokidar
+			.watch(watchedFiles, {
+				cwd: workingDirectory,
+				ignored: ['**/__generated/**'],
 			})
-		).map(({ path }) => build(path)),
-	)
+			.on('add', build)
+			.on('change', build)
+			.on('unlink', removeBuildOutput)
+			.on('ready', () => console.log(new Date(), 'scss modules ready'))
+	} else {
+		await Promise.all(
+			(
+				await readdirp.promise(workingDirectory, {
+					fileFilter: watchedFiles,
+					directoryFilter: ['!__generated'],
+				})
+			).map(({ path }) => build(path)),
+		)
+	}
 }
--- frontend/scripts/build-vanilla-extract.mjs
+++ frontend/scripts/build-vanilla-extract.mjs
@@ -1,103 +1,115 @@
 import { vanillaExtractPlugin } from '@vanilla-extract/esbuild-plugin'
 import chokidar from 'chokidar'
 import esbuild from 'esbuild'
+import * as fs from 'node:fs'
 import * as path_ from 'node:path'
 import readdirp from 'readdirp'
 
-const args = process.argv.slice(2)
-const watch = args.includes('--watch') || args.includes('-w')
+export const run = async () => {
+	const args = process.argv.slice(2)
+	const watch = args.includes('--watch') || args.includes('-w')
 
-// We actually need to watch all .ts/.tsx files and rebuild everything
-// since .css.ts can technically import from anywhere
-// Should probably use them as entry points and use native esbuild watcher instead
-// Blocked by https://github.com/evanw/esbuild/issues/1204#issuecomment-1483294233
-const entryPointGlobs = ['**/**.css.ts']
-const inputGlobs = ['**/**.ts', '**/**.tsx']
+	// We actually need to watch all .ts/.tsx files and rebuild everything
+	// since .css.ts can technically import from anywhere
+	// Should probably use them as entry points and use native esbuild watcher instead
+	// Blocked by https://github.com/evanw/esbuild/issues/1204#issuecomment-1483294233
+	const entryPointGlobs = ['**/**.css.ts']
+	const inputGlobs = ['**/**.ts', '**/**.tsx']
 
-const rootDirectory = process.cwd()
-const workingDirectory = path_.join(rootDirectory, './src')
-const outputDirectory = path_.join(workingDirectory, '__generated/ve')
+	const rootDirectory = process.cwd()
+	const workingDirectory = path_.join(rootDirectory, './src')
+	const outputDirectory = path_.join(workingDirectory, '__generated/ve')
 
-// uncomment for cleanup
-// await fs.promises.rm(outputDirectory, { recursive: true, force: true })
+	const packageJson = JSON.parse(
+		fs.readFileSync(path_.join(rootDirectory, 'package.json')),
+	)
 
-let entryPoints = Object.fromEntries(
-	(
-		await readdirp.promise(workingDirectory, {
-			fileFilter: entryPointGlobs,
-			directoryFilter: ['!__generated', '!graph/generated'],
-		})
-	).map(({ path }) => [path, path_.join(workingDirectory, path)]),
-)
+	// uncomment for cleanup
+	// await fs.promises.rm(outputDirectory, { recursive: true, force: true })
 
-const build = async (path) => {
-	console.log(new Date(), 'building vanilla extract entry points', path)
-	await esbuild.build({
-		entryPoints: Object.values(entryPoints),
-		bundle: true,
-		// TODO: support user supplied sourcemaps
-		// sourcemap: true,
-		format: 'esm',
-		platform: 'browser',
-		outdir: outputDirectory,
-		outbase: workingDirectory,
-		minify: true,
-		splitting: true,
-		target: 'esnext',
-		// Working directory apparently affects vanilla extract's hashes?
-		// Using rootDirectory to stay consistent with vite setup
-		absWorkingDir: rootDirectory,
-		plugins: [
-			vanillaExtractPlugin({
-				// We don't output css here, only the js modules for each stylesheet entrypoint
-				outputCss: false,
-				identifiers: 'short',
-			}),
-		],
-		external: [],
-		logLevel: 'warning',
-	})
-	console.log(new Date(), 'built vanilla extract entry points')
-	return
-}
+	let entryPoints = Object.fromEntries(
+		(
+			await readdirp.promise(workingDirectory, {
+				fileFilter: entryPointGlobs,
+				directoryFilter: ['!__generated', '!graph/generated'],
+			})
+		).map(({ path }) => [path, path_.join(workingDirectory, path)]),
+	)
 
-const isEntryPoint = (path) => path.endsWith('.css.ts')
-
-if (watch) {
-	// No await here since we don't want to delay watcher and miss updates
-	build()
-
-	chokidar
-		.watch(inputGlobs, {
-			cwd: workingDirectory,
-			ignoreInitial: true,
-			ignored: [
-				'**/__generated/**/**',
-				'**/graph/generated/**/**',
-				'**/**.d.ts',
+	const build = async (path) => {
+		console.log(new Date(), 'building vanilla extract entry points', path)
+		await esbuild.build({
+			entryPoints: Object.values(entryPoints),
+			bundle: true,
+			// TODO: support user supplied sourcemaps
+			// sourcemap: true,
+			format: 'esm',
+			platform: 'browser',
+			outdir: outputDirectory,
+			outbase: workingDirectory,
+			minify: true,
+			splitting: true,
+			target: 'esnext',
+			// Working directory apparently affects vanilla extract's hashes?
+			// Using rootDirectory to stay consistent with vite setup
+			absWorkingDir: rootDirectory,
+			plugins: [
+				vanillaExtractPlugin({
+					// We don't output css here, only the js modules for each stylesheet entrypoint
+					outputCss: false,
+					identifiers: 'short',
+				}),
 			],
+			external: [
+				...Object.keys(packageJson.dependencies).flatMap((pkg) => [
+					pkg,
+					`${pkg}/*`,
+				]),
+			],
+			logLevel: 'warning',
 		})
-		.on('add', (path) => {
-			if (isEntryPoint(path)) {
-				console.log(
-					new Date(),
-					'adding vanilla extract entry point',
-					path,
-				)
-				entryPoints[path] = path_.join(workingDirectory, path)
-			}
-			// We'll probably run into race conditions with concurrent edits here...
-			// TODO: make it more robust with some debouncing + transaction tracking
-			build(path)
-		})
-		.on('change', (path) => build(path))
-		.on('unlink', (path) => {
-			if (isEntryPoint(path)) {
-				delete entryPoints[path]
-			}
-			build(path)
-		})
-		.on('ready', () => console.log(new Date(), 'ready'))
-} else {
-	await build()
+		console.log(new Date(), 'built vanilla extract entry points')
+		return
+	}
+
+	const isEntryPoint = (path) => path.endsWith('.css.ts')
+
+	if (watch) {
+		// No await here since we don't want to delay watcher and miss updates
+		build()
+
+		chokidar
+			.watch(inputGlobs, {
+				cwd: workingDirectory,
+				ignoreInitial: true,
+				ignored: [
+					'**/__generated/**/**',
+					'**/graph/generated/**/**',
+					'**/**.d.ts',
+				],
+			})
+			.on('add', (path) => {
+				if (isEntryPoint(path)) {
+					console.log(
+						new Date(),
+						'adding vanilla extract entry point',
+						path,
+					)
+					entryPoints[path] = path_.join(workingDirectory, path)
+				}
+				// We'll probably run into race conditions with concurrent edits here...
+				// TODO: make it more robust with some debouncing + transaction tracking
+				build(path)
+			})
+			.on('change', (path) => build(path))
+			.on('unlink', (path) => {
+				if (isEntryPoint(path)) {
+					delete entryPoints[path]
+				}
+				build(path)
+			})
+			.on('ready', () => console.log(new Date(), 'ready'))
+	} else {
+		await build()
+	}
 }
