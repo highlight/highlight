c07af99af10048a02e606ab3a1d2f444a5109c59
Lewis Liu <lewisl9029@users.noreply.github.com>
2023-05-15 20:35:22 -0700

    Reflame environment workflow improvements (#5329)
    - We now include env vars for both envs in the reflame config, and use a
    single key to select the active one. This means the config will no
    longer change by itself, unless the actual doppler env vars change.
    - We update these using the env var script through doppler if it's
    available, noop otherwise, to seamlessly support outside contributors
    who don't have doppler access (like myself).
    - Also updated vscode config to use prettier as the default formatter
    for jsonc files to prevent whitespace changes on save.
    
    Note that these values are the hard-coded default ones, since I don't
    have access to doppler. Will need someone with doppler access to run
    `yarn reflame-build` to populate these with real values.
    
    Also, I tried to guess the doppler config names based on clues in the
    repo (namely `prd` and `docker`), but unsure if these are the configs we
    actually want to use? Please feel free to update the PR with the real
    configs if these are incorrect.
    
    Cheers!
    
    ---------
    
    Co-authored-by: Vadim Korolik <vkorolik@gmail.com>

--- frontend/scripts/update-reflame-environment.mjs
+++ frontend/scripts/update-reflame-environment.mjs
@@ -4,18 +4,25 @@ import * as fs from 'node:fs'
 const configPath = '../.reflame.config.jsonc'
 const configText = fs.readFileSync(configPath, 'utf8')
 
+const name = process.argv[2]
+
+if (!name) {
+	throw new Error('missing environment name')
+}
+
 const result = jsoncParser.applyEdits(
 	configText,
 	jsoncParser.modify(
 		configText,
-		['environment'],
+		['environments', name],
 		{
-			REACT_APP_FIREBASE_CONFIG_OBJECT: process.env
-				.REACT_APP_FIREBASE_CONFIG_OBJECT ?? {
-				type: 'expression',
-				value: "JSON.stringify({apiKey: 'AIzaSyD7g86A3EzEKmoE7aZ04Re3HZ0B4bWlL68',authDomain: 'auth.highlight.run',databaseURL: 'https://highlight-f5c5b.firebaseio.com',projectId: 'highlight-f5c5b',storageBucket: 'highlight-f5c5b.appspot.com',messagingSenderId: '263184175068',appId: '1:263184175068:web:f8190c20320087d1c6c919',})",
-			},
-			REACT_APP_COMMIT_SHA: process.env.REACT_APP_COMMIT_SHA ?? {
+			REACT_APP_FIREBASE_CONFIG_OBJECT:
+				process.env.REACT_APP_FIREBASE_CONFIG_OBJECT?.replaceAll(
+					'\n',
+					'',
+				) ??
+				"{apiKey: 'AIzaSyD7g86A3EzEKmoE7aZ04Re3HZ0B4bWlL68',authDomain: 'auth.highlight.run',databaseURL: 'https://highlight-f5c5b.firebaseio.com',projectId: 'highlight-f5c5b',storageBucket: 'highlight-f5c5b.appspot.com',messagingSenderId: '263184175068',appId: '1:263184175068:web:f8190c20320087d1c6c919',}",
+			REACT_APP_COMMIT_SHA: {
 				type: 'expression',
 				value: 'Reflame.gitCommitSha',
 			},
@@ -23,11 +30,11 @@ const result = jsoncParser.applyEdits(
 				process.env.REACT_APP_PRIVATE_GRAPH_URI ??
 				'https://pri.highlight.run',
 			REACT_APP_ONPREM: process.env.REACT_APP_ONPREM ?? false,
-			REACT_APP_FRONTEND_URI: process.env.REACT_APP_FRONTEND_URI ?? {
+			REACT_APP_FRONTEND_URI: {
 				type: 'expression',
 				value: 'window.location.origin',
 			},
-			REACT_APP_FRONTEND_ORG: process.env.REACT_APP_FRONTEND_ORG ?? 1,
+			REACT_APP_FRONTEND_ORG: process.env.REACT_APP_FRONTEND_ORG ?? '1',
 			REACT_APP_PUBLIC_GRAPH_URI:
 				process.env.REACT_APP_PUBLIC_GRAPH_URI ??
 				'https://pub.highlight.run',
@@ -50,6 +57,6 @@ const result = jsoncParser.applyEdits(
 )
 
 if (result !== configText) {
-	console.log('updating Reflame config with new env vars')
+	console.log(`updating Reflame config with '${name}' environment`)
 	fs.writeFileSync(configPath, result)
 }
