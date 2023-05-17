d51594d97ca4a4125c810b125bfcdb90a4ed1e4b
Lewis Liu <lewisl9029@users.noreply.github.com>
2023-05-17 13:45:48 -0700

    Update script again

--- scripts/reflame-check.sh
+++ scripts/reflame-check.sh
@@ -1,9 +1,13 @@
+#!/bin/sh
+
 set -e
 yarn reflame-build --exclude-rrweb
 
-if [[ $(git ls-files -om --exclude-standard | wc -l) -gt 0 ]]
+if [ $(git ls-files -om --exclude-standard | wc -l) = 0 ]
 then
-  echo "Please run \`yarn reflame-build\` and commit the following updated files:"
-  git ls-files -om --exclude-standard
-  exit 1
+  exit 0
 fi
+
+echo "Please run \`yarn reflame-build\` and commit the following updated files:"
+git ls-files -om --exclude-standard
+exit 1
