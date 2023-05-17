94849d8212bb0580db86690e572bfdb3c26784d6
Chris Esplin <chris@chrisesplin.com>
2023-05-17 10:59:22 -0600

    tRPC test bed

---
+++ e2e/nextjs/src/app/components/trpc-queries.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import { trpc } from '@/app/utils/trpc'
+
+export const TrpcQueries = trpc.withTRPC(function TrpcQueries() {
+	const helloWorld = trpc.helloWorld.useQuery({ message: 'Hello Chris' })
+
+	return !helloWorld.data ? (
+		<div>loading...</div>
+	) : (
+		<div
+			className="trpc-buttons"
+			style={{
+				display: 'grid',
+				gridTemplateColumns: '20rem',
+				gridGap: '1rem',
+				padding: '2rem',
+			}}
+		>
+			<style
+				dangerouslySetInnerHTML={{
+					__html: `
+					.trpc-buttons a {
+						display: inline-grid;
+					}			
+				`,
+				}}
+			/>
+			<p>Hello World payload: {JSON.stringify(helloWorld.data)}</p>
+		</div>
+	)
+})
