58758ead5fd77c185d518e15b886f5296682e53d
Eric Thomas <eric@highlight.io>
2023-05-18 11:27:59 -0600

    add test

--- backend/filtering/repository_test.go
+++ backend/filtering/repository_test.go
@@ -1,11 +1,11 @@
 package filtering
 
 import (
+	"context"
 	"testing"
 
 	"github.com/highlight-run/highlight/backend/model"
 	"github.com/highlight-run/highlight/backend/util"
-	e "github.com/pkg/errors"
 	"github.com/stretchr/testify/assert"
 	_ "gorm.io/driver/postgres"
 )
@@ -15,9 +15,7 @@ func TestUpdateProjectFilterSettings(t *testing.T) {
 
 	util.RunTestWithDBWipe(t, "UpdateProjectFilterSettings", repo.db, func(t *testing.T) {
 		project := model.Project{}
-		if err := repo.db.Create(&project).Error; err != nil {
-			t.Fatal(e.Wrap(err, "error inserting project"))
-		}
+		repo.db.Create(&project)
 
 		updatedSettings, err := repo.UpdateProjectFilterSettings(project, model.ProjectFilterSettings{
 			FilterSessionsWithoutError: true,
@@ -25,5 +23,34 @@ func TestUpdateProjectFilterSettings(t *testing.T) {
 		assert.NoError(t, err)
 
 		assert.True(t, updatedSettings.FilterSessionsWithoutError)
+	})
+}
+
+func TestFindProjectsWithAutoResolveSetting(t *testing.T) {
+	repo := setupFilteringRepository()
+
+	util.RunTestWithDBWipe(t, "FindProjectsWithAutoResolveSetting", repo.db, func(t *testing.T) {
+		project1 := model.Project{}
+		repo.db.Create(&project1)
+
+		project2 := model.Project{}
+		repo.db.Create(&project2)
+
+		_, err := repo.GetProjectFilterSettings(project1)
+		assert.NoError(t, err)
+		_, err = repo.UpdateProjectFilterSettings(project1, model.ProjectFilterSettings{})
+		assert.NoError(t, err)
+
+		_, err = repo.GetProjectFilterSettings(project2)
+		assert.NoError(t, err)
+		projectWithAutoResolveSetting, err := repo.UpdateProjectFilterSettings(project1, model.ProjectFilterSettings{
+			AutoResolveStaleErrorsDayInterval: 1,
+		})
+		assert.NoError(t, err)
+
+		projectFilterSettings := repo.findProjectsWithAutoResolveSetting(context.TODO())
+
+		assert.Len(t, projectFilterSettings, 1)
+		assert.Equal(t, projectFilterSettings[0].ID, projectWithAutoResolveSetting.ID)
 	})
 }
