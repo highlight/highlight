edee61b39c356715d0208b0098d875eb85d6ddf3
Eric Thomas <eric@highlight.io>
2023-05-19 11:25:06 -0600

    use RunTestWithDBWipe

--- backend/errorgroups/repository_test.go
+++ backend/errorgroups/repository_test.go
@@ -13,77 +13,79 @@ import (
 
 func TestUpdateErrorGroupStateByAdmin(t *testing.T) {
 	repo := setupErrorGroupsRepository()
-	defer util.ClearTablesInDB(repo.db) // nolint:errcheck
+	util.RunTestWithDBWipe(t, "TestUpdateErrorGroupStateByAdmin", repo.db, func(t *testing.T) {
 
-	errorGroup := model.ErrorGroup{
-		State: privateModel.ErrorStateOpen,
-	}
+		errorGroup := model.ErrorGroup{
+			State: privateModel.ErrorStateOpen,
+		}
 
-	admin := model.Admin{}
-	repo.db.Create(&admin)
+		admin := model.Admin{}
+		repo.db.Create(&admin)
 
-	now := time.Now()
-	params := UpdateErrorGroupParams{
-		ID:           -1,
-		State:        privateModel.ErrorStateIgnored,
-		SnoozedUntil: &now,
-	}
+		now := time.Now()
+		params := UpdateErrorGroupParams{
+			ID:           -1,
+			State:        privateModel.ErrorStateIgnored,
+			SnoozedUntil: &now,
+		}
 
-	_, err := repo.UpdateErrorGroupStateByAdmin(context.TODO(), admin, params)
-	assert.EqualError(t, err, "record not found")
+		_, err := repo.UpdateErrorGroupStateByAdmin(context.TODO(), admin, params)
+		assert.EqualError(t, err, "record not found")
 
-	repo.db.Create(&errorGroup)
+		repo.db.Create(&errorGroup)
 
-	params.ID = errorGroup.ID
+		params.ID = errorGroup.ID
 
-	updatedErrorGroup, err := repo.UpdateErrorGroupStateByAdmin(context.TODO(), admin, params)
-	assert.NoError(t, err)
+		updatedErrorGroup, err := repo.UpdateErrorGroupStateByAdmin(context.TODO(), admin, params)
+		assert.NoError(t, err)
 
-	assert.Equal(t, updatedErrorGroup.State, params.State)
-	assert.Equal(t, updatedErrorGroup.SnoozedUntil, params.SnoozedUntil)
+		assert.Equal(t, updatedErrorGroup.State, params.State)
+		assert.Equal(t, updatedErrorGroup.SnoozedUntil, params.SnoozedUntil)
 
-	activityLogs := []model.ErrorGroupActivityLog{}
-	repo.db.Where(model.ErrorGroupActivityLog{}).Find(&activityLogs)
+		activityLogs := []model.ErrorGroupActivityLog{}
+		repo.db.Where(model.ErrorGroupActivityLog{}).Find(&activityLogs)
 
-	assert.Len(t, activityLogs, 1)
-	assert.Equal(t, activityLogs[0].AdminID, admin.ID)
-	assert.Equal(t, activityLogs[0].ErrorGroupID, errorGroup.ID)
-	assert.Equal(t, activityLogs[0].EventType, model.ErrorGroupIgnoredEvent)
+		assert.Len(t, activityLogs, 1)
+		assert.Equal(t, activityLogs[0].AdminID, admin.ID)
+		assert.Equal(t, activityLogs[0].ErrorGroupID, errorGroup.ID)
+		assert.Equal(t, activityLogs[0].EventType, model.ErrorGroupIgnoredEvent)
+	})
 }
 
 func TestUpdateErrorGroupStateBySystem(t *testing.T) {
 	repo := setupErrorGroupsRepository()
-	defer util.ClearTablesInDB(repo.db) // nolint:errcheck
 
-	errorGroup := model.ErrorGroup{
-		State: privateModel.ErrorStateOpen,
-	}
+	util.RunTestWithDBWipe(t, "UpdateErrorGroupStateBySystem", repo.db, func(t *testing.T) {
+		errorGroup := model.ErrorGroup{
+			State: privateModel.ErrorStateOpen,
+		}
 
-	now := time.Now()
-	params := UpdateErrorGroupParams{
-		ID:           -1,
-		State:        privateModel.ErrorStateIgnored,
-		SnoozedUntil: &now,
-	}
+		now := time.Now()
+		params := UpdateErrorGroupParams{
+			ID:           -1,
+			State:        privateModel.ErrorStateIgnored,
+			SnoozedUntil: &now,
+		}
 
-	_, err := repo.UpdateErrorGroupStateBySystem(context.TODO(), params)
-	assert.EqualError(t, err, "record not found")
+		_, err := repo.UpdateErrorGroupStateBySystem(context.TODO(), params)
+		assert.EqualError(t, err, "record not found")
 
-	repo.db.Create(&errorGroup)
+		repo.db.Create(&errorGroup)
 
-	params.ID = errorGroup.ID
+		params.ID = errorGroup.ID
 
-	updatedErrorGroup, err := repo.UpdateErrorGroupStateBySystem(context.TODO(), params)
-	assert.NoError(t, err)
+		updatedErrorGroup, err := repo.UpdateErrorGroupStateBySystem(context.TODO(), params)
+		assert.NoError(t, err)
 
-	assert.Equal(t, updatedErrorGroup.State, params.State)
-	assert.Equal(t, updatedErrorGroup.SnoozedUntil, params.SnoozedUntil)
+		assert.Equal(t, updatedErrorGroup.State, params.State)
+		assert.Equal(t, updatedErrorGroup.SnoozedUntil, params.SnoozedUntil)
 
-	activityLogs := []model.ErrorGroupActivityLog{}
-	repo.db.Where(model.ErrorGroupActivityLog{}).Find(&activityLogs)
+		activityLogs := []model.ErrorGroupActivityLog{}
+		repo.db.Where(model.ErrorGroupActivityLog{}).Find(&activityLogs)
 
-	assert.Len(t, activityLogs, 1)
-	assert.Equal(t, activityLogs[0].AdminID, 0) // 0 means the system generated this
-	assert.Equal(t, activityLogs[0].ErrorGroupID, errorGroup.ID)
-	assert.Equal(t, activityLogs[0].EventType, model.ErrorGroupIgnoredEvent)
+		assert.Len(t, activityLogs, 1)
+		assert.Equal(t, activityLogs[0].AdminID, 0) // 0 means the system generated this
+		assert.Equal(t, activityLogs[0].ErrorGroupID, errorGroup.ID)
+		assert.Equal(t, activityLogs[0].EventType, model.ErrorGroupIgnoredEvent)
+	})
 }
--- backend/filtering/autoresolver_test.go
+++ backend/filtering/autoresolver_test.go
@@ -13,87 +13,88 @@ import (
 
 func TestAutoResolveStaleErrors(t *testing.T) {
 	service := AutoResolverService{setupFilteringRepository(), setupErrorGroupsRepository()}
-	defer util.ClearTablesInDB(service.FilteringRepository.db) // nolint:errcheck
 
-	project := model.Project{}
-	service.db.Create(&project)
+	util.RunTestWithDBWipe(t, "AutoResolveStaleErrors", service.db, func(t *testing.T) {
+		project := model.Project{}
+		service.db.Create(&project)
 
-	_, err := service.UpdateProjectFilterSettings(project, model.ProjectFilterSettings{
-		AutoResolveStaleErrorsDayInterval: 1,
+		_, err := service.UpdateProjectFilterSettings(project, model.ProjectFilterSettings{
+			AutoResolveStaleErrorsDayInterval: 1,
+		})
+		assert.NoError(t, err)
+
+		now := time.Now()
+		twentyThreeHoursAgo := now.Add(-time.Hour * 23)
+		twentyFiveHoursAgo := now.Add(-time.Hour * 25)
+
+		// Should not be autoresolved
+		recentErrorGroup := model.ErrorGroup{
+			State:     privateModel.ErrorStateOpen,
+			ProjectID: project.ID,
+		}
+		service.db.Create(&recentErrorGroup)
+		recentErrorObject := model.ErrorObject{
+			ErrorGroupID: recentErrorGroup.ID,
+			Model: model.Model{
+				CreatedAt: twentyThreeHoursAgo,
+			},
+		}
+		service.db.Create(&recentErrorObject)
+
+		// Should be autoresolved
+		oldErrorGroup := model.ErrorGroup{
+			State:     privateModel.ErrorStateOpen,
+			ProjectID: project.ID,
+		}
+		service.db.Create(&oldErrorGroup)
+		oldErrorObject := model.ErrorObject{
+			ErrorGroupID: oldErrorGroup.ID,
+			Model: model.Model{
+				CreatedAt: twentyFiveHoursAgo,
+			},
+		}
+		service.db.Create(&oldErrorObject)
+
+		// Should not be autoresolved since it belongs to a different project
+		projectWithNoSettings := model.Project{}
+		service.db.Create(&projectWithNoSettings)
+		oldErrorGroupForUnrelatedProject := model.ErrorGroup{
+			State:     privateModel.ErrorStateOpen,
+			ProjectID: projectWithNoSettings.ID,
+		}
+		service.db.Create(&oldErrorGroupForUnrelatedProject)
+		oldErrorObjectForUnrelatedProject := model.ErrorObject{
+			ErrorGroupID: oldErrorGroup.ID,
+			Model: model.Model{
+				CreatedAt: twentyFiveHoursAgo,
+			},
+		}
+		service.db.Create(&oldErrorObjectForUnrelatedProject)
+
+		service.AutoResolveStaleErrors(context.TODO())
+
+		errorGroup1 := model.ErrorGroup{}
+		service.db.Where(model.ErrorGroup{
+			Model: model.Model{
+				ID: recentErrorGroup.ID,
+			},
+		}).Find(&errorGroup1)
+		assert.Equal(t, errorGroup1.State, privateModel.ErrorStateOpen) // was not autoresolved
+
+		errorGroup2 := model.ErrorGroup{}
+		service.db.Where(model.ErrorGroup{
+			Model: model.Model{
+				ID: oldErrorGroup.ID,
+			},
+		}).Find(&errorGroup2)
+		assert.Equal(t, errorGroup2.State, privateModel.ErrorStateResolved) // was autoresolved
+
+		errorGroup3 := model.ErrorGroup{}
+		service.db.Where(model.ErrorGroup{
+			Model: model.Model{
+				ID: oldErrorGroupForUnrelatedProject.ID,
+			},
+		}).Find(&errorGroup3)
+		assert.Equal(t, errorGroup3.State, privateModel.ErrorStateOpen) // was not autoresolved
 	})
-	assert.NoError(t, err)
-
-	now := time.Now()
-	twentyThreeHoursAgo := now.Add(-time.Hour * 23)
-	twentyFiveHoursAgo := now.Add(-time.Hour * 25)
-
-	// Should not be autoresolved
-	recentErrorGroup := model.ErrorGroup{
-		State:     privateModel.ErrorStateOpen,
-		ProjectID: project.ID,
-	}
-	service.db.Create(&recentErrorGroup)
-	recentErrorObject := model.ErrorObject{
-		ErrorGroupID: recentErrorGroup.ID,
-		Model: model.Model{
-			CreatedAt: twentyThreeHoursAgo,
-		},
-	}
-	service.db.Create(&recentErrorObject)
-
-	// Should be autoresolved
-	oldErrorGroup := model.ErrorGroup{
-		State:     privateModel.ErrorStateOpen,
-		ProjectID: project.ID,
-	}
-	service.db.Create(&oldErrorGroup)
-	oldErrorObject := model.ErrorObject{
-		ErrorGroupID: oldErrorGroup.ID,
-		Model: model.Model{
-			CreatedAt: twentyFiveHoursAgo,
-		},
-	}
-	service.db.Create(&oldErrorObject)
-
-	// Should not be autoresolved since it belongs to a different project
-	projectWithNoSettings := model.Project{}
-	service.db.Create(&projectWithNoSettings)
-	oldErrorGroupForUnrelatedProject := model.ErrorGroup{
-		State:     privateModel.ErrorStateOpen,
-		ProjectID: projectWithNoSettings.ID,
-	}
-	service.db.Create(&oldErrorGroupForUnrelatedProject)
-	oldErrorObjectForUnrelatedProject := model.ErrorObject{
-		ErrorGroupID: oldErrorGroup.ID,
-		Model: model.Model{
-			CreatedAt: twentyFiveHoursAgo,
-		},
-	}
-	service.db.Create(&oldErrorObjectForUnrelatedProject)
-
-	service.AutoResolveStaleErrors(context.TODO())
-
-	errorGroup1 := model.ErrorGroup{}
-	service.db.Where(model.ErrorGroup{
-		Model: model.Model{
-			ID: recentErrorGroup.ID,
-		},
-	}).Find(&errorGroup1)
-	assert.Equal(t, errorGroup1.State, privateModel.ErrorStateOpen) // was not autoresolved
-
-	errorGroup2 := model.ErrorGroup{}
-	service.db.Where(model.ErrorGroup{
-		Model: model.Model{
-			ID: oldErrorGroup.ID,
-		},
-	}).Find(&errorGroup2)
-	assert.Equal(t, errorGroup2.State, privateModel.ErrorStateResolved) // was autoresolved
-
-	errorGroup3 := model.ErrorGroup{}
-	service.db.Where(model.ErrorGroup{
-		Model: model.Model{
-			ID: oldErrorGroupForUnrelatedProject.ID,
-		},
-	}).Find(&errorGroup3)
-	assert.Equal(t, errorGroup3.State, privateModel.ErrorStateOpen) // was not autoresolved
 }
