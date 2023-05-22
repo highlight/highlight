f211810e33e65ea6cafd39d07931109ec6a21fd5
Anthony Teo <yihonganthony@gmail.com>
2023-05-22 13:55:29 -0400

    Endpoint that saves all settings (#5387)
    ## Summary
    
    Figma for the new settings show that project settings are saved using a
    single button. This PR adds that endpoint, and changed much of the logic
    in the project settings to use the new context that manages settings.
    
    Discussion in Slack about it:
    https://highlightcorp.slack.com/archives/C01FJL7V630/p1683905993153839
    
    ## How did you test this change?
    
    <!--
    Frontend - Leave a screencast or a screenshot to visually describe the
    changes.
    -->
    
    ## Are there any deployment considerations?
    
    <!--
     Backend - Do we need to consider migrations or backfilling data?
    -->

--- frontend/src/pages/ProjectSettings/ErrorFiltersForm/ErrorFiltersForm.tsx
+++ frontend/src/pages/ProjectSettings/ErrorFiltersForm/ErrorFiltersForm.tsx
@@ -1,16 +1,11 @@
 import { FieldsBox } from '@components/FieldsBox/FieldsBox'
 import { LoadingBar } from '@components/Loading/Loading'
 import Select from '@components/Select/Select'
-import { useEditProjectMutation, useGetProjectQuery } from '@graph/hooks'
-import { namedOperations } from '@graph/operations'
-import { useProjectId } from '@hooks/useProjectId'
 import { message } from 'antd'
-import clsx from 'clsx'
-import React, { useEffect, useState } from 'react'
 import { Text } from 'recharts'
 
-import commonStyles from '../../../Common.module.scss'
-import Button from '../../../components/Button/Button/Button'
+import { useProjectSettingsContext } from '@/pages/ProjectSettings/ProjectSettingsContext/ProjectSettingsContext'
+
 import styles from './ErrorFiltersForm.module.scss'
 
 const isValidRegex = function (p: string) {
@@ -24,48 +19,11 @@ const isValidRegex = function (p: string) {
 }
 
 export const ErrorFiltersForm = () => {
-	const { projectId } = useProjectId()
-	const [errorFilters, setErrorFilters] = useState<string[]>([])
-	const { data, loading } = useGetProjectQuery({
-		variables: {
-			id: projectId!,
-		},
-		skip: !projectId,
-	})
-
-	const [editProject, { loading: editProjectLoading }] =
-		useEditProjectMutation({
-			refetchQueries: [
-				namedOperations.Query.GetProjects,
-				namedOperations.Query.GetProject,
-			],
-		})
-
-	const onSubmit = (e: { preventDefault: () => void }) => {
-		e.preventDefault()
-		if (!projectId) {
-			return
-		}
-		if (errorFilters.filter(isValidRegex).length < errorFilters.length) {
-			message.error(
-				`Please correct invalid regex patterns before saving.`,
-			)
-			return
-		}
-
-		editProject({
-			variables: {
-				id: projectId!,
-				error_filters: errorFilters,
-			},
-		}).then(() => {
-			message.success('Updated error filters!', 5)
-		})
-	}
-
-	useEffect(() => {
-		setErrorFilters(data?.project?.error_filters ?? [])
-	}, [data?.project?.error_filters])
+	const {
+		allProjectSettings: data,
+		loading,
+		setAllProjectSettings,
+	} = useProjectSettingsContext()
 
 	if (loading) {
 		return <LoadingBar />
@@ -75,14 +33,14 @@ export const ErrorFiltersForm = () => {
 		<FieldsBox id="filters">
 			<h3>Error Filters</h3>
 
-			<form onSubmit={onSubmit}>
+			<form>
 				<p>Enter regular expression patterns to filter out errors.</p>
 				<div className={styles.inputAndButtonRow}>
 					<Select
 						className={styles.input}
 						mode="tags"
 						placeholder="TypeError: Failed to fetch"
-						defaultValue={data?.project?.error_filters}
+						value={data?.projectSettings?.error_filters}
 						notFoundContent={
 							<Text>
 								Provide a regex pattern to filter out errors.
@@ -90,21 +48,18 @@ export const ErrorFiltersForm = () => {
 						}
 						onChange={(patterns: string[]) => {
 							patterns.filter(isValidRegex)
-							setErrorFilters(patterns)
+							setAllProjectSettings((currentProjectSettings) =>
+								currentProjectSettings?.projectSettings
+									? {
+											projectSettings: {
+												...currentProjectSettings.projectSettings,
+												error_filters: patterns,
+											},
+									  }
+									: currentProjectSettings,
+							)
 						}}
 					/>
-					<Button
-						loading={editProjectLoading}
-						trackingId="UpdateErrorFilters"
-						htmlType="submit"
-						type="primary"
-						className={clsx(
-							commonStyles.submitButton,
-							styles.saveButton,
-						)}
-					>
-						Save
-					</Button>
 				</div>
 			</form>
 		</FieldsBox>
--- frontend/src/pages/ProjectSettings/NetworkRecordingForm/NetworkRecordingForm.tsx
+++ frontend/src/pages/ProjectSettings/NetworkRecordingForm/NetworkRecordingForm.tsx
@@ -1,57 +1,20 @@
 import { FieldsBox } from '@components/FieldsBox/FieldsBox'
 import InfoTooltip from '@components/InfoTooltip/InfoTooltip'
-import { CircularSpinner, LoadingBar } from '@components/Loading/Loading'
+import { LoadingBar } from '@components/Loading/Loading'
 import Select from '@components/Select/Select'
-import { useEditProjectMutation, useGetProjectQuery } from '@graph/hooks'
-import { namedOperations } from '@graph/operations'
 import { useParams } from '@util/react-router/useParams'
-import { message } from 'antd'
-import clsx from 'clsx'
-import React, { useEffect, useState } from 'react'
 
-import commonStyles from '../../../Common.module.scss'
-import Button from '../../../components/Button/Button/Button'
+import { useProjectSettingsContext } from '@/pages/ProjectSettings/ProjectSettingsContext/ProjectSettingsContext'
+
 import styles from './NetworkRecordingForm.module.scss'
 
 export const NetworkRecordingForm = () => {
 	const { project_id } = useParams<{ project_id: string }>()
-	const [backendDomains, setBackendDomains] = useState<string[]>([])
-	const { data, loading } = useGetProjectQuery({
-		variables: {
-			id: project_id!,
-		},
-		skip: !project_id,
-	})
-
-	const [editProject, { loading: editProjectLoading }] =
-		useEditProjectMutation({
-			refetchQueries: [
-				namedOperations.Query.GetProjects,
-				namedOperations.Query.GetProject,
-			],
-			onCompleted: () => {
-				message.success('Updated network recording settings!', 5)
-			},
-		})
-
-	const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-		e.preventDefault()
-		if (!project_id) {
-			return
-		}
-		editProject({
-			variables: {
-				id: project_id!,
-				backend_domains: backendDomains,
-			},
-		}).catch(console.error)
-	}
-
-	useEffect(() => {
-		if (!loading) {
-			setBackendDomains(data?.project?.backend_domains ?? [])
-		}
-	}, [data?.project?.backend_domains, loading])
+	const {
+		allProjectSettings: data,
+		loading,
+		setAllProjectSettings,
+	} = useProjectSettingsContext()
 
 	if (loading) {
 		return <LoadingBar />
@@ -60,7 +23,7 @@ export const NetworkRecordingForm = () => {
 	return (
 		<FieldsBox id="network">
 			<h3>Network Recording Settings</h3>
-			<form onSubmit={onSubmit} key={project_id}>
+			<form key={project_id}>
 				<p>
 					Adjust how we process your network requests at the project
 					level.
@@ -78,36 +41,20 @@ export const NetworkRecordingForm = () => {
 						mode="tags"
 						placeholder="api.highlight.run"
 						notFoundContent={null}
-						defaultValue={data?.project?.backend_domains || []}
+						value={data?.projectSettings?.backend_domains ?? []}
 						onChange={(domains: string[]) => {
-							setBackendDomains(domains)
+							setAllProjectSettings((currentProjectSettings) =>
+								currentProjectSettings?.projectSettings
+									? {
+											projectSettings: {
+												...currentProjectSettings.projectSettings,
+												backend_domains: domains,
+											},
+									  }
+									: currentProjectSettings,
+							)
 						}}
 					/>
-				</div>
-				<div className={styles.fieldRow}>
-					<div className={styles.fieldKey}></div>
-					<div className={styles.saveButton}>
-						<Button
-							trackingId="NetworkRecordingSettingsUpdate"
-							htmlType="submit"
-							type="primary"
-							className={clsx(
-								commonStyles.submitButton,
-								styles.saveButton,
-							)}
-						>
-							{editProjectLoading ? (
-								<CircularSpinner
-									style={{
-										fontSize: 18,
-										color: 'var(--text-primary-inverted)',
-									}}
-								/>
-							) : (
-								'Save'
-							)}
-						</Button>
-					</div>
 				</div>
 			</form>
 		</FieldsBox>
