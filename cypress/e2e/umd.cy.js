143ede4f73e22545e3a96ae785f14c95d313434d
Vadim Korolik <vkorolik@gmail.com>
2023-05-23 19:07:20 -0700

    fix umd tests

--- cypress/e2e/umd.cy.js
+++ cypress/e2e/umd.cy.js
@@ -1,7 +1,6 @@
 describe('web client recording spec', () => {
-	;[].map((t) => {
-		it('fetch requests are recorded', () => {
-			let events = []
+	;['unpkg', 'jsdelivr'].map((source) => {
+		it('public graph requests are recorded', { baseUrl: null }, () => {
 			cy.intercept('POST', '/public', (req) => {
 				req.alias = req.body.operationName
 				req.continue(() => {
@@ -10,25 +9,7 @@ describe('web client recording spec', () => {
 					}
 				})
 			})
-			cy.visit('/')
-			cy.get('html').then(() => {
-				document
-					.querySelector('head')
-					.appendChild(
-						`<script src="https://unpkg.com/highlight.run"></script>`,
-					)
-				document.querySelector('head').appendChild(`<script>
-	H.init('a1b2c3', {
-        backendUrl: 'https://localhost:8082/public'
-		environment: 'test',
-		networkRecording: {
-			enabled: true,
-			recordHeadersAndBody: true,
-		},
-	});
-</script>`)
-				// ...
-			})
+			cy.visit(`./cypress/pages/${source}.html`)
 			cy.window().then((win) => {
 				// delay can be long because the client test might run first, and waiting for vite to have the dev bundle ready can take a while.
 				cy.wait('@PushPayload', { timeout: 90 * 1000 })
