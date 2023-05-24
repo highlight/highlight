25880e1db37c05369ddd1d0346754cfae50a0732
Chris Esplin <chris@chrisesplin.com>
2023-05-24 08:44:26 -0600

    Reproducing missing headers bug

---
+++ e2e/nextjs/src/app/components/fetch-tests.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import { Button } from '@/app/components/button'
+import ky from 'ky'
+
+export function FetchTests() {
+	return <Button onClick={fireKy}>Test KY</Button>
+}
+
+function fireKy() {
+	ky.post('/api/app-directory-test?success=true', {
+		headers: {
+			Accept: 'application/json',
+			'Content-type': 'application/json',
+		},
+	})
+		.then((res) => res.json())
+		.then((data) => console.info(data))
+}
