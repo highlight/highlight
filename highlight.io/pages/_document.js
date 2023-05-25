c90bf30f13ea9cca75122ba2473547631630ed86
Vadim Korolik <vkorolik@gmail.com>
2023-05-25 16:43:53 -0700

    fix tag

--- highlight.io/pages/_document.js
+++ highlight.io/pages/_document.js
@@ -34,17 +34,12 @@ class HighlightDocument extends Document {
 						strategy="afterInteractive"
 						src="//js.hs-scripts.com/20473940.js"
 					></Script>
-					<Script id="google-analytics" strategy="afterInteractive">
-						{`
-                        <!-- Google Tag Manager -->
-						(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+					<Script id="google-tag-manager" strategy="afterInteractive">
+						{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 						new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 						j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 						'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-						})(window,document,'script','dataLayer','GTM-PT4CCQ4');
-						<!-- End Google Tag Manager -->
-                        window.gtag = gtag
-                    `}
+						})(window,document,'script','dataLayer','GTM-PT4CCQ4');`}
 					</Script>
 					<Script
 						type="text/javascript"
