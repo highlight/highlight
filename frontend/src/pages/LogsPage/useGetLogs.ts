4e1493cbaf7f83df8f1fca553287f20a5b416e91
Eric Thomas <eric@highlight.io>
2023-05-25 14:47:51 -0600

    reset window info when query changes

--- frontend/src/pages/LogsPage/useGetLogs.ts
+++ frontend/src/pages/LogsPage/useGetLogs.ts
@@ -7,13 +7,20 @@ import {
 	parseLogsQuery,
 } from '@pages/LogsPage/SearchForm/utils'
 import moment from 'moment'
-import { useCallback, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 
 export type LogEdgeWithError = LogEdge & {
 	error_object?: Pick<
 		Types.ErrorObject,
 		'log_cursor' | 'error_group_secure_id' | 'id'
 	>
+}
+
+const initialWindowInfo: PageInfo = {
+	hasNextPage: true,
+	hasPreviousPage: true,
+	startCursor: '', // unused but needed for typedef
+	endCursor: '', // unused but needed for typedef
 }
 
 export const useGetLogs = ({
@@ -37,16 +44,15 @@ export const useGetLogs = ({
 	//
 	// If the user scrolls forward to get the next 100 logs, the server will say that hasPreviousPage is `true` since we're on page 2.
 	// Hence, we track the initial information (where "window" is effectively multiple pages) to ensure we aren't making requests unnecessarily.
-	const [windowInfo, setWindowInfo] = useState<PageInfo>({
-		hasNextPage: true,
-		hasPreviousPage: true,
-		startCursor: '', // unused but needed for typedef
-		endCursor: '', // unused but needed for typedef
-	})
+	const [windowInfo, setWindowInfo] = useState<PageInfo>(initialWindowInfo)
 	const [loadingAfter, setLoadingAfter] = useState(false)
 	const [loadingBefore, setLoadingBefore] = useState(false)
 	const queryTerms = parseLogsQuery(query)
 	const serverQuery = buildLogsQueryForServer(queryTerms)
+
+	useEffect(() => {
+		setWindowInfo(initialWindowInfo)
+	}, [query])
 
 	const { data, loading, error, refetch, fetchMore } = useGetLogsQuery({
 		variables: {
