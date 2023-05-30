highlight/highlight

commit 1b22742e9c63bd758abf971824405fca8ac6381d

Only create setup events for backend logs (#5465)

backend/worker/kafka_worker.go
@@ -15,6 +15,7 @@ import (
 	kafkaqueue "github.com/highlight-run/highlight/backend/kafka-queue"
 	"github.com/highlight-run/highlight/backend/model"
 	"github.com/highlight-run/highlight/backend/pricing"
+	privateModel "github.com/highlight-run/highlight/backend/private-graph/graph/model"
 	"github.com/highlight-run/highlight/backend/util"
 	log "github.com/sirupsen/logrus"
 	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
@@ -192,9 +193,14 @@ func (k *KafkaBatchWorker) flush(ctx context.Context) {
 
 	spanW.Finish()
 
-	// Filter out any log rows for projects where the log quota has been exceeded
+	var markBackendSetupProjectIds []uint32
 	var filteredRows []*clickhouse.LogRow
 	for _, logRow := range logRows {
+		if logRow.Source == privateModel.LogSourceBackend {
+			markBackendSetupProjectIds = append(markBackendSetupProjectIds, logRow.ProjectId)
+		}
+
+		// Filter out any log rows for projects where the log quota has been exceeded
 		if quotaExceededByProject[logRow.ProjectId] {
 			continue
 		}
@@ -204,7 +210,7 @@ func (k *KafkaBatchWorker) flush(ctx context.Context) {
 	wSpan, wCtx := tracer.StartSpanFromContext(ctx, "kafkaBatchWorker", tracer.ResourceName("worker.kafka.batched.process"))
 	wSpan.SetTag("BatchSize", len(k.BatchBuffer.messageQueue))
 	wSpan.SetTag("NumProjects", len(workspaceByProject))
-	for projectId := range workspaceByProject {
+	for projectId := range markBackendSetupProjectIds {
 		err := k.Worker.PublicResolver.MarkBackendSetupImpl(wCtx, int(projectId), model.MarkBackendSetupTypeLogs)
 		if err != nil {
 			log.WithContext(wCtx).WithError(err).Error("failed to mark backend logs setup")

commit 1926e824dbc9bb48e03f23e8c1bb48dc3cd5d103

reference the projectId, not the range index (#5475)
## Summary

<!--
Ideally, there is an attached GitHub issue that will describe the "why".

If relevant, use this section to call out any additional information
you'd like to _highlight_ to the reviewer.
-->

Seeing a bunch of these errors locally:

![Screenshot 2023-05-30 at 10 24 53
AM](https://github.com/highlight/highlight/assets/58678/4c9b86ea-c3ba-415e-a8c9-601aabdfd510)


I thought this might have been a regression from #5465 but it looks like
the behavior was always broken?


## How did you test this change?

<!--
Frontend - Leave a screencast or a screenshot to visually describe the
changes.
-->

I debugged locally to confirm I was only marking setup events for
projectId=1 locally.

## Are there any deployment considerations?

<!--
 Backend - Do we need to consider migrations or backfilling data?
-->

backend/worker/kafka_worker.go
@@ -210,7 +210,7 @@ func (k *KafkaBatchWorker) flush(ctx context.Context) {
 	wSpan, wCtx := tracer.StartSpanFromContext(ctx, "kafkaBatchWorker", tracer.ResourceName("worker.kafka.batched.process"))
 	wSpan.SetTag("BatchSize", len(k.BatchBuffer.messageQueue))
 	wSpan.SetTag("NumProjects", len(workspaceByProject))
-	for projectId := range markBackendSetupProjectIds {
+	for _, projectId := range markBackendSetupProjectIds {
 		err := k.Worker.PublicResolver.MarkBackendSetupImpl(wCtx, int(projectId), model.MarkBackendSetupTypeLogs)
 		if err != nil {
 			log.WithContext(wCtx).WithError(err).Error("failed to mark backend logs setup")