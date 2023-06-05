2e7f64a44e718e3732b7ef8bae814d0d15c47228
Chris Schmitz <chris@highlight.io>
2023-06-05 11:52:24 -0500

    Better UX of switching between the inspect and comment modes (#5506)

---
+++ frontend/src/pages/Player/SessionLevelBar/PlayerModeSwitch/PlayerModeSwitch.tsx
@@ -0,0 +1,74 @@
+import {
+	ButtonIcon,
+	IconSolidChatAlt_2,
+	IconSolidInspect,
+	Stack,
+	Tooltip,
+} from '@highlight-run/ui'
+import React from 'react'
+
+import { KeyboardShortcut } from '@/components/KeyboardShortcut/KeyboardShortcut'
+import {
+	RightPanelView,
+	usePlayerUIContext,
+} from '@/pages/Player/context/PlayerUIContext'
+import usePlayerConfiguration from '@/pages/Player/PlayerHook/utils/usePlayerConfiguration'
+
+type Mode = 'comment' | 'inspect'
+
+export const PlayerModeSwitch: React.FC = () => {
+	const { setRightPanelView } = usePlayerUIContext()
+	const { enableInspectElement, setEnableInspectElement, setShowRightPanel } =
+		usePlayerConfiguration()
+
+	const mode: Mode = enableInspectElement ? 'inspect' : 'comment'
+
+	return (
+		<Stack
+			border="secondary"
+			borderRadius="8"
+			p="2"
+			align="center"
+			direction="row"
+			gap="1"
+		>
+			<Tooltip
+				placement="bottom"
+				trigger={
+					<ButtonIcon
+						icon={<IconSolidChatAlt_2 />}
+						kind={mode === 'comment' ? 'primary' : 'secondary'}
+						emphasis={mode === 'comment' ? 'high' : 'low'}
+						size="xSmall"
+						onClick={() => {
+							setEnableInspectElement(false)
+							setRightPanelView(RightPanelView.Comments)
+							setShowRightPanel(true)
+						}}
+					/>
+				}
+				timeout={1000}
+			>
+				<KeyboardShortcut label="Enable comment mode" shortcut="c" />
+			</Tooltip>
+			<Tooltip
+				placement="bottom"
+				trigger={
+					<ButtonIcon
+						icon={<IconSolidInspect />}
+						kind={mode === 'inspect' ? 'primary' : 'secondary'}
+						emphasis={mode === 'inspect' ? 'high' : 'low'}
+						size="xSmall"
+						onClick={() => {
+							setShowRightPanel(false)
+							setEnableInspectElement(true)
+						}}
+					/>
+				}
+				timeout={1000}
+			>
+				<KeyboardShortcut label="Enable inspect mode" shortcut="d" />
+			</Tooltip>
+		</Stack>
+	)
+}
