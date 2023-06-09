dc2d0ecbc731587ce2cbf47a5cc947cb7cfe7afd
Eric Thomas <et@users.noreply.github.com>
2023-06-09 11:20:52 -0600

    remove unneeded utils (#5601)
    ## Summary
    
    <!--
    Ideally, there is an attached GitHub issue that will describe the "why".
    
    If relevant, use this section to call out any additional information
    you'd like to _highlight_ to the reviewer.
    -->
    
    We have some custom util code to get a pointer from a variable. While we
    use both [pointy](https://github.com/openlyinc/pointy) and
    [ptr](https://pkg.go.dev/github.com/aws/smithy-go@v1.13.5/ptr) in the
    codebase and probably need to pick one, we certainly don't need custom
    code.
    
    ## How did you test this change?
    
    <!--
    Frontend - Leave a screencast or a screenshot to visually describe the
    changes.
    -->
    
    Did a find and replace.
    Build should pass.
    
    ## Are there any deployment considerations?
    
    <!--
     Backend - Do we need to consider migrations or backfilling data?
    -->
    
    N/A

--- backend/util/tests.go
+++ backend/util/tests.go
@@ -2,7 +2,6 @@ package util
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"os"
 	"testing"
@@ -69,18 +68,4 @@ func ClearTablesInDB(db *gorm.DB) error {
 		}
 	}
 	return nil
-}
-
-func MakeIntPointer(v int) *int {
-	return &v
-}
-
-func MakeStringPointer(v string) *string {
-	return &v
-}
-
-func MakeStringPointerFromInterface(v interface{}) *string {
-	exampleErrorTraceBytes, _ := json.Marshal(&v)
-	exampleErrorTraceString := string(exampleErrorTraceBytes)
-	return &exampleErrorTraceString
 }
