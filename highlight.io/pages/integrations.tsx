183f42789710e1b453992630096d200658bf8df9
Abhishek More <abhimore74@gmail.com>
2023-06-09 15:49:28 -0500

    added product dropdown link

--- highlight.io/pages/integrations.tsx
+++ highlight.io/pages/integrations.tsx
@@ -10,7 +10,7 @@ const IntegrationsPage = () => {
 		<div>
 			<Navbar />
 			<main>
-				<div className="mt-20 text-center">
+				<div className="flex flex-col gap-2 mt-20 text-center">
 					<h2>
 						Find the{' '}
 						<span className="text-color-selected-light">
@@ -18,18 +18,15 @@ const IntegrationsPage = () => {
 						</span>{' '}
 						you need.
 					</h2>
-					<Typography
-						type="copyHeader"
-						className="text-copy-on-dark mt-4"
-					>
+					<Typography type="copyHeader" className="text-copy-on-dark">
 						Use your favorite tools with highlight.io.
 					</Typography>
 				</div>
 				<div className="my-24 mx-auto max-w-[1250px] px-8">
 					{Object.entries(INTEGRATIONS).map(([category, items]) => (
-						<div key={category}>
+						<div className="my-12" key={category}>
 							<h5 className="mb-4">{category}</h5>
-							<div className="grid grid-cols-3 gap-8 mx-auto">
+							<div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8 mx-auto">
 								{items.map((item) => (
 									<IntegrationCard
 										key={item.name}
