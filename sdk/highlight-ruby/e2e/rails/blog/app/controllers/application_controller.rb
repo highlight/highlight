4419e1b32bd8d02e38371b2da35dca60f606619c
Eric Thomas <eric@highlight.io>
2023-06-13 13:35:14 -0700

    lint ruby

--- sdk/highlight-ruby/e2e/rails/blog/app/controllers/application_controller.rb
+++ sdk/highlight-ruby/e2e/rails/blog/app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
-require 'highlight'
+require "highlight"
 
 class ApplicationController < ActionController::Base
-    include Highlight::Integrations::Rails
+  include Highlight::Integrations::Rails
 
-    around_action :with_highlight_context
+  around_action :with_highlight_context
 end
