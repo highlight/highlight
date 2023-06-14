6772cfe6ceac59c385d2dd17777772fb43166004
Lewis Liu <lewisl9029@users.noreply.github.com>
2023-06-13 18:00:59 -0700

    Remove more outdated .d.ts files

--- frontend/src/Common.module.scss.d.ts
+++
@@ -1,6 +0,0 @@
-export const bannerShown: string
-export const bodyWrapper: string
-export const errorMessage: string
-export const secondaryButton: string
-export const submitButton: string
-export const title: string
