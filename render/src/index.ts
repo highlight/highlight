4b199ca7eeaeb1d3c3d78479d4ee51a3493d627b
Vadim Korolik <vadim@highlight.io>
2023-06-13 17:02:12 -0700

    fix session-screenshot aws lambda (#4649)
    ## Summary
    
    Our backend uses session-screenshot lambda to attach session screenshots
    to slack comments.
    This PR fixes the AWS lambda (hosted at
    https://ygh5bj5f646ix4pixknhvysrje0haeoi.lambda-url.us-east-2.on.aws/session-screenshots?project=1&session=238920399&ts=165876&chunk=2).
    
    ## How did you test this change?
    
    Locally deploying and invoking lambda. `main` push CD will auto-deploy
    lambda.
    <img width="1292" alt="Screenshot 2023-06-12 at 4 19 32 PM"
    src="https://github.com/highlight/highlight/assets/1351531/4a76e424-3b4a-40e4-88e9-857721884e04">
    
    ## Are there any deployment considerations?
    
    CD will redeploy AWS lambda.

--- render/src/index.ts
+++ render/src/index.ts
@@ -1,27 +1,30 @@
 import { APIGatewayEvent } from 'aws-lambda'
 import { serialRender } from './serial'
 import { readFileSync } from 'fs'
+import { encodeGIF, encodeMP4 } from './ffmpeg'
+import { uploadRenderExport } from './s3'
 
 interface Args {
 	project?: string
 	session?: string
 	ts?: string
 	chunk?: string
+	format?: string
 }
 
-export const handler = async (event?: APIGatewayEvent) => {
+const screenshot = async (event?: APIGatewayEvent) => {
 	const args = event?.queryStringParameters as unknown as Args | undefined
-	const files = await serialRender(
+	const { files } = await serialRender(
 		Number(args?.project),
 		Number(args?.session),
 		Number(args?.ts),
+		undefined,
 		args?.chunk?.length ? Number(args?.chunk) : undefined,
 	)
-	const file = readFileSync(files[0])
 	return {
 		statusCode: 200,
 		isBase64Encoded: true,
-		body: Buffer.from(file).toString('base64'),
+		body: Buffer.from(readFileSync(files[0])).toString('base64'),
 		path: files[0],
 		headers: {
 			'content-type': 'image/png',
@@ -29,13 +32,47 @@ export const handler = async (event?: APIGatewayEvent) => {
 	}
 }
 
+const media = async (event?: APIGatewayEvent) => {
+	const args = event?.queryStringParameters as unknown as Args | undefined
+	const { project, session, format } = {
+		project: Number(args?.project),
+		session: Number(args?.session),
+		format: args?.format ?? 'video/mp4',
+	}
+	const { dir } = await serialRender(project, session, undefined, 1)
+	let path = ''
+	if (args?.format === 'image/gif') {
+		path = await encodeGIF(dir)
+	} else {
+		path = await encodeMP4(dir)
+	}
+	const key = await uploadRenderExport(project, session, format, path)
+
+	return {
+		statusCode: 200,
+		body: { key },
+		path,
+		headers: {
+			'content-type': 'application/json',
+		},
+	}
+}
+
+export const handler = (event?: APIGatewayEvent) => {
+	const args = event?.queryStringParameters as unknown as Args | undefined
+	if (!args?.ts) {
+		return media(event)
+	}
+	return screenshot(event)
+}
+
 if (process.env.DEV?.length) {
-	handler({
+	screenshot({
 		queryStringParameters: {
-			project: '122',
-			session: '33249578',
-			ts: '208392',
-			chunk: '3',
+			project: '1',
+			session: '239571781',
+			ts: '1',
+			chunk: '0',
 		},
 	} as unknown as APIGatewayEvent).then(console.info)
 }
--- render/src/serial.ts
+++ render/src/serial.ts
@@ -1,15 +1,33 @@
 import { render } from './render'
 import { getEvents } from './s3'
+import path from 'path'
+import { tmpdir } from 'os'
+import { promisify } from 'util'
+import { exists, mkdir } from 'fs'
 
 export async function serialRender(
 	project: number,
 	session: number,
-	ts: number,
+	ts?: number,
+	fps?: number,
 	chunk?: number,
 ) {
+	const dir = path.join(tmpdir(), `render_${project}_${session}`)
+	if (!(await promisify(exists)(dir))) {
+		await promisify(mkdir)(dir)
+	}
+
 	console.log(
 		`starting serial render for ${project} ${session} ${ts} ${chunk || ''}`,
 	)
 	const events = await getEvents(project, session, chunk)
-	return await render(events, 0, 1, undefined, ts)
+	console.log(
+		`got events ${
+			events.length
+		} serial render for ${project} ${session} ${ts} ${chunk || ''}`,
+	)
+	return {
+		dir,
+		files: await render(events, 0, 1, fps, ts, dir),
+	}
 }
