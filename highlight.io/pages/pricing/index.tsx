6d3ef6df3d02fc120375cb9bb821839dc094e27b
Jay Khatri <jaykhatrimail@gmail.com>
2023-06-22 16:01:13 -0700

    remove enterprise pricing to self host deployment

--- highlight.io/pages/pricing/index.tsx
+++ highlight.io/pages/pricing/index.tsx
@@ -232,7 +232,7 @@ const Faqs: { question: string; answer: string; icon: string }[] = [
 ]
 
 const billingPeriodOptions = ['Monthly', 'Annual'] as const
-type BillingPeriod = (typeof billingPeriodOptions)[number]
+type BillingPeriod = typeof billingPeriodOptions[number]
 
 const retentionOptions = [
 	'30 days',
@@ -241,7 +241,7 @@ const retentionOptions = [
 	'1 year',
 	'2 years',
 ] as const
-type Retention = (typeof retentionOptions)[number]
+type Retention = typeof retentionOptions[number]
 const retentionMultipliers: Record<Retention, number> = {
 	'30 days': 1,
 	'3 months': 1,
@@ -251,7 +251,7 @@ const retentionMultipliers: Record<Retention, number> = {
 } as const
 
 const tierOptions = ['Free', 'UsageBased'] as const
-type TierName = (typeof tierOptions)[number]
+type TierName = typeof tierOptions[number]
 
 type PricingTier = {
 	label: string
