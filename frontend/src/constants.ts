178d5416446c66f81c57b7a7683bb3736486c91b
Vadim Korolik <vkorolik@gmail.com>
2023-06-23 18:14:42 -0700

    fix hobby docker image

---
+++ frontend/src/constants.ts
@@ -0,0 +1,4 @@
+export const PRIVATE_GRAPH_URI =
+	import.meta.env.REACT_APP_PRIVATE_GRAPH_URI ??
+	window.location.origin + '/private'
+export const PUBLIC_GRAPH_URI = import.meta.env.REACT_APP_PUBLIC_GRAPH_URI
