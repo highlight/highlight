6eb43f6f406141b1084a345538cd0fc0a0510825
Abhishek More <abhimore74@gmail.com>
2023-06-26 19:44:51 -0500

    styling changes

--- highlight.io/components/MDXRemote/EmbeddedVideo.tsx
+++ highlight.io/components/MDXRemote/EmbeddedVideo.tsx
@@ -1,10 +1,10 @@
 export function EmbeddedVideo(props: {
 	title: string
 	src: string
-	allowString: string
+	allowString?: string
 }) {
 	return (
-		<div className="overflow-hidden border-[2px] border-darker-copy-on-dark rounded-[8px] aspect-video">
+		<div className="overflow-hidden my-3 border-[1px] border-[#30294e] rounded-[8px] aspect-video">
 			<iframe
 				className="w-full h-full"
 				src={props.src}
