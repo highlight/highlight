644823403b5153d87bd03cbdbe42dd1254e3457c
eightants <yihonganthony@gmail.com>
2023-06-27 12:41:45 -0400

    Handle dev mode requests

--- backend/lambda/lambda.go
+++ backend/lambda/lambda.go
@@ -65,28 +65,35 @@ func (s *Client) GetSessionScreenshot(ctx context.Context, projectID int, sessio
 }
 
 func (s *Client) GetSessionInsight(ctx context.Context, projectID int, sessionID int) (*http.Response, error) {
+	var req *http.Request
+
+	if util.IsDevEnv() {
+		localReq := s.GetSessionInsightRequest(ctx, "http://localhost:8765/session/insight", 1, 232563428)
+		res, localServerErr := s.HTTPClient.Do(localReq)
+		if localServerErr != nil {
+			log.WithContext(ctx).Warnf("failed to make session insight request on local dev server: %+v", localServerErr)
+			req = s.GetSessionInsightRequest(ctx, "https://ohw2ocqp0d.execute-api.us-east-2.amazonaws.com/default/ai-insights", 1, 232563428)
+			return s.HTTPClient.Do(req)
+		}
+		return res, localServerErr
+	} else {
+		req = s.GetSessionInsightRequest(ctx, "https://ohw2ocqp0d.execute-api.us-east-2.amazonaws.com/default/ai-insights", projectID, sessionID)
+	}
+	return s.HTTPClient.Do(req)
+}
+
+func (s *Client) GetSessionInsightRequest(ctx context.Context, url string, projectID int, sessionID int) *http.Request {
 	b, _ := json.Marshal(&modelInputs.SessionQuery{
 		ID:        sessionID,
 		ProjectID: projectID,
 	})
 
-	var req *http.Request
-
-	if util.IsDevEnv() {
-		b, _ = json.Marshal(&modelInputs.SessionQuery{
-			ID:        232563428,
-			ProjectID: 1,
-		})
-		req, _ = http.NewRequest(http.MethodPost, "http://localhost:8765/session/insight", bytes.NewBuffer(b))
-	} else {
-		req, _ = http.NewRequest(http.MethodPost, "https://ohw2ocqp0d.execute-api.us-east-2.amazonaws.com/default/ai-insights", bytes.NewBuffer(b))
-	}
+	req, _ := http.NewRequest(http.MethodPost, url, bytes.NewBuffer(b))
 	req = req.WithContext(ctx)
 	req.Header = http.Header{
 		"Content-Type": []string{"application/json"},
 	}
-
 	signer := v4.NewSigner()
 	_ = signer.SignHTTP(ctx, *s.Credentials, req, NilPayloadHash, string(ExecuteAPI), s.Config.Region, time.Now())
-	return s.HTTPClient.Do(req)
+	return req
 }
