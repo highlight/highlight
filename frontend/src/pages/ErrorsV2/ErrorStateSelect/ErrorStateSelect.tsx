c19069f3ba0061c48f85c024d13c1801a655ec74
Eric Thomas <eric@highlight.io>
2023-06-27 21:33:06 -0600

    remove unneeded update

--- frontend/src/pages/ErrorsV2/ErrorStateSelect/ErrorStateSelect.tsx
+++ frontend/src/pages/ErrorsV2/ErrorStateSelect/ErrorStateSelect.tsx
@@ -88,7 +88,6 @@ const ErrorStateSelectImpl: React.FC<Props> = ({
 				},
 				onCompleted: async () => {
 					showStateUpdateMessage(newState, newSnoozedUntil)
-					setMenuState(MenuState.Default)
 				},
 				onError: async () => {
 					message.destroy(MESSAGE_KEY)
