26c3e76cc6fdd469f68f85baff209b05b84eaf9b
Vadim Korolik <vkorolik@gmail.com>
2023-06-29 15:16:58 -0700

    tie resolution

--- backend/hubspot/merge.go
+++ backend/hubspot/merge.go
@@ -18,12 +18,13 @@ func MergeSimilarCompanies(ctx context.Context, h *HubspotApi) error {
 		c2 := objects[result.ID2]
 		var c1Contacts, c1Sessions int64
 		var c2Contacts, c2Sessions int64
+		var c1HasSessions, c2HasSessions bool
 		c1Contacts, _ = strconv.ParseInt(*c1.Properties["num_associated_contacts"].Value, 10, 64)
-		if _, ok := c1.Properties["highlight_session_count"]; ok {
+		if _, c1HasSessions = c1.Properties["highlight_session_count"]; c1HasSessions {
 			c1Sessions, _ = strconv.ParseInt(*c1.Properties["highlight_session_count"].Value, 10, 64)
 		}
 		c2Contacts, _ = strconv.ParseInt(*c2.Properties["num_associated_contacts"].Value, 10, 64)
-		if _, ok := c2.Properties["highlight_session_count"]; ok {
+		if _, c2HasSessions = c2.Properties["highlight_session_count"]; c2HasSessions {
 			c2Sessions, _ = strconv.ParseInt(*c2.Properties["highlight_session_count"].Value, 10, 64)
 		}
 		if c1Contacts > c2Contacts {
@@ -36,7 +37,14 @@ func MergeSimilarCompanies(ctx context.Context, h *HubspotApi) error {
 			} else if c2Sessions > c1Sessions {
 				Merge(ctx, h, c2, c1)
 			} else {
-				log.WithContext(ctx).Warnf("tie, cannot merge %+v %+v", *c1, *c2)
+				if c1HasSessions && !c2HasSessions {
+					Merge(ctx, h, c1, c2)
+				} else if c2HasSessions && !c1HasSessions {
+					Merge(ctx, h, c2, c1)
+				} else {
+					log.WithContext(ctx).Warnf("tie, keeping %+v merging %+v", *c1, *c2)
+					Merge(ctx, h, c1, c2)
+				}
 			}
 		}
 		log.WithContext(ctx).Infof("%d %d %+v %+v", c1Contacts, c2Contacts, *c1, *c2)
