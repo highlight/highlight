98c1ba034358b38c0e55f76a4a136bc2307ed4c9
Abhishek More <abhimore74@gmail.com>
2023-07-02 15:23:09 -0500

    integrations copy

--- highlight.io/components/Integrations/integration.ts
+++ highlight.io/components/Integrations/integration.ts
@@ -11,22 +11,19 @@ export const INTEGRATIONS: {
 	Analytics: [
 		{
 			name: 'Amplitude',
-			description:
-				'This is some text explaining what this company does. This is some text explaining what this company does. ',
+			description: 'Instrument highlight.io to send Amplitude events',
 			link: '/docs/general/integrations/amplitude-integration',
 			image: '/images/companies/icons/amplitude.png',
 		},
 		{
 			name: 'Mixpanel',
-			description:
-				'This is some text explaining what this company does. This is some text explaining what this company does. ',
+			description: 'Instrument highlight.io to sent Mixpanel events',
 			link: '/docs/general/integrations/mixpanel-integration',
 			image: '/images/companies/icons/mixpanel.png',
 		},
 		{
 			name: 'Segment',
-			description:
-				'This is some text explaining what this company does. This is some text explaining what this company does. ',
+			description: 'Instrument highlight.io to send Segment events',
 			link: '/docs/general/integrations/segment-integration',
 			image: '/images/companies/icons/segment.png',
 		},
@@ -34,15 +31,14 @@ export const INTEGRATIONS: {
 	Messaging: [
 		{
 			name: 'Discord',
-			description:
-				'This is some text explaining what this company does. This is some text explaining what this company does. ',
+			description: 'Create Discord tickets within highlight.io',
 			link: '/docs/general/integrations/discord-integration',
 			image: '/images/companies/icons/discord.png',
 		},
 		{
 			name: 'Front',
 			description:
-				'This is some text explaining what this company does. This is some text explaining what this company does. ',
+				'View highlight.io sessions directly in your Front inbox',
 			link: '/docs/general/integrations/front-integration',
 			image: '/images/companies/icons/front.png',
 		},
@@ -50,14 +46,13 @@ export const INTEGRATIONS: {
 		{
 			name: 'Intercom',
 			description:
-				'This is some text explaining what this company does. This is some text explaining what this company does. ',
+				'Access highlight.io sessions within your Intercom dashboard',
 			link: '/docs/general/integrations/intercom-integration',
 			image: '/images/companies/icons/intercom.png',
 		},
 		{
 			name: 'Slack',
-			description:
-				'This is some text explaining what this company does. This is some text explaining what this company does. ',
+			description: 'Send highlight.io alerts to Slack',
 			link: '/docs/general/integrations/slack-integration',
 			image: '/images/companies/icons/slack.png',
 		},
@@ -66,28 +61,27 @@ export const INTEGRATIONS: {
 		{
 			name: 'Vercel',
 			description:
-				'This is some text explaining what this company does. This is some text explaining what this company does. ',
+				'Automate the uploading of your sourcemaps and integrations.',
 			link: '/docs/getting-started/backend-logging/hosting/vercel',
 			image: '/images/companies/icons/vercel.png',
 		},
 		{
 			name: 'AWS',
 			description:
-				'This is some text explaining what this company does. This is some text explaining what this company does. ',
+				'Stream AWS application and infrastructure logs to highlight.io.',
 			link: '/docs/getting-started/backend-logging/hosting/aws',
 			image: '/images/companies/icons/aws.png',
 		},
 		{
 			name: 'GCP',
 			description:
-				'This is some text explaining what this company does. This is some text explaining what this company does. ',
+				'Stream GCP application and infrastructure logs to highlight.io',
 			link: '/docs/getting-started/backend-logging/hosting/gcp',
 			image: '/images/companies/icons/gcp.png',
 		},
 		{
 			name: 'Fly.io',
-			description:
-				'This is some text explaining what this company does. This is some text explaining what this company does. ',
+			description: 'Setup highlight.io log ingestion on Fly.io',
 			link: '/docs/getting-started/backend-logging/hosting/fly-io',
 			image: '/images/companies/icons/flyio.png',
 		},
@@ -95,23 +89,20 @@ export const INTEGRATIONS: {
 	Productivity: [
 		{
 			name: 'ClickUp',
-			description:
-				'This is some text explaining what this company does. This is some text explaining what this company does. ',
+			description: 'Create Clickup Tickets within highlight.io',
 
 			link: '/docs/general/integrations/clickup-integration',
 			image: '/images/companies/icons/clickup.png',
 		},
 		{
 			name: 'Height',
-			description:
-				'This is some text explaining what this company does. This is some text explaining what this company does. ',
+			description: 'Create Height tasks from within highlight.io',
 			link: '/docs/general/integrations/height-integration',
 			image: '/images/companies/icons/height.png',
 		},
 		{
 			name: 'GitHub',
-			description:
-				'This is some text explaining what this company does. This is some text explaining what this company does. ',
+			description: 'Create GitHub issues within highlight.io',
 			link: '/docs/general/integrations/github-integration',
 			image: '/images/companies/icons/github.png',
 		},
