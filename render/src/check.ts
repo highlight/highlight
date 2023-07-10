2b7f89b7676a12ee3a9e1e814e088b8283fd8c0a
Vadim Korolik <vkorolik@gmail.com>
2023-07-10 11:24:30 -0700

    fix render lambda deploy and add CI size check

---
+++ render/src/check.ts
@@ -0,0 +1,31 @@
+import * as util from 'util'
+import { exec as execAsync } from 'child_process'
+const exec = util.promisify(execAsync)
+
+const unzippedSizeLimitBytes = 123213184
+const unzippedSizeLimitMB = 123213184 / (1024 * 1024)
+
+let unzippedSizeBytes = unzippedSizeLimitBytes + 1
+// we must unzip the lambda code payload before checking the size
+// because AWS lambda sets a limit on the unzipped payload, rather than the zip size.
+try {
+	await exec(
+		'mkdir out && cp function.zip out && cd out && unzip function.zip && rm function.zip',
+	)
+	const { stdout } = await exec('du -b -d0 out')
+	unzippedSizeBytes = Number(stdout.split('\t')[0])
+} finally {
+	await exec('rm -rf out')
+}
+
+const unzippedSizeMB = unzippedSizeBytes / (1024 * 1024)
+if (unzippedSizeBytes >= unzippedSizeLimitBytes) {
+	console.error(
+		`zipped file too large at ${unzippedSizeMB} MB >= ${unzippedSizeLimitMB} MB.`,
+	)
+	process.exit(1)
+} else {
+	console.log(
+		`unzipped file ok at ${unzippedSizeMB} MB < ${unzippedSizeLimitMB} MB`,
+	)
+}
