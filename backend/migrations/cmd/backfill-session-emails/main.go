d649434ca70f4b91edb9a529d9d06a266b4d9f48
Eric Thomas <eric@highlight.io>
2023-07-12 13:08:51 -0600

    update backfill script

--- backend/migrations/cmd/backfill-session-emails/main.go
+++ backend/migrations/cmd/backfill-session-emails/main.go
@@ -4,16 +4,56 @@ import (
 	"context"
 	"encoding/json"
 	"os"
+	"reflect"
+	"sync"
 
 	"github.com/highlight-run/highlight/backend/model"
 	log "github.com/sirupsen/logrus"
 	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 )
 
 const batchSize = 100
 
+func FindInBatches(db *gorm.DB, dest interface{}, batchSize int, fc func(tx *gorm.DB, batch int) error) *gorm.DB {
+	var (
+		tx = db.Order(clause.OrderByColumn{
+			Column: clause.Column{Table: clause.CurrentTable, Name: "created_at"},
+		}).Session(&gorm.Session{})
+		queryDB      = tx
+		rowsAffected int64
+		batch        int
+	)
+
+	for {
+		result := queryDB.Limit(batchSize).Find(dest)
+		rowsAffected += result.RowsAffected
+		batch++
+
+		if result.Error == nil && result.RowsAffected != 0 {
+			_ = tx.AddError(fc(result, batch))
+		} else if result.Error != nil {
+			_ = tx.AddError(result.Error)
+		}
+
+		if tx.Error != nil || int(result.RowsAffected) < batchSize {
+			break
+		} else {
+			resultsValue := reflect.Indirect(reflect.ValueOf(dest))
+			if result.Statement.Schema.PrioritizedPrimaryField == nil {
+				_ = tx.AddError(gorm.ErrPrimaryKeyRequired)
+				break
+			} else {
+				primaryValue, _ := result.Statement.Schema.PrioritizedPrimaryField.ValueOf(resultsValue.Index(resultsValue.Len() - 1))
+				queryDB = tx.Clauses(clause.Gt{Column: clause.Column{Table: clause.CurrentTable, Name: clause.PrimaryKey}, Value: primaryValue})
+			}
+		}
+	}
+	return tx
+}
+
 // Run via
-// `doppler run -- go run backend/migrations/cmd/backfill-session-emails/main.go“
+// `doppler run -- go run backend/migrations/cmd/backfill-session-emails/main.go
 func main() {
 	ctx := context.Background()
 	log.WithContext(ctx).Info("setting up db")
@@ -26,29 +66,39 @@ func main() {
 
 	var sessions []model.Session
 
+	wg := sync.WaitGroup{}
 	inner := func(tx *gorm.DB, batch int) error {
-		var userProperties map[string]interface{}
-		for _, session := range sessions {
-			if err := json.Unmarshal([]byte(session.UserProperties), &userProperties); err != nil {
-				log.WithContext(ctx).Errorf("Error parsing user_properties for session_id:%d : %v\n", session.ID, err)
-				continue
-			}
+		wg.Add(1)
+		go func() {
+			for _, session := range sessions {
+				var userProperties map[string]interface{}
 
-			// Check if the parsed JSON has the "email" key
-			if email, ok := userProperties["email"].(string); ok {
-				// Update the "email" column on the session
-				if err := db.Model(&session).Update("email", email).Error; err != nil {
-					log.WithContext(ctx).Errorf("Error updating email column: %v\n", err)
+				if err := json.Unmarshal([]byte(session.UserProperties), &userProperties); err != nil {
+					log.WithContext(ctx).Errorf("Error parsing user_properties for session_id:%d : %v\n", session.ID, err)
 					continue
 				}
+
+				// Check if the parsed JSON has the "email" key
+				if email, ok := userProperties["email"].(string); ok {
+					// Update the "email" column on the session
+					if err := db.Model(&session).Update("email", email).Error; err != nil {
+						log.WithContext(ctx).Errorf("Error updating email column: %v\n", err)
+						continue
+					}
+				}
 			}
-
-		}
-
+		}()
+		wg.Done()
 		return nil
 	}
 
-	if err := db.Debug().Where("email IS NULL").FindInBatches(&sessions, batchSize, inner).Error; err != nil {
-		log.WithContext(ctx).Fatalf("failed: %v", err)
+	// if err := db.Debug().Where("email IS NULL AND user_properties IS NOT NULL and user_properties != ''").FindInBatches(&sessions, batchSize, inner).Error; err != nil {
+	// 	log.WithContext(ctx).Fatalf("failed: %v", err)
+	// }
+
+	if err := FindInBatches(db.Debug().Clauses().Model(&model.Session{}).Where("created_at > ?", "2023-06-12 14:00:00 PST").Where("created_at <= ?", "now()"), &sessions, batchSize, inner).Error; err != nil {
+		log.WithContext(ctx).Fatalf("error getting sessions: %v", err)
 	}
+
+	wg.Wait()
 }
