24bd6f5ae8e3d47a010a0ae05e6febb59d0f017b
Vadim Korolik <vadim@highlight.io>
2023-07-12 16:51:39 -0700

    update docs for hobby networking (#5874)
    ## Summary
    
    As mentioned in #5828, our hobby deploy would not work for EC2 /
    remote-server deploys because
    the frontend did not route correctly to the remote backend.
    Confirmed that setting these env. vars solved the issue for this
    customer.
    
    ## How did you test this change?
    
    [Docs
    preview](https://highlight-landing-k9ikag77h-highlight-run.vercel.app/docs/getting-started/self-host/self-hosted-hobby-guide).
    
    ## Are there any deployment considerations?
    
    No

--- highlight.io/components/QuickstartContent/self-host/shared-snippets.tsx
+++ highlight.io/components/QuickstartContent/self-host/shared-snippets.tsx
@@ -3,18 +3,41 @@ import { QuickStartStep } from '../QuickstartContent'
 export const dependencies: QuickStartStep = {
 	title: 'Prerequisites',
 	content:
-		'Before we get started, you should install [Go](https://go.dev/) (1.20) and [Node.js](https://nodejs.org/en) (18).' +
+		'Before we get started, you should install [Go](https://go.dev/) (1.20), [Node.js](https://nodejs.org/en) (18), and [yarn](https://yarnpkg.com/getting-started/install) (v3+).' +
 		'You should have the latest version of [Docker](https://docs.docker.com/engine/install/) (19.03.0+) ' +
 		'and [Git](https://git-scm.com/downloads) (2.13+) installed. ' +
-		'For a local deploy, we suggest [configuring docker](https://docs.docker.com/desktop/settings/mac/#resources) ' +
+		'We suggest [configuring docker](https://docs.docker.com/desktop/settings/mac/#resources) ' +
 		'to use at least 8GB of RAM, 4 CPUs, and 64 GB of disk space.',
 	code: [
 		{
 			language: 'bash',
+			copy: 'go version',
 			text: `$ go version
-go version go1.20.3 darwin/arm64
-$ node --version
+go version go1.20.3 darwin/arm64`,
+		},
+		{
+			language: 'bash',
+			copy: 'node --version',
+			text: `$ node --version
 v18.15.0`,
+		},
+		{
+			language: 'bash',
+			copy: 'yarn --version',
+			text: `$ yarn --version
+v3.5.0`,
+		},
+		{
+			language: 'bash',
+			copy: 'docker --version',
+			text: `$ docker --version
+Docker version 20.10.23, build 7155243`,
+		},
+		{
+			language: 'bash',
+			copy: 'docker compose version',
+			text: `$ docker compose version
+Docker Compose version v2.15.1`,
 		},
 	],
 }
