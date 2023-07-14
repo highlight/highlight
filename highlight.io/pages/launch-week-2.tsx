5a96b62bfa93ca22fe7b7f857918500b700b1bed
Abhishek More <abhimore74@gmail.com>
2023-07-14 10:09:13 -0500

    launch week page

---
+++ highlight.io/pages/launch-week-2.tsx
@@ -0,0 +1,120 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import { BiChevronRight } from 'react-icons/bi'
+import { BsTwitter } from 'react-icons/bs'
+import { FooterCallToAction } from '../components/common/CallToAction/FooterCallToAction'
+import Footer from '../components/common/Footer/Footer'
+import Navbar from '../components/common/Navbar/Navbar'
+import { Section } from '../components/common/Section/Section'
+import { Typography } from '../components/common/Typography/Typography'
+import { CompaniesReel } from '../components/Home/CompaniesReel/CompaniesReel'
+import { CustomerReviewTrack } from '../components/Home/CustomerReviewTrack'
+import styles from '../components/Home/Home.module.scss'
+import DayOne from '../components/Launch/DayOne'
+
+const IntegrationsPage = () => {
+	return (
+		<div>
+			<Navbar />
+			<main>
+				<div className="flex flex-col gap-2 mt-20 text-center px-8">
+					<h2>
+						Launch Week 2.{' '}
+						<span className="text-color-selected-light">
+							It&apos;s Hype.
+						</span>{' '}
+					</h2>
+					<Typography type="copy1" className="text-copy-on-dark">
+						July 20th - 25th, 2023
+					</Typography>
+				</div>
+				<div className="flex flex-col lg:flex-row items-center gap-3 w-full max-w-[1200px] mx-auto mt-20 px-8">
+					<Link
+						href="https://highlight.io"
+						className="flex justify-between items-center p-4 max-w-[350px] w-full bg-[#150831] border-[1px] border-divider-on-dark rounded-md"
+					>
+						<div className="flex gap-3 items-center">
+							<Typography
+								className="text-color-selected-light rounded-full border-[1px] border-color-selected-light px-2 py-[1px]"
+								type="copy4"
+								emphasis
+							>
+								New drop
+							</Typography>
+							<Typography
+								className="text-copy-on-dark"
+								type="copy4"
+								emphasis
+							>
+								Go to Day 3
+							</Typography>
+						</div>
+						<BiChevronRight className="text-darker-copy-on-dark h-7 w-7 bg-white bg-opacity-10 p-1 rounded-full" />
+					</Link>
+
+					<Link
+						href="https://twitter.com"
+						className="flex justify-between p-4 max-w-[350px] w-full bg-[#150831] border-[1px] border-divider-on-dark rounded-md"
+					>
+						<div className="flex gap-3 items-center">
+							<BsTwitter className="w-6 h-6" />
+							<Typography
+								className="text-copy-on-dark"
+								type="copy4"
+								emphasis
+							>
+								Keep in touch on Twitter
+							</Typography>
+						</div>
+						<BiChevronRight className="text-darker-copy-on-dark h-6 w-6 bg-white bg-opacity-10 p-1 rounded-full" />
+					</Link>
+
+					<Link
+						href="https://hackernews.com"
+						className="flex justify-between p-4 max-w-[350px] w-full bg-[#150831] border-[1px] border-divider-on-dark rounded-md"
+					>
+						<div className="flex gap-3 items-center">
+							<Image
+								src="/images/companies/icons/hackernews.svg"
+								alt=""
+								width={24}
+								height={24}
+							/>
+							<Typography
+								className="text-copy-on-dark"
+								type="copy4"
+								emphasis
+							>
+								Check in on Hacker News
+							</Typography>
+						</div>
+						<BiChevronRight className="text-darker-copy-on-dark h-6 w-6 bg-white bg-opacity-10 p-1 rounded-full" />
+					</Link>
+				</div>
+				<div className="flex flex-col lg:flex-row items-center gap-3 w-full max-w-[1200px] mx-auto mt-10 px-8">
+					<DayOne />
+				</div>
+				<Section>
+					<CompaniesReel />
+				</Section>
+				<Section>
+					<div className={styles.anchorFeature}>
+						<div className={styles.anchorHead}>
+							<Typography type="copy2" onDark>
+								Don&apos;t take our word.{' '}
+								<Link href="/customers">
+									Read our customer review section →
+								</Link>
+							</Typography>
+						</div>
+					</div>
+				</Section>
+				<CustomerReviewTrack />
+				<FooterCallToAction />
+			</main>
+			<Footer />
+		</div>
+	)
+}
+
+export default IntegrationsPage
