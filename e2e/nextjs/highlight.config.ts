72282b709c7d6958593493c765ff592d229dc212
Chris Esplin <chris@chrisesplin.com>
2023-07-14 07:34:17 -0600

    Restore Winston test

---
+++ e2e/nextjs/highlight.config.ts
@@ -0,0 +1,33 @@
+import { Highlight } from '@highlight-run/next'
+import winston from 'winston'
+
+const projectID = '1jdkoe52'
+
+export const withHighlight = Highlight({
+	projectID,
+	debug: true,
+	backendUrl: 'https://localhost:8082/public',
+	otlpEndpoint: 'http://localhost:4318',
+})
+
+const highlightTransport = new winston.transports.Http({
+	host: 'localhost',
+	port: 8082,
+	ssl: true,
+	path: `/v1/logs/json`,
+	headers: {
+		'x-highlight-project': projectID,
+		'x-highlight-service': 'e2e-nextjs',
+	},
+})
+
+export const logger = winston.createLogger({
+	level: 'info',
+	format: winston.format.combine(
+		winston.format.json(),
+		winston.format.errors({ stack: true }),
+		winston.format.timestamp(),
+		winston.format.prettyPrint(),
+	),
+	transports: [new winston.transports.Console(), highlightTransport],
+})
