09efc1bbbdeaa22569fa36746c4fb286415c6fe2
Eric Thomas <et@users.noreply.github.com>
2023-07-17 09:38:00 -0600

    ungate instances view (#5957)
    ## Summary
    
    <!--
    Ideally, there is an attached GitHub issue that will describe the "why".
    
    If relevant, use this section to call out any additional information
    you'd like to _highlight_ to the reviewer.
    -->
    
    Ungating the button to see all error instances (see #5830).
    
    ## How did you test this change?
    
    <!--
    Frontend - Leave a screencast or a screenshot to visually describe the
    changes.
    -->
    
    See All Instances button shows for all users.
    
    
    ## Are there any deployment considerations?
    
    <!--
     Backend - Do we need to consider migrations or backfilling data?
    -->
    
    
    N/A

--- frontend/src/pages/ErrorsV2/ErrorInstance/ErrorInstance.tsx
+++ frontend/src/pages/ErrorsV2/ErrorInstance/ErrorInstance.tsx
@@ -59,7 +59,6 @@ const ErrorInstance: React.FC<Props> = ({ errorGroup }) => {
 		error_object_id: string
 	}>()
 	const client = useApolloClient()
-	const { isHighlightAdmin } = useAuthContext()
 
 	const { loading, data } = useGetErrorInstanceQuery({
 		variables: {
@@ -111,7 +110,7 @@ const ErrorInstance: React.FC<Props> = ({ errorGroup }) => {
 			<Box id="error-instance-container">
 				<Stack direction="row" my="12">
 					<Stack direction="row" flexGrow={1}>
-						{isHighlightAdmin && <SeeAllInstances data={data} />}
+						<SeeAllInstances data={data} />
 						<PreviousNextInstance data={data} />
 					</Stack>
 					<Stack direction="row" gap="4">
@@ -158,7 +157,7 @@ const ErrorInstance: React.FC<Props> = ({ errorGroup }) => {
 		<Box id="error-instance-container">
 			<Stack direction="row" my="12">
 				<Stack direction="row" flexGrow={1}>
-					{isHighlightAdmin && <SeeAllInstances data={data} />}
+					<SeeAllInstances data={data} />
 					<PreviousNextInstance data={data} />
 				</Stack>
 				<Stack direction="row" gap="4">
