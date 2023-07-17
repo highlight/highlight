a312c0e45706c901df7334cc27a5bf9ad8c37675
Zane Mayberry <mayberryzane@gmail.com>
2023-07-17 14:46:25 -0700

    revert test change

--- render/src/index.ts
+++ render/src/index.ts
@@ -95,13 +95,5 @@ if (process.env.DEV?.length) {
 				tsEnd: '20000',
 			},
 		} as unknown as APIGatewayEvent),
-		handler({
-			queryStringParameters: {
-				project: '1',
-				session: '262083663',
-				ts: '1',
-				chunk: '0',
-			},
-		} as unknown as APIGatewayEvent),
 	])
 }
