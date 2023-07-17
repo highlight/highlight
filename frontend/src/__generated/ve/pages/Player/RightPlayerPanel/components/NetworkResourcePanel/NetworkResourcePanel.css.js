3388f2d1c2b5c62c1f9b5e6f7ce6dd3ef774aa57
Chris Schmitz <chris@highlight.io>
2023-07-17 08:56:57 -0500

    Add logs to network request view (#5932)

--- frontend/src/__generated/ve/pages/Player/RightPlayerPanel/components/NetworkResourcePanel/NetworkResourcePanel.css.js
+++ frontend/src/__generated/ve/pages/Player/RightPlayerPanel/components/NetworkResourcePanel/NetworkResourcePanel.css.js
@@ -1,1 +1,1 @@
-var r="_1h7r9xv0",t="mt0ih28w mt0ih223",a="mt0ih289",e="mt0ih2h6";export{r as container,t as pageContainer,a as pageContainerFull,e as tabsContainer};
+var t="_1h7r9xv0",r="mt0ih289 mt0ih28w mt0ih223",a="mt0ih2h6";export{t as container,r as pageContainer,a as tabsContainer};
