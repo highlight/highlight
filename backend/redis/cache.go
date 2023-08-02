88142a6d534c57bd4271ba60463ff6e705c0722e
Vadim Korolik <vkorolik@gmail.com>
2023-08-02 10:30:42 -0700

    improve performance of redis.CachedEval

--- backend/redis/cache.go
+++ backend/redis/cache.go
@@ -10,16 +10,15 @@ import (
 // CachedEval will return the value at cacheKey if it exists.
 // If it does not exist or is nil, CachedEval calls `fn()` to evaluate the result, and stores it at the cache key.
 func CachedEval[T any](ctx context.Context, redis *Client, cacheKey string, lockTimeout, cacheExpiration time.Duration, fn func() (*T, error)) (value *T, err error) {
-	// wait here to check the cache in case another process is waiting for db query
-	if acquired := redis.AcquireLock(ctx, cacheKey, lockTimeout); acquired {
-		defer func() {
-			if err := redis.ReleaseLock(ctx, cacheKey); err != nil {
-				log.WithContext(ctx).WithError(err).Error("failed to release lock")
-			}
-		}()
-	}
-
 	if err = redis.Cache.Get(ctx, cacheKey, &value); err != nil {
+		// if we do not have a cache hit, take a lock to avoid running fn() more than once
+		if acquired := redis.AcquireLock(ctx, cacheKey, lockTimeout); acquired {
+			defer func() {
+				if err := redis.ReleaseLock(ctx, cacheKey); err != nil {
+					log.WithContext(ctx).WithError(err).Error("failed to release lock")
+				}
+			}()
+		}
 		if value, err = fn(); value == nil || err != nil {
 			return
 		}
