16bd705063b28214d95958995a7d7f55b8754cb4
Eric Thomas <eric@highlight.io>
2023-08-07 14:12:17 -0600

    add inline setting

--- frontend/src/pages/ProjectSettings/FilterSessionsWithoutErrorForm/FilterSessionsWithoutErrorForm.tsx
+++ frontend/src/pages/ProjectSettings/FilterSessionsWithoutErrorForm/FilterSessionsWithoutErrorForm.tsx
@@ -29,7 +29,8 @@ export const FilterSessionsWithoutErrorForm = () => {
 	const categories = [
 		{
 			key: 'Filter sessions without errors',
-			message: 'Filter sessions without an error.',
+			message:
+				'Filter sessions without an error. Any session filtered out will not count towards your billing quota.',
 			checked: filterSessionsWithoutError,
 		},
 	]
