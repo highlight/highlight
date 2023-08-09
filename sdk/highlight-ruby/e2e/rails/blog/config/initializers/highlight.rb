7fbd64d01cee089fff9ebf313472697a33b5cbe4
Eric Thomas <eric@highlight.io>
2023-08-09 14:03:30 -0600

    add service config to ruby sdk

--- sdk/highlight-ruby/e2e/rails/blog/config/initializers/highlight.rb
+++ sdk/highlight-ruby/e2e/rails/blog/config/initializers/highlight.rb
@@ -1,4 +1,7 @@
 require "highlight"
 
-Highlight::H.new("1jdkoe52", "http://localhost:4318")
+Highlight::H.new("1jdkoe52", "http://localhost:4318") do |c|
+    c.service_name = "my-rails-app"
+    c.service_verion = "1.0.0"
+end
 Rails.logger = Highlight::Logger.new(STDOUT)
---
+++ sdk/highlight-ruby/highlight/CHANGELOG.md
@@ -0,0 +1,5 @@
+## 0.1.2
+
+### Patch Changes
+
+- Add ability to set service_name and service_version
