65f126cbbb9947e4d2b6eed23f1a3c78247f4eaf
Eric Thomas <et@users.noreply.github.com>
2023-08-09 08:59:40 -0600

    avoid writing service.* for frontend logs (#6273)
    ## Summary
    
    <!--
    Ideally, there is an attached GitHub issue that will describe the "why".
    
    If relevant, use this section to call out any additional information
    you'd like to _highlight_ to the reviewer.
    -->
    
    We are leaking public graph service name and version into customer's
    frontend logs. The reason this is happening is because our OTEL endpoint
    serves as the backend for client SDK (i.e. the client SDK calls
    pub.highlight.io).
    
    We actually already have similar code that guarded against this for
    leaking our public graph's OTEL information:
    
    https://github.com/highlight/highlight/blob/6268-public-graph-service-information-leaking/backend/otel/extract.go#L263-L265
    
    https://github.com/highlight/highlight/blob/6268-public-graph-service-information-leaking/sdk/highlight-go/otel.go#L42-L48
    
    But we didn't update this when we introduced the service notion. This is
    also the reason why we were having issues when writing a service record
    on ingest (see
    [slack](https://highlightcorp.slack.com/archives/C02CJANPHQS/p1691081920973589?thread_ts=1691080057.488759&cid=C02CJANPHQS)).
    
    ## How did you test this change?
    
    <!--
    Frontend - Leave a screencast or a screenshot to visually describe the
    changes.
    -->
    
    * Create a new project locally (it's important for this to be different
    from project id = 1)
    * Spin up a local react app and use this config:
    
    ```js
    H.init('<NEW_PROJECT_ID>', {
        backendUrl: 'https://localhost:8082/public',
        tracingOrigins: true,
        networkRecording: {
          enabled: true,
          recordHeadersAndBody: true,
        },
    });
    ```
    
    Observe that we do not have the `service_name` and `service_version`
    populated:
    
    ![Screenshot_2023-08-08_at_4_12_08_PM](https://github.com/highlight/highlight/assets/58678/103b6136-7e5e-4cc0-97fa-ceff175ffa1c)
    
    
    ## Are there any deployment considerations?
    
    <!--
     Backend - Do we need to consider migrations or backfilling data?
    -->

--- backend/otel/syslog.go
+++ backend/otel/syslog.go
@@ -1,12 +1,13 @@
 package otel
 
 import (
+	"strconv"
+
 	"github.com/influxdata/go-syslog/v3/rfc5424"
 	"go.opentelemetry.io/collector/pdata/plog"
-	"strconv"
 )
 
-func extractSyslog(fields *extractedFields, attrs map[string]any) {
+func extractSyslog(fields *extractedFields) {
 	p := rfc5424.NewParser(rfc5424.WithBestEffort())
 	message, err := p.Parse([]byte(fields.logBody))
 	if msg, ok := message.(*rfc5424.SyslogMessage); err == nil && ok {
@@ -14,32 +15,35 @@ func extractSyslog(fields *extractedFields, attrs map[string]any) {
 			fields.logBody = *msg.Message
 		}
 		if msg.Facility != nil {
-			attrs["facility"] = strconv.Itoa(int(*msg.Facility))
+			fields.attrs["facility"] = strconv.Itoa(int(*msg.Facility))
 		}
 		if msg.Severity != nil {
 			fields.logSeverity = plog.SeverityNumber(*msg.Severity).String()
 		}
 		if msg.Priority != nil {
-			attrs["priority"] = strconv.Itoa(int(*msg.Priority))
+			fields.attrs["priority"] = strconv.Itoa(int(*msg.Priority))
 		}
 		if msg.Timestamp != nil {
 			fields.timestamp = *msg.Timestamp
 		}
 		if msg.Hostname != nil {
-			attrs["hostname"] = *msg.Hostname
+			fields.attrs["hostname"] = *msg.Hostname
 		}
 		if msg.Appname != nil {
-			attrs["app_name"] = *msg.Appname
+			fields.attrs["app_name"] = *msg.Appname
 		}
 		if msg.ProcID != nil {
-			attrs["proc_id"] = *msg.ProcID
+			fields.attrs["proc_id"] = *msg.ProcID
 		}
 		if msg.MsgID != nil {
-			attrs["msg_id"] = *msg.MsgID
+			fields.attrs["msg_id"] = *msg.MsgID
 		}
 		if msg.StructuredData != nil {
-			for k, v := range *msg.StructuredData {
-				attrs[k] = v
+
+			for topLevelKey, vMap := range *msg.StructuredData {
+				for k, v := range vMap {
+					fields.attrs[topLevelKey+"."+k] = v
+				}
 			}
 		}
 	}
