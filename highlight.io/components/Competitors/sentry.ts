dd40888724fe46d70bd9a4b9f0708470dfedcfa0
Jay Khatri <jaykhatrimail@gmail.com>
2023-08-10 08:59:33 -0700

    add sentry competitor doc

---
+++ highlight.io/components/Competitors/sentry.ts
@@ -0,0 +1,157 @@
+import datadogsmall from '../../public/images/datadog-small.png'
+import datadogfull from '../../public/images/datadog.png'
+import { Competitor } from './competitors'
+import {
+	HighlightIsOpenAndTransparent,
+	HighlightShipsNewFeatures,
+} from './shared-snippets'
+
+export const SentrySpec: Competitor = {
+	name: 'Sentry',
+	header: 'A Truly Open Source Sentry Alternative.',
+	subheader:
+		'Session replay of your frontend, fullstack error monitoring, and powerful logging.',
+	logoDesktop: datadogfull,
+	logoMobile: datadogsmall,
+	sections: [
+		{
+			title: 'General',
+			rows: [
+				{
+					feature: 'Unlimited Team Members',
+					highlight: 1,
+					competitor: 1,
+				},
+				{
+					feature: 'Support Ticket Integrations',
+					tooltip: 'Create tickets with clickup, linear, or jira.',
+					highlight: 1,
+					competitor: 1,
+				},
+				{
+					feature: 'Self-hosted Options',
+					tooltip:
+						'Self-hosted options are available for highlight.io',
+					highlight: 1,
+					competitor: 1,
+				},
+				{
+					feature: 'Analytics Integrations',
+					tooltip: 'Integrations with mixpanel, amplitude, etc..',
+					highlight: 1,
+					competitor: 0,
+				},
+				{
+					feature: 'Self-serve Setup',
+					highlight: 1,
+					competitor: 1,
+				},
+			],
+		},
+		{
+			title: 'Session Replay',
+			rows: [
+				{
+					feature: 'Session Commenting',
+					tooltip:
+						'Create comments on sessions to tag folks on your team of issues.',
+					highlight: 1,
+					competitor: 0,
+				},
+				{
+					feature: 'Session Sharing',
+					tooltip:
+						'Ability to share sessions across your team and with customers.',
+					highlight: 1,
+					competitor: 0,
+				},
+				{
+					feature: 'Privacy SDKs',
+					highlight: 1,
+					competitor: 1,
+				},
+				{
+					feature: 'Embedded, fullstack error monitoring',
+					highlight: 1,
+					competitor: 1,
+				},
+				{
+					feature: 'Canvas & WebGL Recording',
+					tooltip:
+						'Ability to record canvas and webgl elements in your application.',
+					highlight: 1,
+					competitor: 0,
+				},
+				{
+					feature: 'Shadow DOM Recording',
+					tooltip:
+						'Ability to record shadow dom elements in your application.',
+					highlight: 1,
+					competitor: 0,
+				},
+			],
+		},
+		{
+			title: 'Error Monitoring',
+			rows: [
+				{
+					feature: 'Embedded Session Replay',
+					highlight: 1,
+					competitor: 1,
+				},
+				{
+					feature: 'Error Sharing',
+					highlight: 1,
+					competitor: 1,
+				},
+				{
+					feature: 'Support for Backend SDKs',
+					highlight: 1,
+					competitor: 1,
+				},
+				{
+					feature: 'Agent-less architecture',
+					highlight: 1,
+					competitor: 1,
+				},
+			],
+		},
+		{
+			title: 'Logging',
+			rows: [
+				{
+					feature: 'Frontend logging',
+					highlight: 1,
+					competitor: 0,
+				},
+				{
+					feature: 'Backend logging',
+					highlight: 1,
+					competitor: 0,
+				},
+				{
+					feature: 'Embedded replay and stacktraces',
+					highlight: 1,
+					competitor: 0,
+				},
+				{
+					feature: 'Agent-less architecture',
+					highlight: 1,
+					competitor: 0,
+				},
+			],
+		},
+	],
+	paragraphs: [
+		HighlightIsOpenAndTransparent('Sentry'),
+		HighlightShipsNewFeatures,
+		{
+			header: 'Full-stack Observability',
+			body: 'While Sentry provides a comprehensive set of separate features for error monitoring and session replay, it does not support logging. [Highlight.io](https://highlight.io) enables teams to monitor and optimize their entire tech stack, pairing server-side infrastructure with your frontend web applications. This makes Highlight.io a more comprehensive solution for developers who need to monitor their entire tech stack, in a simple, easy to implement solution.',
+		},
+		{
+			header: 'Agent-less Architecture',
+			body: `Datadog's architecture requires you to install an agent on your server. This can be a pain to install and maintain. Highlight.io's architecture is agent-less and requires no installation, but in the case that you want to install an agent long-term, we have an open-source agent/collector that you can install on your servers.`,
+		},
+	],
+}
