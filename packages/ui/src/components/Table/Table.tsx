e02b75de59f3b4ca657bcfd2d6aee4253d1f1065
Spencer Amarantides <Spencer22309@gmail.com>
2023-08-11 14:28:38 -0400

    Create table states (#6280)
    ## Summary
    Create the FullRow component to allow a higher level component to
    implement different states more easily/
    
    Example:
    ```
    <Table>
        <Table.Head>
             <Table.Row gridColumns={["25%", "50px", "auto"]}>
                {COLUMNS.map((column) => (
                    <Table.Header>
                        {column.name}
    	        </Table.Header>
                ))}
            </Table.Row>
        </Table.Head>
        <Table.Body>
            {loading && <Table.FullRow>Loading...</Table.FullRow>}
            {error && <Table.FullRow>{error}</Table.FullRow>}
        </Table.Body>
    </Table>
    ```
    
    ## How did you test this change?
    1) View the Table storybook
    
    
    https://www.loom.com/share/7702a1fecbff401f9606d46e0befbc53?sid=4ca408e4-a50f-429c-b793-27034809ff78
    
    ## Are there any deployment considerations?
    N/A - not being used

--- packages/ui/src/components/Table/Table.tsx
+++ packages/ui/src/components/Table/Table.tsx
@@ -1,38 +1,31 @@
 import React from 'react'
 
-import * as styles from './styles.css'
-import { Box } from '../Box/Box'
-
 import { Body, Props as BodyProps } from './Body/Body'
 import { Cell, Props as CellProps } from './Cell/Cell'
 import {
 	Discoverable,
 	Props as DiscoverableProps,
 } from './Discoverable/Discoverable'
+import { FullRow, Props as FullRowProps } from './FullRow/FullRow'
 import { Head, Props as HeadProps } from './Head/Head'
 import { Header, Props as HeaderProps } from './Header/Header'
 import { Row, Props as RowProps } from './Row/Row'
 
+import { Box } from '../Box/Box'
+
 type Props = {
 	children: React.ReactNode
-	loading?: boolean
-	error?: string
 }
 
-const TableComponent: React.FC<Props> = ({ children, loading, error }) => {
-	return (
-		<Box className={styles.container}>
-			{loading && 'Loading...'}
-			{!!error && error}
-			{!loading && !error && children}
-		</Box>
-	)
+const TableComponent: React.FC<Props> = ({ children }) => {
+	return <Box>{children}</Box>
 }
 
 type TableWithComponents = React.FC<Props> & {
 	Body: React.FC<BodyProps>
 	Cell: React.FC<CellProps>
 	Discoverable: React.FC<DiscoverableProps>
+	FullRow: React.FC<FullRowProps>
 	Head: React.FC<HeadProps>
 	Header: React.FC<HeaderProps>
 	Row: React.FC<RowProps>
@@ -43,6 +36,7 @@ const Table = TableComponent as TableWithComponents
 Table.Body = Body
 Table.Cell = Cell
 Table.Discoverable = Discoverable
+Table.FullRow = FullRow
 Table.Head = Head
 Table.Header = Header
 Table.Row = Row
