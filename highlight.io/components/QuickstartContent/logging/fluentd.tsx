9ae39ed2808a3015ab662ccee7ad1a3f1f407baa
Vadim Korolik <vkorolik@gmail.com>
2023-08-17 18:53:29 -0700

    correct docs for fluentbit binary

--- highlight.io/components/QuickstartContent/logging/fluentd.tsx
+++ highlight.io/components/QuickstartContent/logging/fluentd.tsx
@@ -9,13 +9,19 @@ export const FluentForwardContent: QuickStartContent = {
 		{
 			title: 'Setup fluentd / fluent bit ingest.',
 			content:
-				'Route your [fluentd / fluent bit](https://docs.fluentbit.io/manual/pipeline/outputs/forward/) to forward://otel.highlight.io:24224.',
+				'Route your [fluentd / fluent bit](https://docs.fluentbit.io/manual/pipeline/outputs/forward/) to forward://otel.highlight.io:24224. ' +
+				'Regardless of the  way you are using fluentbit, configure the tag to `highlight.project_id=YOUR_PROJECT_ID` to route the logs to the given highlight project',
 			code: [
 				{
-					text: `bin/fluent-bit -i cpu -t fluent_bit -o forward://otel.highlight.io:24224`,
+					text: `bin/fluent-bit -i cpu -t highlight.project_id=YOUR_PROJECT_ID -o forward://otel.highlight.io:24224`,
 					language: 'bash',
 				},
 			],
+		},
+		{
+			title: 'Setting up for AWS ECS?',
+			content:
+				'If you are setting up for AWS Elastic Container Services, check out our dedicated [docs for AWS ECS.](/docs/getting-started/backend-logging/hosting/aws#aws-ecs-containers) .',
 		},
 		verifyLogs,
 	],
