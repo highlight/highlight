b23ff5164051b2eed8de4bb3717bb88d17bfc8e0
Chris Esplin <chris@chrisesplin.com>
2023-08-24 08:37:10 -0600

    Remove extraneous MatchedErrorTag struct definition

--- backend/error-tags/error-tags.go
+++
@@ -1,8 +0,0 @@
-package error_tags
-
-type MatchedErrorTag struct {
-	ID          int     `json:"id"`
-	Title       string  `json:"title"`
-	Description string  `json:"description"`
-	Score       float64 `json:"score"`
-}
