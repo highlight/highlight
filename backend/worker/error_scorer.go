37669d53a535a1b2b649329347cb17a27ca4b7b8
Spencer <spencer@highlight.io>
2023-09-11 15:49:10 -0400

    Add affected user score

--- backend/worker/error_scorer.go
+++ backend/worker/error_scorer.go
@@ -111,19 +111,87 @@ func (errorScorer *ErrorScorer) scoreErrorGroup(ctx context.Context, errorGroup 
 			avg AS (
 				SELECT AVG(count) FROM trailing_vals
 			)
-			SELECT current_error_count - avg / stddev_pop AS score
+			SELECT (current_error_count - avg) / stddev_pop AS score
 			FROM trailing_vals, stddev, avg, current_error
 			ORDER BY day DESC, hour DESC
-			LIMIT 1`, errorGroup.ProjectID, errorGroup.ID).
-			Find(&occuranceScore).Error; err != nil {
-			return 0, 0, errors.Wrap(err, "error querying calculating number of occurrance score")
+			LIMIT 1
+		`, errorGroup.ProjectID, errorGroup.ID).Find(&occuranceScore).Error; err != nil {
+			return 0, 0, errors.Wrap(err, "error calculating occurance score")
 		}
 
 		// number of users affected
+		if err := errorScorer.db.Raw(`
+			WITH most_occurring_errors AS (
+				SELECT error_group_id, COUNT(*) as errorCount
+				FROM error_objects
+				WHERE project_id = ?
+					AND created_at > NOW() - INTERVAL '7 day'
+				GROUP BY error_group_id
+				ORDER BY errorCount DESC
+				LIMIT 5
+			),
+			current_error AS (
+				SELECT COUNT(DISTINCT(sessions.email)) as current_user_count
+				FROM error_objects
+				INNER JOIN sessions
+					ON sessions.id = error_objects.session_id
+				WHERE error_group_id = ?
+					AND sessions.created_at > NOW() - INTERVAL '1 hour'
+			),
+			current_sessions AS (
+				SELECT COUNT(DISTINCT(sessions.email)) as total_user_count 
+				FROM sessions
+				WHERE sessions.project_id = ?
+					AND sessions.created_at > NOW() - INTERVAL '1 hour'
+			),
+			error_data AS (
+				SELECT error_objects.error_group_id, extract(day from sessions.created_at) AS day, EXTRACT(hour from sessions.created_at) AS hour, COUNT(DISTINCT(sessions.email)) as total_user_count 
+				FROM error_objects
+				INNER JOIN sessions
+					ON sessions.id = error_objects.session_id
+				INNER JOIN most_occurring_errors
+					ON most_occurring_errors.error_group_id = error_objects.error_group_id
+				WHERE sessions.created_at > NOW() - INTERVAL '7 days'
+				GROUP BY error_objects.error_group_id, day, hour
+				ORDER BY day DESC, hour DESC
+			),
+			session_data AS (
+				SELECT extract(day from created_at) AS day, EXTRACT(hour from created_at) AS hour, COUNT(DISTINCT(sessions.email)) as total_user_count 
+				FROM sessions
+				WHERE sessions.project_id = ?
+					AND sessions.created_at > NOW() - INTERVAL '7 days'
+				GROUP BY day, hour
+				ORDER BY day DESC, hour DESC
+			),
+			trailing_vals AS (
+				SELECT session_data.day, session_data.hour, CAST(error_data.total_user_count AS float) / session_data.total_user_count AS ratio
+				FROM error_data
+				INNER JOIN session_data ON error_data.day = session_data.day AND error_data.hour = session_data.hour
+				ORDER BY session_data.day DESC, session_data.hour DESC
+			),
+			stddev AS (
+				SELECT STDDEV_POP(ratio) FROM trailing_vals
+			),
+			avg AS (
+				SELECT AVG(ratio) FROM trailing_vals
+			)
+			SELECT ((CAST(current_user_count / total_user_count AS float)) - avg) / stddev_pop AS score
+			FROM trailing_vals, stddev, avg, current_error, current_sessions
+			ORDER BY day DESC, hour DESC
+			LIMIT 1
+		`, errorGroup.ProjectID, errorGroup.ID, errorGroup.ProjectID, errorGroup.ProjectID).Find(&affectedUsersScore).Error; err != nil {
+			return 0, 0, errors.Wrap(err, "error calculating affected user score")
+		}
 	} else {
 		// number of occurances
 		if err := errorScorer.db.Raw(`
-			WITH trailing_vals AS (
+			WITH current_error AS (
+				SELECT COUNT(*) as current_error_count
+				FROM error_objects
+				WHERE error_group_id = ?
+					AND created_at > NOW() - INTERVAL '1 hour'
+			),
+			trailing_vals AS (
 				SELECT extract(day from created_at) AS day, EXTRACT(hour from created_at) AS hour, COUNT(*) AS count
 				FROM error_objects
 				WHERE error_group_id = ?
@@ -137,15 +205,67 @@ func (errorScorer *ErrorScorer) scoreErrorGroup(ctx context.Context, errorGroup 
 			avg AS (
 				SELECT AVG(count) FROM trailing_vals
 			)
-			SELECT count - avg / stddev_pop AS score
+			SELECT (current_error_count - avg) / stddev_pop AS score
 			FROM trailing_vals, stddev, avg
 			ORDER BY day DESC, hour DESC
-			LIMIT 1`, errorGroup.ID).
-			Find(&occuranceScore).Error; err != nil {
-			return 0, 0, errors.Wrap(err, "error querying calculating number of occurrance score")
+			LIMIT 1
+		`, errorGroup.ID).Find(&occuranceScore).Error; err != nil {
+			return 0, 0, errors.Wrap(err, "error calculating occurrance score")
 		}
 
 		// number of users affected
+		if err := errorScorer.db.Raw(`
+			WITH current_error AS (
+				SELECT COUNT(DISTINCT(sessions.email)) as current_user_count
+				FROM error_objects
+				INNER JOIN sessions
+					ON sessions.id = error_objects.session_id
+				WHERE error_group_id = ?
+					AND sessions.created_at > NOW() - INTERVAL '1 hour'
+			),
+			current_sessions AS (
+				SELECT COUNT(DISTINCT(sessions.email)) as total_user_count 
+				FROM sessions
+				WHERE sessions.project_id = ?
+					AND sessions.created_at > NOW() - INTERVAL '1 hour'
+			),
+			error_data AS (
+				SELECT extract(day from sessions.created_at) AS day, EXTRACT(hour from sessions.created_at) AS hour, COUNT(DISTINCT(sessions.email)) as total_user_count 
+				FROM error_objects
+				INNER JOIN sessions
+					ON sessions.id = error_objects.session_id
+				WHERE error_group_id = ?
+					AND sessions.created_at > NOW() - INTERVAL '7 days'
+				GROUP BY day, hour
+				ORDER BY day DESC, hour DESC
+			),
+			session_data AS (
+				SELECT extract(day from created_at) AS day, EXTRACT(hour from created_at) AS hour, COUNT(DISTINCT(sessions.email)) as total_user_count 
+				FROM sessions
+				WHERE sessions.project_id = ?
+					AND sessions.created_at > NOW() - INTERVAL '7 days'
+				GROUP BY day, hour
+				ORDER BY day DESC, hour DESC
+			),
+			trailing_vals AS (
+				SELECT session_data.day, session_data.hour, CAST(error_data.total_user_count AS float) / session_data.total_user_count AS ratio
+				FROM error_data
+				INNER JOIN session_data ON error_data.day = session_data.day AND error_data.hour = session_data.hour
+				ORDER BY session_data.day DESC, session_data.hour DESC
+			),
+			stddev AS (
+				SELECT STDDEV_POP(ratio) FROM trailing_vals
+			),
+			avg AS (
+				SELECT AVG(ratio) FROM trailing_vals
+			)
+			SELECT ((CAST(current_user_count / total_user_count AS float)) - avg) / stddev_pop AS score
+			FROM trailing_vals, stddev, avg, current_error, current_sessions
+			ORDER BY day DESC, hour DESC
+			LIMIT 1
+		`, errorGroup.ID, errorGroup.ProjectID, errorGroup.ID, errorGroup.ProjectID).Find(&affectedUsersScore).Error; err != nil {
+			return 0, 0, errors.Wrap(err, "error calculating affected user score")
+		}
 	}
 
 	return occuranceScore, affectedUsersScore, nil
