300b3481b68c555f3d5368d9e7641a5319dd6005
Vadim Korolik <vkorolik@gmail.com>
2023-09-12 11:28:51 -0700

    update animation

--- frontend/src/pages/Player/Toolbar/TimelineIndicators/TimelineIndicatorsBarGraph/style.css.ts
+++ frontend/src/pages/Player/Toolbar/TimelineIndicators/TimelineIndicatorsBarGraph/style.css.ts
@@ -167,7 +167,7 @@ export const inactivityPeriodMask = style({
 })
 
 export const moveIndicator = style({
-	transition: 'transform 0.25s linear',
+	transition: 'transform 0.20s linear',
 })
 
 export const timeIndicatorContainerWrapper = style({
