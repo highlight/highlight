892b3323bcf04dcbd2b91b8c894a8e3f412f3076
Jemilu Mohammed <jemilu@crowncitytechnologies.com>
2023-09-13 05:19:59 +0000

    update login cypress test to login with ADMIN_PASSWORD

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -6,7 +6,28 @@ describe('login spec', () => {
 		})
 	})
 
-	it('allows you to log in', () => {
+	it('allows you to log in using ADMIN_PASSWORD', () => {
+		cy.visit('/1/buttons').wait(5000)
+		cy.title().then((title) => {
+			console.log('TITLE', title)
+			if (title === 'About You') {
+			}
+		})
+		cy.get('[name="email"]').type('demo@user.com')
+		cy.get('[name="password"]').type('I_AM_GROOT')
+		cy.get('button[type="submit"]').click().wait(5000)
+
+		// Ensure client requests are made
+		cy.wait('@pushMetrics')
+			.its('request.body.variables')
+			.should('have.property', 'metrics')
+		cy.wait('@initializeSession')
+			.its('request.body.variables')
+			.should('have.property', 'session_secure_id')
+		cy.wait('@PushPayload')
+	})
+
+	it.skip('allows you to log in', () => {
 		cy.visit('/1/buttons').wait(5000)
 		cy.title().then((title) => {
 			if (title === 'About You') {
--- frontend/src/pages/Auth/SignIn.tsx
+++ frontend/src/pages/Auth/SignIn.tsx
@@ -163,6 +163,7 @@ export const SignIn: React.FC<Props> = ({ setResolver }) => {
 						trackingId="sign-up-submit"
 						loading={loading}
 						type="submit"
+						id="email-password-signin"
 					>
 						Sign in
 					</Button>
