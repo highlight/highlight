5d6045fc0c108443f82bd8eb686f0d4aec003328
Chris Esplin <chris@chrisesplin.com>
2023-09-15 07:59:24 -0600

    Rename highlight-next/src/util/withHighligh.ts

--- sdk/highlight-next/src/server.ts
+++ sdk/highlight-next/src/server.ts
@@ -1,3 +1,7 @@
 export { registerHighlight } from './util/registerHighlight'
-export { H, Highlight, HighlightEnv } from './util/withHighlight'
+export {
+	H,
+	Highlight,
+	HighlightEnv,
+} from './util/with-highlight-nodejs-page-router'
 export { withHighlightConfig } from './util/withHighlightConfig'
