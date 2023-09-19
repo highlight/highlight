4ca5038dc3ccd5bcc4823a5ff3b6f3ecc580d9d0
Spencer Amarantides <Spencer22309@gmail.com>
2023-09-19 15:17:07 -0400

    Default Github rate limit hit cache to 1 hour (#6641)
    ## Summary
    The `github-rate-limit-exceeded-%s` key is being set indefinitely,
    causing errors to never be enhanced when the rate limit is hit. Default
    this time to one hour
    
    ## How did you test this change?
    Test code logic in sandbox
    
    ## Are there any deployment considerations?
    Monitor rate limits hit
    
    ## Does this work require review from our design team?
    N/A

--- backend/redis/utils.go
+++ backend/redis/utils.go
@@ -492,7 +492,12 @@ func (r *Client) GetHubspotCompanies(ctx context.Context, companies interface{})
 }
 
 func (r *Client) SetGithubRateLimitExceeded(ctx context.Context, gitHubRepo string, expirationTime time.Time) error {
-	return r.setFlag(ctx, GithubRateLimitKey(gitHubRepo), true, time.Until(expirationTime))
+	expirationDuration := time.Until(expirationTime)
+	if expirationDuration <= 0 {
+		expirationDuration = time.Hour
+	}
+
+	return r.setFlag(ctx, GithubRateLimitKey(gitHubRepo), true, expirationDuration)
 }
 
 func (r *Client) GetGithubRateLimitExceeded(ctx context.Context, gitHubRepo string) (bool, error) {
@@ -538,4 +543,8 @@ func (r *Client) Del(ctx context.Context, key string) error {
 		return r.redisClient.Del(ctx, key).Err()
 	}
 	return nil
+}
+
+func (r *Client) TTL(ctx context.Context, key string) time.Duration {
+	return r.redisClient.TTL(ctx, key).Val()
 }
