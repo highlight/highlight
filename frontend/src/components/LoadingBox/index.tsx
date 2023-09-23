78a6b4f00678e3b76f05bb6e9cd2d745ae714c0f
Lewis Liu <lewisl9029@users.noreply.github.com>
2023-09-22 19:15:39 -0700

    Update

--- frontend/src/components/LoadingBox/index.tsx
+++ frontend/src/components/LoadingBox/index.tsx
@@ -30,6 +30,6 @@ const LoadingBox: React.FC<LoadingBoxProps> = ({ height, width, ...props }) => {
 	)
 }
 
-export const LoadingBox_test = <LoadingBox height={500} width={500} />
+export const LoadingBox_test = <LoadingBox height={300} width={300} />
 
 export default LoadingBox
