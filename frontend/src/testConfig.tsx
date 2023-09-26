fee94d7f2bbbd40f75c57e1f018784ac931bc772
Lewis Liu <lewisl9029@users.noreply.github.com>
2023-09-25 23:12:05 -0700

    Add test config

---
+++ frontend/src/testConfig.tsx
@@ -0,0 +1,16 @@
+import 'antd/dist/antd.css'
+import '@highlight-run/rrweb/dist/rrweb.min.css'
+import '@fontsource/poppins'
+import './index.css'
+import './style/tailwind.css'
+import '/__generated/index.css'
+
+import * as React from 'react'
+
+export const init = () => {
+	document.body.className = 'highlight-light-theme'
+}
+
+export const Root = ({ children }: { children: React.ReactNode }) => {
+	return children
+}
