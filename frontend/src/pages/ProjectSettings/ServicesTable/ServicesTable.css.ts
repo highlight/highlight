b783fa92a74d8c0e1b5838885e4d9805bb34b853
Spencer Amarantides <Spencer22309@gmail.com>
2023-09-28 09:45:32 -0400

    Touchup service table (#6669)
    ## Summary
    Update the services page to improve the UI and UX of integrating GitHub
    
    [Figma
    designs](https://www.figma.com/file/rdBPNzn7Klk6RG1LHUCE5B/Screen-Designs?type=design&node-id=11260-325132&mode=design&t=BIFhsYxcb85J9btI-0)
    
    ## How did you test this change?
    1) View the services page with and without the GitHub Integration
    completed
    2) Add the integration from the services page via the new button
    
    https://www.loom.com/share/ace3ebd101344e90ae3ba66013dbddac
    
    ## Are there any deployment considerations?
    N/A
    
    ## Does this work require review from our design team?
    Can't hurt

--- frontend/src/pages/ProjectSettings/ServicesTable/ServicesTable.css.ts
+++ frontend/src/pages/ProjectSettings/ServicesTable/ServicesTable.css.ts
@@ -1,22 +1,5 @@
-import { vars } from '@highlight-run/ui'
-import { sprinkles } from '@highlight-run/ui/src/css/sprinkles.css'
 import { style } from '@vanilla-extract/css'
 
-export const combobox = style([
-	sprinkles({
-		py: '6',
-	}),
-	{
-		background: 'transparent',
-		border: 0,
-		color: vars.theme.static.content.default,
-		display: 'flex',
-		fontSize: 13,
-		width: '100%',
-		selectors: {
-			'&:focus': {
-				outline: 0,
-			},
-		},
-	},
-])
+export const repoButton = style({
+	padding: 0,
+})
---
+++ packages/ui/src/__generated/ve/components/Table/Search/styles.css.js
@@ -0,0 +1,1 @@
+var o="_13s6meo2",e="_13s6meo1 mt0ih23g mt0ih23x";export{o as combobox,e as search};
