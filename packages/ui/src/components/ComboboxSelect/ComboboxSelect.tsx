f0223861ed122353a8e61730fc779cdedc21b08d
Spencer <spencer@highlight.io>
2023-09-29 11:17:54 -0400

    Move combobox to portal

--- packages/ui/src/components/ComboboxSelect/ComboboxSelect.tsx
+++ packages/ui/src/components/ComboboxSelect/ComboboxSelect.tsx
@@ -10,6 +10,7 @@ import {
 	SelectLabel,
 	SelectPopover,
 	PopoverArrow,
+	Portal,
 } from '@ariakit/react'
 
 import { IconSolidCheckCircle, IconSolidSearch } from '../icons'
@@ -112,75 +113,82 @@ export const ComboboxSelect = <T extends string | string[]>({
 					</Text>
 				)}
 			</Select>
-			<SelectPopover
-				store={select}
-				className={styles.selectPopover}
-				gutter={4}
-				autoFocusOnHide={false}
-			>
-				<PopoverArrow size={0} />
-				<div
-					className={clsx(styles.comboboxWrapper, {
-						[styles.comboboxHasResults]:
-							allOptions.length > 0 || isLoading,
-					})}
+			<Portal>
+				<SelectPopover
+					store={select}
+					className={styles.selectPopover}
+					gutter={4}
+					autoFocusOnHide={false}
 				>
-					<IconSolidSearch />
-					<Combobox
+					<PopoverArrow size={0} />
+					<div
+						className={clsx(styles.comboboxWrapper, {
+							[styles.comboboxHasResults]:
+								allOptions.length > 0 || isLoading,
+						})}
+					>
+						<IconSolidSearch />
+						<Combobox
+							store={combobox}
+							type="text"
+							autoSelect
+							autoComplete="none"
+							placeholder={queryPlaceholder}
+							className={styles.combobox}
+						></Combobox>
+					</div>
+					<ComboboxList
 						store={combobox}
-						type="text"
-						autoSelect
-						autoComplete="none"
-						placeholder={queryPlaceholder}
-						className={styles.combobox}
-					></Combobox>
-				</div>
-				<ComboboxList
-					store={combobox}
-					className={clsx([styles.comboboxList, 'hide-scrollbar'])}
-				>
-					{isLoading && (
-						<div
-							className={clsx([
-								styles.selectItem,
-								styles.loadingPlaceholder,
-							])}
-						>
-							{loadingRender}
-						</div>
-					)}
-					{select.useState('open') &&
-						allOptions.map((option: Option) => (
-							<ComboboxItem
-								focusOnHover
-								key={option.key}
-								className={styles.selectItem}
-								render={
-									<SelectItem value={option.key}>
-										{isMultiselect && (
-											<div
-												className={styles.checkbox}
-												style={{
-													backgroundColor:
-														valueSet.has(option.key)
-															? vars.theme
-																	.interactive
-																	.fill
-																	.primary
-																	.enabled
-															: 'white',
-												}}
-											>
-												<IconSolidCheckCircle color="white" />
-											</div>
-										)}
-										{option.render}
-									</SelectItem>
-								}
-							></ComboboxItem>
-						))}
-				</ComboboxList>
-			</SelectPopover>
+						className={clsx([
+							styles.comboboxList,
+							'hide-scrollbar',
+						])}
+					>
+						{isLoading && (
+							<div
+								className={clsx([
+									styles.selectItem,
+									styles.loadingPlaceholder,
+								])}
+							>
+								{loadingRender}
+							</div>
+						)}
+						{select.useState('open') &&
+							allOptions.map((option: Option) => (
+								<ComboboxItem
+									focusOnHover
+									key={option.key}
+									className={styles.selectItem}
+									render={
+										<SelectItem value={option.key}>
+											{isMultiselect && (
+												<div
+													className={styles.checkbox}
+													style={{
+														backgroundColor:
+															valueSet.has(
+																option.key,
+															)
+																? vars.theme
+																		.interactive
+																		.fill
+																		.primary
+																		.enabled
+																: 'white',
+													}}
+												>
+													<IconSolidCheckCircle color="white" />
+												</div>
+											)}
+											{option.render}
+										</SelectItem>
+									}
+								></ComboboxItem>
+							))}
+					</ComboboxList>
+				</SelectPopover>
+			</Portal>
 		</div>
 	)
 }
