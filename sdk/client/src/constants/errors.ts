e2fe654765a66ba97d5a004ba9ae8f41e7221f25
Vadim Korolik <vkorolik@gmail.com>
2023-10-04 09:44:15 -0700

    ignore known internal errors

--- sdk/client/src/constants/errors.ts
+++ sdk/client/src/constants/errors.ts
@@ -1,5 +1,7 @@
 export const ERRORS_TO_IGNORE = [
 	'["\\"Script error.\\""]' /** This is an error that happens from a script that is on a different origin than the origin that Highlight is running on. See: https://sentry.io/answers/script-error/*/,
+	'["\\"Load failed.\\""]',
+	'["\\"Network request failed.\\""]',
 	'[{"isTrusted":true}]' /** Cross-origin errors: https://stackoverflow.com/questions/44815172/log-shows-error-object-istrustedtrue-instead-of-actual-error-data */,
 	'["{}"]',
 	'[""]',
