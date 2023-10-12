6696bab348ad18cf69486218368f871718dbca55
Vadim Korolik <vkorolik@gmail.com>
2023-10-11 22:26:26 -0700

    format

--- sdk/highlight-py/e2e/highlight_oauth/main.py
+++ sdk/highlight-py/e2e/highlight_oauth/main.py
@@ -2,7 +2,7 @@ import os
 
 import requests
 
-GET_SESSIONS_CLICKHOUSE = '''
+GET_SESSIONS_CLICKHOUSE = """
 query GetSessionsClickhouse($project_id: ID!, $count: Int!, $query: ClickhouseQuery!, $sort_desc: Boolean!, $sort_field: String, $page: Int) {
   sessions_clickhouse(
     project_id: $project_id
@@ -57,7 +57,7 @@ query GetSessionsClickhouse($project_id: ID!, $count: Int!, $query: ClickhouseQu
     __typename
   }
 }
-'''
+"""
 
 # in development
 # OAUTH_URL = "https://localhost:8082/oauth"
@@ -65,20 +65,24 @@ query GetSessionsClickhouse($project_id: ID!, $count: Int!, $query: ClickhouseQu
 OAUTH_URL = "https://pri.highlight.io/oauth"
 API_URL = "https://pri.highlight.io"
 
-CLIENT_ID = os.environ['HIGHLIGHT_OAUTH_CLIENT_ID']
-SECRET = os.environ['HIGHLIGHT_OAUTH_CLIENT_SECRET']
+CLIENT_ID = os.environ["HIGHLIGHT_OAUTH_CLIENT_ID"]
+SECRET = os.environ["HIGHLIGHT_OAUTH_CLIENT_SECRET"]
 
 
 def perform_oauth_flow():
-    r = requests.get(f'{OAUTH_URL}/token', verify=False, params={
-        'grant_type': 'client_credentials',
-        'client_id': CLIENT_ID,
-        'client_secret': SECRET,
-    })
+    r = requests.get(
+        f"{OAUTH_URL}/token",
+        verify=False,
+        params={
+            "grant_type": "client_credentials",
+            "client_id": CLIENT_ID,
+            "client_secret": SECRET,
+        },
+    )
     assert r.status_code == 200
     params = r.json()
 
-    return params['access_token']
+    return params["access_token"]
 
 
 def make_request(url):
@@ -89,15 +93,26 @@ def make_request(url):
         verify=False,
         json={
             "operationName": "GetSessionsClickhouse",
-            "variables": {"query": {"isAnd": True, "rules": [
-                ["custom_processed", "is", "true"],
-                ["custom_created_at", "between_date", "2023-09-12T03:51:47.260Z_2023-10-12T03:51:47.260Z"]]},
-                          "count": 10,
-                          "page": 1, "project_id": "1",
-                          "sort_desc": True},
-            "query": GET_SESSIONS_CLICKHOUSE
+            "variables": {
+                "query": {
+                    "isAnd": True,
+                    "rules": [
+                        ["custom_processed", "is", "true"],
+                        [
+                            "custom_created_at",
+                            "between_date",
+                            "2023-09-12T03:51:47.260Z_2023-10-12T03:51:47.260Z",
+                        ],
+                    ],
+                },
+                "count": 10,
+                "page": 1,
+                "project_id": "1",
+                "sort_desc": True,
+            },
+            "query": GET_SESSIONS_CLICKHOUSE,
         },
-        headers={'Authorization': f'Bearer {auth}'}
+        headers={"Authorization": f"Bearer {auth}"},
     )
     print(r.status_code)
     print(r.text)
@@ -107,5 +122,5 @@ def main():
     r = make_request(API_URL)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
