d8410400ffa942dd51e9a81728b0bc39b2426f85
Jemilu Mohammed <jemilu@crowncitytechnologies.com>
2023-10-17 14:45:35 +0000

    delete redundant code

--- backend/private-graph/graph/schema.resolvers.go
+++ backend/private-graph/graph/schema.resolvers.go
@@ -8151,13 +8151,3 @@ type sessionAlertResolver struct{ *Resolver }
 type sessionCommentResolver struct{ *Resolver }
 type subscriptionResolver struct{ *Resolver }
 type timelineIndicatorEventResolver struct{ *Resolver }
-
-// !!! WARNING !!!
-// The code below was going to be deleted when updating resolvers. It has been copied here so you have
-// one last chance to move it out of harms way if you want. There are two reasons this happens:
-//   - When renaming or deleting a resolver the old code will be put in here. You can safely delete
-//     it when you're done.
-//   - You have helper methods in this file. Move them out to keep these resolver files clean.
-func (r *errorObjectResolver) SessionComments(ctx context.Context, obj *model.ErrorObject) ([]*model.SessionComment, error) {
-	panic(fmt.Errorf("not implemented: SessionComments - session_comments"))
-}
