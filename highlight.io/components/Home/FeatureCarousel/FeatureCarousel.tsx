ac341aa1badac7251a375a5f9ddeef3efcedb07f
Abhishek More <abhimore74@gmail.com>
2023-10-19 13:49:43 -0500

    Added traces to carousel (#6843)
    ## Summary
    
    Added Trace Feature to the Landing Page Carousel
    
    ## How did you test this change?
    
    Click Testing
    
    ## Are there any deployment considerations?
    
    Full Front-end
    
    ## Does this work require review from our design team?
    
    Yes

--- highlight.io/components/Home/FeatureCarousel/FeatureCarousel.tsx
+++ highlight.io/components/Home/FeatureCarousel/FeatureCarousel.tsx
@@ -4,25 +4,24 @@ import useEmblaCarousel from 'embla-carousel-react'
 import { StaticImageData } from 'next/image'
 import { useEffect, useState } from 'react'
 import { isMobile } from 'react-device-detect'
-import { AiFillGithub } from 'react-icons/ai'
 import {
 	HiBell,
 	HiChevronDown,
 	HiCloudDownload,
 	HiCode,
 	HiDatabase,
-	HiDesktopComputer,
 	HiDocumentSearch,
+	HiExclamationCircle,
 	HiFilm,
+	HiGlobe,
 	HiLightningBolt,
 	HiPhoneOutgoing,
-	HiPresentationChartLine,
+	HiSparkles,
+	HiTemplate,
 	HiTerminal,
 	HiUserGroup,
 	HiViewBoards,
 } from 'react-icons/hi'
-import selfHosting from '../../../public/images/docker.png'
-import dockerscreenshot from '../../../public/images/dockerscreenshot.png'
 import errorMonitoring from '../../../public/images/error-monitoring.png'
 import fullstackLogging from '../../../public/images/fullstack-logging.png'
 import githubscreenshot from '../../../public/images/githubscreenshot.png'
@@ -31,6 +30,8 @@ import monitoringscreenshot from '../../../public/images/monitoringscreenshot.pn
 import openSource from '../../../public/images/open-source.png'
 import sessionReplay from '../../../public/images/session-replay.png'
 import sessionscreenshot from '../../../public/images/sessionscreenshot.png'
+import traces from '../../../public/images/traces.svg'
+import tracesscreenshot from '../../../public/images/tracesmobile.svg'
 import { PrimaryButton } from '../../common/Buttons/PrimaryButton'
 import { Typography } from '../../common/Typography/Typography'
 import styles from '../Home.module.scss'
@@ -45,6 +46,8 @@ export type Feature = {
 	mobileImage: StaticImageData
 	right?: boolean
 	code?: string[]
+	shortenWidth?: boolean
+	beta?: boolean
 	feature1?: string
 	feature1Link?: string
 	featureImage1?: JSX.Element
@@ -114,13 +117,33 @@ const features: Feature[] = [
 		link: '/logging',
 	},
 	{
+		name: 'Traces',
+		title: 'Traces',
+		description:
+			'Get performance insights on requests and transactions throughout your web application stack.',
+		thumbnail: <HiSparkles className="h-[35px] w-[35px]" />,
+		desktopImage: traces,
+		mobileImage: tracesscreenshot,
+		right: true,
+		shortenWidth: true,
+		beta: true,
+		feature1: 'Powerful Visualization Capabilities',
+		featureImage1: <HiTemplate className="h-[20px] w-[20px]" />,
+		feature2: 'Distributed Tracing Support',
+		featureImage2: <HiGlobe className="h-[20px] w-[20px]" />,
+		feature3: 'OpenTelemetry Support',
+		featureImage3: <HiExclamationCircle className="h-[20px] w-[20px]" />,
+		link: '/docs/getting-started/tracing',
+	},
+
+	{
 		name: 'Self-Hosting',
 		title: 'Self-Hosting highlight.io',
 		description:
 			'Interested in self-hosting highlight? Spin up highlight.io in docker with just a few commands.',
 		thumbnail: <HiCloudDownload className="h-[35px] w-[35px]" />,
-		desktopImage: selfHosting,
-		mobileImage: dockerscreenshot,
+		desktopImage: openSource,
+		mobileImage: githubscreenshot,
 		right: true,
 		code: [
 			`git clone --recurse-submodules https://github.com/highlight/highlight;`,
@@ -128,30 +151,6 @@ const features: Feature[] = [
 			`./run-hobby.sh;`,
 		],
 		link: '/docs/general/company/open-source/hosting/self-host-hobby',
-	},
-	{
-		name: 'Open Source',
-		title: "We're Open Source!",
-		description:
-			'highlight.io is an open source tool for debugging your web application.',
-		thumbnail: <AiFillGithub className="h-[35px] w-[35px]" />,
-		desktopImage: openSource,
-		mobileImage: githubscreenshot,
-		right: true,
-		feature1: 'Join the Community',
-		feature1Link: 'https://discord.gg/yxaXEAqgwN',
-		featureImage1: (
-			<HiPresentationChartLine className="h-[20px] w-[20px]" />
-		),
-		feature2: 'Find us on GitHub',
-		feature2Link: 'https://github.com/highlight/highlight/',
-		featureImage2: <HiDesktopComputer className="h-[20px] w-[20px]" />,
-		feature3: 'Self host highlight.io',
-		feature3Link:
-			'/docs/general/company/open-source/hosting/self-host-hobby',
-		featureImage3: (
-			<ExclamationCircleFilled className="h-[20px] w-[20px]" />
-		),
 	},
 ]
 
