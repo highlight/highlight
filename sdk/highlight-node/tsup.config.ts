b821edb55359e513bec15ef4b896e2a791fa7859
Vadim Korolik <vkorolik@gmail.com>
2023-10-23 09:13:16 -0700

    add express e2e and tweak sdk builds

--- sdk/highlight-node/tsup.config.ts
+++ sdk/highlight-node/tsup.config.ts
@@ -3,16 +3,9 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
 	entry: ['src/index.ts'],
 	format: ['cjs', 'esm'],
+	target: 'esnext',
+	minify: true,
 	dts: true,
 	sourcemap: true,
-	noExternal: [
-		'@opentelemetry/api',
-		'@opentelemetry/auto-instrumentations-node',
-		'@opentelemetry/core',
-		'@opentelemetry/exporter-trace-otlp-http',
-		'@opentelemetry/resources',
-		'@opentelemetry/sdk-node',
-		'@opentelemetry/sdk-trace-base',
-		'@opentelemetry/semantic-conventions',
-	],
+	noExternal: [new RegExp('@opentelemetry/.*')],
 })
