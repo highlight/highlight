a79d276a101b5f51a6c09156bbd137d27852b64f
Vadim Korolik <vkorolik@gmail.com>
2023-10-23 09:49:09 -0700

    use tsup banner

--- sdk/highlight-node/tsup.config.ts
+++ sdk/highlight-node/tsup.config.ts
@@ -4,8 +4,13 @@ export default defineConfig({
 	entry: ['src/index.ts'],
 	format: ['cjs', 'esm'],
 	target: 'esnext',
+	platform: 'node',
 	minify: true,
 	dts: true,
 	sourcemap: true,
 	noExternal: [new RegExp('@opentelemetry/.*')],
+	// https://github.com/evanw/esbuild/issues/1921
+	banner: {
+		js: `const require = (await import("node:module")).createRequire(import.meta.url);`,
+	},
 })
