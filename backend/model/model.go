27534cfd10db815773e1f770256600e2272a8c47
Vadim Korolik <vkorolik@gmail.com>
2023-10-25 13:19:38 -0700

    update filter defauls

--- backend/model/model.go
+++ backend/model/model.go
@@ -396,14 +396,14 @@ type ProjectFilterSettings struct {
 	Project                           *Project
 	ProjectID                         int
 	FilterSessionsWithoutError        bool    `gorm:"default:false"`
-	AutoResolveStaleErrorsDayInterval int     `gorm:"default:0"`
+	AutoResolveStaleErrorsDayInterval int     `gorm:"default:7"`
 	SessionSamplingRate               float64 `gorm:"default:1"`
 	ErrorSamplingRate                 float64 `gorm:"default:1"`
 	LogSamplingRate                   float64 `gorm:"default:1"`
 	TraceSamplingRate                 float64 `gorm:"default:1"`
-	SessionMinuteRateLimit            int64   `gorm:"default:1000"`
-	ErrorMinuteRateLimit              int64   `gorm:"default:10000"`
-	LogMinuteRateLimit                int64   `gorm:"default:100000"`
+	SessionMinuteRateLimit            int64   `gorm:"default:1000000"`
+	ErrorMinuteRateLimit              int64   `gorm:"default:1000000"`
+	LogMinuteRateLimit                int64   `gorm:"default:1000000"`
 	TraceMinuteRateLimit              int64   `gorm:"default:1000000"`
 	SessionExclusionQuery             *string
 	ErrorExclusionQuery               *string
