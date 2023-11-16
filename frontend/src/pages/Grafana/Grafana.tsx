850c3d013898a9bd93ab06c203d3b1f3b40abd67
Vadim Korolik <vkorolik@gmail.com>
2023-11-16 11:56:03 -0800

    WIP

--- frontend/src/pages/Grafana/Grafana.tsx
+++ frontend/src/pages/Grafana/Grafana.tsx
@@ -1,26 +1,40 @@
 import { registerMicroApps, start } from 'qiankun'
 import React from 'react'
 
+export interface HighlightDashboardProps {
+	name: string
+	uid: string
+	slug: string
+	version: number
+	mode: 'dark' | 'light'
+	// queryParams: ParsedQuery<string>
+	// fnError?: ReactNode
+	pageTitle?: string
+	controlsContainer: string | null
+	isLoading: (isLoading: boolean) => void
+	setErrors: (errors?: { [K: number | string]: string }) => void
+	hiddenVariables: readonly string[]
+	container?: HTMLElement | null
+}
+
 export const Grafana = () => {
 	registerMicroApps([
 		{
-			name: 'grafana', // app name registered
-			entry: 'https://localhost:3005',
-			container: '#grafana',
+			name: 'grafanaRoot', // app name registered
+			entry: 'https://localhost:3005/public/microfrontends/fn_dashboard/',
+			container: '#grafanaRoot',
 			activeRule: '/1/grafana',
-			loader: () => {},
-			props: {},
+			props: {
+				name: 'd',
+				uid: 'dc221bdb-f781-4f7b-971b-4b9863750d8d',
+				slug: 'highlight',
+				version: 1,
+				mode: 'dark',
+			} as HighlightDashboardProps,
 		},
 	])
 
 	start({})
-	return (
-		<>
-			<div id="grafana" style={{ width: 100, height: 100 }} />
-			<iframe
-				style={{ width: 100, height: 100 }}
-				src="https://grafana.highlight.io"
-			></iframe>
-		</>
-	)
+
+	return <div id="grafanaRoot" style={{ width: 100, height: 100 }} />
 }
