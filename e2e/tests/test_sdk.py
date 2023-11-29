fc90878251648f7619c0da132b61972f32029f85
Vadim Korolik <vkorolik@gmail.com>
2023-11-29 10:43:27 -0800

    cleanup test

--- e2e/tests/test_sdk.py
+++ e2e/tests/test_sdk.py
@@ -8,16 +8,16 @@ from query_gql import GET_ERROR_GROUPS_CLICKHOUSE, GET_SESSIONS_CLICKHOUSE
 
 
 def query(
-        oauth_api: tuple[str, str],
-        operation_name: str,
-        query: str,
-        variables: dict[str, any],
-        fn: Optional[Callable[[dict[str, any]], None]] = None,
+    oauth_api: tuple[str, str],
+    operation_name: str,
+    query: str,
+    variables: dict[str, any],
+    fn: Optional[Callable[[dict[str, any]], None]] = None,
 ):
     api_url, oauth_token = oauth_api
     exc: Optional[Exception] = None
     # retry up for up to N seconds in case the session needs time to populate from datasync queue
-    for _ in range(60):
+    for _ in range(90):
         try:
             r = requests.post(
                 api_url,
@@ -43,12 +43,21 @@ def query(
 
 
 @pytest.mark.parametrize("success", ["true", "false"])
-@pytest.mark.parametrize("endpoint,expected_error", [
-    ("/api/page-router-test", "Error: /pages/api/page-router-test.ts (Page Router)"),
-    ("/api/page-router-edge-test", "Error: /api/page-router-edge-test (Edge Runtime)"),
-    ("/api/app-router-test", "Error: /api/app-router-test (App Router)"),
-    ("/api/edge-test", "Error: /api/edge-test (Edge Runtime)"),
-])
+@pytest.mark.parametrize(
+    "endpoint,expected_error",
+    [
+        (
+            "/api/page-router-test",
+            "Error: /pages/api/page-router-test.ts (Page Router)",
+        ),
+        (
+            "/api/page-router-edge-test",
+            "Error: /api/page-router-edge-test (Edge Runtime)",
+        ),
+        ("/api/app-router-test", "Error: /api/app-router-test (App Router)"),
+        ("/api/edge-test", "Error: /api/edge-test (Edge Runtime)"),
+    ],
+)
 def test_next_js(next_app, oauth_api, endpoint, expected_error, success):
     start = datetime.utcnow()
     r = requests.get(
@@ -61,6 +70,7 @@ def test_next_js(next_app, oauth_api, endpoint, expected_error, success):
 
     # check that the error came thru to highlight
     if success == "false":
+
         def validate(data: dict[str, any]):
             assert 0 < len(data["error_groups_clickhouse"]["error_groups"]) < 10
             # check that we actually received the edge runtime error
