88c4a3e984b8012a17b1b39532159d12edd052fd
Spencer <spencer@highlight.io>
2023-11-30 20:50:03 -0500

    Add to missing trace creation

--- backend/public-graph/graph/resolver.go
+++ backend/public-graph/graph/resolver.go
@@ -1997,6 +1997,7 @@ func (r *Resolver) PushMetricsImpl(ctx context.Context, sessionSecureID string, 
 			WithSpanName("highlight-metric").
 			WithServiceName(session.ServiceName).
 			WithServiceVersion(ptr.ToString(session.AppVersion)).
+			WithEnvironment(session.Environment).
 			WithTraceAttributes(attributes).
 			WithEvents([]map[string]any{event}))
 	}
