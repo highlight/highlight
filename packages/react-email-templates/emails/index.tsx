09dff76d10c8ca41a49c3c80ec4148d240b46157
Spencer Amarantides <Spencer22309@gmail.com>
2023-12-01 13:28:58 -0500

    Update insights lambda to handle multiple requested emails (#7259)
    ## Summary
    The previously created lambda was handling one email format, but will
    need to respond to multiple email template requests. Allow a parameter
    to be passed to determine which template should be used.
    
    Also updates the session alerts to not show unavailable data (i.e. these
    alerts are sent out for live sessions)
    
    ## How did you test this change?
    Step function test on AWS
    
    ## Are there any deployment considerations?
    Breaking change to previous lambda function
    
    ## Does this work require review from our design team?
    N/A

---
+++ packages/react-email-templates/emails/index.tsx
@@ -0,0 +1,19 @@
+import { ErrorAlertEmail } from './error-alert'
+import { LogAlertEmail } from './log-alert'
+import { NewSessionAlertEmail } from './new-session-alert'
+import { NewUserAlertEmail } from './new-user-alert'
+import { RageClickAlertEmail } from './rage-click-alert'
+import { SessionInsightsEmail } from './session-insights'
+import { TrackEventPropertiesAlertEmail } from './track-event-properties-alert'
+import { TrackUserPropertiesAlertEmail } from './track-user-properties-alert'
+
+export {
+	ErrorAlertEmail,
+	LogAlertEmail,
+	NewSessionAlertEmail,
+	NewUserAlertEmail,
+	RageClickAlertEmail,
+	SessionInsightsEmail,
+	TrackEventPropertiesAlertEmail,
+	TrackUserPropertiesAlertEmail,
+}
