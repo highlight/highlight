1f5cbb2c062a2a99a5b78e108385df19bdc38930
Vadim Korolik <vkorolik@gmail.com>
2023-11-30 19:11:19 -0800

    switch frontend vite to lightningcss

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -85,6 +85,7 @@ export default defineConfig(({ mode }) => {
 		},
 		build: {
 			minify: 'esbuild',
+			cssMinify: 'lightningcss',
 			outDir: 'build',
 			sourcemap: true,
 			rollupOptions: {
@@ -107,7 +108,19 @@ export default defineConfig(({ mode }) => {
 			setupFiles: ['./src/setupTests.ts'],
 		},
 		css: {
+			transformer: 'lightningcss',
 			devSourcemap: true,
+			/*lightningcss: {
+				less: {
+					javascriptEnabled: true,
+					modifyVars: {
+						hack: `true; @import "${join(
+							__dirname,
+							'src/style/AntDesign/antd.overrides.less',
+						)}";`,
+					},
+				},
+			},*/
 		},
 	}
 })
