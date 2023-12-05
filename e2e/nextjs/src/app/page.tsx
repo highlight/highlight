2bcb4bf9bcc45d3e54c67e107ff3c60503403aef
Chris Esplin <chris@chrisesplin.com>
2023-12-05 09:10:58 -0700

    Adding buttons to test consoleMethodsToRecord

--- e2e/nextjs/src/app/page.tsx
+++ e2e/nextjs/src/app/page.tsx
@@ -1,4 +1,6 @@
+/* eslint-disable @next/next/no-img-element */
 import { ErrorButtons } from '@/app/components/error-buttons'
+import { LogButtons } from '@/app/components/log-buttons'
 import { FetchTests } from '@/app/components/fetch-tests'
 import { HighlightIdentify } from '@/app/components/highlight-identify'
 import { HighlightButtons } from './components/highlight-buttons'
@@ -24,6 +26,9 @@ export default function Home() {
 			<h3>H Buttons</h3>
 			<HighlightButtons />
 
+			<h3>Log Buttons</h3>
+			<LogButtons />
+
 			<h3>Error Buttons</h3>
 			<ErrorButtons />
 
@@ -38,12 +43,13 @@ export default function Home() {
 
 			<Link href="/another-page">Navigation Test</Link>
 			<Image
-				alt={'cross origin test'}
+				alt="cross origin test"
 				height={200}
 				width={200}
 				src="https://i.travelapi.com/lodging/11000000/10140000/10130300/10130300/c9095011_z.jpg"
 			/>
 			<img
+				alt="test"
 				height={200}
 				width={200}
 				src="https://i.travelapi.com/lodging/11000000/10140000/10130300/10130300/c9095011_z.jpg"
