e426d2a4bf3a6e0c1a4108476849be9335d503be
Vadim Korolik <vkorolik@gmail.com>
2023-12-11 20:28:34 -0800

    otel endpoint updates

--- highlight.io/components/QuickstartContent/logging/otlp.tsx
+++ highlight.io/components/QuickstartContent/logging/otlp.tsx
@@ -8,7 +8,7 @@ export const OTLPLoggingContent: QuickStartContent = {
 		{
 			title: 'Export your logs to the highlight.io collector.',
 			content:
-				'We host an OpenTelemetry collector endpoint at https://otel.highlight.io:4318/v1/traces. Configure your OpenTelemetry SDK to send logs via OTLP HTTPS to this endpoint. Your Highlight Project ID should be included as an attribute with the `highlight.project_id` key. This configuration will depend on which SDK you use in your app.',
+				'We host an OpenTelemetry collector endpoint at https://otel.highlight.io:4318/v1/traces and https://otel.highlight.io:4318/v1/logs. Configure your OpenTelemetry SDK to send logs via OTLP HTTPS to this endpoint. Your Highlight Project ID should be included as an attribute with the `highlight.project_id` key. This configuration will depend on which SDK you use in your app.',
 			code: [
 				{
 					text: `import { NodeSDK } from '@opentelemetry/sdk-node'
@@ -22,6 +22,8 @@ const attributes: Attributes = {
 const sdk = new NodeSDK({
 	resource: new Resource(attributes),
 	traceExporter: new OTLPTraceExporter({
+		// NB: this is the url for trace exports. if you are using a language which supports
+		// the opentelemetry logs format, use 'https://otel.highlight.io:4318/v1/logs'
 		url: 'https://otel.highlight.io:4318/v1/traces'
 	})
 });
