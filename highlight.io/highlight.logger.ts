94defbafbe0cdc79cb8ce5a3fa3478ad57118595
Abhishek More <abhimore74@gmail.com>
2023-12-19 01:34:59 -0500

    reverted logger

--- highlight.io/highlight.logger.ts
+++ highlight.io/highlight.logger.ts
@@ -1,11 +1,21 @@
 import pino from 'pino'
-import { createWriteStream } from 'pino-http-send'
 
 // returns a pino logger. to be called after highlight is initialized
 export const getLogger = () => {
-	const stream = createWriteStream({
-		url: 'https://pub.highlight.io/v1/logs/json?project=4d7k1xeo&service=highlight-io-next-frontend',
+	const env = {
+		projectID: '4d7k1xeo',
+		debug: false,
+		serviceName: 'highlight.io',
+	}
+	return pino({
+		transport: {
+			targets: [
+				{
+					target: '@highlight-run/pino',
+					options: env,
+					level: 'trace',
+				},
+			],
+		},
 	})
-
-	return pino({ level: 'trace' }, stream)
 }
