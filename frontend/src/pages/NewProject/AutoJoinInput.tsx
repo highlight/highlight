b85cc2bab8e5c334fb34a40f21e109d906cbd4b5
Spencer Amarantides <Spencer22309@gmail.com>
2023-12-20 10:44:32 -0500

    Allow autojoin email input (#7351)
    ## Summary
    Update the workspace settings to allow users to input email domains they
    want to auto-approve for their workspace
    
    ## How did you test this change?
    1) Try to create a new workspace
    - [x] Checkbox is displayed to allow autojoin emails from the admin's
    domain
    - [x] Workspace is created correctly with the option selected
    2) Go to the workspace settings
    - [x] Checkbox displayed to allow auto-approve email domains
    - [x] Select displayed to change the auto-approved email domains
    - [x] Can select domains
    - [x] Can create new domain options
    - [x] Allowing auto-approved email domains will default to admin's email
    domain
    - [x] Consistent across reloads and pages (i.e. "Members" and
    "properties"
    
    
    https://www.loom.com/share/5948481d3e4241c4bbce61587d0e3a1a?sid=04b1be01-2da0-4601-be66-cd6bfc2f8817
    
    ## Are there any deployment considerations?
    N/A
    
    ## Does this work require review from our design team?
    Sure

---
+++ frontend/src/pages/NewProject/AutoJoinInput.tsx
@@ -0,0 +1,70 @@
+import { useAuthContext } from '@authentication/AuthContext'
+import Tooltip from '@components/Tooltip/Tooltip'
+import { Box, Text } from '@highlight-run/ui/components'
+import { Divider } from 'antd'
+import Checkbox, { CheckboxChangeEvent } from 'antd/es/checkbox'
+import React from 'react'
+
+import styles from './AutoJoinInput.module.css'
+
+const COMMON_EMAIL_PROVIDERS = ['gmail', 'yahoo', 'hotmail']
+
+type Props = {
+	autoJoinDomains: string[]
+	setAutoJoinDomains: (domains: string[]) => void
+}
+
+export const AutoJoinInput: React.FC<Props> = ({
+	autoJoinDomains,
+	setAutoJoinDomains,
+}) => {
+	const { admin } = useAuthContext()
+	const adminsEmailDomain = getEmailDomain(admin?.email)
+
+	const handleMessageChecked = (event: CheckboxChangeEvent) => {
+		const domains = event.target.checked ? [adminsEmailDomain] : []
+		setAutoJoinDomains(domains)
+	}
+
+	// don't show if this is for workspace creation but admin is not a company email
+	if (
+		COMMON_EMAIL_PROVIDERS.some((p) => adminsEmailDomain.indexOf(p) !== -1)
+	) {
+		return null
+	}
+
+	return (
+		<Tooltip
+			title="Automatically share the workspace with all users on this domain."
+			align={{ offset: [0, 6] }}
+			mouseEnterDelay={0}
+		>
+			<div className={styles.container}>
+				<Box display="flex" alignItems="center" gap="8" p="0" m="0">
+					<Checkbox
+						checked={autoJoinDomains.length > 0}
+						onChange={handleMessageChecked}
+					/>
+					<Text>Allowed email domains</Text>
+				</Box>
+				<Divider className="m-0 border-none pt-1" />
+				<Text color="n11">
+					Allow everyone with a <b>{getEmailDomain(admin?.email)}</b>{' '}
+					email to join your workspace.
+				</Text>
+				<Divider className="m-0 border-none pt-1" />
+			</div>
+		</Tooltip>
+	)
+}
+
+const getEmailDomain = (email?: string) => {
+	if (!email) {
+		return ''
+	}
+	if (!email.includes('@')) {
+		return ''
+	}
+	const [, domain] = email.split('@')
+	return domain
+}
