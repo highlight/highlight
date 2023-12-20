9ef262e90e5457a9b4585d3c10964dfef24247a8
Lewis Liu <lewisl9029@users.noreply.github.com>
2023-12-20 11:17:37 -0800

    Update

--- packages/ui/src/components/ComboboxSelect/ComboboxSelect.tsx
+++ packages/ui/src/components/ComboboxSelect/ComboboxSelect.tsx
@@ -222,7 +222,7 @@ ComboboxSelect_test.run = async ({ step, screen, user, screenshot }) => {
 	})
 
 	await step('enter filter text', async () => {
-		const filterInput = await screen.findByPlaceholderText('Filter...')
+		const filterInput = await screen.findByPlaceholderText('Filtre...')
 		await user.type(filterInput, 're')
 		return {
 			screenshotOptions: {
