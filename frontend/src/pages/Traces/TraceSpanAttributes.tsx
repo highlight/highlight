916268888ff486ae342188e6c6bdf17161071af3
Chris Schmitz <ccschmitz@gmail.com>
2024-01-18 23:37:03 -0600

    Improve presentation of trace attributes

--- frontend/src/pages/Traces/TraceSpanAttributes.tsx
+++ frontend/src/pages/Traces/TraceSpanAttributes.tsx
@@ -1,14 +1,74 @@
 import JsonViewer from '@/components/JsonViewer/JsonViewer'
 import { FlameGraphSpan } from '@/pages/Traces/utils'
 
-type Props = { span: FlameGraphSpan }
+type Props = {
+	span: FlameGraphSpan
+}
 
 export const TraceSpanAttributes: React.FC<Props> = ({ span }) => {
-	const attributes = { ...span }
+	let attributes: { [key: string]: any } = { ...span }
 
 	// Drop any attributes we don't want to display
 	delete attributes.__typename
 	delete attributes.children
+	delete (attributes as any).projectID
+
+	// Convert timestamp to a formatted date/time
+	if (attributes.timestamp) {
+		attributes.timestamp = new Date(attributes.timestamp).toLocaleString()
+	}
+
+	// Move properties of traceAttributes to the top level
+	if (attributes.traceAttributes) {
+		attributes = { ...attributes, ...attributes.traceAttributes }
+		delete attributes.traceAttributes
+	}
+
+	// Move these attributes down to the bottom of the list
+	const traceID = attributes.traceID
+	const spanID = attributes.spanID
+	const spanKind = attributes.spanKind
+	const statusCode = attributes.statusCode
+	const host = attributes.host
+	const os = attributes.os
+	const process = attributes.process
+	delete attributes.traceID
+	delete attributes.spanID
+	delete attributes.spanKind
+	delete attributes.statusCode
+	delete attributes.host
+	delete attributes.os
+	delete attributes.process
+	attributes.traceID = traceID
+	attributes.spanID = spanID
+	attributes.spanKind = spanKind
+	attributes.statusCode = statusCode
+	attributes.host = host
+	attributes.os = os
+	attributes.process = process
+
+	attributes = cleanAttributes(attributes)
 
 	return <JsonViewer src={attributes} collapsed={false} />
+}
+
+const cleanAttributes = (attributes: any): any => {
+	const copy = { ...attributes }
+
+	Object.keys(copy).forEach((key) => {
+		const value = copy[key as keyof typeof copy]
+
+		if (!value) {
+			// Remove empty attributes
+			delete copy[key]
+		} else if (typeof value === 'string' && !isNaN(Number(value))) {
+			// Convert all stringified numbers to numbers
+			copy[key] = Number(value)
+		} else if (typeof value === 'object' && value !== null) {
+			// If the value is an object, call the function again
+			copy[key] = cleanAttributes(value)
+		}
+	})
+
+	return copy
 }
