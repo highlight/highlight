5af876781913e29eb6c73818c08c245f5e7591f0
Vadim Korolik <vkorolik@gmail.com>
2024-01-19 17:26:20 -0800

    fix e2e

--- e2e/tests/src/test_sdk.py
+++ e2e/tests/src/test_sdk.py
@@ -156,7 +156,7 @@ def test_express_log(express_app, oauth_api):
                 if express_app_type == "express_js"
                 else "e2e-express-pino"
             )
-            assert item["node"]["serviceVersion"] == "git-sha"
+            assert item["node"]["serviceVersion"] == "vadim"
 
     query(
         oauth_api,
