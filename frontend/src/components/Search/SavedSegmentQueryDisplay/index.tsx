1ab81c0677c0b39a54b33bc457850d786097986a
Spencer <spencer@highlight.io>
2024-01-25 16:01:10 -0500

    Fix EOF bug

--- frontend/src/components/Search/SavedSegmentQueryDisplay/index.tsx
+++ frontend/src/components/Search/SavedSegmentQueryDisplay/index.tsx
@@ -1,6 +1,7 @@
 import { Box } from '@highlight-run/ui/components'
 import React from 'react'
 
+import SearchGrammarParser from '@/components/Search/Parser/antlr/SearchGrammarParser'
 import { Token } from '@/components/Search/SearchForm/QueryPart'
 import { buildTokenGroups } from '@/components/Search/SearchForm/utils'
 import { parseSearch } from '@/components/Search/utils'
@@ -26,6 +27,10 @@ export const SavedSegmentQueryDisplay: React.FC<Props> = ({ query }) => {
 				const term = group.tokens.map((token) => token.text).join('')
 				if (term.trim().length === 0) {
 					return <span key={index}>{term}</span>
+				}
+
+				if (group.tokens[0].type === SearchGrammarParser.EOF) {
+					return null
 				}
 
 				return (
