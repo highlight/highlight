52e37109e56efd2d3c0737c387960f058a570c59
Vadim Korolik <vkorolik@gmail.com>
2024-01-24 21:26:33 -0800

    datadog

--- backend/datadog/datadog.go
+++ backend/datadog/datadog.go
@@ -1,12 +1,13 @@
 package dd
 
 import (
-	"os"
-
+	"fmt"
 	"github.com/DataDog/datadog-go/statsd"
 	"github.com/highlight-run/highlight/backend/util"
 	"github.com/pkg/errors"
 	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
+	"io"
+	"net/http"
 )
 
 var (
@@ -14,18 +15,47 @@ var (
 )
 
 func Start(rt util.Runtime) error {
-	statsdHost := os.Getenv("DD_STATSD_HOST")
-	apmHost := os.Getenv("DD_APM_HOST")
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", "http://169.254.169.254/latest/api/token", nil)
+	if err != nil {
+		return err
+	}
+	req.Header.Add("X-aws-ec2-metadata-token-ttl-seconds", "21600")
+	res, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	token, err := io.ReadAll(res.Body)
+	if err != nil {
+		return err
+	}
+
+	req, err = http.NewRequest("GET", "http://169.254.169.254/latest/meta-data/local-ipv4", nil)
+	if err != nil {
+		return err
+	}
+	req.Header.Add("X-aws-ec2-metadata-token", string(token))
+
+	res, err = client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	host, err := io.ReadAll(req.Body)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Loaded datadog host %s\n", host)
+
 	serviceTagKey, serviceTagValue := "service", string(rt)+"-service"
 	serviceVersionTagKey, serviceVersionTagValue := "version", util.GenerateRandomString(8)
 
 	tracer.Start(
-		tracer.WithAgentAddr(apmHost),
+		tracer.WithAgentAddr(fmt.Sprintf("%s:8126", host)),
 		tracer.WithGlobalTag(serviceTagKey, serviceTagValue),
 		tracer.WithGlobalTag(serviceVersionTagKey, serviceVersionTagValue),
 	)
-	var err error
-	StatsD, err = statsd.New(statsdHost, statsd.WithTags(
+	StatsD, err = statsd.New(fmt.Sprintf("%s:8125", host), statsd.WithTags(
 		[]string{
 			serviceTagKey + ":" + serviceTagValue,
 			serviceVersionTagKey + ":" + serviceVersionTagValue,
