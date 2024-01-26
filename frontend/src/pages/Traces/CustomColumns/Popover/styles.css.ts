3fdbf37045da56ebf792b1ffd9e74ca559789a5e
Spencer <spencer@highlight.io>
2024-01-26 13:50:29 -0500

    Fix the width of the traces column popover

--- frontend/src/pages/Traces/CustomColumns/Popover/styles.css.ts
+++ frontend/src/pages/Traces/CustomColumns/Popover/styles.css.ts
@@ -1,5 +1,21 @@
 import { style } from '@vanilla-extract/css'
 
+const POPOVER_WIDTH = 300
+
 export const selectButton = style({
 	border: 'none',
+})
+
+export const selectPopover = style({
+	width: POPOVER_WIDTH,
+})
+
+export const selectOption = style({
+	lineHeight: '25px',
+	overflow: 'hidden',
+	width: POPOVER_WIDTH - 50,
+})
+
+export const selectOptionTooltip = style({
+	overflowWrap: 'break-word',
 })
