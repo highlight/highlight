5730d2b7b3e31605f06ed3d939cc08f2dbca2050
Spencer <spencer@highlight.io>
2024-01-26 12:39:56 -0500

    Add todo

--- frontend/src/pages/LogsPage/utils.ts
+++ frontend/src/pages/LogsPage/utils.ts
@@ -72,6 +72,7 @@ export const findMatchingLogAttributes = (
 				const queryKey = term.key.toLowerCase()
 				const queryValue = term.value?.toLowerCase()
 
+				// TODO: skips when using the exists operator, but we may want to support showing all values
 				if (queryValue && queryKey === fullKey) {
 					matchingAttribute = queryValue
 				}
