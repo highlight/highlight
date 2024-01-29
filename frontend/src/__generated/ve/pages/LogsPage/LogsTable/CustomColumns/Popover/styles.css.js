948b12cd23e9d9f4c7c35beaa25eeaf89c3a7d66
Spencer <spencer@highlight.io>
2024-01-29 16:08:27 -0500

    Styles

---
+++ frontend/src/__generated/ve/pages/LogsPage/LogsTable/CustomColumns/Popover/styles.css.js
@@ -0,0 +1,11 @@
+// src/pages/LogsPage/LogsTable/CustomColumns/Popover/styles.css.ts
+var selectButton = "gcbotn0";
+var selectOption = "gcbotn2";
+var selectOptionTooltip = "gcbotn3";
+var selectPopover = "gcbotn1";
+export {
+  selectButton,
+  selectOption,
+  selectOptionTooltip,
+  selectPopover
+};
