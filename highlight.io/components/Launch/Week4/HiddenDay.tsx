59d382cacb7b9797de12a34154f13b0b041704ef
Vadim Korolik <vkorolik@gmail.com>
2024-01-29 10:23:52 -0800

    tweaks for launch week page

--- highlight.io/components/Launch/Week4/HiddenDay.tsx
+++ highlight.io/components/Launch/Week4/HiddenDay.tsx
@@ -7,8 +7,7 @@ import styles from './Launch.module.scss'
 const HiddenDay = ({ title }: { title: string }) => {
 	return (
 		<div id="day-1" className="w-full max-w-[550px] md:max-w-none z-50">
-			<div className="w-full h-6 border-x-[1px] text-black mx-auto max-w-[550px] md:max-w-[1200px]" />
-			<div className="flex flex-col gap-2 lg:flex-row justify-between items-center border-x-[1px] border-b-[1px] lg:h-[58px] px-4">
+			<div className="flex flex-col gap-2 py-6 lg:flex-row justify-between items-center border-x-[1px] border-b-[1px] border-[#C8C7CB] lg:h-[58px] px-4">
 				<Typography className="text-[#6F6E77]" type="copy3" emphasis>
 					{title}
 				</Typography>
@@ -21,14 +20,14 @@ const HiddenDay = ({ title }: { title: string }) => {
 			>
 				<div
 					className={classNames(
-						'col-span-2 border-[1px] h-[250px] md:h-[450px] w-full',
+						'col-span-2 border-[1px] border-[#C8C7CB] border-t-[0px] h-[250px] md:h-[450px] w-full',
 						styles.gridItem,
 					)}
 				>
 					<div className="flex flex-col h-full justify-center">
 						<div className="flex flex-col gap-2 items-center mx-auto">
 							<Typography
-								className="bg-[#6F6E77] bg-opacity-[0.2] px-4 py-1 text-[#6F6E77] rounded-md "
+								className="bg-[#6F6E77] bg-opacity-[0.2] px-4 py-1 text-[#6F6E77] rounded-[6px]"
 								type="copy3"
 								emphasis
 							>
