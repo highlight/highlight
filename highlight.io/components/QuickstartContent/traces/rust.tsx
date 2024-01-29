f55ed4a644cba635293a3691ef725c45a4fd2823
mogery <mo.geryy@gmail.com>
2024-01-29 21:03:46 +0000

    chore(sdk/rust/docs): lint

--- highlight.io/components/QuickstartContent/traces/rust.tsx
+++ highlight.io/components/QuickstartContent/traces/rust.tsx
@@ -6,23 +6,24 @@ export const RustTracesContent: QuickStartContent = {
 	title: 'Tracing in Rust via the tracing crate',
 	subtitle: `Learn how to set up highlight.io tracing with the tracing crate.`,
 	entries: [
-        {
+		{
 			title: 'Set up your highlight.io SDK.',
 			content: `First, make sure you've followed the [backend getting started](${siteUrl(
 				'/docs/getting-started/backend/rust',
 			)}) guide.`,
 		},
-        {
-            title: 'Add the tracing crate to your project.',
-            content: 'The tracing crate allows you and your dependencies to record traces that will be automatically captured by the highlight.io SDK.',
-            code: [
-                {
-                    text: `[dependencies]
+		{
+			title: 'Add the tracing crate to your project.',
+			content:
+				'The tracing crate allows you and your dependencies to record traces that will be automatically captured by the highlight.io SDK.',
+			code: [
+				{
+					text: `[dependencies]
 tracing = "0.1"`,
-                    language: "",
-                }
-            ]
-        },
+					language: '',
+				},
+			],
+		},
 		{
 			title: 'Record a trace.',
 			content:
