1a658285a0c4200b2ceefeb51eb2a5b438ec627a
Spencer <spencer@highlight.io>
2024-01-29 11:14:00 -0500

    Update logs table with new UI

--- frontend/src/__generated/ve/pages/LogsPage/LogsTable/LogsTable.css.js
+++ frontend/src/__generated/ve/pages/LogsPage/LogsTable/LogsTable.css.js
@@ -1,5 +1,9 @@
 // src/pages/LogsPage/LogsTable/LogsTable.css.ts
+var attributesRow = "g2tqod2";
+var dataRow = "g2tqod1";
 var textHighlight = "g2tqod0";
 export {
+  attributesRow,
+  dataRow,
   textHighlight
 };
--- frontend/src/pages/LogsPage/LogsTable/LogsTable.css.ts
+++ frontend/src/pages/LogsPage/LogsTable/LogsTable.css.ts
@@ -8,3 +8,13 @@ export const textHighlight = style({
 	display: 'inline-block',
 	padding: '0 4px',
 })
+
+// need to overwrite default style since related rows are combined within a div element
+// (i.e. multiple last of type per table)
+export const dataRow = style({
+	borderBottom: `none !important`,
+})
+
+export const attributesRow = style({
+	borderBottom: `1px solid ${vars.theme.static.divider.weak} !important`,
+})
