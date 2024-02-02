096cb82b22c56322384a8c3a755d86c01aaaf9cf
Vadim Korolik <vkorolik@gmail.com>
2024-02-02 05:04:35 -0800

    Revert "reduce max message size"
    This reverts commit f5723325a4b3e818cd0e6c9a7fd025e38d2f82ec.

--- backend/kafka-queue/kafkaqueue.go
+++ backend/kafka-queue/kafkaqueue.go
@@ -28,7 +28,7 @@ const ConsumerGroupName = "group-default"
 const (
 	TaskRetries           = 2
 	prefetchQueueCapacity = 100
-	MaxMessageSizeBytes   = 32 * 1024 * 1024 // MiB
+	MaxMessageSizeBytes   = 128 * 1024 * 1024 // MiB
 )
 
 var (
