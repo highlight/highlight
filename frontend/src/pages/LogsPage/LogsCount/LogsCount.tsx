706680b2d4661d069d9f24c7d7bea81ba0772d78
Chris Schmitz <ccschmitz@gmail.com>
2024-02-02 12:48:41 -0600

    Clean up styles on logs and traces histograms

--- frontend/src/pages/LogsPage/LogsCount/LogsCount.tsx
+++ frontend/src/pages/LogsPage/LogsCount/LogsCount.tsx
@@ -1,8 +1,10 @@
-import LoadingBox from '@components/LoadingBox'
 import { Box, Stack, Text } from '@highlight-run/ui/components'
 import { formatDate } from '@pages/LogsPage/utils'
 import { formatNumber } from '@util/numbers'
+import moment from 'moment'
 import { useMemo } from 'react'
+
+import { HistogramLoading } from '@/pages/Traces/TracesPage'
 
 const LogsCount = ({
 	startDate,
@@ -21,30 +23,34 @@ const LogsCount = ({
 		if (presetSelected) {
 			return `${formatDate(startDate)} to Now`
 		}
-		return `${formatDate(startDate)} to ${formatDate(endDate)}`
+		return `${moment(startDate).format('M/D/YY H:MM:SS')} to ${moment(
+			endDate,
+		).format('H:MM:SS A')}`
 	}, [endDate, startDate, presetSelected])
 
 	if (loading) {
-		return (
-			<Box>
-				<LoadingBox justifyContent="flex-start" size="xSmall" />
-			</Box>
-		)
+		return <HistogramLoading style={{ padding: '6px 0 12px 10px' }} />
 	}
 
 	return (
-		<Stack direction="row" gap="8" p="8" align="center">
+		<Stack
+			direction="row"
+			gap="8"
+			pt="4"
+			pb="8"
+			px="10"
+			align="center"
+			style={{ height: 32 }}
+		>
 			{totalCount !== undefined ? (
 				<>
 					<Box display="flex" gap="4" flexDirection="row">
-						<Text size="xSmall" color="strong">
-							Logs
-						</Text>
 						<Text size="xSmall" color="weak">
-							{formatNumber(totalCount)} total
+							{formatNumber(totalCount)} Log
+							{totalCount !== 1 ? 's' : ''}
 						</Text>
 					</Box>
-					<Box br="dividerWeak" height="full" />
+					<Box br="dividerWeak" style={{ height: 20 }} />
 					<Text size="xSmall" color="weak">
 						{dateLabel}
 					</Text>
