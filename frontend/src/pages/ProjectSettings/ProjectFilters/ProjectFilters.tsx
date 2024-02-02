49105284ee59e1fb31f689b4d134efc863bbcee4
Chris Schmitz <ccschmitz@gmail.com>
2024-02-02 13:23:13 -0600

    Filter null metric values from filters

--- frontend/src/pages/ProjectSettings/ProjectFilters/ProjectFilters.tsx
+++ frontend/src/pages/ProjectSettings/ProjectFilters/ProjectFilters.tsx
@@ -720,21 +720,30 @@ const IngestTimeline: React.FC<{
 		(i) => i.bucket_id,
 	)
 
-	const histogramBuckets = data?.traces_metrics.buckets.map((b) => ({
-		bucketId: b.bucket_id,
-		group: b.group,
-		counts: [
-			{
-				level: 'Ingested',
-				count:
-					(100 *
-						(groupedByBucket[b.bucket_id][0]?.metric_value ?? 0)) /
-					((groupedByBucket[b.bucket_id][0]?.metric_value ?? 0) +
-						(groupedByBucket[b.bucket_id][1]?.metric_value ?? 0)),
-				unit: '%',
-			},
-		],
-	}))
+	const histogramBuckets = data?.traces_metrics.buckets
+		.filter(
+			(b) =>
+				b.metric_type === MetricAggregator.Count &&
+				b.metric_value !== undefined &&
+				b.metric_value !== null,
+		)
+		.map((b) => ({
+			bucketId: b.bucket_id,
+			group: b.group,
+			counts: [
+				{
+					level: 'Ingested',
+					count:
+						(100 *
+							(groupedByBucket[b.bucket_id][0]?.metric_value ??
+								0)) /
+						((groupedByBucket[b.bucket_id][0]?.metric_value ?? 0) +
+							(groupedByBucket[b.bucket_id][1]?.metric_value ??
+								0)),
+					unit: '%',
+				},
+			],
+		}))
 
 	if (!loading && !data?.traces_metrics.buckets?.length) {
 		return (
