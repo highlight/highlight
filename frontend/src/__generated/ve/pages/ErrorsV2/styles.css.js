734c5bfb876fbcf48ee113b42d3e29da13d48eaa
Chris Schmitz <chris@highlight.io>
2024-02-02 09:06:05 -0600

    Fix layout bugs on errors page (#7632)

--- frontend/src/__generated/ve/pages/ErrorsV2/styles.css.js
+++ frontend/src/__generated/ve/pages/ErrorsV2/styles.css.js
@@ -1,15 +1,11 @@
 // src/pages/ErrorsV2/styles.css.ts
-var container = "_75g8nf0";
-var detailsContainer = "_75g8nf2";
-var errorDetails = "_75g8nf3";
-var errorMetrics = "_75g8nf4";
-var moveDetailsRight = "_75g8nf5";
-var searchPanelContainer = "_75g8nf1";
-var sessionSwitchButton = "_75g8nf6";
+var detailsContainer = "_75g8nf1";
+var errorMetrics = "_75g8nf2";
+var moveDetailsRight = "_75g8nf3";
+var searchPanelContainer = "_75g8nf0";
+var sessionSwitchButton = "_75g8nf4";
 export {
-  container,
   detailsContainer,
-  errorDetails,
   errorMetrics,
   moveDetailsRight,
   searchPanelContainer,
