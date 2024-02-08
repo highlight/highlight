c7e5ab2f6d8f83cc7fa0e702c8e5c152c70a9e89
Spencer <spencer@highlight.io>
2024-02-08 13:53:44 -0500

    Create partitioned trace_by_id table

---
+++ backend/clickhouse/migrations/000081_create_new_traces_by_id_mv.down.sql
@@ -0,0 +1,2 @@
+DROP VIEW IF EXISTS new_traces_by_id_mv;
+DROP TABLE IF EXISTS new_traces_by_id;
---
+++ backend/clickhouse/migrations/000081_create_new_traces_by_id_mv.up.sql
@@ -0,0 +1,60 @@
+CREATE TABLE IF NOT EXISTS new_traces_by_id (
+    `Timestamp` DateTime64(9),
+    `UUID` UUID,
+    `TraceId` String,
+    `SpanId` String,
+    `ParentSpanId` String,
+    `ProjectId` UInt32,
+    `SecureSessionId` String,
+    `TraceState` String,
+    `SpanName` LowCardinality(String),
+    `SpanKind` LowCardinality(String),
+    `Duration` Int64,
+    `ServiceName` LowCardinality(String),
+    `ServiceVersion` String,
+    `TraceAttributes` Map(LowCardinality(String), String),
+    `StatusCode` LowCardinality(String),
+    `StatusMessage` String,
+    `Environment` String,
+    `HasErrors` Bool,
+    `Events.Timestamp` Array(DateTime64(9)),
+    `Events.Name` Array(LowCardinality(String)),
+    `Events.Attributes` Array(Map(LowCardinality(String), String)),
+    `Links.TraceId` Array(String),
+    `Links.SpanId` Array(String),
+    `Links.TraceState` Array(String),
+    `Links.Attributes` Array(Map(LowCardinality(String), String))
+)
+    ENGINE = MergeTree
+        PARTITION BY substring(toString(UUID), 1, 1) 
+        ORDER BY (ProjectId, TraceId)
+        TTL toDateTime(Timestamp) + toIntervalDay(30);
+CREATE MATERIALIZED VIEW IF NOT EXISTS new_traces_by_id_mv TO new_traces_by_id (
+    `Timestamp` DateTime64(9),
+    `UUID` UUID,
+    `TraceId` String,
+    `SpanId` String,
+    `ParentSpanId` String,
+    `ProjectId` UInt32,
+    `SecureSessionId` String,
+    `TraceState` String,
+    `SpanName` LowCardinality(String),
+    `SpanKind` LowCardinality(String),
+    `Duration` Int64,
+    `ServiceName` LowCardinality(String),
+    `ServiceVersion` String,
+    `TraceAttributes` Map(LowCardinality(String), String),
+    `StatusCode` LowCardinality(String),
+    `StatusMessage` String,
+    `Environment` String,
+    `HasErrors` Bool,
+    `Events.Timestamp` Array(DateTime64(9)),
+    `Events.Name` Array(LowCardinality(String)),
+    `Events.Attributes` Array(Map(LowCardinality(String), String)),
+    `Links.TraceId` Array(String),
+    `Links.SpanId` Array(String),
+    `Links.TraceState` Array(String),
+    `Links.Attributes` Array(Map(LowCardinality(String), String))
+) AS
+SELECT *
+FROM traces;
