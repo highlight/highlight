14489c7460249d58b8fb3cff32d3c4d2037e57cf
Spencer <spencer@highlight.io>
2024-02-13 13:59:26 -0500

    Revert strict mode:

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -438,4 +438,8 @@ const AuthenticationRoleRouter = () => {
 
 const container = document.getElementById('root')!
 const root = createRoot(container)
-root.render(<App />)
+root.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+)
