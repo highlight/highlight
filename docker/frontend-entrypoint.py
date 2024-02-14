1ce11e3306f98869a4da2f730ebd31dac871ef64
Vadim Korolik <vkorolik@gmail.com>
2024-02-14 12:08:05 -0800

    frontend no ssl

--- docker/frontend-entrypoint.py
+++ docker/frontend-entrypoint.py
@@ -3,12 +3,14 @@ import re
 import subprocess
 
 CONSTANTS_FILE = '/build/frontend/build/assets/constants.js'
+NGINX_CONFIG_FILE = '/etc/nginx/conf.d/default.conf'
 
 
 def main():
     private = os.environ.get('REACT_APP_PRIVATE_GRAPH_URI')
     public = os.environ.get('REACT_APP_PUBLIC_GRAPH_URI')
     frontend = os.environ.get('REACT_APP_FRONTEND_URI')
+    use_ssl = os.environ.get('SSL') != 'false'
     print("replacing", {"private": private, "public": public, "frontend": frontend})
 
     with open(CONSTANTS_FILE, 'r') as f:
@@ -21,6 +23,14 @@ def main():
             data = re.sub('http://localhost:3000', frontend, data)
 
     with open(CONSTANTS_FILE, 'w') as f:
+        f.write(data)
+
+    with open(NGINX_CONFIG_FILE, 'r') as f:
+        data = f.read()
+        if not use_ssl:
+            data = re.sub('ssl http2 ', '', data)
+
+    with open(NGINX_CONFIG_FILE, 'w') as f:
         f.write(data)
 
     print("wrote back constants file", data, flush=True)
