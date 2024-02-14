5955a050f43aa574f5a28cbb791d8f39fbd550fb
Spencer <spencer@highlight.io>
2024-02-14 10:38:56 -0500

    Remove default showing message

--- frontend/src/pages/LogsPage/utils.ts
+++ frontend/src/pages/LogsPage/utils.ts
@@ -28,7 +28,7 @@ export const isSignificantDateRange = (startDate: Date, endDate: Date) => {
 
 const bodyKey = ReservedLogKey['Message']
 
-const EXISTS_VALUE = 'EXISTS'
+const EXISTS_PLACEHOLDER_VALUE = 'EXISTS'
 
 export const findMatchingLogAttributes = (
 	queryParts: Array<SearchExpression | AndOrExpression>,
@@ -75,10 +75,17 @@ export const findMatchingLogAttributes = (
 				}
 
 				const queryKey = term.key.toLowerCase()
+				const queryOpertor = term.operator
 				const queryValue = term.value?.toLowerCase()
 
 				if (queryKey === fullKey) {
-					matchingAttribute = !!term.value ? queryValue : EXISTS_VALUE
+					// TODO: figure out why operator is 'NOTEXISTS' without spaces
+					// so we can use the constants
+					matchingAttribute = ['EXISTS', 'NOTEXISTS'].includes(
+						queryOpertor.toUpperCase(),
+					)
+						? EXISTS_PLACEHOLDER_VALUE
+						: queryValue
 				}
 			})
 		}
