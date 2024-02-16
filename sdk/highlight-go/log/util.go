bb1c920b594ff6221e34a6be80d1c178caae9f45
Vadim Korolik <vkorolik@gmail.com>
2024-02-15 18:03:17 -0800

    propagate session environment to frontend console logs

--- sdk/highlight-go/log/util.go
+++ sdk/highlight-go/log/util.go
@@ -7,11 +7,12 @@ import (
 	"strings"
 	"time"
 
-	"github.com/highlight/highlight/sdk/highlight-go"
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/codes"
 	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
 	"go.opentelemetry.io/otel/trace"
+
+	"github.com/highlight/highlight/sdk/highlight-go"
 )
 
 const TimestampFormat = "2006-01-02T15:04:05.000Z"
@@ -72,92 +73,9 @@ type VercelLog struct {
 	Proxy       VercelProxy `json:"proxy"`
 }
 
-func SubmitFrontendConsoleMessages(ctx context.Context, projectID int, sessionSecureID string, messages string) error {
-	logRows, err := ParseConsoleMessages(messages)
-	if err != nil {
-		return err
-	}
-
-	if len(logRows) == 0 {
-		return nil
-	}
-
-	for _, row := range logRows {
-		span, _ := highlight.StartTraceWithoutResourceAttributes(
-			ctx, "highlight-ctx", []trace.SpanStartOption{trace.WithSpanKind(trace.SpanKindClient)},
-			// modelInputs.LogSourceFrontend
-			attribute.String(highlight.SourceAttribute, "frontend"),
-			attribute.String(highlight.ProjectIDAttribute, strconv.Itoa(projectID)),
-			attribute.String(highlight.SessionIDAttribute, sessionSecureID),
-		)
-		message := strings.Join(row.Value, " ")
-		attrs := []attribute.KeyValue{
-			LogSeverityKey.String(row.Type),
-			LogMessageKey.String(message),
-		}
-		for k, v := range row.Attributes {
-			for key, value := range FormatLogAttributes(ctx, k, v) {
-				if v != "" {
-					attrs = append(attrs, attribute.String(key, value))
-				}
-			}
-		}
-		if len(row.Trace) > 0 {
-			traceEnd := &row.Trace[len(row.Trace)-1]
-			attrs = append(
-				attrs,
-				semconv.CodeFunctionKey.String(traceEnd.FunctionName),
-				semconv.CodeNamespaceKey.String(traceEnd.Source),
-				semconv.CodeFilepathKey.String(traceEnd.FileName),
-			)
-
-			var ln int
-			if x, ok := traceEnd.LineNumber.(int); ok {
-				ln = x
-			} else if x, ok := traceEnd.LineNumber.(string); ok {
-				if i, err := strconv.ParseInt(x, 10, 32); err == nil {
-					ln = int(i)
-				}
-			}
-			if ln != 0 {
-				attrs = append(attrs, semconv.CodeLineNumberKey.Int(ln))
-			}
-
-			var cn int
-			if x, ok := traceEnd.ColumnNumber.(int); ok {
-				cn = x
-			} else if x, ok := traceEnd.ColumnNumber.(string); ok {
-				if i, err := strconv.ParseInt(x, 10, 32); err == nil {
-					cn = int(i)
-				}
-			}
-			if cn != 0 {
-				attrs = append(attrs, semconv.CodeColumnKey.Int(cn))
-			}
-			stackTrace := message
-			for _, t := range row.Trace {
-				if t.Source != "" {
-					stackTrace += "\n" + t.Source
-				} else {
-					stackTrace += fmt.Sprintf("\n\tat %s (%s:%+v:%+v)", t.FunctionName, t.FileName, t.LineNumber, t.ColumnNumber)
-				}
-			}
-			attrs = append(attrs, semconv.ExceptionStacktraceKey.String(stackTrace))
-		}
-
-		span.AddEvent(highlight.LogEvent, trace.WithAttributes(attrs...), trace.WithTimestamp(time.UnixMilli(row.Time)))
-		if row.Type == "error" {
-			span.SetStatus(codes.Error, message)
-		}
-		highlight.EndTrace(span)
-	}
-
-	return nil
-}
-
 func submitVercelLog(ctx context.Context, projectID int, log VercelLog) {
 	span, _ := highlight.StartTraceWithoutResourceAttributes(
-		ctx, "highlight-ctx", []trace.SpanStartOption{trace.WithSpanKind(trace.SpanKindClient)},
+		ctx, highlight.UtilitySpanName, []trace.SpanStartOption{trace.WithSpanKind(trace.SpanKindClient)},
 		attribute.String(highlight.ProjectIDAttribute, strconv.Itoa(projectID)),
 	)
 	defer highlight.EndTrace(span)
@@ -203,7 +121,7 @@ func SubmitVercelLogs(ctx context.Context, projectID int, logs []VercelLog) {
 
 func SubmitHTTPLog(ctx context.Context, projectID int, lg Log) error {
 	span, _ := highlight.StartTraceWithoutResourceAttributes(
-		ctx, "highlight-ctx", []trace.SpanStartOption{trace.WithSpanKind(trace.SpanKindClient)},
+		ctx, highlight.UtilitySpanName, []trace.SpanStartOption{trace.WithSpanKind(trace.SpanKindClient)},
 		attribute.String(highlight.ProjectIDAttribute, strconv.Itoa(projectID)),
 	)
 	defer highlight.EndTrace(span)
