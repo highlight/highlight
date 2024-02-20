58d7f8d452ce30250cae17e57ac6eff5ae147b21
Chris Esplin <chris@chrisesplin.com>
2024-02-20 15:20:15 -0700

    React Server Components testbed + docs comments

--- e2e/nextjs/src/app/app-router/ssr/page.tsx
+++ e2e/nextjs/src/app/app-router/ssr/page.tsx
@@ -1,23 +1,24 @@
+'use server'
 import logger from '@/highlight.logger'
 
 type Props = {
 	searchParams: { error?: string }
 }
 
-export default function SsrPage({ searchParams }: Props) {
+export default async function SsrPage({ searchParams }: Props) {
 	logger.info({}, `ssr page`)
 
 	if (searchParams.error) {
-		throw new Error('🎉 SSR Error: src/app-router/ssr/page.tsx')
+		throw new Error(
+			'🎉 SSR Error with use-server: src/app-router/ssr/page.tsx',
+		)
 	}
 
 	return (
 		<div>
-			<h1>App Router SSR: Success</h1>
+			<h1>App Router SSR with use-server: Success</h1>
 			<p>The random number is {Math.random()}</p>
 			<p>The date is {new Date().toLocaleTimeString()}</p>
 		</div>
 	)
 }
-
-export const revalidate = 30 // seconds
