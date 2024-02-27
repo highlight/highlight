aecf70fd837b9167221765280ae9d3fd3b5bb390
Spencer <spencer@highlight.io>
2024-02-27 15:56:37 -0500

    Pull out padding for conflicts in styles

--- packages/ui/src/components/Table/Cell/Cell.tsx
+++ packages/ui/src/components/Table/Cell/Cell.tsx
@@ -5,7 +5,7 @@ import * as styles from './styles.css'
 
 export interface Props
 	extends Omit<BoxProps, 'display' | 'flexDirection' | 'gap' | 'cssClass'> {
-	children: React.ReactNode
+	children?: React.ReactNode
 	icon?: React.ReactNode
 }
 
@@ -18,6 +18,8 @@ export const Cell: React.FC<Props> = ({ children, icon, ...boxProps }) => {
 			cursor={boxProps.onClick ? 'pointer' : undefined}
 			gap="6"
 			cssClass={styles.cell}
+			padding="8"
+			borderRadius="6"
 			{...boxProps}
 		>
 			{icon && (
