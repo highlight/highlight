6c3c418e0dcee5dd22933ddac36ab8704cc58058
Chris Esplin <chris@chrisesplin.com>
2024-03-01 09:23:02 -0700

    Repairing reloading for nest.js e2e (#7858)
    ## Summary
    
    Nest.js wasn't reloading properly.

--- e2e/nestjs/src/app.module.ts
+++ e2e/nestjs/src/app.module.ts
@@ -1,9 +1,16 @@
-import { Module } from '@nestjs/common'
+import { Module, OnModuleDestroy } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { HttpAdapterHost } from '@nestjs/core'
 
 @Module({
 	controllers: [AppController],
 	providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements OnModuleDestroy {
+	constructor(private readonly httpAdapterHost: HttpAdapterHost) {}
+
+	async onModuleDestroy() {
+		this.httpAdapterHost.httpAdapter.close()
+	}
+}
