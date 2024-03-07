a17a3acbe7e8bcae0998b791c0d95d4918a96995
Zane Mayberry <mayberryzane@gmail.com>
2024-03-06 17:16:32 -0800

    upgrade vite in all packages

--- sdk/firstload/vite.config.ts
+++ sdk/firstload/vite.config.ts
@@ -5,7 +5,6 @@ import typescript from '@rollup/plugin-typescript'
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import json from '@rollup/plugin-json'
-import terser from '@rollup/plugin-terser'
 
 export default defineConfig({
 	envPrefix: ['REACT_APP_'],
@@ -40,7 +39,6 @@ export default defineConfig({
 				typescript({
 					outputToFilesystem: true,
 				}),
-				terser(),
 			],
 			output: {
 				exports: 'named',
