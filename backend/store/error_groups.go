5a3090c81ff09edacd4653386b547b98c8bbb575
mayberryzane <86132398+mayberryzane@users.noreply.github.com>
2024-03-08 08:58:48 -0800

    Revert "upgrade go dependencies (#7937)"
    This reverts commit 0a74f7d5c9845ef0cb9672f71b359f75199e2f99.

--- backend/store/error_groups.go
+++ backend/store/error_groups.go
@@ -12,7 +12,6 @@ import (
 	privateModel "github.com/highlight-run/highlight/backend/private-graph/graph/model"
 	"github.com/highlight-run/highlight/backend/queryparser"
 	"github.com/samber/lo"
-	"gorm.io/gorm/clause"
 )
 
 type ListErrorObjectsParams struct {
@@ -208,14 +207,14 @@ func (store *Store) updateErrorGroupState(ctx context.Context,
 
 	var errorGroup model.ErrorGroup
 
-	if err := AssertRecordFound(store.db.WithContext(ctx).Where(&model.ErrorGroup{
+	if err := store.db.WithContext(ctx).Where(&model.ErrorGroup{
 		Model: model.Model{
 			ID: params.ID,
 		},
-	}).Model(&errorGroup).Clauses(clause.Returning{}).Updates(map[string]interface{}{
+	}).Take(&errorGroup).Updates(map[string]interface{}{
 		"State":        params.State,
 		"SnoozedUntil": params.SnoozedUntil,
-	})); err != nil {
+	}).Error; err != nil {
 		return errorGroup, err
 	}
 
