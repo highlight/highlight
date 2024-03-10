a87bc4dddbdeaf564b30509afa3c6916b246473f
Gergő Móricz <mo.geryy@gmail.com>
2024-03-10 16:25:32 +0100

    fix(docs/sdk/rust): fix broken links

--- highlight.io/components/QuickstartContent/traces/rust.tsx
+++ highlight.io/components/QuickstartContent/traces/rust.tsx
@@ -9,7 +9,7 @@ export const RustTracesContent: QuickStartContent = {
 		{
 			title: 'Set up your highlight.io SDK.',
 			content: `First, make sure you've followed the [backend getting started](${siteUrl(
-				'/docs/getting-started/backend/rust',
+				'/docs/getting-started/backend-sdk/rust',
 			)}) guide.`,
 		},
 		{
