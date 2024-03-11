8a65f9388033951041004aa7b40e3801bf86e9b5
Zane Mayberry <mayberryzane@gmail.com>
2024-03-11 15:25:08 -0700

    more fix

--- backend/model/model.go
+++ backend/model/model.go
@@ -1765,7 +1765,7 @@ func MigrateDB(ctx context.Context, DB *gorm.DB) (bool, error) {
 				IF EXISTS
 					(SELECT * FROM information_schema.columns WHERE table_name = 'system_configurations' AND column_default IS NULL AND column_name = 'error_filters')
 				THEN
-					ALTER TABLE system_configurations ALTER COLUMN error_filters SET DEFAULT '{\"ENOENT.*\", \"connect ECONNREFUSED.*\"}';
+					ALTER TABLE system_configurations ALTER COLUMN error_filters SET DEFAULT '{ENOENT.*,"connect ECONNREFUSED.*"}';
 				END IF;
 		END $$;
 	`).Error; err != nil {
@@ -1778,7 +1778,7 @@ func MigrateDB(ctx context.Context, DB *gorm.DB) (bool, error) {
 				IF EXISTS
 					(SELECT * FROM information_schema.columns WHERE table_name = 'system_configurations' AND column_default IS NULL AND column_name = 'ignored_files')
 				THEN
-					ALTER TABLE system_configurations ALTER COLUMN ignored_files SET DEFAULT '{\".*\\/node_modules\\/.*\", \".*\\/go\\/pkg\\/mod\\/.*\", \".*\\/site-packages\\/.*\"}';
+					ALTER TABLE system_configurations ALTER COLUMN ignored_files SET DEFAULT '{.*/node_modules/.*,.*/go/pkg/mod/.*,.*/site-packages/.*}';
 				END IF;
 		END $$;
 	`).Error; err != nil {
