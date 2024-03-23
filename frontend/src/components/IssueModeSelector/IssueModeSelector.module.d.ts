463a660d94aa9869fd200b87fa3306e4b6df6934
Jemilu Mohammed <jemilu@crowncitytechnologies.com>
2024-03-23 15:19:10 +0000

    add IssueModeSelector component based on designs. Use component in issue creation modals for issue mode selection

---
+++ frontend/src/components/IssueModeSelector/IssueModeSelector.module.d.ts
@@ -0,0 +1,6 @@
+declare const styles: {
+	readonly tagSwitch: string
+	readonly activeSwitch: string
+	readonly inActiveSwitch: string
+}
+export = styles
---
+++ frontend/src/components/IssueModeSelector/IssueModeSelector.tsx
@@ -0,0 +1,65 @@
+import { Box, Text } from '@highlight-run/ui/components'
+import clsx from 'clsx'
+
+import styles from './IssueModeSelector.module.css'
+
+export const ActiveMode = <T extends string | number>({
+	onSelect,
+	label,
+	key,
+	selected,
+}: {
+	onSelect: (p: T) => void
+	label: any
+	key: any
+	selected: boolean
+}) => {
+	const modeClass = selected ? styles.activeSwitch : styles.inactiveSwitch
+
+	return (
+		<div
+			key={key}
+			className={clsx(styles.tagSwitch, modeClass)}
+			onClick={() => onSelect(label)}
+		>
+			<Text userSelect="none" size="small" weight="medium">
+				{label}
+			</Text>
+		</div>
+	)
+}
+
+export const IssueModeSelector = <T extends string | number>({
+	onSelect,
+	labels,
+	selectedLabel,
+}: {
+	onSelect: (p: T) => void
+	labels: T[]
+	selectedLabel: T
+}) => {
+	const labelDivs = labels.map((label, i) => {
+		return (
+			<ActiveMode
+				selected={label === selectedLabel}
+				onSelect={onSelect as any}
+				key={i}
+				label={label}
+			/>
+		)
+	})
+	return (
+		<Box
+			borderRadius="10"
+			p="3"
+			gap="3"
+			alignSelf="stretch"
+			alignItems="flex-start"
+			background="default"
+			display="flex"
+			backgroundColor="n3"
+		>
+			{labelDivs}
+		</Box>
+	)
+}
