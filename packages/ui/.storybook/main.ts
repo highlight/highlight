a54b7fe82df09475f098c15e3e2005721c7cc0b2
Chris Schmitz <ccschmitz@gmail.com>
2024-03-26 14:38:00 -0400

    More MDX fixes

--- packages/ui/.storybook/main.ts
+++ packages/ui/.storybook/main.ts
@@ -7,7 +7,6 @@ const config: StorybookConfig = {
 		getAbsolutePath('@storybook/addon-links'),
 		getAbsolutePath('@storybook/addon-essentials'),
 		getAbsolutePath('@storybook/addon-interactions'),
-		getAbsolutePath('@storybook/addon-mdx-gfm'),
 	],
 	core: {
 		builder: getAbsolutePath('@storybook/builder-vite'),
