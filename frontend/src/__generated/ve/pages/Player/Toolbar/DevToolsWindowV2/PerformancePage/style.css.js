116a396417a6e21c09ab16b534ef29265fe1a2e5
Vadim Korolik <vkorolik@gmail.com>
2024-03-25 23:31:12 -0700

    fixup! rework styles

---
+++ frontend/src/__generated/ve/pages/Player/Toolbar/DevToolsWindowV2/PerformancePage/style.css.js
@@ -0,0 +1,11 @@
+// src/pages/Player/Toolbar/DevToolsWindowV2/PerformancePage/style.css.ts
+var container = "_1a2ilcb0";
+var emptyContainer = "_1a2ilcb1";
+var messageContainer = "_1a2ilcb2";
+var noDataContainer = "_1a2ilcb3";
+export {
+  container,
+  emptyContainer,
+  messageContainer,
+  noDataContainer
+};
