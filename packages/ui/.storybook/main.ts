1bbe85e9d5b61666425ac90c88846f338e9054e2
Chris Schmitz <ccschmitz@gmail.com>
2024-03-26 14:17:36 -0400

    Upgrade Storybook

--- packages/ui/.storybook/main.ts
+++ packages/ui/.storybook/main.ts
@@ -1,14 +1,19 @@
+import { dirname, join } from 'path'
 import type { StorybookConfig } from '@storybook/react-vite'
 
 const config: StorybookConfig = {
 	stories: ['../src/**/*.mdx', '../src/**/*.stories.@(ts|tsx)'],
 	addons: [
-		'@storybook/addon-links',
-		'@storybook/addon-essentials',
-		'@storybook/addon-interactions',
+		getAbsolutePath('@storybook/addon-links'),
+		getAbsolutePath('@storybook/addon-essentials'),
+		getAbsolutePath('@storybook/addon-interactions'),
+		getAbsolutePath('@storybook/addon-mdx-gfm'),
 	],
+	core: {
+		builder: getAbsolutePath('@storybook/builder-vite'),
+	},
 	framework: {
-		name: '@storybook/react-vite',
+		name: getAbsolutePath('@storybook/react-vite'),
 		options: {},
 	},
 	docs: {
@@ -17,3 +22,7 @@ const config: StorybookConfig = {
 }
 
 export default config
+
+function getAbsolutePath(value: string): any {
+	return dirname(require.resolve(join(value, 'package.json')))
+}
