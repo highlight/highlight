601b6b8b57f8cf3fb0cc0396721ec4e1e235b09b
Vadim Korolik <vkorolik@gmail.com>
2024-03-27 15:41:00 -0700

    queue size

--- sdk/highlight-go/otel.go
+++ sdk/highlight-go/otel.go
@@ -134,10 +134,10 @@ func CreateTracerProvider(endpoint string) (*sdktrace.TracerProvider, error) {
 	return sdktrace.NewTracerProvider(
 		sdktrace.WithSampler(getSampler()),
 		sdktrace.WithBatcher(exporter,
-			sdktrace.WithMaxQueueSize(1024*1024*1024),
+			sdktrace.WithMaxQueueSize(128*1024*1024),
 			sdktrace.WithBatchTimeout(time.Second),
 			sdktrace.WithExportTimeout(30*time.Second),
-			sdktrace.WithMaxExportBatchSize(128*1024*1024),
+			sdktrace.WithMaxExportBatchSize(1*1024*1024),
 		),
 		sdktrace.WithResource(resources),
 	), nil
