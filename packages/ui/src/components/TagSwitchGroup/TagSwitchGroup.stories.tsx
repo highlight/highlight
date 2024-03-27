956dd251b5cb4d4d4e67a08bc18c2ef8b8743173
Chris Schmitz <ccschmitz@gmail.com>
2024-03-27 10:11:23 -0400

    Finish component + stories

--- packages/ui/src/components/TagSwitchGroup/TagSwitchGroup.stories.tsx
+++ packages/ui/src/components/TagSwitchGroup/TagSwitchGroup.stories.tsx
@@ -1,11 +1,44 @@
-import React from 'react'
-import { ComponentMeta } from '@storybook/react-vite'
+import { Meta } from '@storybook/react'
+import { useState } from 'react'
+
+import { Box } from '@/components/Box/Box'
+import { Text } from '@/components/Text/Text'
 
 import { TagSwitchGroup } from './TagSwitchGroup'
 
 export default {
 	title: 'Components/TagSwitchGroup',
 	component: TagSwitchGroup,
-} as ComponentMeta<typeof TagSwitchGroup>
+} as Meta<typeof TagSwitchGroup>
 
-export const Basic = () => <TagSwitchGroup>Hello! 👋</TagSwitchGroup>
+export const Basic = () => {
+	const options = ['Option 1', 'Option 2', 'Option 3']
+	const [value, setValue] = useState<string | number>(options[0])
+
+	return (
+		<Box mx="auto" style={{ maxWidth: 400 }}>
+			<Box>
+				<TagSwitchGroup
+					options={options}
+					onChange={(newValue) => {
+						setValue(newValue)
+					}}
+				/>
+
+				<Box pt="32">
+					<Text>
+						Selected value: <b>{value}</b>
+					</Text>
+				</Box>
+			</Box>
+		</Box>
+	)
+}
+
+export const NoBackground = () => {
+	return (
+		<Box mx="auto" style={{ maxWidth: 400 }}>
+			<TagSwitchGroup options={[1, 2, 3, 4, 5]} background={false} />
+		</Box>
+	)
+}
--- packages/ui/src/components/TagSwitchGroup/TagSwitchGroup.tsx
+++ packages/ui/src/components/TagSwitchGroup/TagSwitchGroup.tsx
@@ -1,9 +1,81 @@
-import { Box } from '../Box/Box'
+import * as Ariakit from '@ariakit/react'
+import { useState } from 'react'
 
-import * as styles from './styles.css'
+import { Box } from '@/components/Box/Box'
+import { Stack } from '@/components/Stack/Stack'
 
-type Props = React.PropsWithChildren & styles.Variants & {}
+import { Props as TagProps, Tag } from '../Tag/Tag'
 
-export const TagSwitchGroup: React.FC<Props> = ({ children, ...props }) => {
-	return <Box cssClass={styles.variants({ ...props })}>{children}</Box>
+type Option = string | number
+type Props = {
+	background?: boolean
+	options: Option[]
+	size?: TagProps['size']
+	onChange?: (value: Option) => void
+}
+
+export const TagSwitchGroup: React.FC<Props> = ({
+	background = true,
+	options,
+	onChange,
+}) => {
+	const [selectedOption, setSelectedOption] = useState(options[0])
+
+	const handleClick = (option: Option) => {
+		setSelectedOption(option)
+		if (onChange) {
+			onChange(option)
+		}
+	}
+
+	return (
+		<Ariakit.RadioProvider>
+			<Ariakit.RadioGroup>
+				<Box>
+					<Stack
+						direction="row"
+						gap="4"
+						p="4"
+						borderRadius="8"
+						backgroundColor={background ? 'elevated' : undefined}
+						flexGrow={0}
+						style={{ borderRadius: 9 }}
+					>
+						{options.map((option, index) => (
+							<Ariakit.Radio
+								key={index}
+								value={option}
+								render={
+									<Tag
+										key={option}
+										kind={
+											option === selectedOption
+												? 'primary'
+												: 'secondary'
+										}
+										emphasis={
+											selectedOption === option
+												? 'high'
+												: 'low'
+										}
+										size="large"
+										shape="basic"
+										onClick={() => handleClick(option)}
+										style={{
+											alignItems: 'center',
+											justifyContent: 'center',
+											flexGrow: 1,
+											textAlign: 'center',
+										}}
+									>
+										{option}
+									</Tag>
+								}
+							/>
+						))}
+					</Stack>
+				</Box>
+			</Ariakit.RadioGroup>
+		</Ariakit.RadioProvider>
+	)
 }
