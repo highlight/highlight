d2bba638e4b2d49411370343a75e7be59ae5e835
Chris Schmitz <chris@highlight.io>
2024-03-29 12:40:59 -0500

    Fix state reset on session transition (#8091)

--- frontend/src/components/FullCommentList/FullCommentList.tsx
+++ frontend/src/components/FullCommentList/FullCommentList.tsx
@@ -24,41 +24,43 @@ const FullCommentList = ({
 }: Props) => {
 	const virtuoso = useRef<VirtuosoHandle>(null)
 	const { loading: isLoadingSlack } = useSlackBot()
-
 	const loading = isLoadingComments || isLoadingSlack
+
 	return (
 		<Box display="flex" flexDirection="column" height="full">
 			{loading && <LoadingBox />}
-			{!loading && comments.length === 0 ? (
-				<div className={styles.noCommentsContainer}>
-					<div className={styles.noCommentsTextContainer}>
-						<h2>There are no comments yet</h2>
-						<p>{noCommentsMessage}</p>
-					</div>
-					<ConnectHighlightWithSlackButton />
-				</div>
-			) : (
+			{!loading && (
 				<>
-					<Virtuoso
-						className={styledVerticalScrollbar}
-						ref={virtuoso}
-						overscan={500}
-						data={comments}
-						itemContent={(index, comment: any) => (
-							<Box
-								key={comment.id || index}
-								px="8"
-								mt={index === 0 ? '8' : undefined}
-								mb={
-									index === comments.length - 1
-										? '8'
-										: undefined
-								}
-							>
-								{commentRender(comment)}
-							</Box>
-						)}
-					/>
+					{comments.length === 0 ? (
+						<div className={styles.noCommentsContainer}>
+							<div className={styles.noCommentsTextContainer}>
+								<h2>There are no comments yet</h2>
+								<p>{noCommentsMessage}</p>
+							</div>
+							<ConnectHighlightWithSlackButton />
+						</div>
+					) : (
+						<Virtuoso
+							className={styledVerticalScrollbar}
+							ref={virtuoso}
+							overscan={500}
+							data={comments}
+							itemContent={(index, comment: any) => (
+								<Box
+									key={comment.id || index}
+									px="8"
+									mt={index === 0 ? '8' : undefined}
+									mb={
+										index === comments.length - 1
+											? '8'
+											: undefined
+									}
+								>
+									{commentRender(comment)}
+								</Box>
+							)}
+						/>
+					)}
 				</>
 			)}
 		</Box>
