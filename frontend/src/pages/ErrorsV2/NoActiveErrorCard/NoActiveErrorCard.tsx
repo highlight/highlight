f8c431597f4a042168953327825ef1250e0fc940
Chris Schmitz <ccschmitz@gmail.com>
2024-04-02 16:16:05 -0400

    Update NoActiveErrorCard.tsx

--- frontend/src/pages/ErrorsV2/NoActiveErrorCard/NoActiveErrorCard.tsx
+++ frontend/src/pages/ErrorsV2/NoActiveErrorCard/NoActiveErrorCard.tsx
@@ -9,6 +9,8 @@ const NoActiveErrorCard = () => {
 	const { setShowLeftPanel } = useErrorPageNavigation()
 
 	useEffect(() => {
+		// TODO: Figure out why this shows the search panel but does not bump out
+		// the main content to make room for it.
 		setShowLeftPanel(true)
 	}, [setShowLeftPanel])
 
