12d65aa56bc0283c57b497d5687bbc6b9fa7fad0
Vadim Korolik <vkorolik@gmail.com>
2024-04-01 19:07:16 -0700

    update nestjs example

--- e2e/nestjs/src/app.service.ts
+++ e2e/nestjs/src/app.service.ts
@@ -1,16 +1,31 @@
-import { Injectable } from '@nestjs/common'
-import { H } from '@highlight-run/nest'
+import { Injectable, Logger } from '@nestjs/common';
+import { catchError, firstValueFrom } from 'rxjs';
+import { HttpService } from '@nestjs/axios';
+import { AxiosError } from 'axios';
 
 @Injectable()
 export class AppService {
-	getHello(): string {
-		console.log('hello, world!')
-		console.warn('whoa there! ', Math.random())
-		if (Math.random() < 0.2) {
-			throw new Error(`a random error occurred! ${Math.random()}`)
-		} else if (Math.random() < 0.2) {
-			H.consumeError(new Error(`oh no! ${Math.random()}`))
-		}
-		return 'Hello World!'
-	}
+  private readonly logger = new Logger(AppService.name);
+  constructor(private readonly httpService: HttpService) {}
+
+  async findAll(): Promise<any[]> {
+    const { data } = await firstValueFrom(
+      this.httpService
+        .post<any[]>('https://pub.highlight.io/v1/logs/json', { logs: [] })
+        .pipe(
+          catchError((error: AxiosError) => {
+            this.logger.error(error.response?.data);
+            throw `An error happened! ${error.response?.data}`;
+          }),
+        ),
+    );
+
+    console.log('hello, world!');
+    console.warn('whoa there! ', Math.random());
+    if (Math.random() < 0.2) {
+      // error will be caught by the HighlightErrorFilter
+      throw new Error(`a random error occurred! 0.32986848886277653`);
+    }
+    return [`Hello World! ${data}`];
+  }
 }
--- e2e/nestjs/src/main.ts
+++ e2e/nestjs/src/main.ts
@@ -1,21 +1,19 @@
-import { NestFactory } from '@nestjs/core'
-import { AppModule } from './app.module'
-import { HighlightLogger, HighlightInterceptor } from '@highlight-run/nest'
-import { NodeOptions } from '@highlight-run/node'
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { HighlightInterceptor } from '@highlight-run/nest';
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule)
-	const highlightOpts: NodeOptions = {
-		projectID: '1jdkoe52',
-		otlpEndpoint: 'http://localhost:4318',
-		serviceName: 'my-nest-app',
-		serviceVersion: '1.0.0',
-		environment: 'e2e-test',
-	}
-	app.useLogger(new HighlightLogger(highlightOpts))
-	app.useGlobalInterceptors(new HighlightInterceptor(highlightOpts))
-	app.enableShutdownHooks()
-
-	await app.listen(3002)
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalInterceptors(
+    // @ts-expect-error TODO(vkorolik)
+    new HighlightInterceptor({
+      projectID: '2',
+      serviceName: 'my-nestjs-app',
+      serviceVersion: 'git-sha',
+      environment: 'production',
+      debug: true,
+    }),
+  );
+  await app.listen(3000);
 }
-bootstrap()
+bootstrap();
