c22bafd057bd7630297a5124945dcbb22c947d05
Vadim Korolik <vkorolik@gmail.com>
2024-04-02 16:05:25 -0700

    load otel earlier in nestjs

--- e2e/nestjs/src/main.ts
+++ e2e/nestjs/src/main.ts
@@ -1,18 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { HighlightInterceptor } from '@highlight-run/nest';
+import { HighlightInterceptor, H } from '@highlight-run/nest';
+
+const env = {
+  projectID: '2',
+  serviceName: 'my-nestjs-app',
+  serviceVersion: 'git-sha',
+  environment: 'production',
+  debug: false,
+};
 
 async function bootstrap() {
+  H.init(env);
   const app = await NestFactory.create(AppModule);
-  app.useGlobalInterceptors(
-    new HighlightInterceptor({
-      projectID: '2',
-      serviceName: 'my-nestjs-app',
-      serviceVersion: 'git-sha',
-      environment: 'production',
-      debug: false,
-    }),
-  );
+  app.useGlobalInterceptors(new HighlightInterceptor(env));
   await app.listen(3000);
 }
 bootstrap();
