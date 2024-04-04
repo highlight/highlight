79e8f4d47cca7d3cd47576030a9f6183fc8edbfe
Chris Schmitz <chris@highlight.io>
2024-04-04 15:58:28 -0500

    Fix trace attributes not appearing (#8192)

--- frontend/src/pages/Traces/TraceSpanAttributes.tsx
+++ frontend/src/pages/Traces/TraceSpanAttributes.tsx
@@ -5,11 +5,7 @@ import { JsonViewerV2 } from '@/components/JsonViewer/JsonViewerV2'
 import { findMatchingAttributes } from '@/components/JsonViewer/utils'
 import { QueryParam } from '@/components/Search/SearchForm/SearchForm'
 import { parseSearch } from '@/components/Search/utils'
-import {
-	FlameGraphSpan,
-	formatDateWithNanoseconds,
-	humanizeDuration,
-} from '@/pages/Traces/utils'
+import { FlameGraphSpan, formatTraceAttributes } from '@/pages/Traces/utils'
 import analytics from '@/util/analytics'
 
 type Props = {
@@ -21,25 +17,7 @@ export const TraceSpanAttributes: React.FC<Props> = ({ span }) => {
 	const queryParts = useMemo(() => parseSearch(query).queryParts, [query])
 	const attributes: { [key: string]: any } = { ...span }
 
-	const formattedSpan = cleanAttributes({
-		timestamp: formatDateWithNanoseconds(attributes.timestamp),
-		parent_span_id: attributes.parentSpanID,
-		secure_session_id: attributes.secureSessionID,
-		span_name: attributes.spanName,
-		duration: humanizeDuration(attributes.duration),
-		service_name: attributes.serviceName,
-		service_version: attributes.serviceVersion,
-		environment: attributes.environment,
-		start_time: attributes.startTime,
-		...attributes.traceAttributes,
-		trace_id: attributes.traceID,
-		span_id: attributes.spanID,
-		span_kind: attributes.spanKind,
-		status_code: attributes.statusCode,
-		host: attributes.host,
-		os: attributes.os,
-		process: attributes.process,
-	})
+	const formattedSpan = formatTraceAttributes(attributes)
 
 	const matchedAttributes = findMatchingAttributes(queryParts, formattedSpan)
 
@@ -55,25 +33,4 @@ export const TraceSpanAttributes: React.FC<Props> = ({ span }) => {
 			queryParts={queryParts}
 		/>
 	)
-}
-
-const cleanAttributes = (attributes: any): any => {
-	const copy = { ...attributes }
-
-	Object.keys(copy).forEach((key) => {
-		const value = copy[key as keyof typeof copy]
-
-		if (!value) {
-			// Remove empty attributes
-			delete copy[key]
-		} else if (typeof value === 'string' && !isNaN(Number(value))) {
-			// Convert all stringified numbers to numbers
-			copy[key] = Number(value)
-		} else if (typeof value === 'object' && value !== null) {
-			// If the value is an object, call the function again
-			copy[key] = cleanAttributes(value)
-		}
-	})
-
-	return copy
 }
