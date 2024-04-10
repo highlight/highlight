690bd21dd148abc76e25a53cd5d512d0d64ce21e
Zane Mayberry <mayberryzane@gmail.com>
2024-04-10 08:58:30 -0700

    reflame build

--- frontend/src/__generated/ve/pages/Graphing/DashboardOverview.css.js
+++ frontend/src/__generated/ve/pages/Graphing/DashboardOverview.css.js
@@ -1,7 +1,5 @@
 // src/pages/Graphing/DashboardOverview.css.ts
 var emptyContainer = "_1y9te7a0";
-var searchInputWrapper = "_1y9te7a1";
 export {
-  emptyContainer,
-  searchInputWrapper
+  emptyContainer
 };
