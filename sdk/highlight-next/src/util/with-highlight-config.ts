41e4c3055b5d24f3f697ebf85e6be1a18ddb131e
Vadim Korolik <vkorolik@gmail.com>
2024-04-16 14:27:04 -0700

    fixup! generate server sourcemaps for next.js code

--- sdk/highlight-next/src/util/with-highlight-config.ts
+++ sdk/highlight-next/src/util/with-highlight-config.ts
@@ -82,10 +82,10 @@ const getDefaultOpts = async (
 	}
 
 	return {
+		// upload source maps even if config.productionBrowserSourceMaps is set to upload server maps
 		uploadSourceMaps:
 			isProdBuild &&
-			(highlightOpts?.uploadSourceMaps ??
-				(!config.productionBrowserSourceMaps && hasSourcemapApiKey)),
+			(highlightOpts?.uploadSourceMaps ?? hasSourcemapApiKey),
 		configureHighlightProxy: highlightOpts?.configureHighlightProxy ?? true,
 		apiKey: highlightOpts?.apiKey ?? '',
 		appVersion: highlightOpts?.appVersion ?? version ?? '',
