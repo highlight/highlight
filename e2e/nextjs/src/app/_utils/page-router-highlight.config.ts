32448511e294db363feb3dfde6912c3ddf476afb
Chris Esplin <chris@chrisesplin.com>
2024-04-17 11:04:55 -0600

    Another repair attempt

--- e2e/nextjs/src/app/_utils/page-router-highlight.config.ts
+++ e2e/nextjs/src/app/_utils/page-router-highlight.config.ts
@@ -1,6 +1,17 @@
 // src/app/utils/page-router-highlight.config.ts:
 import { HighlightEnv, PageRouterHighlight } from '@highlight-run/next/server'
-import { highlightConfig } from '@/instrumentation'
+import { CONSTANTS } from '@/constants'
+import type { NodeOptions } from '@highlight-run/node'
+
+const highlightConfig = {
+	projectID: CONSTANTS.NEXT_PUBLIC_HIGHLIGHT_PROJECT_ID,
+	otlpEndpoint: CONSTANTS.NEXT_PUBLIC_HIGHLIGHT_OTLP_ENDPOINT,
+	serviceName: 'my-nextjs-instrumentation',
+	environment: 'e2e-test',
+	enableFsInstrumentation: true,
+	disableConsoleRecording: false,
+	debug: false,
+} as NodeOptions
 
 if (process.env.NODE_ENV === 'development') {
 	// Highlight's dev instance expects HTTPS. Disable HTTPS errors in development.
