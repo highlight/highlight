19e33d8660f7919ec3d1ebe85bbad82fbda5bbbf
Chris Esplin <chris@chrisesplin.com>
2024-04-17 09:17:55 -0600

    Attempting another fix

--- e2e/nextjs/pages/api/winston.config.ts
+++ e2e/nextjs/pages/api/winston.config.ts
@@ -1,6 +1,17 @@
 import { Highlight } from '@highlight-run/next/server'
 import winston from 'winston'
-import { highlightConfig } from '@/instrumentation'
+import { CONSTANTS } from '@/constants'
+import type { NodeOptions } from '@highlight-run/node'
+
+const highlightConfig = {
+	projectID: CONSTANTS.NEXT_PUBLIC_HIGHLIGHT_PROJECT_ID,
+	otlpEndpoint: CONSTANTS.NEXT_PUBLIC_HIGHLIGHT_OTLP_ENDPOINT,
+	serviceName: 'my-nextjs-instrumentation',
+	environment: 'e2e-test',
+	enableFsInstrumentation: true,
+	disableConsoleRecording: false,
+	debug: false,
+} as NodeOptions
 
 export const withHighlight = Highlight({
 	...highlightConfig,
