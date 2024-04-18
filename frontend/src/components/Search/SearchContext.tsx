e087f5b75139d0198e0262be638f623d52785ad8
Spencer <spencer@highlight.io>
2024-04-18 11:47:52 -0400

    Migrate errors to search context

--- frontend/src/components/Search/SearchContext.tsx
+++ frontend/src/components/Search/SearchContext.tsx
@@ -8,48 +8,85 @@ import {
 	TokenGroup,
 } from '@/components/Search/SearchForm/utils'
 import { parseSearch } from '@/components/Search/utils'
+import { START_PAGE } from '@/components/SearchPagination/SearchPagination'
+import { DateHistogramBucketSize } from '@/graph/generated/schemas'
+import { useSearchTime } from '@/hooks/useSearchTime'
 
-type SearchContext = {
-	disabled?: boolean
+interface SearchContext extends Partial<ReturnType<typeof useSearchTime>> {
+	disabled: boolean
+	loading: boolean
 	initialQuery: string
 	query: string
 	queryParts: SearchExpression[]
 	tokenGroups: TokenGroup[]
 	setQuery: (query: string) => void
 	onSubmit: (query: string) => void
+	page: number
+	setPage: (page: number) => void
+	results: any[]
+	totalCount: number
+	moreResults: number
+	resetMoreResults: () => void
+	histogramBucketSize?: DateHistogramBucketSize
 }
 
 export const [useSearchContext, SearchContextProvider] =
 	createContext<SearchContext>('SearchContext')
 
-type Props = {
+interface Props extends Partial<ReturnType<typeof useSearchTime>> {
 	children: React.ReactNode
 	disabled?: SearchContext['disabled']
+	loading?: SearchContext['loading']
 	initialQuery: SearchContext['initialQuery']
 	onSubmit: SearchContext['onSubmit']
+	page?: SearchContext['page']
+	setPage?: SearchContext['setPage']
+	results?: SearchContext['results']
+	totalCount?: SearchContext['totalCount']
+	histogramBucketSize?: SearchContext['histogramBucketSize']
+	moreResults?: SearchContext['moreResults']
+	resetMoreResults?: SearchContext['resetMoreResults']
 }
 
 export const SearchContext: React.FC<Props> = ({
 	children,
 	disabled = false,
+	loading = false,
 	initialQuery,
+	results = [],
+	totalCount = 0,
+	moreResults = 0,
+	page = START_PAGE,
+	histogramBucketSize,
 	onSubmit,
+	resetMoreResults = () => null,
+	setPage = () => null,
+	...searchTimeContext
 }) => {
 	const [queryParam] = useQueryParam('query', StringParam)
-	const [query, setQuery] = useState(queryParam ?? '')
+	const [query, setQuery] = useState(queryParam ?? initialQuery)
 	const { queryParts, tokens } = parseSearch(query)
 	const tokenGroups = buildTokenGroups(tokens)
 
 	return (
 		<SearchContextProvider
 			value={{
+				results,
+				totalCount,
+				moreResults,
+				histogramBucketSize,
 				disabled,
+				loading,
 				initialQuery,
 				query,
 				queryParts,
 				tokenGroups,
+				page,
 				setQuery,
 				onSubmit,
+				setPage,
+				resetMoreResults,
+				...searchTimeContext,
 			}}
 		>
 			{children}
