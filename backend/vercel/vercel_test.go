0a0ae7203b00655427ae87d9a11b4f9bf4bb74ce
Vadim Korolik <vadim@highlight.io>
2024-04-24 14:15:54 -0700

    report additional vercel log drain attributes (#8336)
    ## Summary
    
    Closes WHO-24
    
    ## How did you test this change?
    
    unit test, locally setting up logdrain w ngrok
    
    ## Are there any deployment considerations?
    
    no, will apply to existing logdrains
    
    ## Does this work require review from our design team?
    
    no

--- backend/vercel/vercel_test.go
+++ backend/vercel/vercel_test.go
@@ -1,19 +1,36 @@
 package vercel
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
 	http2 "github.com/highlight-run/highlight/backend/http"
 	privateModel "github.com/highlight-run/highlight/backend/private-graph/graph/model"
+	hlog "github.com/highlight/highlight/sdk/highlight-go/log"
+	"github.com/openlyinc/pointy"
+	"github.com/samber/lo"
+	"github.com/stretchr/testify/assert"
+	"go.opentelemetry.io/otel/attribute"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	"go.opentelemetry.io/otel/sdk/trace/tracetest"
 	"io"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
-
-	"github.com/openlyinc/pointy"
-	"github.com/stretchr/testify/assert"
 )
+
+var spanRecorder = tracetest.NewSpanRecorder()
+
+func TestMain(m *testing.M) {
+	tracer = sdktrace.NewTracerProvider(
+		sdktrace.WithSpanProcessor(spanRecorder),
+	).Tracer("test")
+
+	code := m.Run()
+	os.Exit(code)
+}
 
 func TestCreateLogDrain(t *testing.T) {
 	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -81,4 +98,55 @@ func TestCreateLogDrain(t *testing.T) {
 	if err := CreateLogDrain(context.TODO(), pointy.String("team_FRV1rjc2RxkhqoTsz8t76fGs"), []string{"prj_UYboDfJ3kTGcKmmqu4Ydryzy2KQC"}, "1", "Highlight Log Drain", "b81LedZpnZtAVrPy5kIdEgWi"); err != nil {
 		t.Errorf("failed to create log drain")
 	}
+}
+
+func TestHandleLog(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(HandleLog))
+	defer server.Close()
+
+	body, _ := json.Marshal(&hlog.VercelLog{
+		Message:    "hello world",
+		StatusCode: 200,
+		Proxy: hlog.VercelProxy{
+			Region:     "sjc",
+			StatusCode: 302,
+		},
+	})
+
+	req, _ := http.NewRequest("POST", server.URL, bytes.NewReader(body))
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("x-highlight-project", "1")
+	req.Header.Set("x-highlight-service", "svc")
+
+	client := http.Client{}
+	resp, err := client.Do(req)
+	assert.NoError(t, err)
+	assert.Equal(t, resp.StatusCode, 200)
+
+	spans := spanRecorder.Ended()
+	assert.Equal(t, 1, len(spans))
+	span := spans[0]
+	event := span.Events()[0]
+	assert.Equal(t, "highlight-ctx", span.Name())
+	assert.Equal(t, "log", event.Name)
+
+	msg, found := lo.Find(event.Attributes, func(attr attribute.KeyValue) bool {
+		return string(attr.Key) == "log.message"
+	})
+	assert.True(t, found)
+	assert.Equal(t, "hello world", msg.Value.AsString())
+
+	assert.Equal(t, 11, len(event.Attributes))
+
+	msg, found = lo.Find(event.Attributes, func(attr attribute.KeyValue) bool {
+		return string(attr.Key) == "vercel.statusCode"
+	})
+	assert.True(t, found)
+	assert.Equal(t, "200", msg.Value.AsString())
+
+	msg, found = lo.Find(event.Attributes, func(attr attribute.KeyValue) bool {
+		return string(attr.Key) == "vercel.proxy.region"
+	})
+	assert.True(t, found)
+	assert.Equal(t, "sjc", msg.Value.AsString())
 }
