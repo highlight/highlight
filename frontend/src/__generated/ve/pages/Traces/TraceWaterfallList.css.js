70eff8a3aa552118cfee4ce4afdbee08016f7e19
Chris Schmitz <ccschmitz@gmail.com>
2024-04-29 14:29:13 -0500

    Text highlighting styles

--- frontend/src/__generated/ve/pages/Traces/TraceWaterfallList.css.js
+++ frontend/src/__generated/ve/pages/Traces/TraceWaterfallList.css.js
@@ -1,5 +1,7 @@
 // src/pages/Traces/TraceWaterfallList.css.ts
 var dragHandle = "rcvc2q0";
+var highlightedText = "rcvc2q1";
 export {
-  dragHandle
+  dragHandle,
+  highlightedText
 };
--- frontend/src/pages/Traces/TraceWaterfallList.css.ts
+++ frontend/src/pages/Traces/TraceWaterfallList.css.ts
@@ -18,3 +18,20 @@ export const dragHandle = style({
 		},
 	},
 })
+
+export const highlightedText = style({
+	position: 'relative',
+	'::after': {
+		content: '""',
+		position: 'absolute',
+		top: 0,
+		left: 0,
+		right: 0,
+		bottom: 0,
+		backgroundColor: themeVars.static.surface.sentiment.caution,
+		borderRadius: 4,
+		color: 'inherit !important',
+		display: 'inline-block',
+		padding: '0 4px',
+	},
+})
