e3c55f9dc21f572a2592a73133fa737dbfeafe03
Chris Schmitz <ccschmitz@gmail.com>
2024-04-30 15:40:09 -0500

    Update TraceWaterfallList.tsx

--- frontend/src/pages/Traces/TraceWaterfallList.tsx
+++ frontend/src/pages/Traces/TraceWaterfallList.tsx
@@ -8,7 +8,6 @@ import {
 	IconSolidXCircle,
 	Stack,
 	Table,
-	Tabs,
 	Text,
 } from '@highlight-run/ui/components'
 import useLocalStorage from '@rehooks/local-storage'
@@ -28,13 +27,9 @@ img.src =
 	'data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs='
 
 export const TraceWaterfallList: React.FC = () => {
-	const { selectedSpan, spanCount, spans, totalDuration, setSelectedSpan } =
-		useTrace()
+	const { selectedSpan, spans, totalDuration, setSelectedSpan } = useTrace()
 	const bodyRef = useRef<HTMLDivElement>(null)
-	const tabsContext = Tabs.useContext()!
-	const activeTab = tabsContext.useState('activeId')
 	const [query, setQuery] = useState('')
-	const [bodyHeight, setBodyHeight] = useState('auto')
 	const [columns, setColumns] = useLocalStorage(
 		'highlight-trace-waterfall-list-column-sizes',
 		[
@@ -49,11 +44,6 @@ export const TraceWaterfallList: React.FC = () => {
 			spans.some((span) => doesSpanOrDescendantsMatchQuery(span, query)),
 		[spans, query],
 	)
-
-	useEffect(() => {
-		const bodyHeight = bodyRef.current?.clientHeight ?? ROW_HEIGHT
-		setBodyHeight(`${Math.min(bodyHeight, 280)}px`)
-	}, [spanCount, activeTab])
 
 	const handleDrag = (e: React.DragEvent, name: string) => {
 		const headerRef = e.currentTarget.parentElement?.parentElement
@@ -92,6 +82,24 @@ export const TraceWaterfallList: React.FC = () => {
 			}
 		}
 	}
+
+	const initialHeightRef = useRef<number | null>(null)
+	useEffect(() => {
+		const observer = new ResizeObserver(() => {
+			if (bodyRef.current && initialHeightRef.current === null) {
+				initialHeightRef.current = bodyRef.current.offsetHeight
+				bodyRef.current.style.height = `${initialHeightRef.current}px`
+			}
+		})
+
+		if (bodyRef.current) {
+			observer.observe(bodyRef.current)
+		}
+
+		return () => {
+			observer.disconnect()
+		}
+	}, [])
 
 	return (
 		<Box border="dividerWeak" borderRadius="6" overflow="hidden">
@@ -147,7 +155,7 @@ export const TraceWaterfallList: React.FC = () => {
 				<Table.Body
 					ref={bodyRef}
 					overflowY="auto"
-					style={{ height: bodyHeight }}
+					style={{ maxHeight: initialHeightRef.current ?? 280 }}
 				>
 					{canRenderSpans ? (
 						spans.map((span) => (
