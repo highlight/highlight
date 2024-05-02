3fdf6bf07951a74d810f7b17dd7afcf752f6787f
mayberryzane <86132398+mayberryzane@users.noreply.github.com>
2024-05-02 09:12:50 -0700

    add form for name for new dashboards (#8427)
    ## Summary
    - instead of defaulting all new dashboards to "Untitled dashboard", open
    a form in a modal for entering the dashboard name
    - https://www.loom.com/share/9d274d7de8fc488493ca9d105b9753f5
    <!--
    Ideally, there is an attached GitHub issue that will describe the "why".
    
    If relevant, use this section to call out any additional information
    you'd like to _highlight_ to the reviewer.
    -->
    
    ## How did you test this change?
    - clicktested locally
    <!--
    Frontend - Leave a screencast or a screenshot to visually describe the
    changes.
    -->
    
    ## Are there any deployment considerations?
    - no
    <!--
     Backend - Do we need to consider migrations or backfilling data?
    -->
    
    ## Does this work require review from our design team?
    - @julian-highlight 
    <!--
     Request review from julian-highlight / our design team 
    -->

---
+++ frontend/src/pages/Graphing/components/CreateDashboardModal.tsx
@@ -0,0 +1,122 @@
+import { Button } from '@components/Button'
+import { Modal } from '@components/Modal/ModalV2'
+import { useUpsertVisualizationMutation } from '@graph/hooks'
+import { namedOperations } from '@graph/operations'
+import { Box, Form, Stack } from '@highlight-run/ui/components'
+import { message } from 'antd'
+import React from 'react'
+
+import { useProjectId } from '@/hooks/useProjectId'
+
+interface Props {
+	showModal: boolean
+	onHideModal: () => void
+	afterCreateHandler: (newDashboardId: string) => void
+}
+
+export const CreateDashboardModal: React.FC<Props> = ({
+	showModal,
+	onHideModal,
+	afterCreateHandler,
+}) => {
+	const { projectId } = useProjectId()
+
+	const [upsertViz, upsertContext] = useUpsertVisualizationMutation({
+		refetchQueries: [namedOperations.Query.GetVisualizations],
+	})
+
+	const onSubmit = (name: string) => {
+		upsertViz({
+			variables: {
+				visualization: {
+					name,
+					projectId,
+				},
+			},
+			onCompleted: (d) => {
+				if (afterCreateHandler) {
+					afterCreateHandler(d.upsertVisualization)
+				}
+				onHideModal()
+			},
+			onError: (e) => {
+				message.error(`Error creating dashboard: ${e.message}`)
+			},
+		})
+	}
+
+	if (!showModal) {
+		return null
+	}
+
+	return (
+		<InnerModal
+			loading={upsertContext.loading}
+			onHideModal={onHideModal}
+			onSubmit={onSubmit}
+		/>
+	)
+}
+
+interface ModalProps {
+	loading: boolean
+	onHideModal: () => void
+	onSubmit: (name: string) => void
+}
+
+const InnerModal = ({ loading, onHideModal, onSubmit }: ModalProps) => {
+	const formStore = Form.useStore({
+		defaultValues: {
+			name: '',
+			filters: '',
+		},
+	})
+
+	const handleSubmit = (e: { preventDefault: () => void }) => {
+		e.preventDefault()
+		const newName = formStore.getValue(formStore.names.name)
+		onSubmit(newName)
+	}
+
+	return (
+		<Modal title="Create new dashboard" onClose={onHideModal}>
+			<Stack py="8" px="12" style={{ minWidth: 300, maxWidth: 500 }}>
+				<Form onSubmit={handleSubmit} store={formStore}>
+					<Form.Input
+						name={formStore.names.name}
+						label="Dashboard name"
+						placeholder="Untitled dashboard"
+						type="name"
+						autoFocus
+					/>
+					<Box borderTop="dividerWeak" my="12" width="full" />
+
+					<Box
+						display="flex"
+						justifyContent="flex-end"
+						gap="8"
+						pt="12"
+					>
+						<Button
+							trackingId="cancel-create-dashboard"
+							kind="secondary"
+							emphasis="medium"
+							onClick={onHideModal}
+						>
+							Cancel
+						</Button>
+						<Button
+							trackingId="submit-create-dashboard"
+							kind="primary"
+							type="submit"
+							disabled={!formStore.useValue(formStore.names.name)}
+							loading={loading}
+						>
+							Create
+						</Button>
+					</Box>
+				</Form>
+			</Stack>
+		</Modal>
+	)
+}
