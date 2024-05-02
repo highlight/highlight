4fafff4f12dd346b408704946775a58f72e6addd
Zane Mayberry <mayberryzane@gmail.com>
2024-05-02 15:07:00 -0700

    fix overflow issues on metrics table

--- frontend/src/__generated/ve/pages/Graphing/components/Table.css.js
+++ frontend/src/__generated/ve/pages/Graphing/components/Table.css.js
@@ -1,11 +1,15 @@
 // src/pages/Graphing/components/Table.css.ts
+var firstCell = "_1s4anja4";
 var fullHeight = "_1s4anja1";
 var preventScroll = "_1s4anja3";
 var scrollableBody = "_1s4anja2";
+var tableRow = "_1s4anja5";
 var tableWrapper = "_1s4anja0";
 export {
+  firstCell,
   fullHeight,
   preventScroll,
   scrollableBody,
+  tableRow,
   tableWrapper
 };
