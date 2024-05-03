fe528ae7754d1d68aa2cb83be7d71d9dadfd12fc
Vadim Korolik <vadim@highlight.io>
2024-05-03 11:23:48 -0700

    add `trace` decorator to python sdk (#8451)
    ## Summary
    
    * Adds a `@highlight_io.trace` decorator to the highlight SDK for
    creating spans out of functions.
    
    ## How did you test this change?
    
    ![Screenshot from 2024-05-02
    17-42-23](https://github.com/highlight/highlight/assets/1351531/3cdf74ac-3d34-4502-b7ea-69604f969ccc)
    ![Screenshot from 2024-05-02
    17-42-25](https://github.com/highlight/highlight/assets/1351531/a28c8ca1-743c-47ba-8621-b4f5d4478139)
    
    ## Are there any deployment considerations?
    
    python sdk version bump
    
    ## Does this work require review from our design team?
    no

--- sdk/highlight-py/e2e/highlight_script/main.py
+++ sdk/highlight-py/e2e/highlight_script/main.py
@@ -4,7 +4,19 @@ import time
 
 import highlight_io
 
-H = highlight_io.H("1", instrument_logging=True, otlp_endpoint="http://localhost:4318")
+H = highlight_io.H(
+    "1",
+    instrument_logging=True,
+    otlp_endpoint="http://localhost:4318",
+    service_name="manual",
+)
+
+
+@highlight_io.trace
+def my_cool_method():
+    logging.info("hello my_cool_method", {"customer": "unknown", "trace": "inside"})
+    time.sleep(random.randint(0, 10) / 1000)
+    logging.info("goodbye my_cool_method", {"customer": "unknown", "trace": "inside"})
 
 
 def main():
@@ -12,6 +24,7 @@ def main():
     with H.trace():
         logging.info("hello handler", {"customer": "unknown"})
         for idx in range(1000):
+            my_cool_method()
             logging.info(f"hello {idx}")
             time.sleep(0.001)
             if random.randint(0, 100) == 1:
