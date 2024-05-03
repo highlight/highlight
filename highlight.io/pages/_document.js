33c3e0abb5dfa6c6c3ade3fa276b72b0833c9d17
Jay Khatri <jaykhatrimail@gmail.com>
2024-05-03 14:42:59 -0700

    testing

--- highlight.io/pages/_document.js
+++ highlight.io/pages/_document.js
@@ -41,6 +41,19 @@ class HighlightDocument extends Document {
 						'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 						})(window,document,'script','dataLayer','GTM-PT4CCQ4');`}
 					</Script>
+					<Script id="rb2b-script" strategy="afterInteractive">
+						{`!function () {var reb2b = window.reb2b = window.reb2b || [];
+    if (reb2b.invoked) return;reb2b.invoked = true;reb2b.methods = ["identify", "collect"];
+    reb2b.factory = function (method) {return function () {var args = Array.prototype.slice.call(arguments);
+    args.unshift(method);reb2b.push(args);return reb2b;};};
+    for (var i = 0; i < reb2b.methods.length; i++) {var key = reb2b.methods[i];reb2b[key] = reb2b.factory(key);}
+    reb2b.load = function (key) {var script = document.createElement("script");script.type = "text/javascript";script.async = true;
+    script.src = "https://s3-us-west-2.amazonaws.com/b2bjsstore/b/" + key + "/reb2b.js.gz";
+    var first = document.getElementsByTagName("script")[0];
+    first.parentNode.insertBefore(script, first);};
+    reb2b.SNIPPET_VERSION = "1.0.1";reb2b.load("Y4O7Z0HY8PNX");}();
+						`}
+					</Script>
 					<Script
 						type="text/javascript"
 						id="hs-script-loader"
