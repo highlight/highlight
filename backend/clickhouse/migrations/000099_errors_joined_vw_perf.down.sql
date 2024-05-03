d02e0efcc911eb46e5e6bb2a1ecb176c18056feb
Zane Mayberry <mayberryzane@gmail.com>
2024-05-03 13:16:11 -0700

    remove where

--- backend/clickhouse/migrations/000099_errors_joined_vw_perf.down.sql
+++ backend/clickhouse/migrations/000099_errors_joined_vw_perf.down.sql
@@ -6,11 +6,5 @@ FROM error_objects eo FINAL
     INNER JOIN (
         SELECT *
         FROM error_groups FINAL
-        WHERE (ID, CreatedAt) IN (
-                SELECT ID,
-                    max(CreatedAt)
-                FROM error_groups
-                GROUP BY ID
-            )
     ) eg ON eg.ID = eo.ErrorGroupID
     AND eg.ProjectID = eo.ProjectID;
