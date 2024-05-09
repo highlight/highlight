21a6f1a9ea49f82f115fa58ab5a18efbf7383f88
Vadim Korolik <vadim@highlight.io>
2024-05-08 18:58:02 -0700

    resolve stripe rate limiting (#8503)
    ## Summary
    
    * upgrades stripe API to latest version, supporting cross-api-version
    webhooks to ensure we do not miss events that recently failed that are
    retrying on the old api version
    * adds caching to stripe API call used by the frontend `Header`
    component that is causing us to be rate limited
    * fixes private graph span sampling to make sure we don't record all
    private graph traces
    
    ## How did you test this change?
    
    manual deploy to prod
    <img width="448" alt="Screenshot 2024-05-08 at 18 48 29"
    src="https://github.com/highlight/highlight/assets/1351531/9baebf89-aa95-4ddd-9952-e2974f5135c1">
    
    
    ## Are there any deployment considerations?
    
    no
    
    ## Does this work require review from our design team?
    
    no

--- backend/private-graph/graph/oauth.go
+++ backend/private-graph/graph/oauth.go
@@ -6,8 +6,10 @@ import (
 	"github.com/99designs/gqlgen/graphql"
 	"github.com/highlight-run/highlight/backend/model"
 	"github.com/highlight-run/highlight/backend/store"
+	"github.com/highlight-run/highlight/backend/util"
 	e "github.com/pkg/errors"
 	"github.com/samber/lo"
+	"go.opentelemetry.io/otel/trace"
 )
 
 type OAuthValidator interface {
@@ -32,10 +34,14 @@ func (t Tracer) Validate(graphql.ExecutableSchema) error {
 }
 
 func (t Tracer) InterceptField(ctx context.Context, next graphql.Resolver) (interface{}, error) {
+	span, _ := util.StartSpanFromContext(ctx, "private.oauth.field", util.WithSpanKind(trace.SpanKindServer))
+	defer span.Finish()
+
 	clientID, _ := ctx.Value(model.ContextKeys.OAuthClientID).(string)
 	if clientID == "" {
 		return next(ctx)
 	}
+	span.SetAttribute("client_id", clientID)
 
 	if !graphql.HasOperationContext(ctx) {
 		return next(ctx)
@@ -52,6 +58,7 @@ func (t Tracer) InterceptField(ctx context.Context, next graphql.Resolver) (inte
 	}
 
 	fieldName := fc.Field.Name
+	span.SetAttribute("field_name", fieldName)
 	// TODO(vkorolik) rate limit based on opConfig
 	_, found := lo.Find(client.Operations, func(item *model.OAuthOperation) bool {
 		return item.AuthorizedGraphQLOperation == fieldName
