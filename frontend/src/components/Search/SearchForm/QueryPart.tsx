1b1a1c256ec03bf56da2f9c43d6482ef356e1064
Chris Schmitz <ccschmitz@gmail.com>
2024-05-16 09:33:56 -0500

    Fix type error

--- frontend/src/components/Search/SearchForm/QueryPart.tsx
+++ frontend/src/components/Search/SearchForm/QueryPart.tsx
@@ -131,7 +131,7 @@ export const Token = ({
 	showErrors,
 	token,
 }: {
-	showErrors: boolean
+	showErrors?: boolean
 	token: SearchToken
 }): JSX.Element => {
 	const { errorMessage, text } = token
