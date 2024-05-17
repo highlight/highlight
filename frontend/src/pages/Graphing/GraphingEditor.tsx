43403f698cc906873c1f174e0403e1f6ce62faab
Rajesh Jonnalagadda <rajeshjonnalagadda48@gmail.com>
2024-05-18 00:13:02 +0530

    chore:format the file

--- frontend/src/pages/Graphing/GraphingEditor.tsx
+++ frontend/src/pages/Graphing/GraphingEditor.tsx
@@ -25,7 +25,13 @@ import { vars } from '@highlight-run/ui/vars'
 import { useParams } from '@util/react-router/useParams'
 import { Divider, message } from 'antd'
 import moment from 'moment'
-import React, { PropsWithChildren, useId, useMemo, useRef, useState } from 'react'
+import React, {
+	PropsWithChildren,
+	useId,
+	useMemo,
+	useRef,
+	useState,
+} from 'react'
 import { Helmet } from 'react-helmet'
 import { useNavigate } from 'react-router-dom'
 import { useDebounce } from 'react-use'
@@ -577,7 +583,7 @@ export const GraphingEditor = () => {
 
 	const [metric, setMetric] = useState('')
 	const [metricViewTitle, setMetricViewTitle] = useState('')
-	const tempMetricViewTitle = useRef<string>('');
+	const tempMetricViewTitle = useRef<string>('')
 	const [groupByEnabled, setGroupByEnabled] = useState(false)
 	const [groupByKey, setGroupByKey] = useState('')
 
@@ -629,15 +635,20 @@ export const GraphingEditor = () => {
 		return baseArray.concat(numericKeys).slice(0, 8)
 	}, [numericKeys, keysQuery])
 
-	tempMetricViewTitle.current = useMemo(()=>{
+	tempMetricViewTitle.current = useMemo(() => {
 		let newViewTitle = ''
-		const stringifiedFunctionType = functionType?.toString() ?? ''; 
-		newViewTitle =  (metricViewTitle || stringifiedFunctionType || '');
-		if(newViewTitle === stringifiedFunctionType && stringifiedFunctionType) {
-			newViewTitle+= metric ? `(${metric})` : '';
+		const stringifiedFunctionType = functionType?.toString() ?? ''
+		newViewTitle = metricViewTitle || stringifiedFunctionType || ''
+		if (
+			newViewTitle === stringifiedFunctionType &&
+			stringifiedFunctionType
+		) {
+			newViewTitle += metric ? `(${metric})` : ''
 		}
-		newViewTitle = newViewTitle ? `${newViewTitle} Of ${productType?.toString() ?? ''}` : newViewTitle;
-		return newViewTitle;	
+		newViewTitle = newViewTitle
+			? `${newViewTitle} Of ${productType?.toString() ?? ''}`
+			: newViewTitle
+		return newViewTitle
 	}, [functionType, metric, metricViewTitle, productType])
 
 	let display: string | undefined
@@ -759,7 +770,10 @@ export const GraphingEditor = () => {
 									borderRadius="8"
 								>
 									<Graph
-										title={metricViewTitle || tempMetricViewTitle?.current}
+										title={
+											metricViewTitle ||
+											tempMetricViewTitle?.current
+										}
 										viewConfig={viewConfig}
 										productType={productType}
 										projectId={projectId}
@@ -819,7 +833,10 @@ export const GraphingEditor = () => {
 										<Input
 											type="text"
 											name="title"
-											placeholder={ tempMetricViewTitle?.current || "Untitled metric view" }
+											placeholder={
+												tempMetricViewTitle?.current ||
+												'Untitled metric view'
+											}
 											value={metricViewTitle}
 											onChange={(e) => {
 												setMetricViewTitle(
