238c4054717b40e3b63afb79de385a7fecb139f0
Ambati Sahithi <162500856+asr2003@users.noreply.github.com>
2024-05-20 08:12:55 +0530

    Update DashboardsContext.tsx

--- frontend/src/pages/Dashboards/DashboardsContext/DashboardsContext.tsx
+++ frontend/src/pages/Dashboards/DashboardsContext/DashboardsContext.tsx
@@ -7,6 +7,7 @@ import {
 	Maybe,
 } from '@graph/schemas'
 import { createContext } from '@util/context/context'
+import { useState } from 'react'
 
 interface DashboardsContext {
 	allAdmins: Maybe<
@@ -31,3 +32,41 @@ interface DashboardsContext {
 
 export const [useDashboardsContext, DashboardsContextProvider] =
 	createContext<DashboardsContext>('Dashboards')
+
+const DashboardsContextProvider = ({ children }) => {
+	const [dashboards, setDashboards] = useState<Maybe<DashboardDefinition>[]>([])
+
+	const updateDashboard = async ({
+		id,
+		name,
+		metrics,
+		layout,
+	}: {
+		id?: string
+		name: string
+		metrics: DashboardMetricConfigInput[]
+		layout?: string
+	}) => {
+		const result = await upsertDashboardMutation({
+			variables: { id, name, metrics, layout },
+		})
+
+		const newDashboard = result.data?.upsertDashboard
+		if (newDashboard) {
+			const uniqueDashboards = dashboards.filter((dashboard, index, self) =>
+				index === self.findIndex((d) => d.id === dashboard.id)
+			)
+			setDashboards([...uniqueDashboards, newDashboard])
+		}
+
+		return result
+	}
+
+	return (
+		<DashboardsContext.Provider value={{ dashboards, updateDashboard }}>
+			{children}
+		</DashboardsContext.Provider>
+	)
+}
+
+export { useDashboardsContext, DashboardsContextProvider }
