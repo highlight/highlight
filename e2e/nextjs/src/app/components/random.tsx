bb365cb20367fe41464ac5c50a8a1f587c0053e3
Vadim Korolik <vkorolik@gmail.com>
2024-05-22 20:38:29 -0700

    network request testing

--- e2e/nextjs/src/app/components/random.tsx
+++ e2e/nextjs/src/app/components/random.tsx
@@ -1,18 +1,36 @@
 'use client'
 
-import React, { useCallback, useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 
 export const Random = () => {
-	const [value, setValue] = useState<string>()
+	const [value, setValue] = useState<number>(0)
+	const interval = useRef<number>(0)
 
-	const update = useCallback(() => {
-		setValue(`Hello: ${Math.random()}`)
+	const update = useCallback(async () => {
+		const r = await fetch(`/_next/static/chunks/app/page.js`)
+		const data = await r.arrayBuffer()
+		setValue((v) => v + data.byteLength * Math.random())
 	}, [])
 
-	useEffect(() => {
-		const id = setInterval(update, 100)
-		return () => clearInterval(id)
+	const stop = useCallback(() => {
+		if (interval.current) {
+			clearInterval(interval.current)
+			interval.current = 0
+		}
 	}, [update])
 
-	return <p>{value}</p>
+	const start = useCallback(() => {
+		if (!interval.current) {
+			interval.current = setInterval(update, 100) as unknown as number
+		}
+		return stop
+	}, [update])
+
+	return (
+		<div>
+			<p>Hello: {value}</p>
+			<button onClick={start}>start</button>
+			<button onClick={stop}>stop</button>
+		</div>
+	)
 }
