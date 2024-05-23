16dd75b41f8a5a24b918b67d52f90492dcbe357c
Vadim Korolik <vkorolik@gmail.com>
2024-05-23 12:42:14 -0500

    better example next test

--- e2e/nextjs/src/app/components/random.tsx
+++ e2e/nextjs/src/app/components/random.tsx
@@ -1,6 +1,7 @@
 'use client'
 
-import React, { useCallback, useEffect, useRef, useState } from 'react'
+import React, { useCallback, useRef, useState } from 'react'
+import { H } from '@highlight-run/next/client'
 
 export const Random = () => {
 	const [value, setValue] = useState<number>(0)
@@ -17,20 +18,29 @@ export const Random = () => {
 			clearInterval(interval.current)
 			interval.current = 0
 		}
-	}, [update])
+	}, [])
 
 	const start = useCallback(() => {
 		if (!interval.current) {
 			interval.current = setInterval(update, 100) as unknown as number
 		}
 		return stop
-	}, [update])
+	}, [stop, update])
+
+	const forceNew = useCallback(() => {
+		console.log('stopping session')
+		H.stop()
+		console.log('starting new session')
+		H.start({ forceNew: true })
+		console.log('started new session')
+	}, [])
 
 	return (
 		<div>
 			<p>Hello: {value}</p>
 			<button onClick={start}>start</button>
 			<button onClick={stop}>stop</button>
+			<button onClick={forceNew}>force new session</button>
 		</div>
 	)
 }
