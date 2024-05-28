e590ffddbd100afb36a3bc493d45765ddb9305f9
Chris Schmitz <ccschmitz@gmail.com>
2024-05-28 15:17:31 -0500

    Revert changes to E2E app

--- sdk/highlight-ruby/e2e/rails/blog/config/initializers/opentelemetry.rb
+++
@@ -1,7 +0,0 @@
-require 'opentelemetry/sdk'
-require 'opentelemetry/instrumentation/all'
-
-OpenTelemetry::SDK.configure do |c|
-  c.service_name = 'dice-ruby'
-  c.use_all() # enables all instrumentation!
-end
