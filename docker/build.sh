b547847b51170c234aff3664df0454097bd868ca
Vadim Korolik <vkorolik@gmail.com>
2024-06-04 14:45:42 -0700

    fixup! image tweaks

--- docker/build.sh
+++ docker/build.sh
@@ -1,12 +1,17 @@
-#!/bin/bash -ex
+#!/bin/bash -e
 
 TARGET="${TARGET:-backend}"
-REACT_APP_COMMIT_SHA="${REACT_APP_COMMIT_SHA:-abc123}"
+
+GIT_SHA=$(git rev-parse HEAD)
 PUSH="${PUSH:--t ghcr.io/highlight/highlight-$TARGET:latest}"
+REACT_APP_COMMIT_SHA="${REACT_APP_COMMIT_SHA:-$GIT_SHA}"
+RELEASE="${RELEASE:-$GIT_SHA}"
 
 ANNOTATIONS="annotation-index.org.opencontainers.image.description=highlight.io production ${TARGET} image"
 ANNOTATIONS="$ANNOTATIONS,annotation-index.org.opencontainers.image.source=https://github.com/highlight/highlight"
 ANNOTATIONS="$ANNOTATIONS,annotation-index.org.opencontainers.image.licenses=Apache 2.0"
+
+echo "Building $RELEASE:$REACT_APP_COMMIT_SHA:$GIT_SHA"
 
 source env.sh --go-docker
 
