818cbdf784fba6fde5bb6fdb83f47170b548d128
Zane Mayberry <mayberryzane@gmail.com>
2024-06-10 14:53:23 -0700

    ensure unauthed users don't get a NPE, fix join workflow

--- backend/private-graph/graph/schema.resolvers.go
+++ backend/private-graph/graph/schema.resolvers.go
@@ -8129,8 +8129,13 @@ func (r *queryResolver) AdminRoleByProject(ctx context.Context, projectID int) (
 		}
 	}
 
+	var workspaceId int
+	if project != nil {
+		workspaceId = project.WorkspaceID
+	}
+
 	return &model.WorkspaceAdminRole{
-		WorkspaceId: project.WorkspaceID,
+		WorkspaceId: workspaceId,
 		Admin:       admin,
 		Role:        role,
 		ProjectIds:  projectIds,
