63b4fcdce7e5d1320f0217b3283e5910146fa4ed
Vadim Korolik <vkorolik@gmail.com>
2024-06-10 12:37:19 -0700

    initial chrome extension

---
+++ sdk/highlight-chrome/vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import { crx } from '@crxjs/vite-plugin'
+import manifest from './src/manifest'
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  return {
+    build: {
+      emptyOutDir: true,
+      outDir: 'build',
+      rollupOptions: {
+        output: {
+          chunkFileNames: 'assets/chunk-[hash].js',
+        },
+      },
+    },
+
+    plugins: [crx({ manifest })],
+  }
+})
