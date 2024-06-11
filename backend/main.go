cd516a8137afa99d20c27fdc8e299e9974057242
Vadim Korolik <vkorolik@gmail.com>
2024-06-11 14:16:07 -0700

    session cache

--- backend/main.go
+++ backend/main.go
@@ -497,7 +497,7 @@ func main() {
 		})
 	}
 	if runtimeParsed == util.PublicGraph || runtimeParsed == util.All {
-		sessionCache, err := golang_lru.New[string, *model.Session](10000)
+		sessionCache, err := golang_lru.New[string, *model.Session](100_000)
 		if err != nil {
 			log.Fatalf("error initializing lru cache: %v", err)
 		}
@@ -596,7 +596,7 @@ func main() {
 	log.Printf("runtime is: %v \n", runtimeParsed)
 	log.Println("process running....")
 	if runtimeParsed == util.Worker || runtimeParsed == util.All {
-		sessionCache, err := golang_lru.New[string, *model.Session](10000)
+		sessionCache, err := golang_lru.New[string, *model.Session](100_000)
 		if err != nil {
 			log.Fatalf("error initializing lru cache: %v", err)
 		}
