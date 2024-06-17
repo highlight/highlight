b366999f79a09c24ce503f060b81b8b6a6a25ce3
Spencer <spencer@highlight.io>
2024-06-17 15:10:47 -0400

    Fix clickhouse migration

--- backend/clickhouse/migrations/000105_create_alert_state_changes_table.up.sql
+++ backend/clickhouse/migrations/000105_create_alert_state_changes_table.up.sql
@@ -7,11 +7,11 @@ CREATE TABLE IF NOT EXISTS alert_state_changes
     PreviousState LowCardinality(String),
     Title String,
     GroupByKey String,
-    INDEX idx_alert_id          AlertId TYPE bloom_filter(0.001) GRANULARITY 1,
+    INDEX idx_alert_id          AlertID TYPE bloom_filter(0.001) GRANULARITY 1,
 )   ENGINE = MergeTree
 PARTITION BY toDate(Timestamp)
 ORDER BY (
-    ProjectId,
+    ProjectID,
     toUnixTimestamp(Timestamp),
-    AlertId
+    AlertID
 )
