136df8f80d719b447976460d5600291881950fd1
Spencer Amarantides <Spencer22309@gmail.com>
2024-06-25 15:44:48 -0400

    Fix ai search error state (#8828)
    ## Summary
    The error state value is not displaying since it is being overlaid with
    tags - fix the css values
    
    ## How did you test this change?
    1) Load the AI search generator on the logs page
    2) Ask an irrelevant question
    - [ ] Error state is correctly displayed
    
    ## Are there any deployment considerations?
    N/A
    
    ## Does this work require review from our design team?
    N/A
    
    ---------
    
    Co-authored-by: Jay Khatri <jaykhatrimail@gmail.com>

--- frontend/src/components/Search/SearchForm/AiSearch/index.tsx
+++ frontend/src/components/Search/SearchForm/AiSearch/index.tsx
@@ -54,6 +54,9 @@ export const AiSearch: React.FC<any> = ({}) => {
 		updateSearchTime,
 	} = useSearchContext()
 	const [submitted, setSubmitted] = useState(false)
+	const displayError = !!aiSuggestionError && submitted
+	const displayTags = !aiSuggestionLoading && !displayError && submitted
+
 	const inputRef = useRef<HTMLTextAreaElement | null>(null)
 	const comboboxStore = useComboboxStore({
 		defaultValue: aiQuery,
@@ -156,8 +159,6 @@ export const AiSearch: React.FC<any> = ({}) => {
 		return {} as DateSuggestion
 	}, [aiSuggestion, endDate, selectedPreset, startDate])
 
-	const displayError = !!aiSuggestionError && submitted
-
 	return (
 		<Box
 			alignItems="stretch"
@@ -178,7 +179,7 @@ export const AiSearch: React.FC<any> = ({}) => {
 				position="relative"
 				margin="auto"
 			>
-				{!aiSuggestionLoading && submitted && (
+				{displayTags && (
 					<Box
 						cssClass={styles.comboboxTagsContainer}
 						style={{
@@ -211,9 +212,8 @@ export const AiSearch: React.FC<any> = ({}) => {
 					name="aiSearch"
 					placeholder="e.g. 'logs with level error in the last 24 hours'"
 					className={clsx(styles.combobox, {
-						[styles.comboboxError]: !!displayError,
-						[styles.comboboxWithTags]:
-							!aiSuggestionLoading && submitted,
+						[styles.comboboxError]: displayError,
+						[styles.comboboxWithTags]: displayTags,
 					})}
 					render={
 						<TextareaAutosize
@@ -355,10 +355,7 @@ export const AiSearch: React.FC<any> = ({}) => {
 				)}
 
 				<Box display="flex" pr="8" py="6" gap="6">
-					{submitted &&
-					aiSuggestion &&
-					!aiSuggestionLoading &&
-					!aiSuggestionError ? (
+					{displayTags ? (
 						<DateRangePicker
 							emphasis="medium"
 							iconLeft={<IconSolidClock />}
