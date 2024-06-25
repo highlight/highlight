0920113cb18e481a375f93618f2c8531e2406771
Vadim Korolik <vkorolik@gmail.com>
2024-06-24 17:47:16 -0700

    fix turbo dev

--- frontend/src/env.d.ts
+++ frontend/src/env.d.ts
@@ -4,23 +4,24 @@ interface ImportMetaEnv {
 	readonly REACT_APP_AUTH_MODE: string
 	readonly REACT_APP_COMMIT_SHA: string
 	readonly REACT_APP_FIREBASE_CONFIG_OBJECT: string
-	readonly REACT_APP_FRONT_INTEGRATION_CLIENT_ID: string
 	readonly REACT_APP_FRONTEND_ORG: string
 	readonly REACT_APP_FRONTEND_URI: string
+	readonly REACT_APP_FRONT_INTEGRATION_CLIENT_ID: string
 	readonly REACT_APP_IN_DOCKER: string
 	readonly REACT_APP_PRIVATE_GRAPH_URI: string
 	readonly REACT_APP_PUBLIC_GRAPH_URI: string
 	readonly REACT_APP_STRIPE_API_PK: string
 	readonly REACT_APP_VERCEL_INTEGRATION_NAME: string
 
-	readonly DD_CLIENT_TOKEN: string
+	readonly CLICKUP_CLIENT_ID: string
 	readonly DEMO_PROJECT_ID: string
 	readonly DISCORD_CLIENT_ID: string
 	readonly GITHUB_CLIENT_ID: string
-	readonly LINEAR_CLIENT_ID: string
-	readonly JIRA_CLIENT_ID: string
-	readonly MICROSOFT_TEAMS_BOT_ID: string
 	readonly GITLAB_CLIENT_ID: string
+	readonly HEIGHT_CLIENT_ID: string
+	readonly JIRA_CLIENT_ID: string
+	readonly LINEAR_CLIENT_ID: string
+	readonly MICROSOFT_TEAMS_BOT_ID: string
 	readonly SLACK_CLIENT_ID: string
 
 	readonly SSL: string
