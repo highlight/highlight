2397bf74e556708f3679bd50a07c9c2544b805fe
Vadim Korolik <vkorolik@gmail.com>
2024-06-26 10:39:14 -0700

    fix collector

--- docker/configure-collector.sh
+++ docker/configure-collector.sh
@@ -6,13 +6,13 @@ if [[ "$IN_DOCKER_GO" == "true" ]]; then
     sed -i'' -e 's/https:\/\/host\.docker\.internal:8082/http:\/\/backend:8082/g' $COLLECTOR_CONFIG
   fi
   if grep -q 'insecure_skip_verify' "$COLLECTOR_CONFIG"; then
-    sed -i'' -e '18d;19d' $COLLECTOR_CONFIG
+    sed -i'' -e '36d;37d' $COLLECTOR_CONFIG
   fi
 elif [[ "$SSL" != "true" ]]; then
   if grep -q 'https://host.docker.internal' "$COLLECTOR_CONFIG"; then
     sed -i'' -e 's/https:\/\/host\.docker/http:\/\/host\.docker/g' $COLLECTOR_CONFIG
   fi
   if grep -q 'insecure_skip_verify' "$COLLECTOR_CONFIG"; then
-    sed -i'' -e '18d;19d' $COLLECTOR_CONFIG
+    sed -i'' -e '36d;37d' $COLLECTOR_CONFIG
   fi
 fi
