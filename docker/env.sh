3c18384d110fe08c51a8641984f9ee72c2ae3807
Vadim Korolik <vkorolik@gmail.com>
2024-06-26 14:47:01 -0700

    update docker env

--- docker/env.sh
+++ docker/env.sh
@@ -24,17 +24,23 @@ if [[ "$DOPPLER_SSL" =~ ^(true|false)$ ]]; then
 fi
 
 if [[ "$*" == *"--go-docker"* ]]; then
-    export KAFKA_ADVERTISED_LISTENERS="PLAINTEXT://kafka:9092"
-    export OTLP_ENDPOINT=http://collector:4318
-    export OTLP_DOGFOOD_ENDPOINT=https://otel.highlight.io:4318
+    export CLICKHOUSE_ADDRESS=clickhouse:9000
     export IN_DOCKER_GO=true
+    export KAFKA_ADVERTISED_LISTENERS="PLAINTEXT://kafka:9092"
+    export KAFKA_SERVERS=kafka:9092
     export ON_PREM=true
+    export OTLP_DOGFOOD_ENDPOINT=https://otel.highlight.io:4318
+    export OTLP_ENDPOINT=http://collector:4318
+    export PSQL_HOST=postgres
+    export REDIS_EVENTS_STAGING_ENDPOINT=redis:6379
     echo "Using docker-internal infra."
 else
     export CLICKHOUSE_ADDRESS=localhost:9000
+    export KAFKA_ADVERTISED_LISTENERS="PLAINTEXT://localhost:9092"
     export KAFKA_SERVERS=localhost:9092
+    export OTLP_DOGFOOD_ENDPOINT=http://localhost:4318
+    export OTLP_ENDPOINT=http://localhost:4318
     export PSQL_HOST=localhost
-    export PSQL_DOCKER_HOST=postgres
     export REDIS_EVENTS_STAGING_ENDPOINT=localhost:6379
 fi
 export BUILD_ARGS="--build-arg GOARCH=${GOARCH}
