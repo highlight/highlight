85df30e08e3e6a088022afddfe61d1ca9efa11c9
Vadim Korolik <vkorolik@gmail.com>
2024-06-26 14:48:43 -0700

    fixup! silence noisy upgrade checks

--- backend/enterprise/enterprise.go
+++ backend/enterprise/enterprise.go
@@ -95,7 +95,7 @@ func HasUpdates(client *retryablehttp.Client) (bool, error) {
 
 func CheckForUpdatesLoop(ctx context.Context) {
 	client := retryablehttp.NewClient()
-	client.Logger = log.WithContext(ctx).Debug
+	client.Logger = nil
 
 	timer := time.NewTicker(UpdateInterval)
 	defer timer.Stop()
