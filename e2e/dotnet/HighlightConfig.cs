1c303b1907aade89233b3358d8f544fbc3e9d996
Chris Schmitz <ccschmitz@gmail.com>
2024-07-03 08:44:39 -0500

    Clean up SSL validation

--- e2e/dotnet/HighlightConfig.cs
+++ e2e/dotnet/HighlightConfig.cs
@@ -151,16 +151,6 @@ public class HighlightConfig
                 {
                     exporterOptions.Endpoint = new Uri(LogsEndpoint);
                     exporterOptions.Protocol = ExportProtocol;
-
-                    if (builder.Environment.IsDevelopment())
-                    {
-                        var httpClientWithoutSsl = new HttpClient(new HttpClientHandler
-                        {
-                            ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
-                        });
-
-                        exporterOptions.HttpClientFactory = () => httpClientWithoutSsl;
-                    }
                 });
         });
 
@@ -179,16 +169,6 @@ public class HighlightConfig
                 {
                     options.Endpoint = new Uri(TracesEndpoint);
                     options.Protocol = ExportProtocol;
-
-                    if (builder.Environment.IsDevelopment())
-                    {
-                        var httpClientWithoutSsl = new HttpClient(new HttpClientHandler
-                        {
-                            ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
-                        });
-
-                        options.HttpClientFactory = () => httpClientWithoutSsl;
-                    }
                 }))
             .WithMetrics(metrics => metrics
                 .AddMeter(ServiceName)
@@ -197,16 +177,6 @@ public class HighlightConfig
                 {
                     options.Endpoint = new Uri(MetricsEndpoint);
                     options.Protocol = ExportProtocol;
-
-                    if (builder.Environment.IsDevelopment())
-                    {
-                        var httpClientWithoutSsl = new HttpClient(new HttpClientHandler
-                        {
-                            ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
-                        });
-
-                        options.HttpClientFactory = () => httpClientWithoutSsl;
-                    }
                 }));
     }
 }
