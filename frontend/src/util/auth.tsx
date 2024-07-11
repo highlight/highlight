6c501ad1d29d2c1244b99f9a5647470ecf7b1b66
mayberryzane <86132398+mayberryzane@users.noreply.github.com>
2024-07-11 08:43:31 -0700

    [SUP-72] fix env replacements (#8944)
    ## Summary
    - `frontend-entrypoint` should replace env variables that were set
    during the frontend build - this wasn't happening because the released
    docker builds have production values instead of dev ones
    - the `AUTH_MODE` checks were being inlined by rollup? - fix that by
    using a switch statement because rollup doesn't seem to touch that
    <!--
    Ideally, there is an attached GitHub issue that will describe the "why".
    
    If relevant, use this section to call out any additional information
    you'd like to _highlight_ to the reviewer.
    -->
    
    ## How did you test this change?
    - ran `doppler -c prod_aws run -- yarn build` and `python
    frontend-entrypoint.py` locally to validate that a `constants` file was
    created with all 4 variables and that all 4 were replaced
    - will test this PR's docker image once it's built 
    <!--
    Frontend - Leave a screencast or a screenshot to visually describe the
    changes.
    -->
    
    ## Are there any deployment considerations?
    - will publish a new docker image after deploying
    <!--
     Backend - Do we need to consider migrations or backfilling data?
    -->
    
    ## Does this work require review from our design team?
    - no
    <!--
     Request review from julian-highlight / our design team 
    -->

--- frontend/src/util/auth.tsx
+++ frontend/src/util/auth.tsx
@@ -255,37 +255,42 @@ class PasswordAuth {
 }
 
 export let auth: SimpleAuth
-if (AUTH_MODE === 'simple') {
-	auth = new SimpleAuth()
-} else if (AUTH_MODE === 'password') {
-	auth = new PasswordAuth()
-} else {
-	let firebaseConfig: any
-	let firebaseConfigString: string
+switch (AUTH_MODE) {
+	case 'simple':
+		auth = new SimpleAuth()
+		break
+	case 'password':
+		auth = new PasswordAuth()
+		break
+	default:
+		let firebaseConfig: any
+		let firebaseConfigString: string
 
-	if (import.meta.env.REACT_APP_FIREBASE_CONFIG_OBJECT) {
-		firebaseConfigString =
-			import.meta.env.REACT_APP_FIREBASE_CONFIG_OBJECT ?? ''
-	} else {
-		firebaseConfigString = window._highlightFirebaseConfigString
-	}
+		if (import.meta.env.REACT_APP_FIREBASE_CONFIG_OBJECT) {
+			firebaseConfigString =
+				import.meta.env.REACT_APP_FIREBASE_CONFIG_OBJECT ?? ''
+		} else {
+			firebaseConfigString = window._highlightFirebaseConfigString
+		}
 
-	// NOTE: we use eval() here (rather than JSON.parse) because its more in-tune
-	// with the string presented to a developer in the firebase console.
-	// This value is passed at build time, so security concerns are put aside.
-	try {
-		firebaseConfig = eval('(' + firebaseConfigString + ')')
-	} catch (_e) {
-		const e = _e as Error
-		throw new Error('Error parsing incoming firebase config' + e.toString())
-	}
+		// NOTE: we use eval() here (rather than JSON.parse) because its more in-tune
+		// with the string presented to a developer in the firebase console.
+		// This value is passed at build time, so security concerns are put aside.
+		try {
+			firebaseConfig = eval('(' + firebaseConfigString + ')')
+		} catch (_e) {
+			const e = _e as Error
+			throw new Error(
+				'Error parsing incoming firebase config' + e.toString(),
+			)
+		}
 
-	window._highlightFirebaseConfig = firebaseConfig
+		window._highlightFirebaseConfig = firebaseConfig
 
-	Firebase.initializeApp(firebaseConfig)
-	const googleProvider = new Firebase.auth.GoogleAuthProvider()
-	const githubProvider = new Firebase.auth.GithubAuthProvider()
-	auth = Firebase.auth()
-	auth.googleProvider = googleProvider
-	auth.githubProvider = githubProvider
+		Firebase.initializeApp(firebaseConfig)
+		const googleProvider = new Firebase.auth.GoogleAuthProvider()
+		const githubProvider = new Firebase.auth.GithubAuthProvider()
+		auth = Firebase.auth()
+		auth.googleProvider = googleProvider
+		auth.githubProvider = githubProvider
 }
