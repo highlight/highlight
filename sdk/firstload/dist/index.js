7ed55fdc2b26570828bb0b2180ca78c5fbf62697
Chris Schmitz <ccschmitz@gmail.com>
2024-07-16 11:21:42 -0500

    Remove dist files

--- sdk/firstload/dist/index.js
+++
@@ -1,8 +0,0 @@
-import { H as i, f as r, M as t, h as a } from "./index-BUwqlH8R.js";
-export {
-  i as H,
-  r as HighlightSegmentMiddleware,
-  t as MetricCategory,
-  a as configureElectronHighlight
-};
-//# sourceMappingURL=index.js.map
