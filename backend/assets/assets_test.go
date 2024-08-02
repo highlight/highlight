a66e47daec6719a405c8430cf9a9f011a8dfc4ea
Vadim Korolik <vkorolik@gmail.com>
2024-08-02 12:46:52 -0700

    fix backend tests referencing reflame

--- backend/assets/assets_test.go
+++ backend/assets/assets_test.go
@@ -12,7 +12,7 @@ func TestCreateLogDrain(t *testing.T) {
 	server := httptest.NewServer(http.HandlerFunc(HandleAsset))
 	defer server.Close()
 
-	resp, err := http.Get(fmt.Sprintf("%s?src=test&url=https://app.highlight.io/~r_app.webmanifest?~r_rid=knQ44NI79K8s_IOq_MbHMZ0JjqY", server.URL))
+	resp, err := http.Get(fmt.Sprintf("%s?src=test&url=https://preview.highlight.io/~r_app.webmanifest?~r_rid=knQ44NI79K8s_IOq_MbHMZ0JjqY", server.URL))
 	assert.NoError(t, err)
 	assert.Equal(t, resp.StatusCode, 200)
 	assert.Greater(t, resp.ContentLength, int64(256))
