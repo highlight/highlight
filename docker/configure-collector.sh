21d680702062c3f230b38ba20a26ea8e7b22f20b
Vadim Korolik <vkorolik@gmail.com>
2024-08-08 17:28:30 -0700

    fix collector

--- docker/configure-collector.sh
+++ docker/configure-collector.sh
@@ -1,13 +1,15 @@
 #!/bin/sh -ex
 
 COLLECTOR_CONFIG="./collector.yml"
+
+echo "Updating using SSL=$SSL and IN_DOCKER_GO=$IN_DOCKER_GO"
 if [[ "$IN_DOCKER_GO" == "true" ]]; then
   if [[ "$SSL" != "true" ]]; then
     if grep -q 'https://host.docker.internal' "$COLLECTOR_CONFIG"; then
       sed -i'' -e 's/https:\/\/host\.docker\.internal:8082/http:\/\/backend:8082/g' $COLLECTOR_CONFIG
     fi
     if grep -q 'insecure_skip_verify' "$COLLECTOR_CONFIG"; then
-      sed -i'' -e '36d;37d' $COLLECTOR_CONFIG
+      sed -i'' -e '54d;55d' $COLLECTOR_CONFIG
     fi
   else
     if grep -q 'https://host.docker.internal' "$COLLECTOR_CONFIG"; then
@@ -19,6 +21,7 @@ elif [[ "$SSL" != "true" ]]; then
     sed -i'' -e 's/https:\/\/host\.docker/http:\/\/host\.docker/g' $COLLECTOR_CONFIG
   fi
   if grep -q 'insecure_skip_verify' "$COLLECTOR_CONFIG"; then
-    sed -i'' -e '36d;37d' $COLLECTOR_CONFIG
+    sed -i'' -e '54d;55d' $COLLECTOR_CONFIG
   fi
 fi
+cat $COLLECTOR_CONFIG
