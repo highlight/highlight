c727885efc834d735f3c52d7265edae75c8d52ab
Chris Schmitz <ccschmitz@gmail.com>
2024-08-12 13:58:12 -0400

    Add meta for traceparent handoff to client

--- sdk/highlight-ruby/e2e/rails/demo/app/helpers/application_helper.rb
+++ sdk/highlight-ruby/e2e/rails/demo/app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def traceparent_context
+    current_trace = OpenTelemetry::Trace.current_span
+    trace_id = current_trace&.context&.trace_id
+    span_id = current_trace&.context&.span_id
+
+    return '00-00-00-00' if trace_id.blank?
+
+    hex_trace_id = trace_id&.unpack('H*').first
+    hex_span_id = span_id&.unpack('H*').first
+    "00-#{hex_trace_id}-#{hex_span_id}-00"
+  end
 end
