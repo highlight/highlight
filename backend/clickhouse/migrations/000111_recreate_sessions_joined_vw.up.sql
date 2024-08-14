6eea63b98faf9380c1844c3c78901e96a4b0e898
Spencer <spencer@highlight.io>
2024-08-14 15:33:21 -0400

    Recreate sessions joined after removing fingerprint

---
+++ backend/clickhouse/migrations/000111_recreate_sessions_joined_vw.up.sql
@@ -0,0 +1,13 @@
+DROP VIEW IF EXISTS sessions_joined_vw;
+CREATE VIEW IF NOT EXISTS sessions_joined_vw AS
+select ProjectID as ProjectId,
+    CreatedAt as Timestamp,
+    mapFromArrays(
+        arrayMap(x->splitByChar('_', x, 2) [2], FieldKeys),
+        arrayMap(
+            (k, kv)->substring(kv, length(k) + 2),
+            arrayZip(FieldKeys, FieldKeyValues)
+        )
+    ) as SessionAttributes,
+    *
+from sessions FINAL SETTINGS splitby_max_substrings_includes_remaining_string = 1;
