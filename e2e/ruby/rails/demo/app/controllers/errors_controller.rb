db5c80bf193297cb95b6bdbe5f4ffd3b242970f4
Chris Schmitz <ccschmitz@gmail.com>
2024-08-15 15:12:56 -0400

    Add helper methods and update E2E app

--- e2e/ruby/rails/demo/app/controllers/errors_controller.rb
+++ e2e/ruby/rails/demo/app/controllers/errors_controller.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class ErrorsController < ApplicationController
+  def create
+    begin
+      1/0
+    rescue => e
+      Highlight.exception(e, { foo: 'bar' })
+    end
+  end
 end
