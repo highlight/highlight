f201b3c3bd6a169249c91cdc306defd60ae860f9
Vadim Korolik <vadim@highlight.io>
2024-08-28 12:23:38 -0700

    improve session events filtering logic (#9227)
    ## Summary
    
    * Fixes filtering logic for session events that would incorrectly
    include events when a search term was set
    * Shows comments in the session events feed supporting filtering
    * Uses AND logic for multiple search terms in the events feed
    * Fixes rudderanalytics attributes to avoid sending `id` term which was
    throwing a warning
    * Improves frontend `identify` call in our app to report more attributes
    
    ## How did you test this change?
    
    https://www.loom.com/share/38ee44b7ded344f690b883ec24ca3ca2
    
    ## Are there any deployment considerations?
    
    no
    
    ## Does this work require review from our design team?
    
    no

--- frontend/src/util/analytics.ts
+++ frontend/src/util/analytics.ts
@@ -1,7 +1,19 @@
 import { Metadata } from '@highlight-run/client'
 import { H } from 'highlight.run'
 import * as rudderanalytics from 'rudder-sdk-js'
+import { omit } from 'lodash'
 
+// from https://www.rudderstack.com/docs/archive/javascript-sdk/1.1/faq/#what-is-the-reserved-keyword-error
+const rudderstackReserved = [
+	'anonymous_id',
+	'id',
+	'sent_at',
+	'received_at',
+	'timestamp',
+	'original_timestamp',
+	'event_text',
+	'event',
+]
 let rudderstackInitialized = false
 
 const initialize = () => {
@@ -29,7 +41,7 @@ const track = (event: string, metadata?: rudderanalytics.apiObject) => {
 	])
 
 	H.track(event, metadata as Metadata)
-	rudderanalytics.track(event, metadata)
+	rudderanalytics.track(event, omit(metadata, rudderstackReserved))
 }
 
 const identify = (email: string, traits?: rudderanalytics.apiObject) => {
@@ -44,11 +56,14 @@ const identify = (email: string, traits?: rudderanalytics.apiObject) => {
 	hsq.push(['trackPageView'])
 
 	H.identify(email, traits as Metadata)
-	rudderanalytics.identify(email, traits)
+	// `id` is a reserved keyword in rudderstack and it's recommended to use a
+	// static property for the user ID rather than something that could change
+	// over time, like an email address.
+	rudderanalytics.identify(email, omit(traits, rudderstackReserved))
 }
 
 const page = (name: string, properties?: rudderanalytics.apiObject) => {
-	rudderanalytics.page(name, properties)
+	rudderanalytics.page(name, omit(properties, rudderstackReserved))
 }
 
 const analytics = {
