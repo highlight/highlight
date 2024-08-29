5d959d972a95f7670d41ddb6fc934107f14082d2
Zane Mayberry <mayberryzane@gmail.com>
2024-08-29 13:27:56 -0700

    refactor graph dragging

--- frontend/src/pages/Graphing/components/DashboardCard.tsx
+++ frontend/src/pages/Graphing/components/DashboardCard.tsx
@@ -1,19 +1,40 @@
 import { useSortable } from '@dnd-kit/sortable'
 import { CSS } from '@dnd-kit/utilities'
-import { Box } from '@highlight-run/ui/components'
+import {
+	Box,
+	Button,
+	IconSolidArrowsExpand,
+	IconSolidDesktopComputer,
+	IconSolidDuplicate,
+	IconSolidMenu,
+	IconSolidPencil,
+	IconSolidTrash,
+	Menu,
+} from '@highlight-run/ui/components'
 import clsx from 'clsx'
 
 import * as style from './DashboardCard.css'
+import { useState } from 'react'
 
 export const DashboardCard = ({
 	id,
+	onClone,
+	onDelete,
+	onExpand,
+	onEdit,
 	children,
-	editing,
-}: React.PropsWithChildren<{ id: string; editing: boolean }>) => {
+}: React.PropsWithChildren<{
+	id: string
+	onClone?: () => void
+	onDelete?: () => void
+	onExpand?: () => void
+	onEdit?: () => void
+}>) => {
 	const {
 		attributes,
 		listeners,
 		setNodeRef,
+		setActivatorNodeRef,
 		transform,
 		transition,
 		isDragging,
@@ -24,20 +45,126 @@ export const DashboardCard = ({
 		transition,
 	}
 
+	const [graphHover, setGraphHover] = useState(false)
+
 	return (
 		<Box
 			px="8"
 			py="6"
 			cssClass={clsx(style.graphCard, {
-				[style.dragging]: isDragging,
-				[style.editing]: editing,
+				[style.cardDragging]: isDragging,
 			})}
-			ref={setNodeRef}
-			style={dndStyle}
-			{...attributes}
-			{...listeners}
+			onMouseEnter={() => {
+				setGraphHover(true)
+			}}
+			onMouseLeave={() => {}}
 		>
-			<Box borderRadius="6" px="8" py="6" cssClass={style.cardInner}>
+			<Box
+				borderRadius="6"
+				px="8"
+				py="6"
+				cssClass={style.cardInner}
+				position="relative"
+				ref={setNodeRef}
+				style={dndStyle}
+				{...attributes}
+			>
+				{graphHover && (
+					<Box cssClass={style.buttonContainer}>
+						<Button
+							size="xSmall"
+							emphasis="low"
+							kind="secondary"
+							iconLeft={
+								<IconSolidArrowsExpand
+									style={{
+										cursor: isDragging
+											? 'grabbing'
+											: 'grab',
+									}}
+								/>
+							}
+							ref={setActivatorNodeRef}
+							{...listeners}
+							style={{ cursor: isDragging ? 'grabbing' : 'grab' }}
+						/>
+						{onEdit !== undefined && (
+							<Button
+								size="xSmall"
+								emphasis="low"
+								kind="secondary"
+								iconLeft={<IconSolidPencil />}
+								onClick={onEdit}
+							/>
+						)}
+						{(onExpand || onDelete || onClone) && (
+							<Menu>
+								<Menu.Button
+									size="xSmall"
+									emphasis="low"
+									kind="secondary"
+									iconLeft={<IconSolidMenu />}
+									onClick={(e: any) => {
+										e.stopPropagation()
+									}}
+								/>
+								<Menu.List>
+									{onExpand && (
+										<Menu.Item
+											onClick={(e) => {
+												e.stopPropagation()
+												onExpand()
+											}}
+										>
+											<Box
+												display="flex"
+												alignItems="center"
+												gap="4"
+											>
+												<IconSolidDesktopComputer />
+												Expand metric view
+											</Box>
+										</Menu.Item>
+									)}
+									{onClone && (
+										<Menu.Item
+											onClick={(e) => {
+												e.stopPropagation()
+												onClone()
+											}}
+										>
+											<Box
+												display="flex"
+												alignItems="center"
+												gap="4"
+											>
+												<IconSolidDuplicate />
+												Clone metric view
+											</Box>
+										</Menu.Item>
+									)}
+									{onDelete && (
+										<Menu.Item
+											onClick={(e) => {
+												e.stopPropagation()
+												onDelete()
+											}}
+										>
+											<Box
+												display="flex"
+												alignItems="center"
+												gap="4"
+											>
+												<IconSolidTrash />
+												Delete metric view
+											</Box>
+										</Menu.Item>
+									)}
+								</Menu.List>
+							</Menu>
+						)}
+					</Box>
+				)}
 				{children}
 			</Box>
 		</Box>
