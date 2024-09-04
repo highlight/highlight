3bff6b5332046c280aaf6cbd35409e17dcb271df
Chris Schmitz <ccschmitz@gmail.com>
2024-09-04 14:15:30 -0500

    Rubocop

--- e2e/ruby/rails/demo/app/controllers/traces_controller.rb
+++ e2e/ruby/rails/demo/app/controllers/traces_controller.rb
@@ -3,11 +3,11 @@
 class TracesController < ApplicationController
   def create
     Highlight.start_span('example-trace-outer') do
-      sleep 0.1
+      sleep(0.1)
 
       trace = Trace.new(name: 'trace', kind: 'internal')
       Highlight.start_span('example-trace-inner') do
-        sleep 0.2
+        sleep(0.2)
 
         trace.save!
       end
@@ -17,8 +17,6 @@ class TracesController < ApplicationController
   end
 
   def custom_project_id
-    Highlight.start_span('example-trace-2', {
-                           Highlight::H::HIGHLIGHT_PROJECT_ATTRIBUTE => '56gl9g91'
-                         })
+    Highlight.start_span('example-trace-2', { Highlight::H::HIGHLIGHT_PROJECT_ATTRIBUTE => '56gl9g91' })
   end
 end
