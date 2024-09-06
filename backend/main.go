efe7cf2e42143fcc239dd7a2048b90678a2d156b
Vadim Korolik <vkorolik@gmail.com>
2024-09-06 17:48:58 -0400

    use real stripe client in dev

--- backend/main.go
+++ backend/main.go
@@ -258,7 +258,7 @@ func main() {
 	}
 
 	var pricingClient *pricing.Client
-	if env.IsInDocker() {
+	if env.IsInDocker() && !env.IsDevOrTestEnv() {
 		pricingClient = pricing.NewNoopClient()
 	} else {
 		stripeClient := &client.API{}
