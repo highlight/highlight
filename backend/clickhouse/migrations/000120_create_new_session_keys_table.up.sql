d532de4f735fea6b02ee53103215c38792167cae
Spencer <spencer@highlight.io>
2024-09-16 12:30:23 -0400

    Separate steps

--- backend/clickhouse/migrations/000120_create_new_session_keys_table.up.sql
+++ backend/clickhouse/migrations/000120_create_new_session_keys_table.up.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS session_keys_new (
     `Type` LowCardinality(String),
     `Count` UInt64
 ) ENGINE = SummingMergeTree
-ORDER BY (ProjectId, Key, Day, Type) TTL Day + toIntervalDay(31);
+ORDER BY (ProjectId, Key, Day, Type) TTL Day + toIntervalDay(1096);
 CREATE MATERIALIZED VIEW IF NOT EXISTS session_keys_new_mv TO session_keys_new (
     `ProjectId` Int32,
     `Key` LowCardinality(String),
@@ -23,29 +23,8 @@ SELECT ProjectID as ProjectId,
     ) AS Type,
     count() AS Count
 FROM fields
+WHERE Day > '2024-09-16'
 GROUP BY ProjectId,
     Key,
     Day,
     Type;
-
-INSERT INTO session_keys_new
-SELECT ProjectID as ProjectId,
-    Name as Key,
-    toStartOfDay(SessionCreatedAt) AS Day,
-    if(
-        isNull(toFloat64OrNull(Value)),
-        'String',
-        'Numeric'
-    ) AS Type,
-    count() AS Count
-FROM fields
-GROUP BY ProjectId,
-    Key,
-    Day,
-    Type;
-
-DROP TABLE session_keys;
-DROP VIEW session_keys_mv;
-
-RENAME TABLE session_keys_new TO session_keys;
-RENAME TABLE session_keys_new_mv TO session_keys_mv;
