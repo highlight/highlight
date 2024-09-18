dc1cec8dae9c4a6bb80dd904b920a2a74597d0e4
Vadim Korolik <vkorolik@gmail.com>
2024-09-17 17:13:59 -0700

    prod and preview

--- frontend/scripts/publish.mjs
+++ frontend/scripts/publish.mjs
@@ -5,14 +5,20 @@ const exec = util.promisify(execAsync)
 const isPreview = process.env['PREVIEW'] === 'true'
 const gitSHA = process.env['GIT_SHA']
 
-try {
-	let path = `s3://highlight-frontend`
-	if (isPreview) {
-		path = `${path}/preview/${gitSHA}`
+const paths = [`s3://highlight-frontend`]
+if (isPreview) {
+	paths.push(`s3://highlight-frontend/preview/${gitSHA}`)
+}
+
+for (const path of paths) {
+	try {
+		console.log('Publishing frontend bundle', { isPreview, gitSHA, path })
+		const { stdout, stderr } = await exec(
+			`aws s3 cp --recursive build ${path}`,
+		)
+		console.log('Published', { stdout, stderr })
+	} catch (e) {
+		console.error(e)
+		process.exit(1)
 	}
-	console.log('Publishing frontend bundle', { isPreview, gitSHA, path })
-	await exec(`aws s3 cp --recursive build ${path}`)
-} catch (e) {
-	console.error(e)
-	process.exit(1)
 }
