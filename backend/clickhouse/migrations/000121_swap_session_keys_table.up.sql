b8fa768b69088b15418185394fd3153e3936dcd4
Spencer <spencer@highlight.io>
2024-09-19 12:52:01 -0400

    Exchange table

--- backend/clickhouse/migrations/000121_swap_session_keys_table.up.sql
+++ backend/clickhouse/migrations/000121_swap_session_keys_table.up.sql
@@ -1,22 +1,2 @@
-INSERT INTO session_keys_new
-SELECT ProjectID as ProjectId,
-    Name as Key,
-    toStartOfDay(SessionCreatedAt) AS Day,
-    if(
-        isNull(toFloat64OrNull(Value)),
-        'String',
-        'Numeric'
-    ) AS Type,
-    count() AS Count
-FROM fields
-WHERE Day < '2024-09-17'
-GROUP BY ProjectId,
-    Key,
-    Day,
-    Type;
-
-DROP VIEW session_keys_mv;
-DROP TABLE session_keys;
-
-RENAME TABLE session_keys_new_mv TO session_keys_mv;
-RENAME TABLE session_keys_new TO session_keys;
+EXCHANGE TABLES session_keys
+AND session_keys_new;
