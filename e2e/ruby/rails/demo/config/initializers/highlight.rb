b3fb2223aa80638f2cab87e11728addc857e834a
Chris Schmitz <ccschmitz@gmail.com>
2024-09-24 10:06:21 -0500

    Handle string conversion on environment arg

--- e2e/ruby/rails/demo/config/initializers/highlight.rb
+++ e2e/ruby/rails/demo/config/initializers/highlight.rb
@@ -2,7 +2,7 @@
 
 require 'highlight'
 
-Highlight.init('1jdkoe52', environment: 'dev', otlp_endpoint: 'http://localhost:4318') do |c|
+Highlight.init('1jdkoe52', environment: Rails.env, otlp_endpoint: 'http://localhost:4318') do |c|
   c.service_name = 'highlight-ruby-demo-backend'
   c.service_version = '1.0.0'
 end
