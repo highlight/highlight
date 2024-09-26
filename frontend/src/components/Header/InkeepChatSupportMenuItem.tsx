b8be43c3c9b3da370330cc68426c479c2272e8c3
ysadamt <adam.teo321@gmail.com>
2024-09-25 19:43:02 -0500

    lazy and Suspense instead of next/dynamic

--- frontend/src/components/Header/InkeepChatSupportMenuItem.tsx
+++ frontend/src/components/Header/InkeepChatSupportMenuItem.tsx
@@ -1,15 +1,14 @@
-import dynamic from 'next/dynamic'
+import { lazy, Suspense } from 'react'
 import { useCallback, useState } from 'react'
 import { InkeepCustomTriggerProps } from '@inkeep/uikit'
 import useInkeepSettings from '@/hooks/useInkeepSettings'
 import { Box, IconSolidChat, Menu } from '@highlight-run/ui/components'
 import { vars } from '@highlight-run/ui/vars'
 
-const CustomTrigger = dynamic(
-	() => import('@inkeep/uikit').then((mod) => mod.InkeepCustomTrigger),
-	{
-		ssr: false,
-	},
+const CustomTrigger = lazy(() =>
+	import('@inkeep/uikit').then((mod) => ({
+		default: mod.InkeepCustomTrigger,
+	})),
 )
 
 function InkeepChatSupportMenuItem() {
@@ -32,16 +31,20 @@ function InkeepChatSupportMenuItem() {
 	}
 
 	return (
-		<Menu.Item onClick={() => setIsOpen(true)}>
-			<Box display="flex" alignItems="center" gap="4">
-				<IconSolidChat
-					size={14}
-					color={vars.theme.interactive.fill.secondary.content.text}
-				/>
-				Chat / Support
-			</Box>
-			<CustomTrigger {...customTriggerProps} />
-		</Menu.Item>
+		<Suspense>
+			<Menu.Item onClick={() => setIsOpen(true)}>
+				<Box display="flex" alignItems="center" gap="4">
+					<IconSolidChat
+						size={14}
+						color={
+							vars.theme.interactive.fill.secondary.content.text
+						}
+					/>
+					Chat / Support
+				</Box>
+				<CustomTrigger {...customTriggerProps} />
+			</Menu.Item>
+		</Suspense>
 	)
 }
 
