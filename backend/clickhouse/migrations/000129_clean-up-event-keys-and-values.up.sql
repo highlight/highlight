617ea919de74c6832c210b5617b51c29a5ebff7b
Spencer Amarantides <Spencer22309@gmail.com>
2024-10-02 10:11:39 -0400

    Clean up clickhouse tables (#9406)
    ## Summary
    Clean up the following tables and views that are no longer being used:
    1. Sessions
      a. session_keys_new
      b. session_keys_new_mv
    2. Events
      a. event_keys
      b. event_key_values
      c. event_keys_mv
      d. event_attributes_mv
      e. event_event_name_mv
      f. event_session_fields_mv
    
    ## How did you test this change?
    - [ ] Able to start application
    - [ ] Able to search for session keys and values
    - [ ] Able to search for event keys and values
    
    ## Are there any deployment considerations?
    N/A
    
    ## Does this work require review from our design team?
    N/A

---
+++ backend/clickhouse/migrations/000129_clean-up-event-keys-and-values.up.sql
@@ -0,0 +1,7 @@
+DROP VIEW IF EXISTS event_session_fields_mv;
+DROP VIEW IF EXISTS event_event_name_mv;
+DROP VIEW IF EXISTS event_attributes_mv;
+DROP VIEW IF EXISTS event_keys_mv;
+
+DROP TABLE IF EXISTS event_keys;
+DROP TABLE IF EXISTS event_key_values;
