fed07bb17eb9886f39939edd6df16ebd3adc704b
Vadim Korolik <vadim@highlight.io>
2024-10-08 07:22:31 -0700

    support firelens structured logging from kinesis firehose (#9430)
    ## Summary
    
    Ensures the `/logs/firehose` endpoint supports firelens structured logs
    
    ## How did you test this change?
    
    New unit tests
    
    testing deployed in prod
    ![Screenshot from 2024-10-07
    21-39-19](https://github.com/user-attachments/assets/e5e4774d-cf9f-44fe-a2d4-0ed3ceb81b02)
    
    
    ## Are there any deployment considerations?
    
    no
    
    ## Does this work require review from our design team?
    
    no

--- sdk/highlight-go/otel.go
+++ sdk/highlight-go/otel.go
@@ -202,9 +202,13 @@ func StartTraceWithTracer(ctx context.Context, tracer trace.Tracer, name string,
 	sessionID, requestID, _ := validateRequest(ctx)
 	spanCtx := trace.SpanContextFromContext(ctx)
 	if requestID != "" {
-		data, _ := base64.StdEncoding.DecodeString(requestID)
-		hex := fmt.Sprintf("%032x", data)
-		tid, _ := trace.TraceIDFromHex(hex)
+		// try parse the requestID as hex; fall back to parsing as base64
+		tid, err := trace.TraceIDFromHex(requestID)
+		if err != nil {
+			data, _ := base64.StdEncoding.DecodeString(requestID)
+			hex := fmt.Sprintf("%032x", data)
+			tid, _ = trace.TraceIDFromHex(hex)
+		}
 		spanCtx = spanCtx.WithTraceID(tid)
 	}
 	opts = append(opts, trace.WithTimestamp(t))
