c26676289a3a6f77955d0a28c7c627182714f735
Vadim Korolik <vkorolik@gmail.com>
2024-10-31 15:47:33 -0500

    server sourcemaps for highlight.io site

--- highlight.io/next.config.js
+++ highlight.io/next.config.js
@@ -3,13 +3,16 @@ const getStaticPages = require('./scripts/get-static-pages')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	webpack: (config) => {
+	webpack: (config, { isServer }) => {
 		config.resolve.fallback = {
 			...config.resolve.fallback, // if you miss it, all the other options in fallback, specified
 			// by next.js will be dropped. Doesn't make much sense, but how it is
 			fs: false, // the solution
 		}
-
+		// configure server-side sourcemaps
+		if (isServer) {
+			config.devtool = 'source-map'
+		}
 		return config
 	},
 	compress: true,
