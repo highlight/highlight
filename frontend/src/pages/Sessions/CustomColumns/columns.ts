df8c0707616e11da9f83fa36b560b6f61f5cb4e1
Spencer Amarantides <Spencer22309@gmail.com>
2024-11-04 15:27:17 -0500

    Customize related resource table (#9579)
    ## Summary
    Allow users to customize their related resource tables and sort if the
    table supports it (i.e. only traces)
    
    
    https://www.loom.com/share/d9478badded3411686478b160ccd4b2f?sid=bd2d47b9-a03a-4297-b311-39e0b17d6310
    
    ## How did you test this change?
    1. Create a graph or alert of traces
    2. Drill down to a timestamp
    - [ ] Able to sort column
    - [ ] Able to add/drop columns
    - [ ] Able to resize columns
    3. Close the related resources and reopen
    - [ ] Custom columns persist
    4. View trace table
    - [ ] No changes to trace table
    5. Repeat for sessions, errors, logs, and events
    - [ ] Session and event tables persist across each other
    - [ ] Not able to sort any of the above tables
    
    ## Are there any deployment considerations?
    N/A
    
    ## Does this work require review from our design team?
    N/A

--- frontend/src/pages/Sessions/CustomColumns/columns.ts
+++ frontend/src/pages/Sessions/CustomColumns/columns.ts
@@ -41,6 +41,122 @@ const ACTIVE_LENGTH_COLUMN: SessionCustomColumn = {
 	accessor: (row: Session) => row.active_length,
 }
 
+const LENGTH_COLUMN: SessionCustomColumn = {
+	id: 'length',
+	label: 'Length',
+	type: 'duration',
+	size: '1fr',
+	accessor: (row: Session) => row.length,
+}
+
+const BROWSER_NAME_COLUMN: SessionCustomColumn = {
+	id: 'browser_name',
+	label: 'Browser',
+	type: 'string',
+	size: '1fr',
+	accessor: (row: Session) => row.browser_name,
+}
+
+const BROWSER_VERSION_COLUMN: SessionCustomColumn = {
+	id: 'browser_version',
+	label: 'Browser Version',
+	type: 'string',
+	size: '1fr',
+	accessor: (row: Session) => row.browser_version,
+}
+
+const CITY_COLUMN: SessionCustomColumn = {
+	id: 'city',
+	label: 'City',
+	type: 'string',
+	size: '1fr',
+	accessor: (row: Session) => row.city,
+}
+
+const FIRST_TIME_COLUMN: SessionCustomColumn = {
+	id: 'first_time',
+	label: 'First Session',
+	type: 'string',
+	size: '1fr',
+	accessor: (row: Session) => String(row.first_time),
+}
+
+const HAS_RAGE_CLICKS_COLUMN: SessionCustomColumn = {
+	id: 'has_rage_clicks',
+	label: 'Has Rage Clicks',
+	type: 'string',
+	size: '1fr',
+	accessor: (row: Session) => String(row.has_rage_clicks),
+}
+
+const HAS_ERRORS_COLUMN: SessionCustomColumn = {
+	id: 'has_errors',
+	label: 'Has Errors',
+	type: 'string',
+	size: '1fr',
+	accessor: (row: Session) => String(row.has_errors),
+}
+
+const IDENTIFIER_COLUMN: SessionCustomColumn = {
+	id: 'identifier',
+	label: 'Identifier',
+	type: 'string',
+	size: '1fr',
+	accessor: (row: Session) => row.identifier,
+}
+
+const OS_NAME_COLUMN: SessionCustomColumn = {
+	id: 'os_name',
+	label: 'OS',
+	type: 'string',
+	size: '1fr',
+	accessor: (row: Session) => row.os_name,
+}
+
+const OS_VERSION_COLUMN: SessionCustomColumn = {
+	id: 'os_version',
+	label: 'OS Version',
+	type: 'string',
+	size: '1fr',
+	accessor: (row: Session) => row.os_version,
+}
+
+const STATE_COLUMN: SessionCustomColumn = {
+	id: 'state',
+	label: 'State',
+	type: 'string',
+	size: '1fr',
+	accessor: (row: Session) => row.state,
+}
+
+const VIEWED_COLUMN: SessionCustomColumn = {
+	id: 'viewed',
+	label: 'Viewed',
+	type: 'string',
+	size: '1fr',
+	accessor: (row: Session) => String(row.viewed),
+}
+
+export const SESSION_STANDARD_COLUMNS: Record<string, SessionCustomColumn> = {
+	secure_id: SECURE_ID_COLUMN,
+	email: EMAIL_COLUMN,
+	identifier: IDENTIFIER_COLUMN,
+	city: CITY_COLUMN,
+	state: STATE_COLUMN,
+	country: COUNTRY_COLUMN,
+	created_at: CREATED_AT_COLUMN,
+	active_length: ACTIVE_LENGTH_COLUMN,
+	length: LENGTH_COLUMN,
+	first_time: FIRST_TIME_COLUMN,
+	has_errors: HAS_ERRORS_COLUMN,
+	has_rage_clicks: HAS_RAGE_CLICKS_COLUMN,
+	browser_name: BROWSER_NAME_COLUMN,
+	browser_version: BROWSER_VERSION_COLUMN,
+	os_name: OS_NAME_COLUMN,
+	os_version: OS_VERSION_COLUMN,
+	viewed: VIEWED_COLUMN,
+}
+
 export const DEFAULT_SESSION_COLUMNS = [
 	SECURE_ID_COLUMN,
 	EMAIL_COLUMN,
