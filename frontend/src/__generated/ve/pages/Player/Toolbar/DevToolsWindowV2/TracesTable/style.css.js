cdb1bce22f7d70cac5dde85dfde7395c330f5af5
Vadim Korolik <vkorolik@gmail.com>
2024-12-09 19:11:42 -0600

    wire frontend trace search to session service

---
+++ frontend/src/__generated/ve/pages/Player/Toolbar/DevToolsWindowV2/TracesTable/style.css.js
@@ -0,0 +1,11 @@
+// src/pages/Player/Toolbar/DevToolsWindowV2/TracesTable/style.css.ts
+var attributesRow = "prq5bv2";
+var currentRow = "prq5bv3";
+var dataRow = "prq5bv0";
+var pastRow = "prq5bv1";
+export {
+  attributesRow,
+  currentRow,
+  dataRow,
+  pastRow
+};
