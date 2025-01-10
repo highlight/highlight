f57287e289eaf0d16fdc9a7ba48d0606518a8d16
Vadim Korolik <vkorolik@gmail.com>
2025-01-10 15:15:21 -0600

    reworking metric write sdk

--- sdk/highlight-go/metric/metric.go
+++ sdk/highlight-go/metric/metric.go
@@ -4,40 +4,29 @@ import (
 	"context"
 	"github.com/highlight/highlight/sdk/highlight-go"
 	"go.opentelemetry.io/otel/attribute"
-	"math"
 	"math/rand"
-	"time"
 )
 
 // Histogram tracks the statistical distribution of a set of values on each host.
 func Histogram(ctx context.Context, name string, value float64, tags []attribute.KeyValue, rate float64) {
-	if rand.Float64() > math.Min(rate, highlight.GetMetricSamplingRate()) {
+	if rand.Float64() > rate {
 		return
 	}
-	highlight.RecordMetric(ctx, name, value, tags...)
-}
-
-// Timing sends timing information, it is an alias for TimeInMilliseconds
-// It is flushed by statsd with percentiles, mean and other info (https://github.com/etsy/statsd/blob/master/docs/metric_types.md#timing)
-func Timing(ctx context.Context, name string, value time.Duration, tags []attribute.KeyValue, rate float64) {
-	if rand.Float64() > math.Min(rate, highlight.GetMetricSamplingRate()) {
-		return
-	}
-	highlight.RecordMetric(ctx, name, value.Seconds(), tags...)
+	highlight.RecordHistogram(ctx, name, value, tags...)
 }
 
 // Incr is just Count of 1
 // Count tracks how many times something happened per second.
 func Incr(ctx context.Context, name string, tags []attribute.KeyValue, rate float64) {
-	if rand.Float64() > math.Min(rate, highlight.GetMetricSamplingRate()) {
+	if rand.Float64() > rate {
 		return
 	}
-	highlight.RecordMetric(ctx, name, 1, tags...)
+	highlight.RecordCount(ctx, name, 1, tags...)
 }
 
 // Gauge measures the value of a metric at a particular time.
 func Gauge(ctx context.Context, name string, value float64, tags []attribute.KeyValue, rate float64) {
-	if rand.Float64() > math.Min(rate, highlight.GetMetricSamplingRate()) {
+	if rand.Float64() > rate {
 		return
 	}
 	highlight.RecordMetric(ctx, name, value, tags...)
