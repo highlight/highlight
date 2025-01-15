9389b696075ce4b566033ae926b61aa8c87f4c2d
Chris Schmitz <ccschmitz@gmail.com>
2025-01-15 13:29:51 -0600

    Move CF template to main repo

--- frontend/src/pages/Setup/SetupRouter/InfrastructureSetup.tsx
+++ frontend/src/pages/Setup/SetupRouter/InfrastructureSetup.tsx
@@ -1,53 +1,85 @@
-import { Button } from '@/components/Button'
-import { Box, Stack, Text } from '@highlight-run/ui/components'
+import { useProjectId } from '@/hooks/useProjectId'
+import { Box, Stack, Text, Form } from '@highlight-run/ui/components'
 import { Header } from '@pages/Setup/Header'
-import React from 'react'
+import React, { useState } from 'react'
+
+import { LinkButton } from '@/components/LinkButton'
+const AWS_REGIONS = [
+	{ value: 'us-west-1', name: 'US West (California)' },
+	{ value: 'us-west-2', name: 'US West (Oregon)' },
+	{ value: 'us-east-1', name: 'US East (Virginia)' },
+	{ value: 'us-east-2', name: 'US East (Ohio)' },
+]
 
 const CLOUDFORMATION_URL =
-	'https://us-west-2.console.aws.amazon.com/cloudformation/home?region=us-west-2#/stacks/quickcreate?stackName=highlight-metrics-stream&templateURL=https://highlight-demo-video.s3.us-west-2.amazonaws.com/cloudformation-templates/metric-stream.yaml'
+	'https://console.aws.amazon.com/cloudformation/home?region=[REGION]#/stacks/quickcreate?templateURL=https://highlight-demo-video.s3.us-west-2.amazonaws.com/cloudformation-templates/metric-stream.yaml'
+
+// const OTEL_ENDPOINT = 'http://otel.highlight.io:4318/v1/metrics'
+const OTEL_ENDPOINT =
+	'https://c916-174-102-176-170.ngrok-free.app/v1/metrics/firehose'
+
+// TODO: Update
+const STACK_NAME = 'highlight-metrics-stream-local-test'
 
 export const InfrastructureSetup: React.FC = () => {
+	const [selectedRegion, setSelectedRegion] = useState<string>('us-east-2')
+	const { projectId } = useProjectId()
+	const url =
+		CLOUDFORMATION_URL.replace('[REGION]', selectedRegion.toString()) +
+		`&stackName=${STACK_NAME}&param_HighlightProjectID=${projectId}&param_OTeLEndpoint=${encodeURIComponent(OTEL_ENDPOINT)}`
 	return (
-		<Box>
+		<Form>
 			<Box style={{ maxWidth: 560 }} my="40" mx="auto">
 				<Header
-					title="Deploy with AWS CloudFormation"
-					subtitle="Launch a CloudFormation stack to set up AWS metrics streaming."
+					title="Infrastructure Monitoring Setup"
+					subtitle="Monitor your AWS infrastructure metrics with Highlight"
 				/>
 
 				<Stack gap="24">
 					<Box>
 						<Text color="moderate" size="medium">
-							Click the button below to launch the AWS
-							CloudFormation template. This will create a new
-							stack in your AWS account that sets up metrics
-							streaming to Highlight.
+							Select the AWS regions you want to monitor. This
+							will create CloudFormation stacks that set up
+							metrics streaming to Highlight in each selected
+							region.
 						</Text>
 					</Box>
 
 					<Box>
-						<Button
+						<Form.Select
+							name="regions"
+							label="AWS Regions"
+							placeholder="Select regions to monitor"
+							options={AWS_REGIONS}
+							onValueChange={(value) => {
+								setSelectedRegion(value.value)
+							}}
+							value={selectedRegion}
+						/>
+					</Box>
+
+					<Box>
+						<LinkButton
+							to={url}
 							kind="primary"
 							size="medium"
 							emphasis="high"
-							onClick={() =>
-								window.open(CLOUDFORMATION_URL, '_blank')
-							}
-							trackingId="launch-cloudformation"
+							disabled={!selectedRegion}
+							trackingId="launch-cloudformation-stacks"
 						>
 							Launch CloudFormation Stack
-						</Button>
+						</LinkButton>
 					</Box>
 
 					<Box>
 						<Text color="moderate" size="small">
-							Note: You will be redirected to the AWS Console.
-							Make sure you are logged in to your AWS account
-							before proceeding.
+							Note: You will be redirected to the AWS Console for
+							each selected region. Make sure you are logged in to
+							your AWS account before proceeding.
 						</Text>
 					</Box>
 				</Stack>
 			</Box>
-		</Box>
+		</Form>
 	)
 }
---
+++ infra/cloudformation/README.md
@@ -0,0 +1,5 @@
+This directory contains CloudFormation templates to streamline the process of configuring an AWS account to send observability data to Highlight.
+
+## Deployment
+
+Right now these templates are manually uploaded to S3 when changed.
