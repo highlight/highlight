422ab4f6822bcba4611893c04aca1618bce75074
itsalam <vincentthanhlam@gmail.com>
2025-01-24 14:11:42 -0800

    refactored antd references, and removed antd from checkbox/spinner components

--- frontend/src/components/Loading/Loading.tsx
+++ frontend/src/components/Loading/Loading.tsx
@@ -1,4 +1,3 @@
-export { CircularSpinner } from './CircularSpinner'
 export { IconAnimatedLoading } from './IconAnimatedLoading'
 export { LoadingBar } from './LoadingBar'
 export { LoadingPage, LoadingRightPanel } from './LoadingPage'
