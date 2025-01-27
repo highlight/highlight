6c13da2c6f4df54e3d42976b91005cb60f87f593
Vadim Korolik <vkorolik@gmail.com>
2025-01-27 14:28:16 -0600

    3 different graphs

--- frontend/src/pages/Player/Toolbar/DevToolsWindowV2/PerformancePage/PerformancePage.tsx
+++ frontend/src/pages/Player/Toolbar/DevToolsWindowV2/PerformancePage/PerformancePage.tsx
@@ -1,63 +1,54 @@
-import LoadingBox from '@components/LoadingBox'
-import { Box, Text } from '@highlight-run/ui/components'
+import { Box } from '@highlight-run/ui/components'
 import { TIMESTAMP_KEY, useGraphData } from '@pages/Graphing/components/Graph'
 import { LineChart } from '@pages/Graphing/components/LineChart'
 import { useReplayerContext } from '@pages/Player/ReplayerContext'
 import React from 'react'
 
-import * as styles from './style.css'
 import { TIME_FORMAT } from '@components/Search/SearchForm/constants'
 import { useParams } from '@util/react-router/useParams'
 import { useGetMetricsQuery } from '@/graph/generated/hooks'
 import { MetricAggregator, ProductType } from '@/graph/generated/schemas'
 import moment from 'moment'
 
-type Props = {}
+const Graph = React.memo(
+	({ metricName, color }: { metricName: string; color?: string }) => {
+		const { session, sessionMetadata } = useReplayerContext()
+		const { project_id } = useParams<{ project_id: string }>()
 
-const PerformancePage = React.memo(({}: Props) => {
-	const { session, sessionMetadata } = useReplayerContext()
-	const { project_id } = useParams<{ project_id: string }>()
-
-	const start = moment(sessionMetadata.startTime)
-	const end = moment(sessionMetadata.endTime)
-	const { data: metricsData, loading: isLoading } = useGetMetricsQuery({
-		variables: {
-			product_type: ProductType.Metrics,
-			project_id: project_id!,
-			params: {
-				date_range: {
-					start_date: start.format(TIME_FORMAT),
-					end_date: end.format(TIME_FORMAT),
+		const start = moment(sessionMetadata.startTime)
+		const end = moment(sessionMetadata.endTime)
+		const { data: metricsData } = useGetMetricsQuery({
+			variables: {
+				product_type: ProductType.Metrics,
+				project_id: project_id!,
+				params: {
+					date_range: {
+						start_date: start.format(TIME_FORMAT),
+						end_date: end.format(TIME_FORMAT),
+					},
+					query: `secure_session_id=${session?.secure_id} AND metric_name=${metricName}`,
 				},
-				query: `secure_session_id=${session?.secure_id} AND (metric_name=Jank OR metric_name=usedJSHeapSize OR metric_name=fps)`,
+				group_by: ['metric_name'],
+				bucket_by: TIMESTAMP_KEY,
+				bucket_count: 60,
+				limit: 1_000,
+				expressions: [
+					{ aggregator: MetricAggregator.Avg, column: 'value' },
+				],
 			},
-			group_by: ['metric_name'],
-			bucket_by: TIMESTAMP_KEY,
-			bucket_count: 60,
-			limit: 1_000,
-			expressions: [
-				{ aggregator: MetricAggregator.Avg, column: 'value' },
-			],
-		},
-	})
+		})
 
-	const data = useGraphData(metricsData, TIMESTAMP_KEY) ?? []
+		const data = useGraphData(metricsData, TIMESTAMP_KEY) ?? []
 
-	const hasNoPerformancePayloads = metricsData?.metrics?.buckets?.length === 0
-
-	console.log('vadim', { metricsData, data })
-	return (
-		<div className={styles.container}>
-			{isLoading && <LoadingBox />}
-			{hasNoPerformancePayloads && (
-				<div className={styles.emptyContainer}>
-					<div className={styles.messageContainer}>
-						<Text>This session has no performance data.</Text>
-					</div>
-				</div>
-			)}
-			{!isLoading && !hasNoPerformancePayloads ? (
-				<Box width="full" height="full" p="16">
+		if (!data?.length) return null
+		return (
+			<Box width="full" height="full" p="16">
+				<Box
+					width="full"
+					height="full"
+					p="16"
+					key=";" // Hacky but recharts' ResponsiveContainer has issues when this height changes so just rerender the whole thing
+				>
 					<LineChart
 						data={data}
 						xAxisMetric={TIMESTAMP_KEY}
@@ -67,10 +58,22 @@ const PerformancePage = React.memo(({}: Props) => {
 							showLegend: true,
 							nullHandling: 'Connected',
 						}}
+						strokeColors={color ? [color] : undefined}
+						syncId="session"
 					/>
 				</Box>
-			) : null}
-		</div>
+			</Box>
+		)
+	},
+)
+
+const PerformancePage = React.memo(() => {
+	return (
+		<Box width="full" height="full" display="flex" gap="4">
+			<Graph metricName="usedJSHeapSize" color="#0090FF" />
+			<Graph metricName="fps" color="#D6409F" />
+			<Graph metricName="Jank" color="#744ED4" />
+		</Box>
 	)
 })
 
