79702aa1bffbe400e1ad05d72636d7143ac2ee98
Vadim Korolik <vkorolik@gmail.com>
2025-01-31 15:32:48 -0600

    fix docs

--- highlight.io/pages/api/og/util.ts
+++ highlight.io/pages/api/og/util.ts
@@ -14,9 +14,24 @@ export const getResources = async (req: NextRequest) => {
 		new URL(`${prefix}/public/images/backdrop.png`),
 	).then((res) => res.arrayBuffer())
 
+	const logoOnDark = fetch(
+		new URL(`${prefix}/public/images/logo-on-dark.png`),
+	).then((res) => res.arrayBuffer())
+
+	const bug1 = fetch(new URL(`${prefix}/public/images/bug1.png`)).then(
+		(res) => res.arrayBuffer(),
+	)
+
+	const bug2 = fetch(new URL(`${prefix}/public/images/bug2.png`)).then(
+		(res) => res.arrayBuffer(),
+	)
+
 	return {
 		font: await font,
 		fontLight: await fontLight,
 		backdrop: await backdrop,
+		logoOnDark: await logoOnDark,
+		bug1: await bug1,
+		bug2: await bug2,
 	}
 }
