9456504feb1f489bbb8a7c130330a2b38982e790
Vadim Korolik <vkorolik@gmail.com>
2025-01-31 10:55:18 -0500

    update migration

--- backend/clickhouse/migrations/000141_metrics_table_reduce_raw_ttl.down.sql
+++ backend/clickhouse/migrations/000141_metrics_table_reduce_raw_ttl.down.sql
@@ -1,4 +1,4 @@
 
-ALTER TABLE default.metrics_sum MODIFY TTL toDateTime(Timestamp) + toIntervalDay(RetentionDays);
-ALTER TABLE default.metrics_histogram MODIFY TTL toDateTime(Timestamp) + toIntervalDay(RetentionDays);
-ALTER TABLE default.metrics_summary MODIFY TTL toDateTime(Timestamp) + toIntervalDay(RetentionDays);
+ALTER TABLE metrics_sum MODIFY TTL toDateTime(Timestamp) + toIntervalDay(RetentionDays);
+ALTER TABLE metrics_histogram MODIFY TTL toDateTime(Timestamp) + toIntervalDay(RetentionDays);
+ALTER TABLE metrics_summary MODIFY TTL toDateTime(Timestamp) + toIntervalDay(RetentionDays);
--- backend/clickhouse/migrations/000141_metrics_table_reduce_raw_ttl.up.sql
+++ backend/clickhouse/migrations/000141_metrics_table_reduce_raw_ttl.up.sql
@@ -1,3 +1,3 @@
-ALTER TABLE default.metrics_sum MODIFY TTL toDateTime(Timestamp) + INTERVAL 1 DAY;
-ALTER TABLE default.metrics_histogram MODIFY TTL toDateTime(Timestamp) + INTERVAL 1 DAY;
-ALTER TABLE default.metrics_summary MODIFY TTL toDateTime(Timestamp) + INTERVAL 1 DAY;
+ALTER TABLE metrics_sum MODIFY TTL toDateTime(Timestamp) + INTERVAL 1 HOUR;
+ALTER TABLE metrics_histogram MODIFY TTL toDateTime(Timestamp) + INTERVAL 1 HOUR;
+ALTER TABLE metrics_summary MODIFY TTL toDateTime(Timestamp) + INTERVAL 1 HOUR;
