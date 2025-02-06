e04cc97810242f99d6de26a3edd6eb0ce551b1d4
mayberryzane <86132398+mayberryzane@users.noreply.github.com>
2025-02-06 13:29:25 -0800

    add missing fields to mvs (#9923)
    ## Summary
    - there were no MVs to write these attributes to the `trace_key_values`
    table, so they're missing from key and value suggestions
    <!--
    Ideally, there is an attached GitHub issue that will describe the "why".
    
    If relevant, use this section to call out any additional information
    you'd like to _highlight_ to the reviewer.
    -->
    
    ## How did you test this change?
    - checked a couple fields (e.g. `telemetry.distro.name`) after running
    migration script locally
    <!--
    Frontend - Leave a screencast or a screenshot to visually describe the
    changes.
    -->
    
    ## Are there any deployment considerations?
    - no
    <!--
     Backend - Do we need to consider migrations or backfilling data?
    -->
    
    ## Does this work require review from our design team?
    - no
    <!--
     Request review from julian-highlight / our design team 
    -->

---
+++ backend/clickhouse/migrations/000142_trace_mvs.down.sql
@@ -0,0 +1,7 @@
+DROP VIEW IF EXISTS trace_http_attributes_mv;
+DROP VIEW IF EXISTS trace_process_attributes_mv;
+DROP VIEW IF EXISTS trace_os_attributes_mv;
+DROP VIEW IF EXISTS trace_telemetry_attributes_mv;
+DROP VIEW IF EXISTS trace_ws_attributes_mv;
+DROP VIEW IF EXISTS trace_event_attributes_mv;
+DROP VIEW IF EXISTS trace_db_attributes_mv;
