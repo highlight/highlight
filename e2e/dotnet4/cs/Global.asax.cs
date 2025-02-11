485a8a1218e9316d95736b09ea73a2e71349e262
Vadim Korolik <vkorolik@gmail.com>
2025-02-10 21:14:02 -0600

    fixup! fixup! update dotnet4 e2e example

--- e2e/dotnet4/cs/Global.asax.cs
+++ e2e/dotnet4/cs/Global.asax.cs
@@ -32,7 +32,7 @@ public class MvcApplication : System.Web.HttpApplication {
             config => {
                 config.ProjectId = "1";
                 config.ServiceName = "example-dotnet-backend";
-                options.OtlpEndpoint = "http://localhost:4317";
+                config.OtlpEndpoint = "http://localhost:4317";
             });
         });
         Logger = loggerFactory.CreateLogger("example-dotnet-backend");
