7df51666002e9c7494b5ecf94bf9f8005c8649a1
Vadim Korolik <vkorolik@gmail.com>
2025-03-20 14:12:55 -0700

    date sdk/highlight-dotnet4 otel dependencies

--- sdk/highlight-dotnet4/OpenTelemetry.cs
+++ sdk/highlight-dotnet4/OpenTelemetry.cs
@@ -98,7 +98,7 @@ public static class OpenTelemetry {
             ["highlight.project_id"] = Cfg.ProjectId,
             ["service.name"] = Cfg.ServiceName,
             ["telemetry.distro.name"] = "Highlight.ASP4",
-            ["telemetry.distro.version"] = "0.2.9",
+            ["telemetry.distro.version"] = "0.2.10",
         };
     }
 
