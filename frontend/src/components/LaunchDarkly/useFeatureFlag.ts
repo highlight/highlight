dd9b23773f1152b07e3140fd6850cf82dee86c9f
Chris Schmitz <ccschmitz@gmail.com>
2025-03-27 08:22:03 -0500

    Clean up tests and use hook in component

--- frontend/src/components/LaunchDarkly/useFeatureFlag.ts
+++ frontend/src/components/LaunchDarkly/useFeatureFlag.ts
@@ -2,8 +2,7 @@ import { useFlags } from 'launchdarkly-react-client-sdk'
 import { flags } from './flags'
 
 export function useFeatureFlag(flag: keyof typeof flags) {
-	const flagConfig = flags[flag]
+	const flagConfig = flags[flag] ?? {}
 	const ldFlags = useFlags() ?? {}
-
 	return ldFlags[flag] ?? flagConfig.defaultValue
 }
