50c21cb60e376340e11b08d533be5d8c1edd1f02
Vadim Korolik <vkorolik@gmail.com>
2025-03-28 16:52:33 -0700

    fixup! improve test

--- cypress/e2e/sso.cy.js
+++ cypress/e2e/sso.cy.js
@@ -2,7 +2,7 @@ describe('login spec', () => {
 	it('allows you to log in using SSO', () => {
 		cy.visit('/1/buttons').wait(5000)
 		cy.url().should('include', '/sign_in')
-		cy.get('[name="email"]').type('vadim@highlight.io').blur().wait(1000)
+		cy.get('[name="email"]').type('vadim@highlight.io').blur().wait(5000)
 		cy.get('[name="password"]').should('not.exist')
 		cy.get('button[type="submit"]').click()
 		// Verify redirect to Google SSO page
@@ -15,7 +15,7 @@ describe('login spec', () => {
 		cy.visit('/sign_in').url().should('include', '/sign_in') // Verify we're redirected to sign_in page
 
 		// Try again with different email domain
-		cy.get('[name="email"]').clear().type('vadim@highlight.run').blur().wait(1000)
+		cy.get('[name="email"]').clear().type('vadim@highlight.run').blur().wait(5000)
 		cy.get('[name="password"]').should('not.exist')
 		cy.get('button[type="submit"]').click()
 		// Verify redirect to Google SSO page
