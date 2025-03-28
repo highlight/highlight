24993be7f7ceeaf8ef1cb3cabe0622f69896323c
Chris Schmitz <ccschmitz@gmail.com>
2025-03-28 17:39:33 -0500

    Update useFeatureFlag.ts

--- frontend/src/components/LaunchDarkly/useFeatureFlag.ts
+++ frontend/src/components/LaunchDarkly/useFeatureFlag.ts
@@ -1,15 +1,9 @@
-import { useFlags, useLDClient } from 'launchdarkly-react-client-sdk'
+import { useFlags } from 'launchdarkly-react-client-sdk'
 import { flags } from './flags'
 
 export function useFeatureFlag(flag: keyof typeof flags) {
 	const flagConfig = flags[flag] ?? {}
 	const ldFlags = useFlags() ?? {}
-	const client = useLDClient()
-
-	// console.log('::: flag being checked:', flag)
-	// console.log('::: client ready:', client?.waitForInitialization())
-	console.log('::: current context:', client?.getContext())
-	console.log('::: flag value:', flag, ldFlags[flag])
 
 	return ldFlags[flag] ?? flagConfig.defaultValue
 }
