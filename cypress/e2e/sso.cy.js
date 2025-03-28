7fb99a560701bde3e5bc0b6ce75727ecadfa04b2
Vadim Korolik <vkorolik@gmail.com>
2025-03-28 13:03:39 -0700

    improve test

--- cypress/e2e/sso.cy.js
+++ cypress/e2e/sso.cy.js
@@ -1,9 +1,10 @@
 describe('login spec', () => {
 	it('allows you to log in using SSO', () => {
-		cy.visit('/1/buttons').wait(1000).url().should('include', '/sign_in')
+		cy.visit('/1/buttons').wait(5000)
+		cy.url().should('include', '/sign_in')
 		cy.get('[name="email"]').type('vadim@highlight.io').blur().wait(1000)
 		cy.get('[name="password"]').should('not.exist')
-		cy.get('button[type="submit"]').click().wait(5000)
+		cy.get('button[type="submit"]').click()
 		// Verify redirect to Google SSO page
 		cy.origin('accounts.google.com', () => {
 			// We're now in the Google domain context
@@ -11,16 +12,12 @@ describe('login spec', () => {
 		})
 
 		// Return to local buttons page after Google SSO
-		cy.visit('/1/buttons').wait(1000).url().should('include', '/sign_in') // Verify we're redirected to sign_in page
+		cy.visit('/sign_in').url().should('include', '/sign_in') // Verify we're redirected to sign_in page
 
 		// Try again with different email domain
-		cy.get('[name="email"]')
-			.clear()
-			.type('vadim@highlight.run')
-			.blur()
-			.wait(1000)
+		cy.get('[name="email"]').clear().type('vadim@highlight.run').blur().wait(1000)
 		cy.get('[name="password"]').should('not.exist')
-		cy.get('button[type="submit"]').click().wait(5000)
+		cy.get('button[type="submit"]').click()
 		// Verify redirect to Google SSO page
 		cy.origin('accounts.google.com', () => {
 			// We're now in the Google domain context
