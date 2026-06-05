73eac6de53f69356480139a911bc150ebc38f45b
longtn <tnlong1214@gmail.com>
2026-06-05 20:21:46 +0700

    fix: remove antd Divider from Alerts page
    Replace antd Divider import with native <hr> element using Tailwind classes.
    This is part of the ongoing antd removal effort (issue #9091).
    
    Changes:
    - Remove
    - Replace all  with styled <hr> elements
    - Uses Tailwind border classes for consistent styling

--- frontend/src/pages/Alerts/AlertForm/index.tsx
+++ frontend/src/pages/Alerts/AlertForm/index.tsx
@@ -13,7 +13,6 @@ import {
 	Tag,
 	Text,
 } from '@highlight-run/ui/components'
-import { Divider } from 'antd'
 import React, { PropsWithChildren, useEffect, useMemo, useState } from 'react'
 import { Helmet } from 'react-helmet'
 import { useNavigate, useSearchParams } from 'react-router-dom'
@@ -640,7 +639,7 @@ export const AlertForm: React.FC = () => {
 										/>
 									</LabeledRow>
 								</SidebarSection>
-								<Divider className="m-0" />
+								<hr className="m-0 border-0 border-t border-solid border-[--color-gray-300]" />
 								<SidebarSection>
 									<Box cssClass={style.editorSection}>
 										<Box cssClass={style.editorHeader}>
@@ -718,7 +717,7 @@ export const AlertForm: React.FC = () => {
 															</Callout>
 														)}
 												</SidebarSection>
-												<Divider className="m-0" />
+												<hr className="m-0 border-0 border-t border-solid border-[--color-gray-300]" />
 												<SidebarSection>
 													{productType ===
 													ProductType.Events ? (
@@ -775,7 +774,7 @@ export const AlertForm: React.FC = () => {
 														!isErrorAlert)) && (
 													<>
 														<Box px="12">
-															<Divider className="m-0" />
+															<hr className="m-0 border-0 border-t border-solid border-[--color-gray-300]" />
 														</Box>
 														<SidebarSection>
 															<LabeledRow
@@ -850,7 +849,7 @@ export const AlertForm: React.FC = () => {
 										)}
 									</Box>
 								</SidebarSection>
-								<Divider className="m-0" />
+								<hr className="m-0 border-0 border-t border-solid border-[--color-gray-300]" />
 								<SidebarSection>
 									<LabeledRow
 										label="Alert threshold type"
@@ -970,7 +969,7 @@ export const AlertForm: React.FC = () => {
 										</>
 									)}
 								</SidebarSection>
-								<Divider className="m-0" />
+								<hr className="m-0 border-0 border-t border-solid border-[--color-gray-300]" />
 								<SidebarSection>
 									<DestinationInput
 										initialDestinations={
